370ead7395fc9194a3dc2d05afc4b3409f6b27da
Namit Jain <namit@apache.org>
2009-03-18 22:35:23 +0000

    HIVE-350. Fix wrong order of aggregations in explain plan. (namit)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@755763 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/parse/QBParseInfo.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/QBParseInfo.java
@@ -58,7 +58,7 @@ public class QBParseInfo {
   private int outerQueryLimit;
 
   // used by GroupBy
-  private HashMap<String, HashMap<String, ASTNode> > destToAggregationExprs;
+  private LinkedHashMap<String, LinkedHashMap<String, ASTNode> > destToAggregationExprs;
   private HashMap<String, ASTNode> destToDistinctFuncExpr;
 
   @SuppressWarnings("unused")
@@ -76,7 +76,7 @@ public class QBParseInfo {
     this.destToSortby = new HashMap<String, ASTNode>();
     this.destToLimit = new HashMap<String, Integer>();
     
-    this.destToAggregationExprs = new HashMap<String, HashMap<String, ASTNode> >();
+    this.destToAggregationExprs = new LinkedHashMap<String, LinkedHashMap<String, ASTNode> >();
     this.destToDistinctFuncExpr = new HashMap<String, ASTNode>();
     
     this.alias = alias;
@@ -84,7 +84,7 @@ public class QBParseInfo {
     this.outerQueryLimit = -1;
   }
 
-  public void setAggregationExprsForClause(String clause, HashMap<String, ASTNode> aggregationTrees) {
+  public void setAggregationExprsForClause(String clause, LinkedHashMap<String, ASTNode> aggregationTrees) {
     this.destToAggregationExprs.put(clause, aggregationTrees);
   }
 
@@ -273,7 +273,7 @@ public class QBParseInfo {
        (!destToClusterby.isEmpty()))
       return false;
     
-    Iterator<Map.Entry<String, HashMap<String, ASTNode>>> aggrIter = destToAggregationExprs.entrySet().iterator();
+    Iterator<Map.Entry<String, LinkedHashMap<String, ASTNode>>> aggrIter = destToAggregationExprs.entrySet().iterator();
     while (aggrIter.hasNext()) {
       HashMap<String, ASTNode> h = aggrIter.next().getValue();
       if ((h != null) && (!h.isEmpty()))
--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -170,11 +170,11 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     }
   }
 
-  private HashMap<String, ASTNode> doPhase1GetAggregationsFromSelect(
+  private LinkedHashMap<String, ASTNode> doPhase1GetAggregationsFromSelect(
       ASTNode selExpr) {
     // Iterate over the selects search for aggregation Trees.
     // Use String as keys to eliminate duplicate trees.
-    HashMap<String, ASTNode> aggregationTrees = new HashMap<String, ASTNode>();
+    LinkedHashMap<String, ASTNode> aggregationTrees = new LinkedHashMap<String, ASTNode>();
     for (int i = 0; i < selExpr.getChildCount(); ++i) {
       ASTNode sel = (ASTNode) selExpr.getChild(i).getChild(0);
       doPhase1GetAllAggregations(sel, aggregationTrees);
@@ -348,7 +348,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       case HiveParser.TOK_SELECT:
         qb.countSel();
         qbp.setSelExprForClause(ctx_1.dest, ast);
-        HashMap<String, ASTNode> aggregations = doPhase1GetAggregationsFromSelect(ast);
+        LinkedHashMap<String, ASTNode> aggregations = doPhase1GetAggregationsFromSelect(ast);
         qbp.setAggregationExprsForClause(ctx_1.dest, aggregations);
         qbp.setDistinctFuncExprForClause(ctx_1.dest,
             doPhase1GetDistinctFuncExpr(aggregations));
