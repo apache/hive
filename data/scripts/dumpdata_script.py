95ab04afe0b6317fbb7cb4c251f898d54bfc5a39
Namit Jain <namit@apache.org>
2009-04-07 01:40:34 +0000

    HIVE-382. fixed problems in map-side aggr - backported from trunk. (namit)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/branches/branch-0.3@762591 13f79535-47bb-0310-9956-ffa450edef68

---
+++ data/scripts/dumpdata_script.py
@@ -0,0 +1,11 @@
+for i in xrange(100):
+   for j in xrange(10):
+      for k in xrange(42022):      
+         print 42000 * i + k
+
+
+for i in xrange(100):
+   for j in xrange(10):
+      for k in xrange(42022):      
+         print 5000000 + (42000 * i) + k
+
