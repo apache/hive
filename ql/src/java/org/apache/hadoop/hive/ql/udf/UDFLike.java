5036cee86430855f1ff9b2d06bbafa76319371e1
Zheng Shao <zshao@apache.org>
2009-06-19 00:50:16 +0000

    HIVE-542. UDF: Faster String Like. (Yuntao Jia via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@786345 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/udf/UDFLike.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/UDFLike.java
@@ -32,7 +32,19 @@ public class UDFLike extends UDF {
   private static Log LOG = LogFactory.getLog(UDFLike.class.getName());
   private Text lastLikePattern = new Text();
   private Pattern p = null;
-
+  
+  // Doing characters comparison directly instead of regular expression 
+  // matching for simple patterns like "%abc%".
+  private enum PatternType {  
+    NONE,         // "abc"
+    BEGIN,        // "abc%"
+    END,          // "%abc"
+    MIDDLE,       // "%abc%"
+    COMPLEX,      // all other cases, such as "ab%c_de"
+  }
+  private PatternType type = PatternType.COMPLEX;
+  private Text simplePattern = new Text();
+  
   private BooleanWritable result = new BooleanWritable();
   public UDFLike() {
   }
@@ -63,16 +75,133 @@ public class UDFLike extends UDF {
     return sb.toString();
   }
   
+  /**
+   * Parses the likePattern. Based on it is a simple pattern 
+   * or not, the function might change two member variables. 
+   * {@link #type} will be changed to the corresponding 
+   * pattern type; {@link #simplePattern} will record 
+   * the string in it for later pattern matching if it 
+   * is a simple pattern.
+   * <p>
+   * Examples: 
+   * <blockquote><pre>
+   * parseSimplePattern("%abc%") changes {@link #type} to PatternType.MIDDLE
+   * and changes {@link #simplePattern} to "abc"
+   * parseSimplePattern("%ab_c%") changes {@link #type} to PatternType.COMPLEX
+   * and does not change {@link #simplePattern}
+   * </pre></blockquote>
+   * 
+   * @param likePattern the input LIKE query pattern
+   */
+  private void parseSimplePattern(String likePattern) {
+    int length     = likePattern.length();
+    int beginIndex = 0;
+    int endIndex   = length;
+    char lastChar  = 'a';
+    String strPattern = new String();
+    type = PatternType.NONE;
+    
+    for (int i=0; i<length; i++) {
+      char n = likePattern.charAt(i);
+      if (n == '_') {     // such as "a_b"  
+        if(lastChar != '\\') {                // such as "a%bc"
+          type = PatternType.COMPLEX;
+          return;            
+        } else {                              // such as "abc\%de%"                 
+          strPattern += likePattern.substring(beginIndex, i-1);                         
+          beginIndex = i;
+        }
+      } else if(n == '%') {
+        if (i==0) {                           // such as "%abc"
+          type = PatternType.END;
+          beginIndex = 1;
+        } else if(i<length-1) {
+          if(lastChar != '\\') {              // such as "a%bc"
+            type = PatternType.COMPLEX;
+            return;            
+          } else {                            // such as "abc\%de%"                 
+            strPattern += likePattern.substring(beginIndex, i-1);                         
+            beginIndex = i;
+          }
+        } else {
+          if(lastChar != '\\') {              
+            endIndex = length-1;
+            if (type == PatternType.END) {    // such as "%abc%"
+              type = PatternType.MIDDLE;
+            } else {
+              type = PatternType.BEGIN;       // such as "abc%"
+            }
+          } else {                            // such as "abc\%"
+            strPattern += likePattern.substring(beginIndex, i-1);                         
+            beginIndex = i;
+            endIndex = length;
+          }            
+        }
+      }
+      lastChar = n;
+    }
+    
+    strPattern += likePattern.substring(beginIndex, endIndex); 
+    simplePattern.set(strPattern);
+  }
+  
+  private static boolean find(Text s, Text sub, int startS, int endS) {
+    byte[] byteS   = s.getBytes();
+    byte[] byteSub = sub.getBytes();
+    int    lenSub  = sub.getLength();    
+    boolean match  = false;
+    for (int i=startS; (i<endS-lenSub+1) && (!match); i++) {
+      match = true;
+      for (int j=0; j<lenSub; j++) {
+        if (byteS[j+i] != byteSub[j]) {
+          match = false;
+          break;
+        }
+      }     
+    }    
+    return match;
+  }
+  
   public BooleanWritable evaluate(Text s, Text likePattern) {
     if (s == null || likePattern == null) {
       return null;
     }
     if (!likePattern.equals(lastLikePattern)) {
       lastLikePattern.set(likePattern);
-      p = Pattern.compile(likePatternToRegExp(likePattern.toString()));
+      String strLikePattern = likePattern.toString();
+  
+      parseSimplePattern(strLikePattern);
+      if (type == PatternType.COMPLEX)
+        p = Pattern.compile(likePatternToRegExp(strLikePattern));
     }
-    Matcher m = p.matcher(s.toString());
-    result.set(m.matches());
+
+    if (type == PatternType.COMPLEX) {
+      Matcher m = p.matcher(s.toString());
+      result.set(m.matches());
+    } else {            
+      int startS = 0;
+      int endS = s.getLength();
+      // if s is shorter than the required pattern
+      if(endS < simplePattern.getLength()) {
+        result.set(false);
+        return result;
+      }        
+      switch (type) {
+      case BEGIN: 
+        endS = simplePattern.getLength();
+        break;
+      case END:
+        startS = endS-simplePattern.getLength();
+        break;
+      case NONE:
+        if (simplePattern.getLength() != s.getLength()) {          
+          result.set(false);
+          return result;
+        }          
+        break;        
+      }      
+      result.set(find(s, simplePattern, startS, endS));
+    }
     return result;
   }
   
---
+++ ql/src/test/queries/clientpositive/udf_like.q
@@ -0,0 +1,10 @@
+EXPLAIN
+SELECT '_%_' LIKE '%\_\%\_%', '__' LIKE '%\_\%\_%', '%%_%_' LIKE '%\_\%\_%', '%_%_%' LIKE '%\%\_\%',
+  '_%_' LIKE '\%\_%', '%__' LIKE '__\%%', '_%' LIKE '\_\%\_\%%', '_%' LIKE '\_\%_%',
+  '%_' LIKE '\%\_', 'ab' LIKE '\%\_', 'ab' LIKE '_a%', 'ab' LIKE 'a'
+FROM src WHERE src.key = 86;
+
+SELECT '_%_' LIKE '%\_\%\_%', '__' LIKE '%\_\%\_%', '%%_%_' LIKE '%\_\%\_%', '%_%_%' LIKE '%\%\_\%',
+  '_%_' LIKE '\%\_%', '%__' LIKE '__\%%', '_%' LIKE '\_\%\_\%%', '_%' LIKE '\_\%_%',
+  '%_' LIKE '\%\_', 'ab' LIKE '\%\_', 'ab' LIKE '_a%', 'ab' LIKE 'a'
+FROM src WHERE src.key = 86;
