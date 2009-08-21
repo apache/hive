ddfab2213db771b738d1f971e36e664f5a02f4ab
Namit Jain <namit@apache.org>
2009-08-21 23:44:20 +0000

    HIVE-782. Bug in reducer progress (Raghu Murthy via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@806761 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/ExecDriver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/ExecDriver.java
@@ -351,7 +351,7 @@ public class ExecDriver extends Task<mapredWork> implements Serializable {
     ExecDriverTaskHandle th = (ExecDriverTaskHandle)t;
     RunningJob rj = th.getRunningJob();
     this.mapProgress = Math.round(rj.mapProgress() * 100);
-    this.reduceProgress = Math.round(rj.mapProgress() * 100);
+    this.reduceProgress = Math.round(rj.reduceProgress() * 100);
     taskCounters.put("CNTR_NAME_" + getId() + "_MAP_PROGRESS", Long.valueOf(this.mapProgress));
     taskCounters.put("CNTR_NAME_" + getId() + "_REDUCE_PROGRESS", Long.valueOf(this.reduceProgress));
     Counters ctrs = th.getCounters();
