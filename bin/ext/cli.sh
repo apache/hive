1ab35e78d0eae34bde020f32b39c016536e641f5
Zheng Shao <zshao@apache.org>
2009-10-19 23:00:52 +0000

    HIVE-890. Fix cli.sh for detecting Hadoop versions. (Paul Huff via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/branches/branch-0.4@826859 13f79535-47bb-0310-9956-ffa450edef68

--- bin/ext/cli.sh
+++ bin/ext/cli.sh
@@ -13,20 +13,15 @@ cli () {
   if $cygwin; then
     HIVE_LIB=`cygpath -w "$HIVE_LIB"`
   fi
-  
-  for f in ${HADOOP_HOME}/hadoop*core.jar ${HADOOP_HOME}/lib/hadoop*core.jar ${HADOOP_HOME}/build/hadoop*core.jar; do
-      if [[ ! -f $f ]]; then
-          continue;
-      fi
-      if [[ $f == *17* ]] || [[ $f == *18* ]] || [[ $f == *19* ]]; then
-          exec $HADOOP jar $AUX_JARS_CMD_LINE ${HIVE_LIB}/hive_cli.jar $CLASS $HIVE_OPTS "$@"
-          break
-      else
-          # hadoop 20 or newer - skip the aux_jars option. picked up from hiveconf
-          exec $HADOOP jar ${HIVE_LIB}/hive_cli.jar $CLASS $HIVE_OPTS "$@" 
-          break
-      fi
-  done
+
+  version=$($HADOOP version | awk '{print $2;}');
+
+  if [[ $version =~ "^0\.17" ]] || [[ $version =~ "^0\.18" ]] || [[ $version =~ "^0.19" ]]; then
+      exec $HADOOP jar $AUX_JARS_CMD_LINE ${HIVE_LIB}/hive_cli.jar $CLASS $HIVE_OPTS "$@"
+  else
+      # hadoop 20 or newer - skip the aux_jars option. picked up from hiveconf
+      exec $HADOOP jar ${HIVE_LIB}/hive_cli.jar $CLASS $HIVE_OPTS "$@" 
+  fi
 
 }
 
