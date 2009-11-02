01cc6345e9b3057f1b57cc5d82c3dc87bb5d1050
Zheng Shao <zshao@apache.org>
2009-11-02 07:19:02 +0000

    HIVE-902. Fix cli.sh to work with hadoop versions less than 20. (Carl Steinbach via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@831810 13f79535-47bb-0310-9956-ffa450edef68

--- bin/ext/cli.sh
+++ bin/ext/cli.sh
@@ -14,9 +14,20 @@ cli () {
     HIVE_LIB=`cygpath -w "$HIVE_LIB"`
   fi
 
-  version=$($HADOOP version | awk '{print $2;}');
+  version=$($HADOOP version | awk '{if (NR == 1) {print $2;}}');
 
-  if [[ $version =~ "^0\.17" ]] || [[ $version =~ "^0\.18" ]] || [[ $version =~ "^0.19" ]]; then
+  if [[ $version =~ ^([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+).*$ ]]; then
+      major_ver=${BASH_REMATCH[1]}
+      minor_ver=${BASH_REMATCH[2]}
+      patch_ver=${BASH_REMATCH[3]}
+  else
+      echo "Unable to determine Hadoop version information."
+      echo "'hadoop version' returned:"
+      echo `$HADOOP version`
+      exit 6
+  fi
+
+  if [ $minor_ver -le 20 ]; then
       exec $HADOOP jar $AUX_JARS_CMD_LINE ${HIVE_LIB}/hive_cli.jar $CLASS $HIVE_OPTS "$@"
   else
       # hadoop 20 or newer - skip the aux_jars option. picked up from hiveconf
