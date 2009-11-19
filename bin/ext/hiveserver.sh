8d763751d22237046440e099e4e435593503ffed
Namit Jain <namit@apache.org>
2009-11-19 01:54:44 +0000

    HIVE-941 hiveserver.sh doesn't work with Hadoop 0.20.0 or higher (Ning Zhang via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@882026 13f79535-47bb-0310-9956-ffa450edef68

--- bin/ext/hiveserver.sh
+++ bin/ext/hiveserver.sh
@@ -8,10 +8,30 @@ hiveserver() {
     HIVE_LIB=`cygpath -w "$HIVE_LIB"`
   fi
   JAR=${HIVE_LIB}/hive_service.jar
-  if [ "$HIVE_PORT" != "" ]; then
-    HIVE_OPTS=$HIVE_PORT
+
+  version=$($HADOOP version | awk '{if (NR == 1) {print $2;}}');
+
+  # Save the regex to a var to workaround quoting incompatabilities
+  # between Bash 3.1 and 3.2
+  version_re="^([[:digit:]]+)\.([[:digit:]]+)(\.([[:digit:]]+))?.*$"
+
+  if [[ "$version" =~ $version_re ]]; then
+      major_ver=${BASH_REMATCH[1]}
+      minor_ver=${BASH_REMATCH[2]}
+      patch_ver=${BASH_REMATCH[4]}
+  else
+      echo "Unable to determine Hadoop version information."
+      echo "'hadoop version' returned:"
+      echo `$HADOOP version`
+      exit 6
   fi
-  exec $HADOOP jar $AUX_JARS_CMD_LINE $JAR $CLASS $HIVE_OPTS "$@"
+
+  if [ $minor_ver -lt 20 ]; then
+    exec $HADOOP jar $AUX_JARS_CMD_LINE $JAR $CLASS $HIVE_PORT "$@"
+  else 
+    # hadoop 20 or newer - skip the aux_jars option and hiveconf
+    exec $HADOOP jar $JAR $CLASS $HIVE_PORT "$@"
+  fi
 }
 
 hiveserver_help() {
