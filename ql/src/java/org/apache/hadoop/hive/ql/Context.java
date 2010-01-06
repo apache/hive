69a1eeb5107bb928a2fe817b493d789a854b5d67
Zheng Shao <zshao@apache.org>
2010-01-06 02:18:48 +0000

    HIVE-458. Better error messages for fs.default.name. (Paul Yang via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@896305 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/Context.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Context.java
@@ -180,6 +180,9 @@ public class Context {
         makeMRScratchDir();
       } catch (IOException e) {
         throw new RuntimeException (e);
+      } catch (IllegalArgumentException e) {
+        throw new RuntimeException("Error while making MR scratch " + 
+            "directory - check filesystem config (" + e.getCause() + ")", e);
       }
     }
     return MRScratchDir.toString();
@@ -194,6 +197,9 @@ public class Context {
         makeLocalScratchDir();
       } catch (IOException e) {
         throw new RuntimeException(e);
+      } catch (IllegalArgumentException e) {
+        throw new RuntimeException("Error while making local scratch " + 
+            "directory - check filesystem config (" + e.getCause() + ")", e);
       }
     }
     return localScratchDir.toString();
