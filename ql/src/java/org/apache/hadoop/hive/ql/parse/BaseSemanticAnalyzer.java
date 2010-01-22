bab2c526fc7523880700fd33d963748e09c56ccd
Zheng Shao <zshao@apache.org>
2010-01-22 04:55:57 +0000

    HIVE-1072. Keep CreateTime when a partition is overwritten. (Paul Yang via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@901995 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/parse/BaseSemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/BaseSemanticAnalyzer.java
@@ -432,8 +432,13 @@ public abstract class BaseSemanticAnalyzer {
               .toLowerCase()), val);
         }
         try {
-          // this doesn't create partition. partition is created in MoveTask
-          partHandle = new Partition(tableHandle, partSpec, null);
+          // In case the partition already exists, we need to get the partition
+          // data from the metastore
+          partHandle = db.getPartition(tableHandle, partSpec, false);
+          if(partHandle == null) {
+            // this doesn't create partition. partition is created in MoveTask
+            partHandle = new Partition(tableHandle, partSpec, null);
+          }
         } catch (HiveException e) {
           throw new SemanticException(ErrorMsg.INVALID_PARTITION.getMsg(ast
               .getChild(childIndex)));
