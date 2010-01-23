364f2d84ab40612d6ad3d99dde8f69311aa5acb0
Ning Zhang <nzhang@apache.org>
2010-01-23 01:21:12 +0000

    HIVE-1085: ColumnarSerde should not be the default Serde when user specified a fileformat using stored as (Yongqiang He via Ning Zhang)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@902329 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -5884,8 +5884,8 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     String mapKeyDelim = null;
     String lineDelim = null;
     String comment = null;
-    String inputFormat = TEXTFILE_INPUT;
-    String outputFormat = TEXTFILE_OUTPUT;
+    String inputFormat = null;
+    String outputFormat = null;
     String location = null;
     String serde = null;
     Map<String, String> mapProp = null;
@@ -5896,17 +5896,6 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     final int CTLT = 1; // CREATE TABLE LIKE ... (CTLT)
     final int CTAS = 2; // CREATE TABLE AS SELECT ... (CTAS)
     int command_type = CREATE_TABLE;
-
-    if ("SequenceFile".equalsIgnoreCase(conf
-        .getVar(HiveConf.ConfVars.HIVEDEFAULTFILEFORMAT))) {
-      inputFormat = SEQUENCEFILE_INPUT;
-      outputFormat = SEQUENCEFILE_OUTPUT;
-    } else if ("RCFile".equalsIgnoreCase(conf
-        .getVar(HiveConf.ConfVars.HIVEDEFAULTFILEFORMAT))) {
-      inputFormat = RCFILE_INPUT;
-      outputFormat = RCFILE_OUTPUT;
-      serde = COLUMNAR_SERDE;
-    }
 
     LOG.info("Creating table " + tableName + " position="
         + ast.getCharPositionInLine());
@@ -6052,6 +6041,21 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       }
     }
 
+    if (inputFormat == null) {
+      assert outputFormat == null;
+      if ("SequenceFile".equalsIgnoreCase(conf.getVar(HiveConf.ConfVars.HIVEDEFAULTFILEFORMAT))) {
+        inputFormat = SEQUENCEFILE_INPUT;
+        outputFormat = SEQUENCEFILE_OUTPUT;
+      } else if ("RCFile".equalsIgnoreCase(conf.getVar(HiveConf.ConfVars.HIVEDEFAULTFILEFORMAT))) {
+        inputFormat = RCFILE_INPUT;
+        outputFormat = RCFILE_OUTPUT;
+        serde = COLUMNAR_SERDE;
+      } else {
+        inputFormat = TEXTFILE_INPUT;
+        outputFormat = TEXTFILE_OUTPUT;
+      }
+    }
+    
     // check for existence of table
     if (ifNotExists) {
       try {
--- ql/src/test/queries/clientpositive/rcfile_default_format.q
+++ ql/src/test/queries/clientpositive/rcfile_default_format.q
@@ -6,6 +6,15 @@ DESCRIBE EXTENDED rcfile_default_format;
 CREATE TABLE rcfile_default_format_ctas AS SELECT key,value FROM src;
 DESCRIBE EXTENDED rcfile_default_format_ctas; 
 
+CREATE TABLE rcfile_default_format_txtfile (key STRING) STORED AS TEXTFILE;
+INSERT OVERWRITE TABLE rcfile_default_format_txtfile SELECT key from src;
+DESCRIBE EXTENDED rcfile_default_format_txtfile; 
+
 SET hive.default.fileformat = TextFile;
 CREATE TABLE textfile_default_format_ctas AS SELECT key,value FROM rcfile_default_format_ctas;
-DESCRIBE EXTENDED textfile_default_format_ctas; 
+DESCRIBE EXTENDED textfile_default_format_ctas;
+
+DROP TABLE  rcfile_default_format;
+DROP TABLE  rcfile_default_format_ctas;
+DROP TABLE rcfile_default_format_txtfile;
+DROP TABLE textfile_default_format_ctas;
