ce4cb40fd95ffc635374e074be1f9f8650dd57d4
Zheng Shao <zshao@apache.org>
2010-04-15 07:45:41 +0000

    HIVE-1311. Bug in use of parameter hadoop supports splittable. (Namit Jain via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@934321 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/io/CombineHiveInputFormat.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/CombineHiveInputFormat.java
@@ -252,7 +252,7 @@ public class CombineHiveInputFormat<K extends WritableComparable, V extends Writ
 
       // Since there is no easy way of knowing whether MAPREDUCE-1597 is present in the tree or not,
       // we use a configuration variable for the same
-      if (this.mrwork != null && this.mrwork.getHadoopSupportsSplittable()) {
+      if (this.mrwork != null && !this.mrwork.getHadoopSupportsSplittable()) {
         // The following code should be removed, once
         // https://issues.apache.org/jira/browse/MAPREDUCE-1597 is fixed.
         // Hadoop does not handle non-splittable files correctly for CombineFileInputFormat,
