10e74cdb75c29ec4119c3c91cbca6ab356e40ebb
zznate <nate@vervewireless.com>
2011-04-26 11:59:08 -0500

    updated tests to have a common base class in prep for BRISK-100

---
+++ cassandra-handler/src/test/org/apache/hadoop/hive/cassandra/BaseCassandraConnectionTest.java
@@ -0,0 +1,108 @@
+package org.apache.hadoop.hive.cassandra;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.config.ConfigurationException;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.config.KSMetaData;
+import org.apache.cassandra.contrib.utils.service.CassandraServiceDataCleaner;
+import org.apache.cassandra.db.ColumnFamilyType;
+import org.apache.cassandra.db.marshal.BytesType;
+import org.apache.cassandra.locator.AbstractReplicationStrategy;
+import org.apache.cassandra.locator.SimpleStrategy;
+import org.apache.cassandra.service.EmbeddedCassandraService;
+import org.apache.cassandra.thrift.Cassandra;
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TTransportException;
+
+public abstract class BaseCassandraConnectionTest extends TestCase {
+
+  protected Cassandra.Iface client;
+  private EmbeddedCassandraService cassandra;
+  protected String ksName = "TestKeyspace";
+  protected String cfName = "TestColumnFamily";
+
+  /**
+   * Start the embedded cassandra server if it is not up.
+   *
+   * @throws IOException
+   * @throws TTransportException
+   * @throws TException
+   * @throws CassandraException
+   */
+  protected void maybeStartServer() throws IOException, TTransportException, TException, CassandraException
+  {
+
+      if (cassandra==null){
+          CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
+          cleaner.prepare();
+          cassandra = new EmbeddedCassandraService();
+          cassandra.start();
+          client = (Cassandra.Iface) CassandraProxyClient.newProxyConnection(
+                  "127.0.0.1", 9170, true, true);
+          loadSchema();
+      }
+
+      client.describe_cluster_name();
+  }
+
+
+
+  @Override
+  protected void tearDown() throws Exception
+  {
+      client.truncate(cfName);
+  }
+
+
+  private void loadSchema()
+  {
+      try
+      {
+          for (KSMetaData ksm : schemaDefinition())
+          {
+              for (CFMetaData cfm : ksm.cfMetaData().values()) {
+                CFMetaData.map(cfm);
+              }
+              DatabaseDescriptor.setTableDefinition(ksm, DatabaseDescriptor.getDefsVersion());
+          }
+      }
+      catch (ConfigurationException e)
+      {
+          throw new RuntimeException(e);
+      }
+  }
+
+  private Collection<KSMetaData> schemaDefinition()
+  {
+      List<KSMetaData> schema = new ArrayList<KSMetaData>();
+
+      Class<? extends AbstractReplicationStrategy> simple = SimpleStrategy.class;
+
+      Map<String, String> opts_rf1 = KSMetaData.optsWithRF(1);
+
+      // initial test keyspace
+      schema.add(new KSMetaData(
+              ksName,
+              simple,
+              opts_rf1,
+              // Add more Column Families as needed
+              standardCFMD(ksName, "TestColumnFamily")));
+
+
+      return schema;
+  }
+
+  private static CFMetaData standardCFMD(String ksName, String cfName)
+  {
+      return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard, BytesType.instance, null).keyCacheSize(0);
+  }
+
+}
