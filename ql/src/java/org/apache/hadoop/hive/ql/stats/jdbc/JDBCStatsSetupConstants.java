225e719b0bfb8ad124b6613c95ea8ce9385a3844
Ning Zhang <nzhang@apache.org>
2011-06-06 04:37:07 +0000

    HIVE-2192. Stats table schema incompatible after HIVE-2185 (Tomasz Nykiel via Ning Zhang)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1132520 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/stats/jdbc/JDBCStatsSetupConstants.java
+++ ql/src/java/org/apache/hadoop/hive/ql/stats/jdbc/JDBCStatsSetupConstants.java
@@ -21,7 +21,7 @@ public final class JDBCStatsSetupConstants {
 
   public static final String PART_STAT_ID_COLUMN_NAME = "ID";
 
-  public static final String PART_STAT_TABLE_NAME = "PARTITION_STAT_TBL";
+  public static final String PART_STAT_TABLE_NAME = "PARTITION_STATS";
 
   // supported statistics - column names
 
