c1eb5735c724fa54a777cf92e2417b9a5bab4ddf
Ning Zhang <nzhang@apache.org>
2011-08-16 04:25:44 +0000

    HIVE-1916. Change Default Alias For Aggregated Columns (_c1) (sameerm via nzhang)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1158104 13f79535-47bb-0310-9956-ffa450edef68

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -461,6 +461,11 @@ public class HiveConf extends Configuration {
     HIVE_REWORK_MAPREDWORK("hive.rework.mapredwork", false),
     HIVE_CONCATENATE_CHECK_INDEX ("hive.exec.concatenate.check.index", true),
 
+    //prefix used to auto generated column aliases
+    HIVE_AUTOGEN_COLUMNALIAS_PREFIX_LABEL("hive.autogen.columnalias.prefix.label", "_c"),
+    HIVE_AUTOGEN_COLUMNALIAS_PREFIX_INCLUDEFUNCNAME(
+                               "hive.autogen.columnalias.prefix.includefuncname", false),
+
     // The class responsible for logging client side performance metrics
     // Must be a subclass of org.apache.hadoop.hive.ql.log.PerfLogger
     HIVE_PERF_LOGGER("hive.exec.perf.logger", "org.apache.hadoop.hive.ql.log.PerfLogger"),
