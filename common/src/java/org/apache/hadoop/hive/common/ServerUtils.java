751ee91740d50ff7ed8db7d0348e0771f951694f
John Sichi <jvs@apache.org>
2011-09-23 17:19:45 +0000

    HIVE-2181. Clean up the scratch.dir (tmp/hive-root) while restarting Hive server. (Chinna Rao Lalam via jvs)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1174892 13f79535-47bb-0310-9956-ffa450edef68

---
+++ common/src/java/org/apache/hadoop/hive/common/ServerUtils.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.common;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.conf.HiveConf;
+
+/**
+ *
+ * ServerUtils.
+ *
+ */
+public class ServerUtils {
+
+  public static final Log LOG = LogFactory.getLog(ServerUtils.class);
+
+  public static void cleanUpScratchDir(HiveConf hiveConf) {
+    if (hiveConf.getBoolVar(HiveConf.ConfVars.HIVE_START_CLEANUP_SCRATCHDIR)) {
+      String hiveScratchDir = hiveConf.get(HiveConf.ConfVars.SCRATCHDIR.varname);
+      try {
+        Path jobScratchDir = new Path(hiveScratchDir);
+        LOG.info("Cleaning scratchDir : " + hiveScratchDir);
+        FileSystem fileSystem = jobScratchDir.getFileSystem(hiveConf);
+        fileSystem.delete(jobScratchDir, true);
+      }
+      // Even if the cleanup throws some exception it will continue.
+      catch (Throwable e) {
+        LOG.warn("Unable to delete scratchDir : " + hiveScratchDir, e);
+      }
+    }
+  }
+
+}
