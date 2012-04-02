f4ba48fe2c22e25363d78c6daf078e342bdeb770
Namit Jain <namit@apache.org>
2012-04-02 15:45:40 +0000

    HIVE-2805 Move metastore upgrade scripts labeled 0.10.0 into scripts labeled 0.9.0 (Kevin Wilfong via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1308392 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/scripts/upgrade/derby/upgrade-0.9.0-to-0.10.0.derby.sql
+++
@@ -1,2 +0,0 @@
--- Upgrade MetaStore schema from 0.9.0 to 0.10.0
-RUN '010-HIVE-2612.derby.sql';
--- metastore/scripts/upgrade/mysql/upgrade-0.8.0-to-0.9.0.mysql.sql
+++ metastore/scripts/upgrade/mysql/upgrade-0.8.0-to-0.9.0.mysql.sql
@@ -1,2 +1,3 @@
 SELECT 'Upgrading MetaStore schema from 0.8.0 to 0.9.0' AS ' ';
+SOURCE 009-HIVE-2612.mysql.sql;
 SELECT 'Finished upgrading MetaStore schema from 0.8.0 to 0.9.0' AS ' ';
