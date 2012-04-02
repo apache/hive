b0b35578123be76ce05646b2f7ace4b781ef0b87
Namit Jain <namit@apache.org>
2012-04-02 15:51:59 +0000

    HIVE-2853 Add pre event listeners to metastore (Kevin Wilfong via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1308399 13f79535-47bb-0310-9956-ffa450edef68

---
+++ metastore/src/java/org/apache/hadoop/hive/metastore/events/PreAlterTableEvent.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.metastore.events;
+
+import org.apache.hadoop.hive.metastore.HiveMetaStore.HMSHandler;
+import org.apache.hadoop.hive.metastore.api.Table;
+
+public class PreAlterTableEvent extends PreEventContext {
+
+  private final Table newTable;
+  private final Table oldTable;
+
+  public PreAlterTableEvent (Table oldTable, Table newTable, HMSHandler handler) {
+    super (PreEventType.ALTER_TABLE, handler);
+    this.oldTable = oldTable;
+    this.newTable = newTable;
+  }
+
+  /**
+   * @return the old table
+   */
+  public Table getOldTable() {
+    return oldTable;
+  }
+
+  /**
+   * @return the new table
+   */
+  public Table getNewTable() {
+    return newTable;
+  }
+}
