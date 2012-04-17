240b7b96ecf9a681d10f6f0853f825cad544e18b
Ashutosh Chauhan <hashutosh@apache.org>
2012-04-17 18:03:04 +0000

    HIVE-2930 [jira] Add license to the Hive files (Owen O'Malley via Ashutosh Chauhan)
    Summary:
    <<Enter Revision Title>>
    
    Fixing rat warnings
    
    We need to clean up the RAT report to 0. Apache projects aren't supposed to
    release until they have shown that all of their files have proper headers.
    
    Note that although most of the files are just missing headers, some of them
    explicitly say copyright by facebook and released under the Thrift (not Apache)
    license. I'll generate a list of them, but I'd really appreciate it if someone
    from facebook could in verify that they intend to license them to Apache.
    
    Test Plan: EMPTY
    
    Reviewers: JIRA, ashutoshc
    
    Reviewed By: ashutoshc
    
    Differential Revision: https://reviews.facebook.net/D2835
    
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1327205 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/udf/UDFOPPlus.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/UDFOPPlus.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE+2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,7 +31,7 @@ import org.apache.hadoop.io.LongWritable;
  * better performance and type checking (so we know int + int is still an int
  * instead of a double); otherwise a single method that takes (Number a, Number
  * b) and use a.doubleValue() == b.doubleValue() is enough.
- * 
+ *
  * The case of int + double will be handled by implicit type casting using
  * UDFRegistry.implicitConvertable method.
  */
--- serde/src/java/org/apache/hadoop/hive/serde2/columnar/LazyBinaryColumnarSerDe.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/columnar/LazyBinaryColumnarSerDe.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hive.serde2.columnar;
 
 import java.util.List;
@@ -24,7 +41,7 @@ import org.apache.hadoop.io.Writable;
  * LazyBinaryColumnarSerDe. This serde combines elements of columnar serde and lazybinary serde
  * to produce a serde which serializes columns into a BytesRefArrayWritable in a compact binary
  * format and which is deserialized in a lazy, i.e. on-demand fashion.
- * 
+ *
  */
 public class LazyBinaryColumnarSerDe extends ColumnarSerDeBase {
 
@@ -46,7 +63,7 @@ public class LazyBinaryColumnarSerDe extends ColumnarSerDeBase {
     LazyUtils.extractColumnInfo(tbl, serdeParams, getClass().getName());
     columnNames = serdeParams.getColumnNames();
     columnTypes = serdeParams.getColumnTypes();
-    
+
     cachedObjectInspector = LazyBinaryFactory.createColumnarStructInspector(
         columnNames, columnTypes);
     java.util.ArrayList<Integer> notSkipIDs = ColumnProjectionUtils.getReadColumnIDs(conf);
@@ -77,7 +94,7 @@ public class LazyBinaryColumnarSerDe extends ColumnarSerDeBase {
       // Get the field objectInspector and the field object.
       ObjectInspector foi = fields.get(i).getFieldObjectInspector();
       Object f = (list == null ? null : list.get(i));
-      //empty strings are marked by an invalid utf single byte sequence. A valid utf stream cannot 
+      //empty strings are marked by an invalid utf single byte sequence. A valid utf stream cannot
       //produce this sequence
       if ((f != null) && (foi.getCategory().equals(ObjectInspector.Category.PRIMITIVE))
           && ((PrimitiveObjectInspector) foi).getPrimitiveCategory().equals(
