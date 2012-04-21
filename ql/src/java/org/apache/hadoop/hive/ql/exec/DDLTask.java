f40c0af4c2a2e0d5e4b05d182b81857730b9f7ac
Ashutosh Chauhan <hashutosh@apache.org>
2012-04-21 02:36:57 +0000

    HIVE-2966 :Revert HIVE-2795  (Thejas Nair via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1328568 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
@@ -191,10 +191,11 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
     // Pick the formatter to use to display the results.  Either the
     // normal human readable output or a json object.
     if ("json".equals(conf.get(
-            HiveConf.ConfVars.HIVE_DDL_OUTPUT_FORMAT.varname, "text")))
+            HiveConf.ConfVars.HIVE_DDL_OUTPUT_FORMAT.varname, "text"))) {
       formatter = new JsonMetaDataFormatter();
-    else
+    } else {
       formatter = new TextMetaDataFormatter();
+    }
 
     INTERMEDIATE_ARCHIVED_DIR_SUFFIX =
       HiveConf.getVar(conf, ConfVars.METASTORE_INT_ARCHIVED);
@@ -2361,8 +2362,9 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
                           formatter.MISSING);
       } else {
           Map<String, String> params = null;
-          if(descDatabase.isExt())
-              params = database.getParameters();
+          if(descDatabase.isExt()) {
+            params = database.getParameters();
+          }
 
           formatter.showDatabaseDescription(outStream,
                                             database.getName(),
@@ -2588,7 +2590,8 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
 
       List<FieldSchema> cols = null;
       if (colPath.equals(tableName)) {
-        cols = (part == null) ? tbl.getCols() : part.getCols();
+        cols = (part == null || tbl.getTableType() == TableType.VIRTUAL_VIEW) ?
+            tbl.getCols() : part.getCols();
 
         if (!descTbl.isFormatted()) {
           if (tableName.equals(colPath)) {
