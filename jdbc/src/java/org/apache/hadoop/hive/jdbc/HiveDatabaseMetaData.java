5c33d6e90c8bc3fcb60b53f744b132e2cf3bf45e
Namit Jain <namit@apache.org>
2012-05-24 20:36:54 +0000

    HIVE-3051 JDBC cannot find metadata for tables/columns containing uppercase character (Navis via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1342428 13f79535-47bb-0310-9956-ffa450edef68

--- jdbc/src/java/org/apache/hadoop/hive/jdbc/HiveDatabaseMetaData.java
+++ jdbc/src/java/org/apache/hadoop/hive/jdbc/HiveDatabaseMetaData.java
@@ -171,7 +171,7 @@ public class HiveDatabaseMetaData implements java.sql.DatabaseMetaData {
           } else if (c == '_') {
             result.append('.');
           } else {
-            result.append(c);
+            result.append(Character.toLowerCase(c));
           }
         }
       }
