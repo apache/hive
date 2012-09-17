d2215954f69c7ac901e1189f84991e18299edc74
Carl Steinbach <cws@apache.org>
2012-09-17 21:40:10 +0000

    Backout HIVE-3443 (cws)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1386857 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/src/java/org/apache/hadoop/hive/metastore/tools/HiveMetaTool.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/tools/HiveMetaTool.java
@@ -20,9 +20,8 @@ package org.apache.hadoop.hive.metastore.tools;
 
 import java.net.URI;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.cli.CommandLine;
@@ -50,10 +49,9 @@ public class HiveMetaTool {
   private static final Log LOG = LogFactory.getLog(HiveMetaTool.class.getName());
   private final Options cmdLineOptions = new Options();
   private ObjectStore objStore;
-  private boolean isObjStoreInitialized;
+
 
   public HiveMetaTool() {
-    this.isObjStoreInitialized = false;
   }
 
   @SuppressWarnings("static-access")
@@ -77,78 +75,48 @@ public class HiveMetaTool {
     Option updateFSRootLoc =
         OptionBuilder
             .withArgName("new-loc> " + "<old-loc")
-            .hasArgs(2)
+            .hasArgs(3)
             .withDescription(
-                "Update FS root location in the metastore to new location.Both new-loc and " +
-                    "old-loc should be valid URIs with valid host names and schemes." +
-                    "When run with the dryRun option changes are displayed but are not " +
-                    "persisted. When run with the serdepropKey/tablePropKey option " +
-                    "updateLocation looks for the serde-prop-key/table-prop-key that is " +
-                    "specified and updates its value if found.")
-                    .create("updateLocation");
-    Option dryRun = new Option("dryRun" , "Perform a dry run of updateLocation changes.When " +
-      "run with the dryRun option updateLocation changes are displayed but not persisted. " +
-      "dryRun is valid only with the updateLocation option.");
-    Option serdePropKey =
-        OptionBuilder.withArgName("serde-prop-key")
-        .hasArgs()
-        .withValueSeparator()
-        .withDescription("Specify the key for serde property to be updated. serdePropKey option " +
-           "is valid only with updateLocation option.")
-        .create("serdePropKey");
-    Option tablePropKey =
-        OptionBuilder.withArgName("table-prop-key")
-        .hasArg()
-        .withValueSeparator()
-        .withDescription("Specify the key for table property to be updated. tablePropKey option " +
-          "is valid only with updateLocation option.")
-        .create("tablePropKey");
+                "update FS root location in the metastore to new location. Both new-loc and" +
+                    " old-loc should be valid URIs with valid host names and schemes. " +
+                    "when run with the dryRun option changes are displayed but are not persisted.")
+            .create("updateLocation");
+    Option dryRun = new Option("dryRun" , "dryRun is valid only with updateLocation option. when " +
+      "run with the dryRun option updateLocation changes are displayed but are not persisted.");
 
     cmdLineOptions.addOption(help);
     cmdLineOptions.addOption(listFSRoot);
     cmdLineOptions.addOption(executeJDOQL);
     cmdLineOptions.addOption(updateFSRootLoc);
     cmdLineOptions.addOption(dryRun);
-    cmdLineOptions.addOption(serdePropKey);
-    cmdLineOptions.addOption(tablePropKey);
+
   }
 
   private void initObjectStore(HiveConf hiveConf) {
-    if (!isObjStoreInitialized) {
-      objStore = new ObjectStore();
-      objStore.setConf(hiveConf);
-      isObjStoreInitialized = true;
-    }
+    objStore = new ObjectStore();
+    objStore.setConf(hiveConf);
   }
 
   private void shutdownObjectStore() {
-    if (isObjStoreInitialized) {
+    if (objStore != null) {
       objStore.shutdown();
-      isObjStoreInitialized = false;
     }
   }
 
   private void listFSRoot() {
-    HiveConf hiveConf = new HiveConf(HiveMetaTool.class);
-    initObjectStore(hiveConf);
-
     Set<String> hdfsRoots = objStore.listFSRoots();
     if (hdfsRoots != null) {
-      System.out.println("Listing FS Roots..");
+      System.out.println("HiveMetaTool:Listing FS Roots..");
       for (String s : hdfsRoots) {
         System.out.println(s);
       }
     } else {
-      System.err.println("Encountered error during listFSRoot - " +
+      System.err.println("HiveMetaTool:Encountered error during listFSRoot - " +
         "commit of JDO transaction failed");
     }
   }
 
   private void executeJDOQLSelect(String query) {
-    HiveConf hiveConf = new HiveConf(HiveMetaTool.class);
-    initObjectStore(hiveConf);
-
-    System.out.println("Executing query: " + query);
     Collection<?> result = objStore.executeJDOQLSelect(query);
     if (result != null) {
       Iterator<?> iter = result.iterator();
@@ -157,205 +125,46 @@ public class HiveMetaTool {
         System.out.println(o.toString());
       }
     } else {
-      System.err.println("Encountered error during executeJDOQLSelect -" +
+      System.err.println("HiveMetaTool:Encountered error during executeJDOQLSelect -" +
         "commit of JDO transaction failed.");
     }
   }
 
-  private long executeJDOQLUpdate(String query) {
-    HiveConf hiveConf = new HiveConf(HiveMetaTool.class);
-    initObjectStore(hiveConf);
-
-    System.out.println("Executing query: " + query);
+  private void executeJDOQLUpdate(String query) {
     long numUpdated = objStore.executeJDOQLUpdate(query);
     if (numUpdated >= 0) {
-      System.out.println("Number of records updated: " + numUpdated);
+      System.out.println("HiveMetaTool:Number of records updated: " + numUpdated);
     } else {
-      System.err.println("Encountered error during executeJDOQL -" +
+      System.err.println("HiveMetaTool:Encountered error during executeJDOQL -" +
         "commit of JDO transaction failed.");
     }
-    return numUpdated;
   }
 
-  private int printUpdateLocations(Map<String, String> updateLocations) {
-    int count = 0;
+  private void printUpdateLocations(HashMap<String, String> updateLocations) {
     for (String key: updateLocations.keySet()) {
       String value = updateLocations.get(key);
-      System.out.println("old location: " + key + " new location: " + value);
-      count++;
+      System.out.println("current location: " + key + " new location: " + value);
     }
-    return count;
   }
 
-  private void printTblURIUpdateSummary(ObjectStore.UpdateMStorageDescriptorTblURIRetVal retVal,
-    boolean isDryRun) {
-    String tblName = new String("SDS");
-    String fieldName = new String("LOCATION");
-
-    if (retVal == null) {
-      System.err.println("Encountered error while executing updateMStorageDescriptorTblURI - " +
-          "commit of JDO transaction failed. Failed to update FSRoot locations in " +
-          fieldName + "field in " + tblName + " table.");
+  private void updateFSRootLocation(URI oldURI, URI newURI, boolean dryRun) {
+    HashMap<String, String> updateLocations = new HashMap<String, String>();
+    int count = objStore.updateFSRootLocation(oldURI, newURI, updateLocations, dryRun);
+    if (count == -1) {
+      System.err.println("HiveMetaTool:Encountered error while executing updateFSRootLocation - " +
+        "commit of JDO transaction failed, failed to update FS Root locations.");
     } else {
-      Map<String, String> updateLocations = retVal.getUpdateLocations();
-      if (isDryRun) {
-        System.out.println("Dry Run of updateLocation on table " + tblName + "..");
+      if (!dryRun) {
+        System.out.println("HiveMetaTool: Successfully updated " + count + "FS Root locations");
       } else {
-        System.out.println("Successfully updated the following locations..");
-      }
-      int count = printUpdateLocations(updateLocations);
-      if (isDryRun) {
-        System.out.println("Found " + count + " records in " + tblName + " table to update");
-      } else {
-        System.out.println("Updated " + count + " records in " + tblName + " table");
-      }
-      List<String> badRecords = retVal.getBadRecords();
-      if (badRecords.size() > 0) {
-        System.err.println("Found records with bad " + fieldName +  " in " + tblName + " table.. ");
-        for (String badRecord:badRecords) {
-          System.err.println("bad location URI: " + badRecord);
-        }
+        printUpdateLocations(updateLocations);
       }
     }
-  }
-
-  private void printDatabaseURIUpdateSummary(ObjectStore.UpdateMDatabaseURIRetVal retVal,
-    boolean isDryRun) {
-    String tblName = new String("DBS");
-    String fieldName = new String("LOCATION_URI");
-
-    if (retVal == null) {
-      System.err.println("Encountered error while executing updateMDatabaseURI - " +
-          "commit of JDO transaction failed. Failed to update FSRoot locations in " +
-          fieldName + "field in " + tblName + " table.");
-    } else {
-      Map<String, String> updateLocations = retVal.getUpdateLocations();
-      if (isDryRun) {
-        System.out.println("Dry Run of updateLocation on table " + tblName + "..");
-      } else {
-        System.out.println("Successfully updated the following locations..");
-      }
-      int count = printUpdateLocations(updateLocations);
-      if (isDryRun) {
-        System.out.println("Found " + count + " records in " + tblName + " table to update");
-      } else {
-        System.out.println("Updated " + count + " records in " + tblName + " table");
-      }
-      List<String> badRecords = retVal.getBadRecords();
-      if (badRecords.size() > 0) {
-        System.err.println("Found records with bad " + fieldName +  " in " + tblName + " table.. ");
-        for (String badRecord:badRecords) {
-          System.err.println("bad location URI: " + badRecord);
-        }
-      }
-    }
-  }
-
-  private void printTblPropURIUpdateSummary(
-    ObjectStore.UpdateMStorageDescriptorTblPropURIRetVal retVal, String tablePropKey,
-    boolean isDryRun) {
-    String tblName = new String("SD_PARAMS");
-
-    if (retVal == null) {
-      System.err.println("Encountered error while executing updateMStorageDescriptorTblPropURI - " +
-        "commit of JDO transaction failed. Failed to update FSRoot locations in " +
-        "value field corresponding to" + tablePropKey + " in " + tblName + " table.");
-    } else {
-      Map<String, String> updateLocations = retVal.getUpdateLocations();
-      if (isDryRun) {
-        System.out.println("Dry Run of updateLocation on table " + tblName + "..");
-      } else {
-        System.out.println("Successfully updated the following locations..");
-      }
-      int count = printUpdateLocations(updateLocations);
-      if (isDryRun) {
-        System.out.println("Found " + count + " records in " + tblName + " table to update");
-      } else {
-        System.out.println("Updated " + count + " records in " + tblName + " table");
-      }
-      List<String> badRecords = retVal.getBadRecords();
-      if (badRecords.size() > 0) {
-        System.err.println("Found records with bad " + tablePropKey +  " key in " +
-        tblName + " table.. ");
-        for (String badRecord:badRecords) {
-          System.err.println("bad location URI: " + badRecord);
-        }
-      }
-    }
-  }
-
-  private void printSerdePropURIUpdateSummary(ObjectStore.UpdateSerdeURIRetVal retVal,
-    String serdePropKey, boolean isDryRun) {
-    String tblName = new String("SERDE_PARAMS");
-
-    if (retVal == null) {
-      System.err.println("Encountered error while executing updateSerdeURI - " +
-        "commit of JDO transaction failed. Failed to update FSRoot locations in " +
-        "value field corresponding to " + serdePropKey + " in " + tblName + " table.");
-    } else {
-      Map<String, String> updateLocations = retVal.getUpdateLocations();
-      if (isDryRun) {
-        System.out.println("Dry Run of updateLocation on table " + tblName + "..");
-      } else {
-        System.out.println("Successfully updated the following locations..");
-      }
-      int count = printUpdateLocations(updateLocations);
-      if (isDryRun) {
-        System.out.println("Found " + count + " records in " + tblName + " table to update");
-      } else {
-        System.out.println("Updated " + count + " records in " + tblName + " table");
-      }
-      List<String> badRecords = retVal.getBadRecords();
-      if (badRecords.size() > 0) {
-        System.err.println("Found records with bad " + serdePropKey +  " key in " +
-        tblName + " table.. ");
-        for (String badRecord:badRecords) {
-          System.err.println("bad location URI: " + badRecord);
-        }
-      }
-    }
-  }
-
-  public void updateFSRootLocation(URI oldURI, URI newURI, String serdePropKey,
-      String tablePropKey, boolean isDryRun) {
-    HiveConf hiveConf = new HiveConf(HiveMetaTool.class);
-    initObjectStore(hiveConf);
-
-    System.out.println("Looking for LOCATION_URI field in DBS table to update..");
-    ObjectStore.UpdateMDatabaseURIRetVal updateMDBURIRetVal = objStore.updateMDatabaseURI(oldURI,
-                                                                 newURI, isDryRun);
-    printDatabaseURIUpdateSummary(updateMDBURIRetVal, isDryRun);
-
-    System.out.println("Looking for LOCATION field in SDS table to update..");
-    ObjectStore.UpdateMStorageDescriptorTblURIRetVal updateTblURIRetVal =
-                         objStore.updateMStorageDescriptorTblURI(oldURI, newURI, isDryRun);
-    printTblURIUpdateSummary(updateTblURIRetVal, isDryRun);
-
-    if (tablePropKey != null) {
-      System.out.println("Looking for value of " + tablePropKey + " key in SD_PARAMS table " +
-        "to update..");
-      ObjectStore.UpdateMStorageDescriptorTblPropURIRetVal updateTblPropURIRetVal =
-                           objStore.updateMStorageDescriptorTblPropURI(oldURI, newURI,
-                               tablePropKey, isDryRun);
-      printTblPropURIUpdateSummary(updateTblPropURIRetVal, tablePropKey, isDryRun);
-    }
-
-    if (serdePropKey != null) {
-      System.out.println("Looking for value of " + serdePropKey + " key in SERDE_PARAMS table " +
-        "to update..");
-      ObjectStore.UpdateSerdeURIRetVal updateSerdeURIretVal = objStore.updateSerdeURI(oldURI,
-                                                                newURI, serdePropKey, isDryRun);
-      printSerdePropURIUpdateSummary(updateSerdeURIretVal, serdePropKey, isDryRun);
-    }
-  }
-
-  private static void printAndExit(HiveMetaTool metaTool) {
-    HelpFormatter formatter = new HelpFormatter();
-    formatter.printHelp("metatool", metaTool.cmdLineOptions);
-    System.exit(1);
   }
 
   public static void main(String[] args) {
+
+    HiveConf hiveConf = new HiveConf(HiveMetaTool.class);
     HiveMetaTool metaTool = new HiveMetaTool();
     metaTool.init();
     CommandLineParser parser = new GnuParser();
@@ -366,55 +175,35 @@ public class HiveMetaTool {
         line = parser.parse(metaTool.cmdLineOptions, args);
       } catch (ParseException e) {
         System.err.println("HiveMetaTool:Parsing failed.  Reason: " + e.getLocalizedMessage());
-        printAndExit(metaTool);
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("metatool", metaTool.cmdLineOptions);
+        System.exit(1);
       }
 
       if (line.hasOption("help")) {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp("metatool", metaTool.cmdLineOptions);
       } else if (line.hasOption("listFSRoot")) {
-        if (line.hasOption("dryRun")) {
-          System.err.println("HiveMetaTool: dryRun is not valid with listFSRoot");
-          printAndExit(metaTool);
-        } else if (line.hasOption("serdePropKey")) {
-          System.err.println("HiveMetaTool: serdePropKey is not valid with listFSRoot");
-          printAndExit(metaTool);
-        } else if (line.hasOption("tablePropKey")) {
-          System.err.println("HiveMetaTool: tablePropKey is not valid with listFSRoot");
-          printAndExit(metaTool);
-        }
+        metaTool.initObjectStore(hiveConf);
         metaTool.listFSRoot();
       } else if (line.hasOption("executeJDOQL")) {
         String query = line.getOptionValue("executeJDOQL");
-        if (line.hasOption("dryRun")) {
-          System.err.println("HiveMetaTool: dryRun is not valid with executeJDOQL");
-          printAndExit(metaTool);
-        } else if (line.hasOption("serdePropKey")) {
-          System.err.println("HiveMetaTool: serdePropKey is not valid with executeJDOQL");
-          printAndExit(metaTool);
-        } else if (line.hasOption("tablePropKey")) {
-          System.err.println("HiveMetaTool: tablePropKey is not valid with executeJDOQL");
-          printAndExit(metaTool);
-        }
+        metaTool.initObjectStore(hiveConf);
         if (query.toLowerCase().trim().startsWith("select")) {
           metaTool.executeJDOQLSelect(query);
         } else if (query.toLowerCase().trim().startsWith("update")) {
           metaTool.executeJDOQLUpdate(query);
         } else {
           System.err.println("HiveMetaTool:Unsupported statement type");
-          printAndExit(metaTool);
         }
       } else if (line.hasOption("updateLocation")) {
         String[] loc = line.getOptionValues("updateLocation");
-        boolean isDryRun = false;
-        String serdepropKey = null;
-        String tablePropKey = null;
+        boolean dryRun = false;
 
         if (loc.length != 2 && loc.length != 3) {
           System.err.println("HiveMetaTool:updateLocation takes in 2 required and 1 " +
-              "optional arguments but " +
-              "was passed " + loc.length + " arguments");
-          printAndExit(metaTool);
+              "optional arguements but " +
+              "was passed " + loc.length + " arguements");
         }
 
         Path newPath = new Path(loc[0]);
@@ -424,15 +213,7 @@ public class HiveMetaTool {
         URI newURI = newPath.toUri();
 
         if (line.hasOption("dryRun")) {
-          isDryRun = true;
-        }
-
-        if (line.hasOption("serdePropKey")) {
-          serdepropKey = line.getOptionValue("serdePropKey");
-        }
-
-        if (line.hasOption("tablePropKey")) {
-          tablePropKey = line.getOptionValue("tablePropKey");
+          dryRun = true;
         }
 
         /*
@@ -448,24 +229,17 @@ public class HiveMetaTool {
           } else if (oldURI.getScheme() == null || newURI.getScheme() == null) {
             System.err.println("HiveMetaTool:A valid scheme is required in both old-loc and new-loc");
           } else {
-            metaTool.updateFSRootLocation(oldURI, newURI, serdepropKey, tablePropKey, isDryRun);
+            metaTool.initObjectStore(hiveConf);
+            metaTool.updateFSRootLocation(oldURI, newURI, dryRun);
           }
         } else {
-          if (line.hasOption("dryRun")) {
-            System.err.println("HiveMetaTool: dryRun is not a valid standalone option");
-          } else if (line.hasOption("serdePropKey")) {
-            System.err.println("HiveMetaTool: serdePropKey is not a valid standalone option");
-          } else if (line.hasOption("tablePropKey")) {
-            System.err.println("HiveMetaTool: tablePropKey is not a valid standalone option");
-            printAndExit(metaTool);
-          } else {
-            System.err.print("HiveMetaTool:Parsing failed.  Reason: Invalid arguments: " );
-            for (String s : line.getArgs()) {
-              System.err.print(s + " ");
-            }
-            System.err.println();
+          System.err.print("HiveMetaTool:Invalid option:" + line.getOptions());
+          for (String s : line.getArgs()) {
+            System.err.print(s + " ");
           }
-          printAndExit(metaTool);
+          System.err.println();
+          HelpFormatter formatter = new HelpFormatter();
+          formatter.printHelp("metatool", metaTool.cmdLineOptions);
         }
       } finally {
         metaTool.shutdownObjectStore();
--- metastore/src/test/org/apache/hadoop/hive/metastore/TestHiveMetaTool.java
+++ metastore/src/test/org/apache/hadoop/hive/metastore/TestHiveMetaTool.java
@@ -37,7 +37,6 @@ import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.metastore.api.Type;
 import org.apache.hadoop.hive.metastore.tools.HiveMetaTool;
 import org.apache.hadoop.hive.serde.Constants;
-import org.apache.hadoop.hive.serde2.avro.AvroSerdeUtils;
 import org.apache.hadoop.util.StringUtils;
 
 public class TestHiveMetaTool extends TestCase {
@@ -73,20 +72,16 @@ public class TestHiveMetaTool extends TestCase {
       ps = new PrintStream(os);
 
       // create a dummy database and a couple of dummy tables
-      String dbName = "TestHiveMetaToolDB";
+      String dbName = "testDB";
       String typeName = "Person";
       String tblName = "simpleTbl";
-      String badTblName = "badSimpleTbl";
 
       Database db = new Database();
       db.setName(dbName);
       client.dropTable(dbName, tblName);
-      client.dropTable(dbName, badTblName);
       dropDatabase(dbName);
       client.createDatabase(db);
       locationUri = db.getLocationUri();
-      String avroUri = "hdfs://nn.example.com/warehouse/hive/ab.avsc";
-      String badAvroUri = new String("hdfs:/hive");
 
       client.dropType(typeName);
       Type typ1 = new Type();
@@ -115,34 +110,10 @@ public class TestHiveMetaTool extends TestCase {
       sd.getSerdeInfo().setParameters(new HashMap<String, String>());
       sd.getSerdeInfo().getParameters().put(
           org.apache.hadoop.hive.serde.Constants.SERIALIZATION_FORMAT, "1");
-      sd.getParameters().put(AvroSerdeUtils.SCHEMA_URL, avroUri);
       sd.getSerdeInfo().setSerializationLib(
-          org.apache.hadoop.hive.serde2.avro.AvroSerDe.class.getName());
+          org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe.class.getName());
       tbl.setPartitionKeys(new ArrayList<FieldSchema>());
-      client.createTable(tbl);
 
-      //create a table with bad avro uri
-      tbl = new Table();
-      tbl.setDbName(dbName);
-      tbl.setTableName(badTblName);
-      sd = new StorageDescriptor();
-      tbl.setSd(sd);
-      sd.setCols(typ1.getFields());
-      sd.setCompressed(false);
-      sd.setNumBuckets(1);
-      sd.setParameters(new HashMap<String, String>());
-      sd.getParameters().put("test_param_1", "Use this for comments etc");
-      sd.setBucketCols(new ArrayList<String>(2));
-      sd.getBucketCols().add("name");
-      sd.setSerdeInfo(new SerDeInfo());
-      sd.getSerdeInfo().setName(tbl.getTableName());
-      sd.getSerdeInfo().setParameters(new HashMap<String, String>());
-      sd.getSerdeInfo().getParameters().put(
-          org.apache.hadoop.hive.serde.Constants.SERIALIZATION_FORMAT, "1");
-      sd.getParameters().put(AvroSerdeUtils.SCHEMA_URL, badAvroUri);
-      sd.getSerdeInfo().setSerializationLib(
-          org.apache.hadoop.hive.serde2.avro.AvroSerDe.class.getName());
-      tbl.setPartitionKeys(new ArrayList<FieldSchema>());
       client.createTable(tbl);
       client.close();
     } catch (Exception e) {
@@ -202,29 +173,37 @@ public class TestHiveMetaTool extends TestCase {
   public void testUpdateFSRootLocation() throws Exception {
 
     redirectOutputStream();
-    String oldLocationUri = "hdfs://nn.example.com/";
-    String newLocationUri = "hdfs://nn-ha-uri/";
-    String[] args = new String[5];
+    String newLocationUri = "hdfs://nn-ha-uri/user/hive/warehouse";
+    String[] args = new String[3];
     args[0] = new String("-updateLocation");
     args[1] = new String(newLocationUri);
-    args[2] = new String(oldLocationUri);
-    args[3] = new String("-tablePropKey");
-    args[4] = new String("avro.schema.url");
+    args[2] = new String(locationUri);
+
+    String[] args2 = new String[1];
+    args2[0] = new String("-listFSRoot");
 
     try {
+
       // perform HA upgrade
       HiveMetaTool.main(args);
+
+      // obtain new HDFS root
+      HiveMetaTool.main(args2);
+
       String out = os.toString();
       boolean b = out.contains(newLocationUri);
-      restoreOutputStream();
-      assertTrue(b);
 
-      //restore the original HDFS root
-      args[1] = new String(oldLocationUri);
-      args[2] = new String(newLocationUri);
-      redirectOutputStream();
-      HiveMetaTool.main(args);
-      restoreOutputStream();
+      if (b) {
+        System.out.println("updateFSRootLocation successful");
+      } else {
+        System.out.println("updateFSRootLocation failed");
+      }
+      // restore the original HDFS root if needed
+      if (b) {
+        args[1] = new String(locationUri);
+        args[2] = new String(newLocationUri);
+        HiveMetaTool.main(args);
+      }
     } finally {
       restoreOutputStream();
       System.out.println("Completed testUpdateFSRootLocation..");
@@ -235,6 +214,7 @@ public class TestHiveMetaTool extends TestCase {
   protected void tearDown() throws Exception {
     try {
       super.tearDown();
+
     } catch (Throwable e) {
       System.err.println("Unable to close metastore");
       System.err.println(StringUtils.stringifyException(e));
