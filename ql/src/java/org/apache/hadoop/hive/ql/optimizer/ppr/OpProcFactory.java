915f6135eabbdb0474b5909750939a5fcbcf3bb3
Namit Jain <namit@apache.org>
2012-10-05 04:13:06 +0000

    HIVE-3514 Refactor Partition Pruner so that logic can be reused. (Gang Tim Liu via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1394358 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/ppr/OpProcFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/ppr/OpProcFactory.java
@@ -18,17 +18,13 @@
 
 package org.apache.hadoop.hive.ql.optimizer.ppr;
 
-import java.util.Map;
-import java.util.Stack;
-
 import org.apache.hadoop.hive.ql.exec.FilterOperator;
 import org.apache.hadoop.hive.ql.exec.TableScanOperator;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
-import org.apache.hadoop.hive.ql.lib.Node;
 import org.apache.hadoop.hive.ql.lib.NodeProcessor;
 import org.apache.hadoop.hive.ql.lib.NodeProcessorCtx;
+import org.apache.hadoop.hive.ql.optimizer.PrunerOperatorFactory;
 import org.apache.hadoop.hive.ql.parse.SemanticException;
-import org.apache.hadoop.hive.ql.parse.TypeCheckProcFactory;
 import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
 
 /**
@@ -37,50 +33,27 @@ import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
  * We then pass the filter to the partition pruner to construct a pruner for
  * that table alias and store a mapping from the table scan operator to that
  * pruner. We call that pruner later during plan generation.
+ *
+ *
+ * Refactor:
+ * Move main logic to PrunerOperatorFactory. OpProcFactory extends it to reuse logic.
+ *
+ * Any other pruner can reuse it by creating a class extending from PrunerOperatorFactory.
+ *
+ * Only specific logic is in generatePredicate(..) which is in its own class like OpProcFactory.
  */
-public final class OpProcFactory {
+public final class OpProcFactory extends PrunerOperatorFactory {
 
   /**
    * Determines the partition pruner for the filter. This is called only when
    * the filter follows a table scan operator.
    */
-  public static class FilterPPR implements NodeProcessor {
+  public static class FilterPPR extends FilterPruner {
 
     @Override
-    public Object process(Node nd, Stack<Node> stack, NodeProcessorCtx procCtx,
-        Object... nodeOutputs) throws SemanticException {
+    protected void generatePredicate(NodeProcessorCtx procCtx, FilterOperator fop,
+        TableScanOperator top) throws SemanticException, UDFArgumentException {
       OpWalkerCtx owc = (OpWalkerCtx) procCtx;
-      FilterOperator fop = (FilterOperator) nd;
-      FilterOperator fop2 = null;
-
-      // The stack contains either ... TS, Filter or
-      // ... TS, Filter, Filter with the head of the stack being the rightmost
-      // symbol. So we just pop out the two elements from the top and if the
-      // second one of them is not a table scan then the operator on the top of
-      // the stack is the Table scan operator.
-      Node tmp = stack.pop();
-      Node tmp2 = stack.pop();
-      TableScanOperator top = null;
-      if (tmp2 instanceof TableScanOperator) {
-        top = (TableScanOperator) tmp2;
-      } else {
-        top = (TableScanOperator) stack.peek();
-        fop2 = (FilterOperator) tmp2;
-      }
-      stack.push(tmp2);
-      stack.push(tmp);
-
-      // If fop2 exists (i.e this is not the top level filter and fop2 is not
-      // a sampling filter then we ignore the current filter
-      if (fop2 != null && !fop2.getConf().getIsSamplingPred()) {
-        return null;
-      }
-
-      // ignore the predicate in case it is not a sampling predicate
-      if (fop.getConf().getIsSamplingPred()) {
-        return null;
-      }
-
       // Otherwise this is not a sampling predicate and we need to
       ExprNodeDesc predicate = fop.getConf().getPredicate();
       String alias = top.getConf().getAlias();
@@ -93,48 +66,12 @@ public final class OpProcFactory {
 
       // Add the pruning predicate to the table scan operator
       addPruningPred(owc.getOpToPartPruner(), top, ppr_pred);
-
-      return null;
     }
 
-    private void addPruningPred(Map<TableScanOperator, ExprNodeDesc> opToPPR,
-        TableScanOperator top, ExprNodeDesc new_ppr_pred) throws UDFArgumentException {
-      ExprNodeDesc old_ppr_pred = opToPPR.get(top);
-      ExprNodeDesc ppr_pred = null;
-      if (old_ppr_pred != null) {
-        // or the old_ppr_pred and the new_ppr_pred
-        ppr_pred = TypeCheckProcFactory.DefaultExprProcessor
-            .getFuncExprNodeDesc("OR", old_ppr_pred, new_ppr_pred);
-      } else {
-        ppr_pred = new_ppr_pred;
-      }
-
-      // Put the mapping from table scan operator to ppr_pred
-      opToPPR.put(top, ppr_pred);
-
-      return;
-    }
-  }
-
-  /**
-   * Default processor which just merges its children.
-   */
-  public static class DefaultPPR implements NodeProcessor {
-
-    @Override
-    public Object process(Node nd, Stack<Node> stack, NodeProcessorCtx procCtx,
-        Object... nodeOutputs) throws SemanticException {
-      // Nothing needs to be done.
-      return null;
-    }
   }
 
   public static NodeProcessor getFilterProc() {
     return new FilterPPR();
-  }
-
-  public static NodeProcessor getDefaultProc() {
-    return new DefaultPPR();
   }
 
   private OpProcFactory() {
