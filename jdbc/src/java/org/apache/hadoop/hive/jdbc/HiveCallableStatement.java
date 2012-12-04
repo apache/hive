f3e54b07a91a54753d873340f16bbed7119a6559
Ashutosh Chauhan <hashutosh@apache.org>
2012-12-04 23:17:43 +0000

    HIVE-3384 : HIVE JDBC module won't compile under JDK1.7 as new methods added in JDBC specification (Shengsheng Huang, Chris Drome, Mikhail Bautin via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1417235 13f79535-47bb-0310-9956-ffa450edef68

--- jdbc/src/java/org/apache/hadoop/hive/jdbc/HiveCallableStatement.java
+++ jdbc/src/java/org/apache/hadoop/hive/jdbc/HiveCallableStatement.java
@@ -462,6 +462,17 @@ public class HiveCallableStatement implements java.sql.CallableStatement {
     throw new SQLException("Method not supported");
   }
 
+  public <T> T getObject(int parameterIndex, Class<T> type) throws SQLException {
+    // TODO JDK 1.7
+     throw new SQLException("Method not supported");
+  }
+
+  public <T> T getObject(String parameterName, Class<T> type) throws SQLException {
+    // TODO JDK 1.7
+    throw new SQLException("Method not supported");
+  }
+
+
   /*
    * (non-Javadoc)
    * 
@@ -2030,6 +2041,16 @@ public class HiveCallableStatement implements java.sql.CallableStatement {
     // TODO Auto-generated method stub
     throw new SQLException("Method not supported");
   }
+
+  public void closeOnCompletion() throws SQLException {
+    // JDK 1.7
+    throw new SQLException("Method not supported");
+  }
+
+   public boolean isCloseOnCompletion() throws SQLException {
+     // JDK 1.7
+     throw new SQLException("Method not supported");
+   }
 
   /*
    * (non-Javadoc)
