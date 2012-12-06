b666e9ef48976fa2c772bf7f601bde292159e28f
Namit Jain <namit@apache.org>
2012-12-06 06:16:51 +0000

    HIVE-2723 should throw "Ambiguous column reference key" Exception in particular join condition (Navis via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1417743 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/parse/RowResolver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/RowResolver.java
@@ -29,6 +29,7 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hive.ql.ErrorMsg;
 import org.apache.hadoop.hive.ql.exec.ColumnInfo;
 import org.apache.hadoop.hive.ql.exec.RowSchema;
 
@@ -163,6 +164,17 @@ public class RowResolver implements Serializable{
     }
 
     return ret;
+  }
+
+  /**
+   * check if column name is already exist in RR
+   */
+  public void checkColumn(String tableAlias, String columnAlias) throws SemanticException {
+    ColumnInfo prev = get(null, columnAlias);
+    if (prev != null &&
+        (tableAlias == null || !tableAlias.equalsIgnoreCase(prev.getTabAlias()))) {
+      throw new SemanticException(ErrorMsg.AMBIGUOUS_COLUMN.getMsg(columnAlias));
+    }
   }
 
   public ArrayList<ColumnInfo> getColumnInfos() {
