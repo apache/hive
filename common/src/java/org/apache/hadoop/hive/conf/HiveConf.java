72895462ab817d0fdc25b089bf305e1a8349b335
Kevin Wilfong <kevinwilfong@apache.org>
2012-12-20 04:33:21 +0000

    HIVE-3728. make optimizing multi-group by configurable. (njain via kevinwilfong)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1424292 13f79535-47bb-0310-9956-ffa450edef68

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -392,6 +392,8 @@ public class HiveConf extends Configuration {
     HIVEALIAS("hive.alias", ""),
     HIVEMAPSIDEAGGREGATE("hive.map.aggr", true),
     HIVEGROUPBYSKEW("hive.groupby.skewindata", false),
+    HIVE_OPTIMIZE_MULTI_GROUPBY_COMMON_DISTINCTS("hive.optimize.multigroupby.common.distincts",
+        true),
     HIVEJOINEMITINTERVAL("hive.join.emit.interval", 1000),
     HIVEJOINCACHESIZE("hive.join.cache.size", 25000),
     HIVEMAPJOINBUCKETCACHESIZE("hive.mapjoin.bucket.cache.size", 100),
