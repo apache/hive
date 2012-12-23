96ff9df55ac125ab0a0d67082fd9acade179aa47
Namit Jain <namit@apache.org>
2012-12-23 03:32:19 +0000

    HIVE-3828 insert overwrite fails with stored-as-dir in cluster (Gang Tim Liu via namit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1425398 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
@@ -189,7 +189,8 @@ public class FileSinkOperator extends TerminalOperator<FileSinkDesc> implements
     private void commit(FileSystem fs) throws HiveException {
       for (int idx = 0; idx < outPaths.length; ++idx) {
         try {
-          if (bDynParts && !fs.exists(finalPaths[idx].getParent())) {
+          if ((bDynParts || isSkewedStoredAsSubDirectories)
+              && !fs.exists(finalPaths[idx].getParent())) {
             fs.mkdirs(finalPaths[idx].getParent());
           }
           if (!fs.rename(outPaths[idx], finalPaths[idx])) {
