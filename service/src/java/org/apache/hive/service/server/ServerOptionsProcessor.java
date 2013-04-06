2dcef876f5e6a81e41d107425a9917433df0bfd5
Ashutosh Chauhan <hashutosh@apache.org>
2013-04-06 03:05:18 +0000

    HIVE-4292 : hiveserver2 should support -hiveconf commandline parameter (Thejas Nair via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1465169 13f79535-47bb-0310-9956-ffa450edef68

---
+++ service/src/java/org/apache/hive/service/server/ServerOptionsProcessor.java
@@ -0,0 +1,85 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hive.service.server;
+
+import java.util.Properties;
+
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * ServerOptionsProcessor.
+ * Process arguments given to servers (-hiveconf property=value)
+ * Set properties in System properties
+ */
+public class ServerOptionsProcessor {
+  protected static final Log LOG = LogFactory.getLog(ServerOptionsProcessor.class.getName());
+  private final Options options = new Options();
+  private org.apache.commons.cli.CommandLine commandLine;
+  private final String serverName;
+
+
+  @SuppressWarnings("static-access")
+  public ServerOptionsProcessor(String serverName) {
+    this.serverName = serverName;
+    // -hiveconf x=y
+    options.addOption(OptionBuilder
+        .withValueSeparator()
+        .hasArgs(2)
+        .withArgName("property=value")
+        .withLongOpt("hiveconf")
+        .withDescription("Use value for given property")
+        .create());
+
+    options.addOption(new Option("H", "help", false, "Print help information"));
+
+  }
+
+  public boolean process(String[] argv) {
+    try {
+      commandLine = new GnuParser().parse(options, argv);
+      if (commandLine.hasOption('H')) {
+        printUsage();
+        return false;
+      }
+      //get hiveconf param values and set the System property values
+      Properties confProps = commandLine.getOptionProperties("hiveconf");
+      for (String propKey : confProps.stringPropertyNames()) {
+        LOG.debug("Setting " + propKey + "=" + confProps.getProperty(propKey) + ";");
+        System.setProperty(propKey, confProps.getProperty(propKey));
+      }
+    } catch (ParseException e) {
+      System.err.println(e.getMessage());
+      printUsage();
+      return false;
+    }
+    return true;
+  }
+
+  private void printUsage() {
+    new HelpFormatter().printHelp(serverName, options);
+  }
+
+}
