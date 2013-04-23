d7d9fdd17d8b81f11f417cb911083b8d103599bf
Ashutosh Chauhan <hashutosh@apache.org>
2013-04-23 21:41:05 +0000

    HIVE-4379 : Implement Vectorized Column-Column expressions (Jitendra Nath Pandey via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/vectorization@1471159 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/gen/FilterDoubleColEqualDoubleColumn.java
@@ -0,0 +1,222 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.apache.hadoop.hive.ql.exec.vector.expressions.gen;
+
+import org.apache.hadoop.hive.ql.exec.vector.expressions.VectorExpression;
+import org.apache.hadoop.hive.ql.exec.vector.*;
+import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
+
+public class FilterDoubleColEqualDoubleColumn extends VectorExpression {
+  int colNum1;
+  int colNum2;
+
+  public FilterDoubleColEqualDoubleColumn(int colNum1, int colNum2) { 
+    this.colNum1 = colNum1;
+    this.colNum2 = colNum2;
+  }
+
+  @Override
+  public void evaluate(VectorizedRowBatch batch) {
+    DoubleColumnVector inputColVector1 = (DoubleColumnVector) batch.cols[colNum1];
+    DoubleColumnVector inputColVector2 = (DoubleColumnVector) batch.cols[colNum2];
+    int[] sel = batch.selected;
+    boolean[] nullPos1 = inputColVector1.isNull;
+    boolean[] nullPos2 = inputColVector2.isNull;
+    int n = batch.size;
+    double[] vector1 = inputColVector1.vector;
+    double[] vector2 = inputColVector2.vector;
+    
+    // return immediately if batch is empty
+    if (n == 0) {
+      return;
+    }
+    
+    if (inputColVector1.noNulls && inputColVector2.noNulls) {
+      if (inputColVector1.isRepeating && inputColVector2.isRepeating) {
+        //All must be selected otherwise size would be zero
+        //Repeating property will not change.
+        if (!(vector1[0] == vector2[0])) {
+          batch.size = 0;
+        }
+      } else if (inputColVector1.isRepeating) {
+        if (batch.selectedInUse) {
+          int newSize = 0;
+          for(int j=0; j != n; j++) {
+            int i = sel[j];
+            if (vector1[0] == vector2[i]) {
+              sel[newSize++] = i;
+            }
+          }
+          batch.size = newSize;
+        } else {
+          int newSize = 0;
+          for(int i = 0; i != n; i++) {
+            if (vector1[0] == vector2[i]) {
+              sel[newSize++] = i;
+            }
+          }
+          if (newSize < batch.size) {
+            batch.size = newSize;
+            batch.selectedInUse = true;
+          }
+        }
+      } else if (inputColVector2.isRepeating) {
+        if (batch.selectedInUse) {
+          int newSize = 0;
+          for(int j=0; j != n; j++) {
+            int i = sel[j];
+            if (vector1[i] == vector2[0]) {
+              sel[newSize++] = i;
+            }
+          }
+          batch.size = newSize;
+        } else {
+          int newSize = 0;
+          for(int i = 0; i != n; i++) {
+            if (vector1[i] == vector2[0]) {
+              sel[newSize++] = i;
+            }
+          }
+          if (newSize < batch.size) {
+            batch.size = newSize;
+            batch.selectedInUse = true;
+          }
+        }
+      } else if (batch.selectedInUse) {
+        int newSize = 0;
+        for(int j=0; j != n; j++) {
+          int i = sel[j];
+          if (vector1[i] == vector2[i]) {
+            sel[newSize++] = i;
+          }
+        }
+        batch.size = newSize;
+      } else {
+        int newSize = 0;
+        for(int i = 0; i != n; i++) {
+          if (vector1[i] ==  vector2[i]) {
+            sel[newSize++] = i;
+          }
+        }
+        if (newSize < batch.size) {
+          batch.size = newSize;
+          batch.selectedInUse = true;
+        }
+      }
+    } else if (inputColVector1.isRepeating && inputColVector2.isRepeating) {
+      if (nullPos1[0] || nullPos2[0]) {
+        batch.size = 0; 
+      } 
+    } else if (inputColVector1.isRepeating) {
+      if (nullPos1[0]) {
+        batch.size = 0;
+      } else {
+        if (batch.selectedInUse) {
+          int newSize = 0;
+          for(int j=0; j != n; j++) {
+            int i = sel[j];
+            if (!nullPos2[i]) {
+              if (vector1[0] == vector2[i]) {
+                sel[newSize++] = i;
+              }
+            }
+          }
+          batch.size = newSize;
+        } else {
+          int newSize = 0;
+          for(int i = 0; i != n; i++) {
+            if (!nullPos2[i]) {
+              if (vector1[0] == vector2[i]) {
+                sel[newSize++] = i;
+              }
+            }
+          }
+          if (newSize < batch.size) {
+            batch.size = newSize;
+            batch.selectedInUse = true;
+          }
+        }
+      }
+    } else if (inputColVector2.isRepeating) {
+      if (nullPos2[0]) {
+        batch.size = 0;
+      } else {
+        if (batch.selectedInUse) {
+          int newSize = 0;
+          for(int j=0; j != n; j++) {
+            int i = sel[j];
+            if (!nullPos1[i]) {
+              if (vector1[i] == vector2[0]) {
+                sel[newSize++] = i;
+              }
+            }
+          }
+          batch.size = newSize;
+        } else {
+          int newSize = 0;
+          for(int i = 0; i != n; i++) {
+            if (!nullPos1[i]) {
+              if (vector1[i] == vector2[0]) {
+                sel[newSize++] = i;
+              }
+            }
+          }
+          if (newSize < batch.size) {
+            batch.size = newSize;
+            batch.selectedInUse = true;
+          }
+        }
+      }
+    } else if (batch.selectedInUse) {
+      int newSize = 0;
+      for(int j=0; j != n; j++) {
+        int i = sel[j];
+        if (!nullPos1[i] && !nullPos2[i]) {
+          if (vector1[i] == vector2[i]) {
+            sel[newSize++] = i;
+          }
+        }
+      }
+      batch.size = newSize;
+    } else {
+      int newSize = 0;
+      for(int i = 0; i != n; i++) {
+        if (!nullPos1[i] && !nullPos2[i]) {
+          if (vector1[i] == vector2[i]) {
+            sel[newSize++] = i;
+          }
+        }
+      }
+      if (newSize < batch.size) {
+        batch.size = newSize;
+        batch.selectedInUse = true;
+      }
+    }
+  }
+
+  @Override
+  public String getOutputType() {
+    return "boolean";
+  }
+
+  @Override
+  public int getOutputColumn() {
+    return -1;
+  }
+}
--- ql/src/test/org/apache/hadoop/hive/ql/exec/vector/expressions/TestVectorFilterExpressions.java
+++ ql/src/test/org/apache/hadoop/hive/ql/exec/vector/expressions/TestVectorFilterExpressions.java
@@ -1,9 +1,29 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.hive.ql.exec.vector.expressions;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.hadoop.hive.ql.exec.vector.expressions.gen.FilterLongColEqualLongScalar;
+import org.apache.hadoop.hive.ql.exec.vector.expressions.gen.FilterLongColGreaterLongColumn;
 import org.junit.Test;
 
 public class TestVectorFilterExpressions {
@@ -11,11 +31,38 @@ public class TestVectorFilterExpressions {
   @Test
   public void testFilterLongColEqualLongScalar() {
     VectorizedRowBatch vrg =
-        VectorizedRowGroupGenUtil.getVectorizedRowBatch(1024, 2, 23);
-    FilterLongColEqualLongScalar expr = new FilterLongColEqualLongScalar(1, 46);
+        VectorizedRowGroupGenUtil.getVectorizedRowBatch(1024, 1, 23);
+    FilterLongColEqualLongScalar expr = new FilterLongColEqualLongScalar(0, 46);
     expr.evaluate(vrg);
     assertEquals(1, vrg.size);
-    assertEquals(2, vrg.selected[0]);
+    assertEquals(1, vrg.selected[0]);
   }
 
+  @Test
+  public void testFilterLongColEqualLongColumn() {
+    int seed = 17;
+    VectorizedRowBatch vrg = VectorizedRowGroupGenUtil.getVectorizedRowBatch(
+        VectorizedRowBatch.DEFAULT_SIZE,
+        2, seed);
+    LongColumnVector lcv0 = (LongColumnVector) vrg.cols[0];
+    LongColumnVector lcv1 = (LongColumnVector) vrg.cols[1];
+    FilterLongColGreaterLongColumn expr = new FilterLongColGreaterLongColumn(0, 1);
+
+    //Basic case
+    lcv0.vector[1] = 23;
+    lcv1.vector[1] = 19;
+    lcv0.vector[5] = 23;
+    lcv1.vector[5] = 19;
+    expr.evaluate(vrg);
+    assertEquals(2, vrg.size);
+    assertEquals(1, vrg.selected[0]);
+    assertEquals(5, vrg.selected[1]);
+
+    //handle null
+    lcv0.noNulls = false;
+    lcv0.isNull[1] = true;
+    expr.evaluate(vrg);
+    assertEquals(1, vrg.size);
+    assertEquals(5, vrg.selected[0]);
+  }
 }
