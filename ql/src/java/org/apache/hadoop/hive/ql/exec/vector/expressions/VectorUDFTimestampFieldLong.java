27415140b51a37d0ec19c1eca8f4d899b4c7364c
Ashutosh Chauhan <hashutosh@apache.org>
2013-06-04 13:40:38 +0000

    HIVE-4608 : Vectorized UDFs for Timestamp in nanoseconds (Gopal V via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/vectorization@1489443 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/VectorUDFTimestampFieldLong.java
@@ -0,0 +1,136 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.ql.exec.vector.expressions;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
+
+
+
+public abstract class VectorUDFTimestampFieldLong extends VectorExpression {
+
+  protected final int colNum;
+  protected final int outputColumn;
+  protected final int field;
+  protected final Calendar calendar = Calendar.getInstance();
+  protected final Timestamp ts = new Timestamp(0);
+
+  public VectorUDFTimestampFieldLong(int field, int colNum, int outputColumn) {
+    super();
+    this.colNum = colNum;
+    this.outputColumn = outputColumn;
+    this.field = field;
+  }
+
+  protected final Timestamp getTimestamp(long nanos) {
+    /*
+     * new Timestamp() stores the millisecond precision values in the nanos field.
+     * If you wanted to store 200ms it will result in nanos being set to 200*1000*1000.
+     * When you call setNanos(0), because there are no sub-ms times, it will set it to 0,
+     * ending up with a Timestamp which refers to 0ms by accident.
+     * CAVEAT: never use a sub-second value in new Timestamp() args, just use setNanos to set it.
+     */
+    long ms = (nanos / (1000 * 1000 * 1000)) * 1000;
+    /* the milliseconds should be kept in nanos */
+    long ns = nanos % (1000*1000*1000);
+    if (ns < 0) {
+      /*
+       * Due to the way java.sql.Timestamp stores sub-second values, it throws an exception
+       * if nano seconds are negative. The timestamp implementation handles this by using
+       * negative milliseconds and adjusting the nano seconds up by the same to be positive.
+       * Read Timestamp.java:setTime() implementation for this code.
+       */
+      ms -= 1000;
+      ns += 1000*1000*1000;
+    }
+    ts.setTime(ms);
+    ts.setNanos((int) ns);
+    return ts;
+  }
+
+  protected long getField(long time) {
+    calendar.setTime(getTimestamp(time));
+    return calendar.get(field);
+  }
+
+  @Override
+  public void evaluate(VectorizedRowBatch batch) {
+    LongColumnVector outV = (LongColumnVector) batch.cols[outputColumn];
+    LongColumnVector inputCol = (LongColumnVector)batch.cols[this.colNum];
+    /* every line below this is identical for evaluateLong & evaluateString */
+    final int n = inputCol.isRepeating ? 1 : batch.size;
+    int[] sel = batch.selected;
+
+    if(batch.size == 0) {
+      /* n != batch.size when isRepeating */
+      return;
+    }
+
+    /* true for all algebraic UDFs with no state */
+    outV.isRepeating = inputCol.isRepeating;
+
+    if (inputCol.noNulls) {
+      outV.noNulls = true;
+      if (batch.selectedInUse) {
+        for(int j=0; j < n ; j++) {
+          int i = sel[j];
+          outV.vector[i] = getField(inputCol.vector[i]);
+        }
+      } else {
+        for(int i = 0; i < n; i++) {
+          outV.vector[i] = getField(inputCol.vector[i]);
+        }
+      }
+    } else {
+      // Handle case with nulls. Don't do function if the value is null, to save time,
+      // because calling the function can be expensive.
+      outV.noNulls = false;
+      if (batch.selectedInUse) {
+        for(int j=0; j < n; j++) {
+          int i = sel[j];
+          outV.isNull[i] = inputCol.isNull[i];
+          if (!inputCol.isNull[i]) {
+            outV.vector[i] = getField(inputCol.vector[i]);
+          }
+        }
+      } else {
+        for(int i = 0; i < n; i++) {
+          outV.isNull[i] = inputCol.isNull[i];
+          if (!inputCol.isNull[i]) {
+            outV.vector[i] = getField(inputCol.vector[i]);
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  public int getOutputColumn() {
+    return this.outputColumn;
+  }
+
+  @Override
+  public String getOutputType() {
+    return "long";
+  }
+
+}
---
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/VectorUDFUnixTimeStampLong.java
@@ -0,0 +1,41 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.ql.exec.vector.expressions;
+
+
+
+public final class VectorUDFUnixTimeStampLong extends VectorUDFTimestampFieldLong {
+
+  @Override
+  protected long getField(long time) {
+    long ms = time / (1000*1000);
+    long remainder = time % (1000*1000);
+    /* negative timestamps need to be adjusted */
+    if(remainder < 0) {
+      ms -= 1;
+    }
+    return ms/1000;
+  }
+
+  VectorUDFUnixTimeStampLong(int colNum, int outputColumn) {
+    /* not a real field */
+    super(-1, colNum,outputColumn);
+  }
+
+}
