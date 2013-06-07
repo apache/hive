271b0a2fb688246768d52a2add1bd0d15694fd6e
Ashutosh Chauhan <hashutosh@apache.org>
2013-06-07 01:36:58 +0000

    HIVE-4665 : error at VectorExecMapper.close in group-by-agg query over ORC, vectorized (Jitendra Nath Pandey via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/vectorization@1490491 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/VectorExpressionWriterFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/VectorExpressionWriterFactory.java
@@ -27,34 +27,36 @@ import org.apache.hadoop.hive.ql.exec.vector.ColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.DoubleColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.TimestampUtils;
-import org.apache.hadoop.hive.ql.exec.vector.expressions.VectorExpressionWriterFactory.Closure;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.plan.AggregationDesc;
 import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
+import org.apache.hadoop.hive.serde2.io.ByteWritable;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
 import org.apache.hadoop.hive.serde2.io.ShortWritable;
 import org.apache.hadoop.hive.serde2.io.TimestampWritable;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
-import org.apache.hadoop.io.ByteWritable;
+import org.apache.hadoop.hive.serde2.objectinspector.StructField;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
 import org.apache.hadoop.io.BooleanWritable;
-import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
 /**
  * VectorExpressionWritableFactory helper class for generating VectorExpressionWritable objects.
  */
 public final class VectorExpressionWriterFactory {
-  
+
   /**
    * VectorExpressionWriter base implementation, to be specialized for Long/Double/Bytes columns
    */
   private static abstract class VectorExpressionWriterBase implements VectorExpressionWriter {
-    
+
     protected ObjectInspector objectInspector;
-    
+
     /**
      * The object inspector associated with this expression. This is created from the expression
      * NodeDesc (compile metadata) not from the VectorColumn info and thus preserves the type info
@@ -63,12 +65,12 @@ public final class VectorExpressionWriterFactory {
     public ObjectInspector getObjectInspector() {
       return objectInspector;
     }
-    
+
     public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
       this.objectInspector = nodeDesc.getWritableObjectInspector();
       return this;
     }
-    
+
     /**
      * The base implementation must be overridden by the Long specialization
      */
@@ -93,10 +95,10 @@ public final class VectorExpressionWriterFactory {
       throw new HiveException("Internal error: should not reach here");
     }
   }
-  
+
   /**
    * Specialized writer for LongVectorColumn expressions. Will throw cast exception
-   * if the wrong vector column is used. 
+   * if the wrong vector column is used.
    */
   private static abstract class VectorExpressionWriterLong
     extends VectorExpressionWriterBase {
@@ -122,10 +124,10 @@ public final class VectorExpressionWriterFactory {
           row, lcv.noNulls, lcv.isRepeating, lcv.isNull[row], lcv.isNull[0]));
     }
    }
-  
+
   /**
    * Specialized writer for DoubleColumnVector. Will throw cast exception
-   * if the wrong vector column is used. 
+   * if the wrong vector column is used.
    */
   private static abstract class VectorExpressionWriterDouble extends VectorExpressionWriterBase {
     @Override
@@ -150,10 +152,10 @@ public final class VectorExpressionWriterFactory {
           row, dcv.noNulls, dcv.isRepeating, dcv.isNull[row], dcv.isNull[0]));
     }
    }
-  
+
   /**
    * Specialized writer for BytesColumnVector. Will throw cast exception
-   * if the wrong vector column is used. 
+   * if the wrong vector column is used.
    */
   private static abstract class VectorExpressionWriterBytes extends VectorExpressionWriterBase {
     @Override
@@ -182,15 +184,15 @@ public final class VectorExpressionWriterFactory {
   /**
    * Compiles the appropriate vector expression writer based on an expression info (ExprNodeDesc)
    */
-  public static VectorExpressionWriter genVectorExpressionWritable(ExprNodeDesc nodeDesc) 
+  public static VectorExpressionWriter genVectorExpressionWritable(ExprNodeDesc nodeDesc)
     throws HiveException {
     String nodeType = nodeDesc.getTypeString();
     if (nodeType.equalsIgnoreCase("tinyint")) {
       return new VectorExpressionWriterLong()
       {
         private ByteWritable writable;
-        
-        @Override 
+
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new ByteWritable();
@@ -207,7 +209,7 @@ public final class VectorExpressionWriterFactory {
       return new VectorExpressionWriterLong()
       {
         private ShortWritable writable;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new ShortWritable();
@@ -218,13 +220,13 @@ public final class VectorExpressionWriterFactory {
         public Object writeValue(long value) {
           writable.set((short) value);
           return writable;
-        }      
+        }
       }.init(nodeDesc);
     } else if (nodeType.equalsIgnoreCase("int")) {
       return new VectorExpressionWriterLong()
       {
         private IntWritable writable;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new IntWritable();
@@ -235,13 +237,13 @@ public final class VectorExpressionWriterFactory {
         public Object writeValue(long value) {
           writable.set((int) value);
           return writable;
-        }      
-      }.init(nodeDesc);      
+        }
+      }.init(nodeDesc);
     } else if (nodeType.equalsIgnoreCase("bigint")) {
       return new VectorExpressionWriterLong()
       {
         private LongWritable writable;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new LongWritable();
@@ -258,7 +260,7 @@ public final class VectorExpressionWriterFactory {
       return new VectorExpressionWriterLong()
       {
         private BooleanWritable writable;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new BooleanWritable();
@@ -276,7 +278,7 @@ public final class VectorExpressionWriterFactory {
       {
         private TimestampWritable writable;
         private Timestamp timestamp;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new TimestampWritable();
@@ -294,14 +296,15 @@ public final class VectorExpressionWriterFactory {
     } else if (nodeType.equalsIgnoreCase("string")) {
       return new VectorExpressionWriterBytes()
       {
-        private BytesWritable writable;
+        private Text writable;
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
-          writable = new BytesWritable();
+          writable = new Text();
           return this;
         }
-        
-        @Override 
+
+        @Override
         public Object writeValue(byte[] value, int start, int length) throws HiveException {
           writable.set(value, start, length);
           return writable;
@@ -311,7 +314,7 @@ public final class VectorExpressionWriterFactory {
       return new VectorExpressionWriterDouble()
       {
         private FloatWritable writable;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new FloatWritable();
@@ -328,7 +331,7 @@ public final class VectorExpressionWriterFactory {
       return new VectorExpressionWriterDouble()
       {
         private DoubleWritable writable;
-        @Override 
+        @Override
         public VectorExpressionWriter init(ExprNodeDesc nodeDesc) throws HiveException {
           super.init(nodeDesc);
           writable = new DoubleWritable();
@@ -342,16 +345,16 @@ public final class VectorExpressionWriterFactory {
         }
       }.init(nodeDesc);
     }
-    
+
     throw new HiveException(String.format(
-        "Unimplemented genVectorExpressionWritable type: %s for expression: %s", 
+        "Unimplemented genVectorExpressionWritable type: %s for expression: %s",
         nodeType, nodeDesc));
   }
-  
+
   /**
    * Helper function to create an array of writers from a list of expression descriptors.
    */
-  public static VectorExpressionWriter[] getExpressionWriters(List<ExprNodeDesc> nodesDesc) 
+  public static VectorExpressionWriter[] getExpressionWriters(List<ExprNodeDesc> nodesDesc)
       throws HiveException {
     VectorExpressionWriter[] writers = new VectorExpressionWriter[nodesDesc.size()];
     for(int i=0; i<writers.length; ++i) {
@@ -360,7 +363,7 @@ public final class VectorExpressionWriterFactory {
     }
     return writers;
   }
-  
+
   /**
    * A poor man Java closure. Works around the problem of having to return multiple objects
    * from one function call.
@@ -368,15 +371,15 @@ public final class VectorExpressionWriterFactory {
   public static interface Closure {
     void assign(VectorExpressionWriter[] writers, ObjectInspector objectInspector);
   }
-  
+
   /**
-   * Creates the value writers for a column vector expression list. 
+   * Creates the value writers for a column vector expression list.
    * Creates an appropriate output object inspector.
    */
   public static void processVectorExpressions(
       List<ExprNodeDesc> nodesDesc,
       List<String> outputColumnNames,
-      Closure closure) 
+      Closure closure)
       throws HiveException {
     VectorExpressionWriter[] writers = getExpressionWriters(nodesDesc);
     List<ObjectInspector> oids = new ArrayList<ObjectInspector>(writers.length);
@@ -386,5 +389,38 @@ public final class VectorExpressionWriterFactory {
     ObjectInspector objectInspector = ObjectInspectorFactory.
         getStandardStructObjectInspector(outputColumnNames,oids);
     closure.assign(writers, objectInspector);
+  }
+
+
+  /**
+   * Returns {@link VectorExpressionWriter} objects for the fields in the given
+   * object inspector.
+   *
+   * @param objInspector
+   * @return
+   * @throws HiveException
+   */
+  public static VectorExpressionWriter[] getExpressionWriters(StructObjectInspector objInspector)
+      throws HiveException {
+    List<ExprNodeDesc> outputNodeDescs = new ArrayList<ExprNodeDesc>();
+    for (StructField fieldRef : (objInspector)
+        .getAllStructFieldRefs()) {
+      String typeName = fieldRef.getFieldObjectInspector().getTypeName();
+      TypeInfo ti = TypeInfoFactory.getPrimitiveTypeInfo(typeName);
+      outputNodeDescs.add(new ExprNodeDesc(ti) {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public ExprNodeDesc clone() { /* Not needed */
+          return null;
+        }
+
+        @Override
+        public boolean isSame(Object o) { /* Not needed */
+          return false;
+        }
+      });
+    }
+    return VectorExpressionWriterFactory.getExpressionWriters(outputNodeDescs);
   }
 }
