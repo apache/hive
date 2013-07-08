bb2b243f7bac037e8942d8f5496c66c804ddafce
Ashutosh Chauhan <hashutosh@apache.org>
2013-07-08 14:52:50 +0000

    HIVE-4802 : Fix url check for missing / or /<db> after hostname in jdb uri (Thejas Nair via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1500781 13f79535-47bb-0310-9956-ffa450edef68

--- jdbc/src/test/org/apache/hive/jdbc/TestJdbcDriver2.java
+++ jdbc/src/test/org/apache/hive/jdbc/TestJdbcDriver2.java
@@ -196,17 +196,22 @@ public class TestJdbcDriver2 extends TestCase {
   }
 
   public void testBadURL() throws Exception {
-    Class.forName(driverName);
-     try{
-
-       DriverManager.getConnection("jdbc:hive2://localhost:10000;principal=test", "", "");
-       fail("should have thrown IllegalArgumentException but did not ");
-
-     }catch(IllegalArgumentException i){
-       assertEquals("Bad URL format and it should be in the format of jdbc:hive2://<hostame>:<port>/<DB_name>", i.getMessage());
-     }
+    checkBadUrl("jdbc:hive2://localhost:10000;principal=test");
+    checkBadUrl("jdbc:hive2://localhost:10000;" +
+    		"principal=hive/HiveServer2Host@YOUR-REALM.COM");
+    checkBadUrl("jdbc:hive2://localhost:10000test");
   }
 
+
+  private void checkBadUrl(String url) throws SQLException {
+    try{
+      DriverManager.getConnection(url, "", "");
+      fail("should have thrown IllegalArgumentException but did not ");
+    }catch(IllegalArgumentException i){
+      assertTrue(i.getMessage().contains("Bad URL format. Hostname not found "
+          + " in authority part of the url"));
+    }
+  }
 
   public void testDataTypes2() throws Exception {
     Statement stmt = con.createStatement();
