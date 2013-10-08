4fdf88cf7ac627b983d9eae510d488851fc365cd
Thejas Madhavan Nair <thejas@apache.org>
2013-10-08 02:25:34 +0000

    HIVE-5452 : HCatalog e2e test Pig_HBase_1 and Pig_HBase_2 are failing with ClassCastException (Deepesh Khandelwal via Thejas Nair)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1530135 13f79535-47bb-0310-9956-ffa450edef68

--- hcatalog/src/test/e2e/hcatalog/drivers/TestDriverPig.pm
+++ hcatalog/src/test/e2e/hcatalog/drivers/TestDriverPig.pm
@@ -184,7 +184,8 @@ sub runTest
 	               $testCmd->{'group'} .  "_" .  $testCmd->{'num'} . ".$i.out";
                    $tableName = $results[$i];
 	           $modifiedTestCmd{'num'} = $testCmd->{'num'} . "_" . $i . "_benchmark";
-                   $modifiedTestCmd{'pig'} = "a = load '$tableName' using org.apache.hive.hcatalog.pig.HCatLoader(); store a into ':OUTPATH:';";
+                   $tableLoader = (defined $testCmd->{'result_table_loader'} ? $testCmd->{'result_table_loader'} : "org.apache.hive.hcatalog.pig.HCatLoader()");
+                   $modifiedTestCmd{'pig'} = "a = load '$tableName' using $tableLoader; store a into ':OUTPATH:';";
                    my $r = $self->runPig(\%modifiedTestCmd, $log, 1, 1);
 	           $outputs[$i] = $r->{'output'};
                } else {
