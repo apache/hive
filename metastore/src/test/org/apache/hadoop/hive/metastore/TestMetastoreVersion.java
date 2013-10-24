9a82df7385b9f6a924c062578e56689e1214bbdc
Brock Noland <brock@apache.org>
2013-10-24 15:07:06 +0000

    HIVE-5625 - Fix issue with metastore version restriction test. (Vikram Dixit K via Brock Noland)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1535402 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/src/test/org/apache/hadoop/hive/metastore/TestMetastoreVersion.java
+++ metastore/src/test/org/apache/hadoop/hive/metastore/TestMetastoreVersion.java
@@ -89,6 +89,7 @@ public class TestMetastoreVersion extends TestCase {
     // session creation should fail since the schema didn't get created
     try {
       SessionState.start(new CliSessionState(hiveConf));
+      fail("Expected exception");
     } catch (RuntimeException re) {
       assertTrue(re.getCause().getCause() instanceof MetaException);
     }
