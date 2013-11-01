d07afad27d4a73f63df795f7a40108b1352eca51
Gunther Hagleitner <gunther@apache.org>
2013-11-01 00:38:11 +0000

    HIVE-5688: TestCliDriver compilation fails on tez branch. (Vikram Dixit K via Gunther Hagleitner)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/tez@1537781 13f79535-47bb-0310-9956-ffa450edef68

--- hbase-handler/src/test/org/apache/hadoop/hive/hbase/HBaseQTestUtil.java
+++ hbase-handler/src/test/org/apache/hadoop/hive/hbase/HBaseQTestUtil.java
@@ -18,13 +18,14 @@
 package org.apache.hadoop.hive.hbase;
 
 import org.apache.hadoop.hive.ql.QTestUtil;
+import org.apache.hadoop.hive.ql.QTestUtil.MiniClusterType;
 
 /**
  * HBaseQTestUtil initializes HBase-specific test fixtures.
  */
 public class HBaseQTestUtil extends QTestUtil {
   public HBaseQTestUtil(
-    String outDir, String logDir, String miniMr, HBaseTestSetup setup)
+    String outDir, String logDir, MiniClusterType miniMr, HBaseTestSetup setup)
     throws Exception {
 
     super(outDir, logDir, miniMr, null);
