51eed494e542f4f18451da659a71d9ba968e893e
Brock Noland <brock@apache.org>
2013-11-27 18:58:25 +0000

    HIVE-5704 - A couple of generic UDFs are not in the right folder/package (Xuefu Zhang via Brock Noland)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1546161 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFEncode.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFEncode.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.hadoop.hive.ql.udf;
+package org.apache.hadoop.hive.ql.udf.generic;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
