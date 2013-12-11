190d504cf3f0f634d371cd153c9b4becbaae65c3
Ashutosh Chauhan <hashutosh@apache.org>
2013-12-11 21:32:04 +0000

    HIVE-5979. Failure in cast to timestamps. (Jitendra Pandey)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1550270 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/TimestampUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/TimestampUtils.java
@@ -22,9 +22,27 @@ import java.sql.Timestamp;
 
 public final class TimestampUtils {
 
+  /**
+   * Store the given timestamp in nanoseconds into the timestamp object.
+   * @param timeInNanoSec Given timestamp in nanoseconds
+   * @param t             The timestamp object
+   */
   public static void assignTimeInNanoSec(long timeInNanoSec, Timestamp t) {
-    t.setTime((timeInNanoSec)/1000000);
-    t.setNanos((int)((t.getNanos()) + (timeInNanoSec % 1000000)));
+    /*
+     * java.sql.Timestamp consists of a long variable to store milliseconds and an integer variable for nanoseconds.
+     * The long variable is used to store only the full seconds converted to millis. For example for 1234 milliseconds,
+     * 1000 is stored in the long variable, and 234000000 (234 converted to nanoseconds) is stored as nanoseconds.
+     * The negative timestamps are also supported, but nanoseconds must be positive therefore millisecond part is
+     * reduced by one second.
+     */
+    long integralSecInMillis = (timeInNanoSec / 1000000000) * 1000; // Full seconds converted to millis.
+    long nanos = timeInNanoSec % 1000000000; // The nanoseconds.
+    if (nanos < 0) {
+      nanos = 1000000000 + nanos; // The positive nano-part that will be added to milliseconds.
+      integralSecInMillis = ((timeInNanoSec / 1000000000) - 1) * 1000; // Reduce by one second.
+    }
+    t.setTime(integralSecInMillis);
+    t.setNanos((int) nanos);
   }
 
   public static long getTimeNanoSec(Timestamp t) {
