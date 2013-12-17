3727df7cd1acd063630681af84457f05ec7708ac
Brock Noland <brock@apache.org>
2013-12-17 15:19:31 +0000

    HIVE-5812 - HiveServer2 SSL connection transport binds to loopback address by default (Prasad Mujumdar via Brock Noland)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1551568 13f79535-47bb-0310-9956-ffa450edef68

--- jdbc/src/java/org/apache/hive/jdbc/HiveConnection.java
+++ jdbc/src/java/org/apache/hive/jdbc/HiveConnection.java
@@ -42,6 +42,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 import javax.security.sasl.Sasl;
 import javax.security.sasl.SaslException;
@@ -100,7 +101,7 @@ public class HiveConnection implements java.sql.Connection {
   private int loginTimeout = 0;
 
   public HiveConnection(String uri, Properties info) throws SQLException {
-    loginTimeout = DriverManager.getLoginTimeout();
+    setupLoginTimeout();
     jdbcURI = uri;
     // parse the connection uri
     Utils.JdbcConnectionParams connParams = Utils.parseURL(jdbcURI);
@@ -324,6 +325,16 @@ public class HiveConnection implements java.sql.Connection {
       varValue = varDefault;
     }
     return varValue;
+  }
+
+  // copy loginTimeout from driver manager. Thrift timeout needs to be in millis
+  private void setupLoginTimeout() {
+    long timeOut = TimeUnit.SECONDS.toMillis(DriverManager.getLoginTimeout());
+    if (timeOut > Integer.MAX_VALUE) {
+      loginTimeout = Integer.MAX_VALUE;
+    } else {
+      loginTimeout = (int) timeOut;
+    }
   }
 
   public void abort(Executor executor) throws SQLException {
