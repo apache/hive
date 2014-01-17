f8788a0f570adea9395e35d1b7e0e451914bd23b
Thejas Madhavan Nair <thejas@apache.org>
2014-01-17 21:24:11 +0000

    HIVE-5928 : Add a hive authorization plugin api that does not assume privileges needed - 2 (Thejas Nair reviewed by Brock Noland)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1559255 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/HivePrivilegeObject.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.ql.security.authorization.plugin;
+
+import org.apache.hadoop.hive.common.classification.InterfaceAudience.Public;
+import org.apache.hadoop.hive.common.classification.InterfaceStability.Unstable;
+
+/**
+ * Represents the object on which privilege is being granted/revoked
+ */
+@Public
+@Unstable
+public class HivePrivilegeObject {
+
+  public enum HivePrivilegeObjectType { DATABASE, TABLE, VIEW, PARTITION, URI};
+  private final HivePrivilegeObjectType type;
+  private final String dbname;
+  private final String tableviewname;
+
+  public HivePrivilegeObject(HivePrivilegeObjectType type, String dbname, String tableviewname){
+    this.type = type;
+    this.dbname = dbname;
+    this.tableviewname = tableviewname;
+  }
+
+  public HivePrivilegeObjectType getType() {
+    return type;
+  }
+
+  public String getDbname() {
+    return dbname;
+  }
+
+  public String getTableviewname() {
+    return tableviewname;
+  }
+}
