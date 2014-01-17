aa2ea20781db21ad333dc479cb73c7404180d322
Xuefu Zhang <xuefu@apache.org>
2014-01-17 18:07:38 +0000

    HIVE-6180: Hive JDBC Driver connection fail when no default database passed in the connection string (Prasad via Xuefu)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1559206 13f79535-47bb-0310-9956-ffa450edef68

--- jdbc/src/java/org/apache/hive/jdbc/Utils.java
+++ jdbc/src/java/org/apache/hive/jdbc/Utils.java
@@ -183,17 +183,16 @@ public class Utils {
 
     // dbname and session settings
     String sessVars = jdbcURI.getPath();
-    if ((sessVars == null) || sessVars.isEmpty()) {
-      connParams.setDbName(DEFAULT_DATABASE);
-    } else {
+    if ((sessVars != null) && !sessVars.isEmpty()) {
+      String dbName = "";
       // removing leading '/' returned by getPath()
       sessVars = sessVars.substring(1);
       if (!sessVars.contains(";")) {
         // only dbname is provided
-        connParams.setDbName(sessVars);
+        dbName = sessVars;
       } else {
         // we have dbname followed by session parameters
-        connParams.setDbName(sessVars.substring(0, sessVars.indexOf(';')));
+        dbName = sessVars.substring(0, sessVars.indexOf(';'));
         sessVars = sessVars.substring(sessVars.indexOf(';')+1);
         if (sessVars != null) {
           Matcher sessMatcher = pattern.matcher(sessVars);
@@ -201,6 +200,9 @@ public class Utils {
             connParams.getSessionVars().put(sessMatcher.group(1), sessMatcher.group(2));
           }
         }
+      }
+      if (!dbName.isEmpty()) {
+        connParams.setDbName(dbName);
       }
     }
 
