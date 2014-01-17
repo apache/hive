39663d17f7c4fc8acb78dae61d621dba1dc1069d
Thejas Nair <thejas@apache.org>
2014-01-17 21:24:11 +0000

    HIVE-5928 : Add a hive authorization plugin api that does not assume privileges needed - 2 (Thejas Nair reviewed by Brock Noland)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1559255 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/HiveOperationType.java
@@ -0,0 +1,110 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.ql.security.authorization.plugin;
+
+import org.apache.hadoop.hive.common.classification.InterfaceAudience.Public;
+
+/**
+ * List of hive operations types.
+ */
+@Public
+public enum HiveOperationType {
+  EXPLAIN,
+  LOAD,
+  EXPORT,
+  IMPORT,
+  CREATEDATABASE,
+  DROPDATABASE,
+  SWITCHDATABASE,
+  LOCKDB,
+  UNLOCKDB,
+  DROPTABLE ,
+  DESCTABLE,
+  DESCFUNCTION,
+  MSCK,
+  ALTERTABLE_ADDCOLS,
+  ALTERTABLE_REPLACECOLS,
+  ALTERTABLE_RENAMECOL,
+  ALTERTABLE_RENAMEPART,
+  ALTERTABLE_RENAME,
+  ALTERTABLE_DROPPARTS,
+  ALTERTABLE_ADDPARTS,
+  ALTERTABLE_TOUCH,
+  ALTERTABLE_ARCHIVE,
+  ALTERTABLE_UNARCHIVE,
+  ALTERTABLE_PROPERTIES,
+  ALTERTABLE_SERIALIZER,
+  ALTERPARTITION_SERIALIZER,
+  ALTERTABLE_SERDEPROPERTIES,
+  ALTERPARTITION_SERDEPROPERTIES,
+  ALTERTABLE_CLUSTER_SORT,
+  ANALYZE_TABLE,
+  ALTERTABLE_BUCKETNUM,
+  ALTERPARTITION_BUCKETNUM,
+  SHOWDATABASES,
+  SHOWTABLES,
+  SHOWCOLUMNS,
+  SHOW_TABLESTATUS,
+  SHOW_TBLPROPERTIES,
+  SHOW_CREATETABLE,
+  SHOWFUNCTIONS,
+  SHOWINDEXES,
+  SHOWPARTITIONS,
+  SHOWLOCKS,
+  CREATEFUNCTION,
+  DROPFUNCTION,
+  CREATEMACRO,
+  DROPMACRO,
+  CREATEVIEW,
+  DROPVIEW,
+  CREATEINDEX,
+  DROPINDEX,
+  ALTERINDEX_REBUILD,
+  ALTERVIEW_PROPERTIES,
+  DROPVIEW_PROPERTIES,
+  LOCKTABLE,
+  UNLOCKTABLE,
+  CREATEROLE,
+  DROPROLE,
+  GRANT_PRIVILEGE,
+  REVOKE_PRIVILEGE,
+  SHOW_GRANT,
+  GRANT_ROLE,
+  REVOKE_ROLE,
+  SHOW_ROLES,
+  SHOW_ROLE_GRANT,
+  ALTERTABLE_PROTECTMODE,
+  ALTERPARTITION_PROTECTMODE,
+  ALTERTABLE_FILEFORMAT,
+  ALTERPARTITION_FILEFORMAT,
+  ALTERTABLE_LOCATION,
+  ALTERPARTITION_LOCATION,
+  CREATETABLE,
+  TRUNCATETABLE,
+  CREATETABLE_AS_SELECT,
+  QUERY,
+  ALTERINDEX_PROPS,
+  ALTERDATABASE,
+  DESCDATABASE,
+  ALTERTABLE_MERGEFILES,
+  ALTERPARTITION_MERGEFILES,
+  ALTERTABLE_SKEWED,
+  ALTERTBLPART_SKEWED_LOCATION,
+  ALTERVIEW_RENAME,
+
+}
