f7b2304eca942c74ecb97fd4eff1d568abc97f0b
Alan Gates <gates@apache.org>
2014-01-24 20:37:50 +0000

    HIVE-6248 HCatReader/Writer should hide Hadoop and Hive classes
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1561158 13f79535-47bb-0310-9956-ffa450edef68

--- hcatalog/core/src/main/java/org/apache/hive/hcatalog/data/transfer/WriterContext.java
+++ hcatalog/core/src/main/java/org/apache/hive/hcatalog/data/transfer/WriterContext.java
@@ -20,46 +20,14 @@
 package org.apache.hive.hcatalog.data.transfer;
 
 import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
-
-import org.apache.hadoop.conf.Configurable;
-import org.apache.hadoop.conf.Configuration;
 
 /**
- * This contains information obtained at master node to help prepare slave nodes
- * for writer. This class implements {@link Externalizable} so it can be
- * serialized using standard java mechanisms. Master should serialize it and
+ * This contains information obtained at master node to be distributed to
+ * slaves nodes that will do the writing.
+ * This class implements {@link Externalizable} so it can be
+ * serialized using standard Java mechanisms. Master should serialize it and
  * make it available to slaves to prepare for writes.
  */
-public class WriterContext implements Externalizable, Configurable {
+public interface WriterContext extends Externalizable {
 
-  private static final long serialVersionUID = -5899374262971611840L;
-  private Configuration conf;
-
-  public WriterContext() {
-    conf = new Configuration();
-  }
-
-  @Override
-  public Configuration getConf() {
-    return conf;
-  }
-
-  @Override
-  public void setConf(final Configuration config) {
-    this.conf = config;
-  }
-
-  @Override
-  public void writeExternal(ObjectOutput out) throws IOException {
-    conf.write(out);
-  }
-
-  @Override
-  public void readExternal(ObjectInput in) throws IOException,
-    ClassNotFoundException {
-    conf.readFields(in);
-  }
 }
---
+++ hcatalog/core/src/main/java/org/apache/hive/hcatalog/data/transfer/impl/ReaderContextImpl.java
@@ -0,0 +1,92 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.hive.hcatalog.data.transfer.impl;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hive.hcatalog.data.transfer.ReaderContext;
+import org.apache.hive.hcatalog.mapreduce.HCatSplit;
+
+/**
+ * This class contains the list of {@link InputSplit}s obtained
+ * at master node and the configuration.
+ */
+class ReaderContextImpl implements ReaderContext, Configurable {
+
+    private static final long serialVersionUID = -2656468331739574367L;
+    private List<InputSplit> splits;
+    private Configuration conf;
+
+    public ReaderContextImpl() {
+        this.splits = new ArrayList<InputSplit>();
+        this.conf = new Configuration();
+    }
+
+    void setInputSplits(final List<InputSplit> splits) {
+        this.splits = splits;
+    }
+
+    List<InputSplit> getSplits() {
+        return splits;
+    }
+
+    @Override
+    public int numSplits() {
+        return splits.size();
+    }
+
+    @Override
+    public Configuration getConf() {
+        return conf;
+    }
+
+    @Override
+    public void setConf(final Configuration config) {
+        conf = config;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        conf.write(out);
+        out.writeInt(splits.size());
+        for (InputSplit split : splits) {
+            ((HCatSplit) split).write(out);
+        }
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException,
+        ClassNotFoundException {
+        conf.readFields(in);
+        int numOfSplits = in.readInt();
+        for (int i = 0; i < numOfSplits; i++) {
+            HCatSplit split = new HCatSplit();
+            split.readFields(in);
+            splits.add(split);
+        }
+    }
+}
