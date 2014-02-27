4b45d2e90bd3d7b51cacf0463c63647e02907f7b
Ashutosh Chauhan <hashutosh@apache.org>
2014-02-27 16:01:00 +0000

    HIVE-6458 : Add schema upgrade scripts for metastore changes related to permanent functions (Jason Dere via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1572639 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/scripts/upgrade/derby/upgrade-0.12.0-to-0.13.0.derby.sql
+++ metastore/scripts/upgrade/derby/upgrade-0.12.0-to-0.13.0.derby.sql
@@ -1,2 +1,5 @@
--- Upgrade MetaStore schema from 0.11.0 to 0.12.0
+-- Upgrade MetaStore schema from 0.12.0 to 0.13.0
+RUN '016-HIVE-6386.derby.sql';
+RUN '017-HIVE-6458.derby.sql';
+
 UPDATE "APP".VERSION SET SCHEMA_VERSION='0.13.0', VERSION_COMMENT='Hive release version 0.13.0' where VER_ID=1;
