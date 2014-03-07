3322a95cf8db6255e05d1ae3356991b7f0a98d5e
Ashutosh Chauhan <hashutosh@apache.org>
2014-03-07 19:32:24 +0000

    HIVE-6555 : TestSchemaTool is failing on trunk after branching (Ashutosh Chauhan via Prasad Mujumdar)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1575379 13f79535-47bb-0310-9956-ffa450edef68

---
+++ metastore/scripts/upgrade/postgres/upgrade-0.13.0-to-0.14.0.postgres.sql
@@ -0,0 +1,6 @@
+SELECT 'Upgrading MetaStore schema from 0.13.0 to 0.14.0';
+
+UPDATE "VERSION" SET "SCHEMA_VERSION"='0.14.0', "VERSION_COMMENT"='Hive release version 0.14.0' where "VER_ID"=1;
+SELECT 'Finished upgrading MetaStore schema from 0.13.0 to 0.14.0';
+
+
