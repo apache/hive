749c8dcc648f8b119ca87d3de98f1fd914c8705d
Ashutosh Chauhan <hashutosh@apache.org>
2014-03-11 01:23:19 +0000

    HIVE-6585 : bucket map join fails in presence of _SUCCESS file (Ashutosh Chauhan via Vikram Dixit)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/branch-0.13@1576164 13f79535-47bb-0310-9956-ffa450edef68

--- common/src/java/org/apache/hadoop/hive/common/FileUtils.java
+++ common/src/java/org/apache/hadoop/hive/common/FileUtils.java
@@ -31,6 +31,7 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.fs.permission.FsAction;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hive.conf.HiveConf;
@@ -297,7 +298,14 @@ public final class FileUtils {
       List<FileStatus> results) throws IOException {
 
     if (fileStatus.isDir()) {
-      for (FileStatus stat : fs.listStatus(fileStatus.getPath())) {
+      for (FileStatus stat : fs.listStatus(fileStatus.getPath(), new PathFilter() {
+
+        @Override
+        public boolean accept(Path p) {
+          String name = p.getName();
+          return !name.startsWith("_") && !name.startsWith(".");
+        }
+      })) {
         listStatusRecursively(fs, stat, results);
       }
     } else {
