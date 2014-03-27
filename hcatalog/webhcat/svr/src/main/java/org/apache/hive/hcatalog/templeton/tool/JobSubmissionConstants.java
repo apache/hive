ce47a09b5957cad717af298a7b50c690c885b3bb
Thejas Madhavan Nair <thejas@apache.org>
2014-03-27 20:49:18 +0000

    HIVE-6546 : WebHCat job submission for pig with -useHCatalog argument fails on Windows (Eric Hanson via Thejas Nair)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/branch-0.13@1582470 13f79535-47bb-0310-9956-ffa450edef68

--- hcatalog/webhcat/svr/src/main/java/org/apache/hive/hcatalog/templeton/tool/JobSubmissionConstants.java
+++ hcatalog/webhcat/svr/src/main/java/org/apache/hive/hcatalog/templeton/tool/JobSubmissionConstants.java
@@ -33,7 +33,14 @@ public interface JobSubmissionConstants {
   public static final String EXIT_FNAME = "exit";
   public static final int WATCHER_TIMEOUT_SECS = 10;
   public static final int KEEP_ALIVE_MSEC = 60 * 1000;
-  public static final String TOKEN_FILE_ARG_PLACEHOLDER = "__WEBHCAT_TOKEN_FILE_LOCATION__";
+  /*
+   * The = sign in the string for TOKEN_FILE_ARG_PLACEHOLDER is required because
+   * org.apache.hadoop.util.GenericOptionsParser.preProcessForWindows() prepares
+   * arguments expecting an = sign. It will fail to prepare the arguments correctly
+   * without the = sign present.
+   */
+  public static final String TOKEN_FILE_ARG_PLACEHOLDER =
+    "__MR_JOB_CREDENTIALS_OPTION=WEBHCAT_TOKEN_FILE_LOCATION__";
   /**
    * constants needed for Pig job submission
    */
