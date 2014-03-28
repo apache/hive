dcd3c26d04b5d7c6b176e01d923a96a7d746a162
Thejas Nair <thejas@apache.org>
2014-03-28 18:11:20 +0000

    HIVE-6697 : HiveServer2 secure thrift/http authentication needs to support SPNego  (Dilli Arumugam via Thejas Nair)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1582848 13f79535-47bb-0310-9956-ffa450edef68

--- shims/0.20/src/main/java/org/apache/hadoop/hive/shims/Hadoop20Shims.java
+++ shims/0.20/src/main/java/org/apache/hadoop/hive/shims/Hadoop20Shims.java
@@ -599,6 +599,13 @@ public class Hadoop20Shims implements HadoopShims {
   }
 
   @Override
+  public UserGroupInformation loginUserFromKeytabAndReturnUGI(
+      String principal, String keytabFile) throws IOException {
+    throwKerberosUnsupportedError();
+    return null;
+  }
+
+  @Override
   public void reLoginUserFromKeytab() throws IOException{
     throwKerberosUnsupportedError();
   }
