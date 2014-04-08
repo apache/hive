24d8562847f3a29ce41ae23e2eefd0c26a9dcc4f
Harish Butani <rhbutani@apache.org>
2014-04-08 15:37:43 +0000

    Revert HIVE-6757(r1585758) going back to r1585754, didn't rm files
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1585760 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/scripts/upgrade/derby/upgrade-0.12.0-to-0.13.0.derby.sql
+++ metastore/scripts/upgrade/derby/upgrade-0.12.0-to-0.13.0.derby.sql
@@ -2,6 +2,5 @@
 RUN 'hive-txn-schema-0.13.0.derby.sql';
 RUN '016-HIVE-6386.derby.sql';
 RUN '017-HIVE-6458.derby.sql';
-RUN '018-HIVE-6757.derby.sql';
 
 UPDATE "APP".VERSION SET SCHEMA_VERSION='0.13.0', VERSION_COMMENT='Hive release version 0.13.0' where VER_ID=1;
