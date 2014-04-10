273aaa3226b234866281698792936f75d82238a5
Ashutosh Chauhan <hashutosh@apache.org>
2014-04-10 22:41:06 +0000

    HIVE-6871 : Build fixes to allow Windows to run TestCliDriver (Jason Dere via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1586497 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/test/org/apache/hadoop/hive/ql/WindowsPathUtil.java
+++ ql/src/test/org/apache/hadoop/hive/ql/WindowsPathUtil.java
@@ -30,9 +30,6 @@ public class WindowsPathUtil {
     String orgTestTempDir = System.getProperty("test.tmp.dir");
     System.setProperty("test.tmp.dir", getHdfsUriString(orgTestTempDir));
 
-    String orgTestDataDir = System.getProperty("test.src.data.dir");
-    System.setProperty("test.src.data.dir", getHdfsUriString(orgTestDataDir));
-
     String orgScratchDir = conf.getVar(HiveConf.ConfVars.SCRATCHDIR);
     conf.setVar(HiveConf.ConfVars.SCRATCHDIR, getHdfsUriString(orgScratchDir));
   }
