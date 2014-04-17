5561d21d514a80c14e9f8ef24ef6bde76da83389
Ashutosh Chauhan <hashutosh@apache.org>
2014-04-17 21:41:05 +0000

    HIVE-6862 : add DB schema DDL and upgrade 12to13 scripts for MS SQL Server (Eugene Koifman via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1588377 13f79535-47bb-0310-9956-ffa450edef68

---
+++ metastore/scripts/upgrade/mssql/upgrade-0.12.0-to-0.13.0.mssql.sql
@@ -0,0 +1,7 @@
+SELECT 'Upgrading MetaStore schema from 0.12.0 to 0.13.0' AS MESSAGE;
+
+:r 001-HIVE-6862.mssql.sql;
+:r hive-txn-schema-0.13.0.mssql.sql;
+
+UPDATE VERSION SET SCHEMA_VERSION='0.13.0', VERSION_COMMENT='Hive release version 0.13.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 0.12.0 to 0.13.0' AS MESSAGE;
