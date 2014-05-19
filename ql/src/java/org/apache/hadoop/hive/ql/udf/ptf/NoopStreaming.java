410fcd44d3ff71ff1fafe9754af3bcd6ff02f1f3
Ashutosh Chauhan <hashutosh@apache.org>
2014-05-19 06:44:33 +0000

    HIVE-6999 : Add streaming mode to PTFs (Harish Butani via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1595755 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/ptf/NoopStreaming.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.ql.udf.ptf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.plan.PTFDesc;
+import org.apache.hadoop.hive.ql.plan.ptf.PartitionedTableFunctionDef;
+
+public class NoopStreaming extends Noop {
+  
+  List<Object> rows;
+  
+  NoopStreaming() {
+    rows = new ArrayList<Object>();
+  }
+  
+  public boolean canAcceptInputAsStream() {
+    return true;
+  } 
+  
+  public List<Object> processRow(Object row) throws HiveException {
+    if (!canAcceptInputAsStream() ) {
+      throw new HiveException(String.format(
+          "Internal error: PTF %s, doesn't support Streaming",
+          getClass().getName()));
+    }
+    rows.clear();
+    rows.add(row);
+    return rows;
+  }
+  
+  public static class NoopStreamingResolver extends NoopResolver {
+
+    @Override
+    protected TableFunctionEvaluator createEvaluator(PTFDesc ptfDesc, PartitionedTableFunctionDef tDef) {
+      return new NoopStreaming();
+    }
+  }
+}
