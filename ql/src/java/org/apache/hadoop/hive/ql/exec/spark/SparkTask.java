a9be67ee68c5aff637a3ddad3f1e218c5d54d764
Xuefu Zhang <xuefu@apache.org>
2014-07-29 10:18:00 +0000

    HIVE-7338: Create SparkPlanGenerator
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/spark@1614300 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/spark/SparkTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/spark/SparkTask.java
@@ -18,18 +18,57 @@
 
 package org.apache.hadoop.hive.ql.exec.spark;
 
+import java.util.List;
+
 import org.apache.hadoop.hive.ql.DriverContext;
+import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.Task;
+import org.apache.hadoop.hive.ql.exec.Utilities;
+import org.apache.hadoop.hive.ql.plan.BaseWork;
 import org.apache.hadoop.hive.ql.plan.SparkWork;
 import org.apache.hadoop.hive.ql.plan.api.StageType;
+import org.apache.hadoop.util.StringUtils;
 
 public class SparkTask extends Task<SparkWork> {
   private static final long serialVersionUID = 1L;
 
   @Override
   public int execute(DriverContext driverContext) {
-    SparkClient client = SparkClient.getInstance(driverContext.getCtx().getConf());
-    return client.execute(driverContext, getWork());
+    int rc = 1;
+    SparkClient client = null;
+    try {
+      client = SparkClient.getInstance(driverContext.getCtx().getConf());
+      rc = client.execute(driverContext, getWork());
+    } finally {
+      if (client != null) {
+        rc = close(rc);
+      }
+    }
+    return rc;
+  }
+  
+  /**
+   * close will move the temp files into the right place for the fetch
+   * task. If the job has failed it will clean up the files.
+   */
+  private int close(int rc) {
+    try {
+      List<BaseWork> ws = work.getAllWork();
+      for (BaseWork w: ws) {
+        for (Operator<?> op: w.getAllOperators()) {
+          op.jobClose(conf, rc == 0);
+        }
+      }
+    } catch (Exception e) {
+      // jobClose needs to execute successfully otherwise fail task
+      if (rc == 0) {
+        rc = 3;
+        String mesg = "Job Commit failed with exception '"
+          + Utilities.getNameMessage(e) + "'";
+        console.printError(mesg, "\n" + StringUtils.stringifyException(e));
+      }
+    }
+    return rc;
   }
 
   @Override
