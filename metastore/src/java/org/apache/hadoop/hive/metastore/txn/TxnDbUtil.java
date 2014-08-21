30ffc96963fb0446730b05f6cdde666834effb58
Ashutosh Chauhan <hashutosh@apache.org>
2014-08-21 20:32:21 +0000

    HIVE-7281 : DbTxnManager acquiring wrong level of lock for dynamic partitioning (Alan Gates via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1619567 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/src/java/org/apache/hadoop/hive/metastore/txn/TxnDbUtil.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/txn/TxnDbUtil.java
@@ -49,121 +49,135 @@ public class TxnDbUtil {
     // intended for creating derby databases, and thus will inexorably get
     // out of date with it.  I'm open to any suggestions on how to make this
     // read the file in a build friendly way.
-    Connection conn = getConnection();
-    Statement s = conn.createStatement();
-    s.execute("CREATE TABLE TXNS (" +
-        "  TXN_ID bigint PRIMARY KEY," +
-        "  TXN_STATE char(1) NOT NULL," +
-        "  TXN_STARTED bigint NOT NULL," +
-        "  TXN_LAST_HEARTBEAT bigint NOT NULL," +
-        "  TXN_USER varchar(128) NOT NULL," +
-        "  TXN_HOST varchar(128) NOT NULL)");
+    Connection conn = null;
+    boolean committed = false;
+    try {
+      conn = getConnection();
+      Statement s = conn.createStatement();
+      s.execute("CREATE TABLE TXNS (" +
+          "  TXN_ID bigint PRIMARY KEY," +
+          "  TXN_STATE char(1) NOT NULL," +
+          "  TXN_STARTED bigint NOT NULL," +
+          "  TXN_LAST_HEARTBEAT bigint NOT NULL," +
+          "  TXN_USER varchar(128) NOT NULL," +
+          "  TXN_HOST varchar(128) NOT NULL)");
 
-        s.execute("CREATE TABLE TXN_COMPONENTS (" +
-        "  TC_TXNID bigint REFERENCES TXNS (TXN_ID)," +
-        "  TC_DATABASE varchar(128) NOT NULL," +
-        "  TC_TABLE varchar(128)," +
-        "  TC_PARTITION varchar(767))");
-    s.execute("CREATE TABLE COMPLETED_TXN_COMPONENTS (" +
-        "  CTC_TXNID bigint," +
-        "  CTC_DATABASE varchar(128) NOT NULL," +
-        "  CTC_TABLE varchar(128)," +
-        "  CTC_PARTITION varchar(767))");
-    s.execute("CREATE TABLE NEXT_TXN_ID (" +
-        "  NTXN_NEXT bigint NOT NULL)");
-    s.execute("INSERT INTO NEXT_TXN_ID VALUES(1)");
-    s.execute("CREATE TABLE HIVE_LOCKS (" +
-        " HL_LOCK_EXT_ID bigint NOT NULL," +
-        " HL_LOCK_INT_ID bigint NOT NULL," +
-        " HL_TXNID bigint," +
-        " HL_DB varchar(128) NOT NULL," +
-        " HL_TABLE varchar(128)," +
-        " HL_PARTITION varchar(767)," +
-        " HL_LOCK_STATE char(1) NOT NULL," +
-        " HL_LOCK_TYPE char(1) NOT NULL," +
-        " HL_LAST_HEARTBEAT bigint NOT NULL," +
-        " HL_ACQUIRED_AT bigint," +
-        " HL_USER varchar(128) NOT NULL," +
-        " HL_HOST varchar(128) NOT NULL," +
-        " PRIMARY KEY(HL_LOCK_EXT_ID, HL_LOCK_INT_ID))");
-    s.execute("CREATE INDEX HL_TXNID_INDEX ON HIVE_LOCKS (HL_TXNID)");
+      s.execute("CREATE TABLE TXN_COMPONENTS (" +
+      "  TC_TXNID bigint REFERENCES TXNS (TXN_ID)," +
+      "  TC_DATABASE varchar(128) NOT NULL," +
+      "  TC_TABLE varchar(128)," +
+      "  TC_PARTITION varchar(767))");
+      s.execute("CREATE TABLE COMPLETED_TXN_COMPONENTS (" +
+          "  CTC_TXNID bigint," +
+          "  CTC_DATABASE varchar(128) NOT NULL," +
+          "  CTC_TABLE varchar(128)," +
+          "  CTC_PARTITION varchar(767))");
+      s.execute("CREATE TABLE NEXT_TXN_ID (" +
+          "  NTXN_NEXT bigint NOT NULL)");
+      s.execute("INSERT INTO NEXT_TXN_ID VALUES(1)");
+      s.execute("CREATE TABLE HIVE_LOCKS (" +
+          " HL_LOCK_EXT_ID bigint NOT NULL," +
+          " HL_LOCK_INT_ID bigint NOT NULL," +
+          " HL_TXNID bigint," +
+          " HL_DB varchar(128) NOT NULL," +
+          " HL_TABLE varchar(128)," +
+          " HL_PARTITION varchar(767)," +
+          " HL_LOCK_STATE char(1) NOT NULL," +
+          " HL_LOCK_TYPE char(1) NOT NULL," +
+          " HL_LAST_HEARTBEAT bigint NOT NULL," +
+          " HL_ACQUIRED_AT bigint," +
+          " HL_USER varchar(128) NOT NULL," +
+          " HL_HOST varchar(128) NOT NULL," +
+          " PRIMARY KEY(HL_LOCK_EXT_ID, HL_LOCK_INT_ID))");
+      s.execute("CREATE INDEX HL_TXNID_INDEX ON HIVE_LOCKS (HL_TXNID)");
 
-    s.execute("CREATE TABLE NEXT_LOCK_ID (" +
-        " NL_NEXT bigint NOT NULL)");
-    s.execute("INSERT INTO NEXT_LOCK_ID VALUES(1)");
+      s.execute("CREATE TABLE NEXT_LOCK_ID (" +
+          " NL_NEXT bigint NOT NULL)");
+      s.execute("INSERT INTO NEXT_LOCK_ID VALUES(1)");
 
-    s.execute("CREATE TABLE COMPACTION_QUEUE (" +
-        " CQ_ID bigint PRIMARY KEY," +
-        " CQ_DATABASE varchar(128) NOT NULL," +
-        " CQ_TABLE varchar(128) NOT NULL," +
-        " CQ_PARTITION varchar(767)," +
-        " CQ_STATE char(1) NOT NULL," +
-        " CQ_TYPE char(1) NOT NULL," +
-        " CQ_WORKER_ID varchar(128)," +
-        " CQ_START bigint," +
-        " CQ_RUN_AS varchar(128))");
+      s.execute("CREATE TABLE COMPACTION_QUEUE (" +
+          " CQ_ID bigint PRIMARY KEY," +
+          " CQ_DATABASE varchar(128) NOT NULL," +
+          " CQ_TABLE varchar(128) NOT NULL," +
+          " CQ_PARTITION varchar(767)," +
+          " CQ_STATE char(1) NOT NULL," +
+          " CQ_TYPE char(1) NOT NULL," +
+          " CQ_WORKER_ID varchar(128)," +
+          " CQ_START bigint," +
+          " CQ_RUN_AS varchar(128))");
 
-    s.execute("CREATE TABLE NEXT_COMPACTION_QUEUE_ID (NCQ_NEXT bigint NOT NULL)");
-    s.execute("INSERT INTO NEXT_COMPACTION_QUEUE_ID VALUES(1)");
+      s.execute("CREATE TABLE NEXT_COMPACTION_QUEUE_ID (NCQ_NEXT bigint NOT NULL)");
+      s.execute("INSERT INTO NEXT_COMPACTION_QUEUE_ID VALUES(1)");
 
-    conn.commit();
-    conn.close();
+      conn.commit();
+      committed = true;
+    } finally {
+      if (!committed) conn.rollback();
+      conn.close();
+    }
   }
 
   public static void cleanDb() throws  Exception {
-    Connection conn = getConnection();
-    Statement s = conn.createStatement();
-    // We want to try these, whether they succeed or fail.
+    Connection conn = null;
+    boolean committed = false;
     try {
-      s.execute("DROP INDEX HL_TXNID_INDEX");
-    } catch (Exception e) {
-      System.err.println("Unable to drop index HL_TXNID_INDEX " +
-          e.getMessage());
+      conn = getConnection();
+      Statement s = conn.createStatement();
+      // We want to try these, whether they succeed or fail.
+      try {
+        s.execute("DROP INDEX HL_TXNID_INDEX");
+      } catch (Exception e) {
+        System.err.println("Unable to drop index HL_TXNID_INDEX " +
+            e.getMessage());
+      }
+      try {
+        s.execute("DROP TABLE TXN_COMPONENTS");
+      } catch (Exception e) {
+        System.err.println("Unable to drop table TXN_COMPONENTS " +
+            e.getMessage());
+      }
+      try {
+        s.execute("DROP TABLE COMPLETED_TXN_COMPONENTS");
+      } catch (Exception e) {
+        System.err.println("Unable to drop table COMPLETED_TXN_COMPONENTS " +
+            e.getMessage());
+      }
+      try {
+        s.execute("DROP TABLE TXNS");
+      } catch (Exception e) {
+        System.err.println("Unable to drop table TXNS " +
+            e.getMessage());
+      }
+      try {
+        s.execute("DROP TABLE NEXT_TXN_ID");
+      } catch (Exception e) {
+        System.err.println("Unable to drop table NEXT_TXN_ID " +
+            e.getMessage());
+      }
+      try {
+        s.execute("DROP TABLE HIVE_LOCKS");
+      } catch (Exception e) {
+        System.err.println("Unable to drop table HIVE_LOCKS " +
+            e.getMessage());
+      }
+      try {
+        s.execute("DROP TABLE NEXT_LOCK_ID");
+      } catch (Exception e) {
+      }
+      try {
+        s.execute("DROP TABLE COMPACTION_QUEUE");
+      } catch (Exception e) {
+      }
+      try {
+        s.execute("DROP TABLE NEXT_COMPACTION_QUEUE_ID");
+      } catch (Exception e) {
+      }
+      conn.commit();
+      committed = true;
+    } finally {
+      if (!committed) conn.rollback();
+      conn.close();
     }
-    try {
-      s.execute("DROP TABLE TXN_COMPONENTS");
-    } catch (Exception e) {
-      System.err.println("Unable to drop table TXN_COMPONENTS " +
-          e.getMessage());
-    }
-    try {
-      s.execute("DROP TABLE COMPLETED_TXN_COMPONENTS");
-    } catch (Exception e) {
-      System.err.println("Unable to drop table COMPLETED_TXN_COMPONENTS " +
-          e.getMessage());
-    }
-    try {
-      s.execute("DROP TABLE TXNS");
-    } catch (Exception e) {
-      System.err.println("Unable to drop table TXNS " +
-          e.getMessage());
-    }
-    try {
-      s.execute("DROP TABLE NEXT_TXN_ID");
-    } catch (Exception e) {
-      System.err.println("Unable to drop table NEXT_TXN_ID " +
-          e.getMessage());
-    }
-    try {
-      s.execute("DROP TABLE HIVE_LOCKS");
-    } catch (Exception e) {
-      System.err.println("Unable to drop table HIVE_LOCKS " +
-          e.getMessage());
-    }
-    try {
-      s.execute("DROP TABLE NEXT_LOCK_ID");
-    } catch (Exception e) {
-    }
-    try {
-      s.execute("DROP TABLE COMPACTION_QUEUE");
-    } catch (Exception e) {
-    }
-    try {
-      s.execute("DROP TABLE NEXT_COMPACTION_QUEUE_ID");
-    } catch (Exception e) {
-    }
-    conn.commit();
-    conn.close();
   }
 
   /**
@@ -174,25 +188,34 @@ public class TxnDbUtil {
    */
   public static int countLockComponents(long lockId) throws  Exception {
     Connection conn = getConnection();
-    Statement s = conn.createStatement();
-    ResultSet rs = s.executeQuery("select count(*) from hive_locks where " +
-        "hl_lock_ext_id = " + lockId);
-    if (!rs.next()) return 0;
-    int rc = rs.getInt(1);
-    conn.rollback();
-    conn.close();
-    return rc;
+    try {
+      Statement s = conn.createStatement();
+      ResultSet rs = s.executeQuery("select count(*) from hive_locks where hl_lock_ext_id = " +
+          lockId);
+      if (!rs.next()) return 0;
+      int rc = rs.getInt(1);
+      return rc;
+    } finally {
+      conn.rollback();
+      conn.close();
+    }
   }
 
   public static int findNumCurrentLocks() throws Exception {
-    Connection conn = getConnection();
-    Statement s = conn.createStatement();
-    ResultSet rs = s.executeQuery("select count(*) from hive_locks");
-    if (!rs.next()) return 0;
-    int rc = rs.getInt(1);
-    conn.rollback();
-    conn.close();
-    return rc;
+    Connection conn = null;
+    try {
+      conn = getConnection();
+      Statement s = conn.createStatement();
+      ResultSet rs = s.executeQuery("select count(*) from hive_locks");
+      if (!rs.next()) return 0;
+      int rc = rs.getInt(1);
+      return rc;
+    } finally {
+      if (conn != null) {
+        conn.rollback();
+        conn.close();
+      }
+    }
   }
 
   private static Connection getConnection() throws Exception {
