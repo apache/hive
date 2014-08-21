34bdc58e8d570d3174d30a49d54f2f294f5a79dc
Ashutosh Chauhan <hashutosh@apache.org>
2014-08-21 20:36:03 +0000

    HIVE-7784 : Created the needed indexes on Hive.PART_COL_STATS for CBO (Mostafa Mokhtar via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1619569 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/scripts/upgrade/oracle/upgrade-0.13.0-to-0.14.0.oracle.sql
+++ metastore/scripts/upgrade/oracle/upgrade-0.13.0-to-0.14.0.oracle.sql
@@ -1,4 +1,6 @@
 SELECT 'Upgrading MetaStore schema from 0.13.0 to 0.14.0' AS Status from dual;
 
+@020-HIVE-7784.oracle.sql;
+
 UPDATE VERSION SET SCHEMA_VERSION='0.14.0', VERSION_COMMENT='Hive release version 0.14.0' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 0.13.0 to 0.14.0' AS Status from dual;
