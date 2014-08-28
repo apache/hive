688fb80d852cb1d5509a6e679760a02f9facb6c3
Brock Noland <brock@apache.org>
2014-08-28 23:30:22 +0000

    HIVE-6245 - HS2 creates DBs/Tables with wrong ownership when HMS setugi is true (Venki Korukanti via Brock)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1621228 13f79535-47bb-0310-9956-ffa450edef68

--- service/src/java/org/apache/hive/service/cli/session/HiveSessionImplwithUGI.java
+++ service/src/java/org/apache/hive/service/cli/session/HiveSessionImplwithUGI.java
@@ -48,6 +48,14 @@ public class HiveSessionImplwithUGI extends HiveSessionImpl {
     super(protocol, username, password, hiveConf, ipAddress);
     setSessionUGI(username);
     setDelegationToken(delegationToken);
+
+    // create a new metastore connection for this particular user session
+    Hive.set(null);
+    try {
+      sessionHive = Hive.get(getHiveConf());
+    } catch (HiveException e) {
+      throw new HiveSQLException("Failed to setup metastore connection", e);
+    }
   }
 
   // setup appropriate UGI for the session
@@ -114,13 +122,6 @@ public class HiveSessionImplwithUGI extends HiveSessionImpl {
         ShimLoader.getHadoopShims().setTokenStr(sessionUgi, delegationTokenStr, HS2TOKEN);
       } catch (IOException e) {
         throw new HiveSQLException("Couldn't setup delegation token in the ugi", e);
-      }
-      // create a new metastore connection using the delegation token
-      Hive.set(null);
-      try {
-        sessionHive = Hive.get(getHiveConf());
-      } catch (HiveException e) {
-        throw new HiveSQLException("Failed to setup metastore connection", e);
       }
     }
   }
