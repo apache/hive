24f33be8a18521531263bc435dc8873dfdc4749c
Navis Ryu <navis@apache.org>
2014-09-12 07:43:28 +0000

    HIVE-8041 : Hadoop-2 build is broken with JDK6 (Navis, reviewed by Xuefu Zhang)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1624470 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFIf.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFIf.java
@@ -77,7 +77,7 @@ import org.apache.hadoop.hive.ql.exec.vector.expressions.IfExprVarCharScalarStri
   IfExprCharScalarStringGroupColumn.class, IfExprVarCharScalarStringGroupColumn.class,
   IfExprStringScalarStringScalar.class,
   IfExprStringScalarCharScalar.class, IfExprStringScalarVarCharScalar.class,
-  IfExprCharScalarStringScalar.class, IfExprVarCharScalarStringScalar.class,
+  IfExprCharScalarStringScalar.class, IfExprVarCharScalarStringScalar.class
 })
 public class GenericUDFIf extends GenericUDF {
   private transient ObjectInspector[] argumentOIs;
