5330a1a56113a43c9884eb003c57bd6cb52f65a7
Ashutosh Chauhan <hashutosh@apache.org>
2014-09-12 23:21:03 +0000

    HIVE-8080 : CBO: function name may not match UDF name during translation (Sergey Shelukhin via John Pullokkaran)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/cbo@1624678 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/optiq/translator/SqlFunctionConverter.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/optiq/translator/SqlFunctionConverter.java
@@ -21,6 +21,7 @@ import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.FunctionInfo;
 import org.apache.hadoop.hive.ql.exec.FunctionRegistry;
@@ -66,21 +67,22 @@ public class SqlFunctionConverter {
   static final Map<SqlOperator, String>    reverseOperatorMap;
 
   static {
-    Builder builder = new Builder();
+    StaticBlockBuilder builder = new StaticBlockBuilder();
     hiveToOptiq = ImmutableMap.copyOf(builder.hiveToOptiq);
     optiqToHiveToken = ImmutableMap.copyOf(builder.optiqToHiveToken);
     reverseOperatorMap = ImmutableMap.copyOf(builder.reverseOperatorMap);
   }
 
-  public static SqlOperator getOptiqOperator(GenericUDF hiveUDF,
+  public static SqlOperator getOptiqOperator(String funcTextName, GenericUDF hiveUDF,
       ImmutableList<RelDataType> optiqArgTypes, RelDataType retType) throws OptiqSemanticException {
     // handle overloaded methods first
     if (hiveUDF instanceof GenericUDFOPNegative) {
       return SqlStdOperatorTable.UNARY_MINUS;
     } else if (hiveUDF instanceof GenericUDFOPPositive) {
       return SqlStdOperatorTable.UNARY_PLUS;
-    } // do genric lookup
-    return getOptiqFn(getName(hiveUDF), optiqArgTypes, retType);
+    } // do generic lookup
+    String name = StringUtils.isEmpty(funcTextName) ? getName(hiveUDF) : funcTextName;
+    return getOptiqFn(name, optiqArgTypes, retType);
   }
 
   public static GenericUDF getHiveUDF(SqlOperator op, RelDataType dt) {
@@ -197,6 +199,9 @@ public class SqlFunctionConverter {
 
   }
 
+  // TODO: this is not valid. Function names for built-in UDFs are specified in FunctionRegistry,
+  //       and only happen to match annotations. For user UDFs, the name is what user specifies at
+  //       creation time (annotation can be absent, different, or duplicate some other function).
   private static String getName(GenericUDF hiveUDF) {
     String udfName = null;
     if (hiveUDF instanceof GenericUDFBridge) {
@@ -228,12 +233,13 @@ public class SqlFunctionConverter {
     return udfName;
   }
 
-  private static class Builder {
+  /** This class is used to build immutable hashmaps in the static block above. */
+  private static class StaticBlockBuilder {
     final Map<String, SqlOperator>    hiveToOptiq        = Maps.newHashMap();
     final Map<SqlOperator, HiveToken> optiqToHiveToken   = Maps.newHashMap();
     final Map<SqlOperator, String>    reverseOperatorMap = Maps.newHashMap();
 
-    Builder() {
+    StaticBlockBuilder() {
       registerFunction("+", SqlStdOperatorTable.PLUS, hToken(HiveParser.PLUS, "+"));
       registerFunction("-", SqlStdOperatorTable.MINUS, hToken(HiveParser.MINUS, "-"));
       registerFunction("*", SqlStdOperatorTable.MULTIPLY, hToken(HiveParser.STAR, "*"));
