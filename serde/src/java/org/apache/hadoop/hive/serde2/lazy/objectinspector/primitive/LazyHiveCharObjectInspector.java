49f3e8fc0ad2448b811436a948dca385de3329b5
Jason Dere <jdere@apache.org>
2014-09-18 22:56:12 +0000

    HIVE-8047: Lazy char/varchar are not using escape char defined in serde params (Jason Dere, reviewed by Thejas Nair)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1626101 13f79535-47bb-0310-9956-ffa450edef68

--- serde/src/java/org/apache/hadoop/hive/serde2/lazy/objectinspector/primitive/LazyHiveCharObjectInspector.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/lazy/objectinspector/primitive/LazyHiveCharObjectInspector.java
@@ -29,12 +29,21 @@ public class LazyHiveCharObjectInspector
     extends AbstractPrimitiveLazyObjectInspector<HiveCharWritable>
     implements HiveCharObjectInspector {
 
+  private boolean escaped;
+  private byte escapeChar;
+
   // no-arg ctor required for Kyro
   public LazyHiveCharObjectInspector() {
   }
 
   public LazyHiveCharObjectInspector(CharTypeInfo typeInfo) {
+    this(typeInfo, false, (byte)0);
+  }
+
+  public LazyHiveCharObjectInspector(CharTypeInfo typeInfo, boolean escaped, byte escapeChar) {
     super(typeInfo);
+    this.escaped = escaped;
+    this.escapeChar = escapeChar;
   }
 
   @Override
@@ -61,6 +70,14 @@ public class LazyHiveCharObjectInspector
       return newValue;
     }
     return ret;
+  }
+
+  public boolean isEscaped() {
+    return escaped;
+  }
+
+  public byte getEscapeChar() {
+    return escapeChar;
   }
 
   @Override
