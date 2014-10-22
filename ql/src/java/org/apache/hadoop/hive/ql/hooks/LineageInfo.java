ab2660bed8da36e8a540906b60e63041bc5ff86d
Xuefu Zhang <xuefu@apache.org>
2014-10-22 02:42:04 +0000

    HIVE-8362 - Investigate flaky test parallel.q (Jimmy Xiang via Brock) merged from trunk r1632136
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/spark@1633507 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/hooks/LineageInfo.java
+++ ql/src/java/org/apache/hadoop/hive/ql/hooks/LineageInfo.java
@@ -19,6 +19,7 @@
 package org.apache.hadoop.hive.ql.hooks;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -396,7 +397,7 @@ public class LineageInfo implements Serializable {
    * Constructor.
    */
   public LineageInfo() {
-    index = new LinkedHashMap<DependencyKey, Dependency>();
+    index = Collections.synchronizedMap(new LinkedHashMap<DependencyKey, Dependency>());
   }
 
   /**
