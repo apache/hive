59c90eb2822aaab165f7cf2cad205dc45d469e58
Gunther Hagleitner <gunther@apache.org>
2014-11-07 07:50:26 +0000

    HIVE-8759: HiveServer2 dynamic service discovery should add hostname instead of ipaddress to ZooKeeper (Vaibhav Gumashta, reviewed by Thejas M Nair)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1637304 13f79535-47bb-0310-9956-ffa450edef68

--- service/src/java/org/apache/hive/service/server/HiveServer2.java
+++ service/src/java/org/apache/hive/service/server/HiveServer2.java
@@ -252,7 +252,7 @@ public class HiveServer2 extends CompositeService {
     if ((thriftCLIService == null) || (thriftCLIService.getServerAddress() == null)) {
       throw new Exception("Unable to get the server address; it hasn't been initialized yet.");
     }
-    return thriftCLIService.getServerAddress().getHostAddress() + ":"
+    return thriftCLIService.getServerAddress().getHostName() + ":"
         + thriftCLIService.getPortNumber();
   }
 
