30570912d4f6be8079c869540a7628f94838f7a5
Ashutosh Chauhan <hashutosh@apache.org>
2014-11-07 01:33:53 +0000

    HIVE-6977 : Delete Hiveserver1 (Ashutosh Chauhan via Vaibhav Gumashta)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1637285 13f79535-47bb-0310-9956-ffa450edef68

--- cli/src/java/org/apache/hadoop/hive/cli/CliSessionState.java
+++ cli/src/java/org/apache/hadoop/hive/cli/CliSessionState.java
@@ -25,13 +25,6 @@ import java.util.Properties;
 
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.session.SessionState;
-import org.apache.hadoop.hive.service.HiveClient;
-import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
 
 /**
  * SessionState for hive cli.
@@ -63,66 +56,16 @@ public class CliSessionState extends SessionState {
    */
   public List<String> initFiles = new ArrayList<String>();
 
-  /**
-   * host name and port number of remote Hive server
-   */
-  protected String host;
-  protected int port;
-
-  private boolean remoteMode;
-
-  private TTransport transport;
-  private HiveClient client;
-
   public CliSessionState(HiveConf conf) {
     super(conf);
-    remoteMode = false;
-  }
-
-  /**
-   * Connect to Hive Server
-   */
-  public void connect() throws TTransportException {
-    transport = new TSocket(host, port);
-    TProtocol protocol = new TBinaryProtocol(transport);
-    client = new HiveClient(protocol);
-    transport.open();
-    remoteMode = true;
-  }
-
-  public void setHost(String host) {
-    this.host = host;
-  }
-
-  public String getHost() {
-    return host;
-  }
-
-  public int getPort() {
-    return port;
   }
 
   @Override
   public void close() {
     try {
       super.close();
-      if (remoteMode) {
-        client.clean();
-        transport.close();
-      }
     } catch (IOException ioe) {
       ioe.printStackTrace();
-    } catch (TException e) {
-      e.printStackTrace();
-    }
+    } 
   }
-
-  public boolean isRemoteMode() {
-    return remoteMode;
-  }
-
-  public HiveClient getClient() {
-    return client;
-  }
-
 }
