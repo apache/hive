8b69d1ca6dcf5aab23ab644583d0d67f9ce97406
Ashutosh Chauhan <hashutosh@apache.org>
2014-11-19 15:54:15 +0000

    HIVE-8435 : Add identity project remover optimization (Jesús Camacho Rodríguez via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1640577 13f79535-47bb-0310-9956-ffa450edef68

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -1072,6 +1072,7 @@ public class HiveConf extends Configuration {
         "Whether to push predicates down into storage handlers.  Ignored when hive.optimize.ppd is false."),
     // Constant propagation optimizer
     HIVEOPTCONSTANTPROPAGATION("hive.optimize.constant.propagation", true, "Whether to enable constant propagation optimizer"),
+    HIVEIDENTITYPROJECTREMOVER("hive.optimize.remove.identity.project", true, "Removes identity project from operator tree"),
     HIVEMETADATAONLYQUERIES("hive.optimize.metadataonly", true, ""),
     HIVENULLSCANOPTIMIZE("hive.optimize.null.scan", true, "Dont scan relations which are guaranteed to not generate any rows"),
     HIVEOPTPPD_STORAGE("hive.optimize.ppd.storage", true,
