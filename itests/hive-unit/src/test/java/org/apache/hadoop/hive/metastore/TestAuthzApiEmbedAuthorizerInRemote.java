896757967853ee9fd5968e7fef782d1064627b76
Brock Noland <brock@apache.org>
2014-11-26 18:31:26 +0000

    HIVE-8971 - HIVE-8965 exposed some classes which start with Test but are not tests (Brock reviewed by Xuefu)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1641880 13f79535-47bb-0310-9956-ffa450edef68

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestAuthzApiEmbedAuthorizerInRemote.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestAuthzApiEmbedAuthorizerInRemote.java
@@ -3,14 +3,14 @@ package org.apache.hadoop.hive.metastore;
 import org.junit.BeforeClass;
 
 /**
- * Test {@link TestAuthorizationApiAuthorizer} in remote mode of metastore
+ * Test {@link AbstractTestAuthorizationApiAuthorizer} in remote mode of metastore
  */
-public class TestAuthzApiEmbedAuthorizerInRemote extends TestAuthorizationApiAuthorizer {
+public class TestAuthzApiEmbedAuthorizerInRemote extends AbstractTestAuthorizationApiAuthorizer {
 
   @BeforeClass
   public static void setup() throws Exception {
     isRemoteMetastoreMode = true; // remote metastore mode
-    TestAuthorizationApiAuthorizer.setup();
+    AbstractTestAuthorizationApiAuthorizer.setup();
   }
 
 }
