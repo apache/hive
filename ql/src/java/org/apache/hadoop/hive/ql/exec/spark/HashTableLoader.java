bf9d5a90084384ba9d5a398a123b7b0d192b34af
Xuefu Zhang <xuefu@apache.org>
2014-12-16 05:24:00 +0000

    HIVE-9101: bucket_map_join_spark4.q failed due to NPE.[Spark Branch] (Jimmy via Xuefu)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/spark@1645844 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/spark/HashTableLoader.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/spark/HashTableLoader.java
@@ -97,13 +97,17 @@ public class HashTableLoader implements org.apache.hadoop.hive.ql.exec.HashTable
         }
         String bigInputPath = currentInputPath;
         if (currentInputPath != null && mapJoinCtx != null) {
-          Set<String> aliases =
-            ((SparkBucketMapJoinContext)mapJoinCtx).getPosToAliasMap().get(pos);
-          String alias = aliases.iterator().next();
-          // Any one small table input path
-          String smallInputPath =
-            mapJoinCtx.getAliasBucketFileNameMapping().get(alias).get(bigInputPath).get(0);
-          bigInputPath = mapJoinCtx.getMappingBigFile(alias, smallInputPath);
+          if (!desc.isBucketMapJoin()) {
+            bigInputPath = null;
+          } else {
+            Set<String> aliases =
+              ((SparkBucketMapJoinContext)mapJoinCtx).getPosToAliasMap().get(pos);
+            String alias = aliases.iterator().next();
+            // Any one small table input path
+            String smallInputPath =
+              mapJoinCtx.getAliasBucketFileNameMapping().get(alias).get(bigInputPath).get(0);
+            bigInputPath = mapJoinCtx.getMappingBigFile(alias, smallInputPath);
+          }
         }
         String fileName = localWork.getBucketFileName(bigInputPath);
         Path path = Utilities.generatePath(baseDir, desc.getDumpFilePrefix(), (byte)pos, fileName);
