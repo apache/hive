e3bae67a471b938f3c1f369583e23b7264847303
Vaibhav Gumashta <vgumashta@apache.org>
2015-01-06 23:44:59 +0000

    HIVE-6679: HiveServer2 should support configurable the server side socket timeout and keepalive for various transports types where applicable (Navis Ryu, Vaibhav Gumashta reviewed by Thejas Nair)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/branch-0.14@1649974 13f79535-47bb-0310-9956-ffa450edef68

--- service/src/java/org/apache/hive/service/auth/HiveAuthFactory.java
+++ service/src/java/org/apache/hive/service/auth/HiveAuthFactory.java
@@ -19,6 +19,8 @@ package org.apache.hive.service.auth;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -194,8 +196,8 @@ public class HiveAuthFactory {
     return TSSLTransportFactory.getClientSocket(host, port, loginTimeout, params);
   }
 
-  public static TServerSocket getServerSocket(String hiveHost, int portNum)
-    throws TTransportException {
+  public static TServerSocket getServerSocket(String hiveHost, int portNum, int socketTimeout,
+      boolean keepAlive) throws TTransportException {
     InetSocketAddress serverAddress;
     if (hiveHost == null || hiveHost.isEmpty()) {
       // Wildcard bind
@@ -203,14 +205,17 @@ public class HiveAuthFactory {
     } else {
       serverAddress = new InetSocketAddress(hiveHost, portNum);
     }
-    return new TServerSocket(serverAddress);
+    TServerSocket serverSocket = new TServerSocket(serverAddress, socketTimeout);
+    if (keepAlive) {
+      serverSocket = new TServerSocketKeepAlive(serverSocket.getServerSocket());
+    }
+    return serverSocket;
   }
 
   public static TServerSocket getServerSSLSocket(String hiveHost, int portNum, String keyStorePath,
-      String keyStorePassWord, List<String> sslVersionBlacklist) throws TTransportException,
-      UnknownHostException {
-    TSSLTransportFactory.TSSLTransportParameters params =
-        new TSSLTransportFactory.TSSLTransportParameters();
+      String keyStorePassWord, List<String> sslVersionBlacklist, int socketTimeout,
+      boolean keepAlive) throws TTransportException, UnknownHostException {
+    TSSLTransportFactory.TSSLTransportParameters params = new TSSLTransportFactory.TSSLTransportParameters();
     params.setKeyStore(keyStorePath, keyStorePassWord);
     InetSocketAddress serverAddress;
     if (hiveHost == null || hiveHost.isEmpty()) {
@@ -219,8 +224,8 @@ public class HiveAuthFactory {
     } else {
       serverAddress = new InetSocketAddress(hiveHost, portNum);
     }
-    TServerSocket thriftServerSocket =
-        TSSLTransportFactory.getServerSocket(portNum, 0, serverAddress.getAddress(), params);
+    TServerSocket thriftServerSocket = TSSLTransportFactory.getServerSocket(portNum, socketTimeout,
+        serverAddress.getAddress(), params);
     if (thriftServerSocket.getServerSocket() instanceof SSLServerSocket) {
       List<String> sslVersionBlacklistLocal = new ArrayList<String>();
       for (String sslVersion : sslVersionBlacklist) {
@@ -238,6 +243,9 @@ public class HiveAuthFactory {
       sslServerSocket.setEnabledProtocols(enabledProtocols.toArray(new String[0]));
       LOG.info("SSL Server Socket Enabled Protocols: "
           + Arrays.toString(sslServerSocket.getEnabledProtocols()));
+    }
+    if (keepAlive) {
+      thriftServerSocket = new TServerSocketKeepAlive(thriftServerSocket.getServerSocket());
     }
     return thriftServerSocket;
   }
@@ -325,4 +333,25 @@ public class HiveAuthFactory {
     }
   }
 
+  /**
+   * TServerSocketKeepAlive - like TServerSocket, but will enable keepalive for
+   * accepted sockets.
+   * 
+   */
+  static class TServerSocketKeepAlive extends TServerSocket {
+    public TServerSocketKeepAlive(ServerSocket serverSocket) throws TTransportException {
+      super(serverSocket);
+    }
+
+    @Override
+    protected TSocket acceptImpl() throws TTransportException {
+      TSocket ts = super.acceptImpl();
+      try {
+        ts.getSocket().setKeepAlive(true);
+      } catch (SocketException e) {
+        throw new TTransportException(e);
+      }
+      return ts;
+    }
+  }
 }
