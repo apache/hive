1c437314dff569d31309c704335bba57777e83b3
Brock Noland <brock@apache.org>
2015-01-28 22:48:06 +0000

    Merge HIVE-9317 - Move Microsoft copyright notice from the source code to the NOTICE
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/branch-1.1@1655492 13f79535-47bb-0310-9956-ffa450edef68

--- common/src/java/org/apache/hadoop/hive/common/type/Decimal128.java
+++ common/src/java/org/apache/hadoop/hive/common/type/Decimal128.java
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) Microsoft Corporation
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.hadoop.hive.common.type;
 
 import java.math.BigDecimal;
@@ -23,7 +26,7 @@ import java.nio.IntBuffer;
 import org.apache.hive.common.util.Decimal128FastBuffer;
 
 /**
- * This code was originally written for Microsoft PolyBase.
+ * This code was based on code from Microsoft's PolyBase.
  * <p>
  * A 128-bit fixed-length Decimal value in the ANSI SQL Numeric semantics,
  * representing unscaledValue / 10**scale where scale is 0 or positive.
