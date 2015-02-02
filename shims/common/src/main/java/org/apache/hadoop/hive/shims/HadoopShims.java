032b28023890316785e12b437cabad365de51ea2
Xuefu Zhang <xuefu@apache.org>
2015-02-02 21:10:08 +0000

    HIVE-9211: Research on build mini HoS cluster on YARN for unit test[Spark Branch] (Chengxiang via Xuefu)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/spark@1656573 13f79535-47bb-0310-9956-ffa450edef68

--- shims/common/src/main/java/org/apache/hadoop/hive/shims/HadoopShims.java
+++ shims/common/src/main/java/org/apache/hadoop/hive/shims/HadoopShims.java
@@ -93,7 +93,10 @@ public interface HadoopShims {
       String nameNode, int numDir) throws IOException;
 
   public MiniMrShim getMiniTezCluster(Configuration conf, int numberOfTaskTrackers,
-                                     String nameNode, int numDir) throws IOException;
+      String nameNode, int numDir) throws IOException;
+
+  public MiniMrShim getMiniSparkCluster(Configuration conf, int numberOfTaskTrackers,
+      String nameNode, int numDir) throws IOException;
 
   /**
    * Shim for MiniMrCluster
