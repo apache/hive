14b596c0d4d796eb090f7155498b5453f2b585b4
Brock Noland <brock@apache.org>
2015-02-04 01:45:31 +0000

    HIVE-9554 - Rename 0.15 upgrade scripts to 1.1 (Brock reviewed by Alan Gates)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1656970 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/scripts/upgrade/postgres/hive-schema-1.2.0.postgres.sql
+++ metastore/scripts/upgrade/postgres/hive-schema-1.2.0.postgres.sql
@@ -1490,4 +1490,4 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 -- -----------------------------------------------------------------
 -- Record schema version. Should be the last step in the init script
 -- -----------------------------------------------------------------
-INSERT INTO "VERSION" ("VER_ID", "SCHEMA_VERSION", "VERSION_COMMENT") VALUES (1, '0.15.0', 'Hive release version 0.15.0');
+INSERT INTO "VERSION" ("VER_ID", "SCHEMA_VERSION", "VERSION_COMMENT") VALUES (1, '1.2.0', 'Hive release version 1.2.0');
