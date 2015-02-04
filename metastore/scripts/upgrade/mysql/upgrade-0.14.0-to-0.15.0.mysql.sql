862090a9151b8657ab7d64346927957e91271063
Brock Noland <brock@apache.org>
2015-02-04 01:46:45 +0000

    HIVE-9554 - Rename 0.15 upgrade scripts to 1.1 (Brock reviewed by Alan Gates)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/branch-1.1@1656971 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/scripts/upgrade/mysql/upgrade-0.14.0-to-0.15.0.mysql.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'Upgrading MetaStore schema from 0.14.0 to 0.15.0' AS ' ';
-
-SOURCE 020-HIVE-9296.mysql.sql;
-
-UPDATE VERSION SET SCHEMA_VERSION='0.14.0', VERSION_COMMENT='Hive release version 0.15.0' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 0.14.0 to 0.15.0' AS ' ';
-
