088d18bac9980be0069a78014d648ddedb614811
Jason Dere <jdere@apache.org>
2015-03-05 01:35:19 +0000

    HIVE-9817: fix DateFormat pattern in hive-exec (Alexander Pivovarov via Jason Dere)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1664197 13f79535-47bb-0310-9956-ffa450edef68

--- ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
@@ -2896,7 +2896,7 @@ public final class Utilities {
 
   public static String now() {
     Calendar cal = Calendar.getInstance();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     return sdf.format(cal.getTime());
   }
 
