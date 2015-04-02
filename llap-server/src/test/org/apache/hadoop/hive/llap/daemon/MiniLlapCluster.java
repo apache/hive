f40f72d301ff3967a13172aa15039c0f2891aaf1
Vikram Dixit K <vikram@apache.org>
2015-04-02 21:18:45 +0000

    HIVE-10201: Hive LLAP needs refactoring of the configuration class (Vikram Dixit K, reviewed by Siddharth Seth)
    git-svn-id: https://svn.apache.org/repos/asf/hive/branches/llap@1670973 13f79535-47bb-0310-9956-ffa450edef68

--- llap-server/src/test/org/apache/hadoop/hive/llap/daemon/MiniLlapCluster.java
+++ llap-server/src/test/org/apache/hadoop/hive/llap/daemon/MiniLlapCluster.java
@@ -24,6 +24,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileContext;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.llap.configuration.LlapConfiguration;
 import org.apache.hadoop.hive.llap.daemon.impl.LlapDaemon;
 import org.apache.hadoop.service.AbstractService;
 import org.apache.hadoop.service.Service;
@@ -127,16 +128,16 @@ public class MiniLlapCluster extends AbstractService {
   public void serviceStart() {
     llapDaemon.start();
 
-    clusterSpecificConfiguration.set(LlapDaemonConfiguration.LLAP_DAEMON_SERVICE_HOSTS,
+    clusterSpecificConfiguration.set(LlapConfiguration.LLAP_DAEMON_SERVICE_HOSTS,
         getServiceAddress().getHostName());
-    clusterSpecificConfiguration.setInt(LlapDaemonConfiguration.LLAP_DAEMON_RPC_PORT,
+    clusterSpecificConfiguration.setInt(LlapConfiguration.LLAP_DAEMON_RPC_PORT,
         getServiceAddress().getPort());
 
     clusterSpecificConfiguration.setInt(
-        LlapDaemonConfiguration.LLAP_DAEMON_NUM_EXECUTORS,
+        LlapConfiguration.LLAP_DAEMON_NUM_EXECUTORS,
         numExecutorsPerService);
     clusterSpecificConfiguration.setLong(
-        LlapDaemonConfiguration.LLAP_DAEMON_MEMORY_PER_INSTANCE_MB, execBytesPerService);
+        LlapConfiguration.LLAP_DAEMON_MEMORY_PER_INSTANCE_MB, execBytesPerService);
   }
 
   @Override
