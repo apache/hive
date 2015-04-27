aef763366b91a2ca8122c546d688b4c0e99c489e
Thejas Nair <thejas@hortonworks.com>
2015-04-27 13:38:32 -0700

    HIVE-10465 : whitelist restrictions don't get initialized in new copy of HiveConf (Thejas Nair, reviewed by Daniel Dai)

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -2566,6 +2566,7 @@ public class HiveConf extends Configuration {
     isSparkConfigUpdated = other.isSparkConfigUpdated;
     origProp = (Properties)other.origProp.clone();
     restrictList.addAll(other.restrictList);
+    modWhiteListPattern = other.modWhiteListPattern;
   }
 
   public Properties getAllProperties() {
