eca1cadb59db81e1258bf10926b21256101e3ecd
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-05-09 17:54:33 -0700

    HIVE-10591: Support limited integer type promotion in ORC (Prasanth Jayachandran reviewed by Gunther Hagleitner)

--- ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcRecordUpdater.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcRecordUpdater.java
@@ -17,7 +17,14 @@
  */
 package org.apache.hadoop.hive.ql.io.orc;
 
-import com.google.common.annotations.VisibleForTesting;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -37,14 +44,8 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.charset.CharacterCodingException;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Lists;
 
 /**
  * A RecordUpdater where the files are stored as ORC.
@@ -203,6 +204,11 @@ public class OrcRecordUpdater implements RecordUpdater {
         CURRENT_TRANSACTION));
     fields.add(new OrcStruct.Field("row", rowInspector, ROW));
     return new OrcStruct.OrcStructInspector(fields);
+  }
+
+  public static List<String> getAcidEventFields() {
+    return Lists.newArrayList("operation", "originalTransaction", "bucket", "rowId",
+        "currentTransaction", "row");
   }
 
   OrcRecordUpdater(Path path,
