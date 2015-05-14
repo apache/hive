d567cc445364ec04d816f5f1e50ed637d84666ba
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-05-13 22:39:42 -0700

    HIVE-10662: LLAP: Wait queue pre-emption (Prasanth Jayachandran)

--- llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/TestTaskExecutorService.java
+++ llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/TestTaskExecutorService.java
@@ -18,11 +18,11 @@
 package org.apache.hadoop.hive.llap.daemon.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.RejectedExecutionException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.llap.daemon.KilledTaskHandler;
@@ -39,9 +39,6 @@ import org.apache.tez.dag.records.TezVertexID;
 import org.apache.tez.runtime.api.impl.ExecutionContextImpl;
 import org.apache.tez.runtime.task.EndReason;
 import org.apache.tez.runtime.task.TaskRunner2Result;
-import org.apache.tez.runtime.task.TezChild;
-import org.apache.tez.runtime.task.TezChild.ContainerExecutionResult;
-import org.apache.tez.runtime.task.TezChild.ContainerExecutionResult.ExitStatus;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -62,9 +59,13 @@ public class TestTaskExecutorService {
     }
 
     @Override
-    protected TaskRunner2Result callInternal() throws Exception {
+    protected TaskRunner2Result callInternal() {
       System.out.println(requestId + " is executing..");
-      Thread.sleep(workTime);
+      try {
+        Thread.sleep(workTime);
+      } catch (InterruptedException e) {
+        return new TaskRunner2Result(EndReason.KILL_REQUESTED, null, false);
+      }
       return new TaskRunner2Result(EndReason.SUCCESS, null, false);
     }
 
@@ -102,62 +103,7 @@ public class TestTaskExecutorService {
         .setContainerIdString("MockContainer_1").setUser("MockUser")
         .setTokenIdentifier("MockToken_1").build();
   }
-
-
-  @Test(expected = RejectedExecutionException.class)
-  public void testThreadPoolRejection() throws InterruptedException {
-    TaskExecutorService scheduler = new TaskExecutorService(2, 2, false);
-    scheduler.schedule(new MockRequest(createRequest(1, 4), true, 1000));
-    Thread.sleep(100);
-    scheduler.schedule(new MockRequest(createRequest(2, 4), true, 1000));
-    Thread.sleep(100);
-    assertEquals(0, scheduler.getPreemptionListSize());
-    scheduler.schedule(new MockRequest(createRequest(3, 4), true, 1000));
-    Thread.sleep(100);
-    scheduler.schedule(new MockRequest(createRequest(4, 4), true, 1000));
-    Thread.sleep(100);
-    assertEquals(0, scheduler.getPreemptionListSize());
-    // this request should be rejected
-    scheduler.schedule(new MockRequest(createRequest(5, 8), true, 1000));
-  }
-
-  @Test
-  public void testPreemption() throws InterruptedException {
-    TaskExecutorService scheduler = new TaskExecutorService(2, 2, true);
-    scheduler.schedule(new MockRequest(createRequest(1, 4), false, 100000));
-    Thread.sleep(100);
-    scheduler.schedule(new MockRequest(createRequest(2, 4), false, 100000));
-    Thread.sleep(100);
-    assertEquals(2, scheduler.getPreemptionListSize());
-    // these should invoke preemption
-    scheduler.schedule(new MockRequest(createRequest(3, 8), true, 1000));
-    Thread.sleep(100);
-    scheduler.schedule(new MockRequest(createRequest(4, 8), true, 1000));
-    Thread.sleep(100);
-    assertEquals(0, scheduler.getPreemptionListSize());
-  }
-
-  @Test
-  public void testPreemptionOrder() throws InterruptedException {
-    TaskExecutorService scheduler = new TaskExecutorService(2, 2, true);
-    MockRequest r1 = new MockRequest(createRequest(1, 4), false, 100000);
-    scheduler.schedule(r1);
-    Thread.sleep(100);
-    MockRequest r2 = new MockRequest(createRequest(2, 4), false, 100000);
-    scheduler.schedule(r2);
-    Thread.sleep(100);
-    assertEquals(r1, scheduler.getPreemptionTask());
-    // these should invoke preemption
-    scheduler.schedule(new MockRequest(createRequest(3, 8), true, 1000));
-    // wait till pre-emption to kick-in and complete
-    Thread.sleep(100);
-    assertEquals(r2, scheduler.getPreemptionTask());
-    scheduler.schedule(new MockRequest(createRequest(4, 8), true, 1000));
-    // wait till pre-emption to kick-in and complete
-    Thread.sleep(100);
-    assertEquals(0, scheduler.getPreemptionListSize());
-  }
-
+  
   @Test
   public void testWaitQueueComparator() throws InterruptedException {
     MockRequest r1 = new MockRequest(createRequest(1, 2), false, 100000);
@@ -165,17 +111,18 @@ public class TestTaskExecutorService {
     MockRequest r3 = new MockRequest(createRequest(3, 6), false, 1000000);
     MockRequest r4 = new MockRequest(createRequest(4, 8), false, 1000000);
     MockRequest r5 = new MockRequest(createRequest(5, 10), false, 1000000);
-    BlockingQueue queue = new BoundedPriorityBlockingQueue(
+    EvictingPriorityBlockingQueue queue = new EvictingPriorityBlockingQueue(
         new TaskExecutorService.WaitQueueComparator(), 4);
-    queue.offer(r1);
+    assertNull(queue.offer(r1));
     assertEquals(r1, queue.peek());
-    queue.offer(r2);
+    assertNull(queue.offer(r2));
     assertEquals(r1, queue.peek());
-    queue.offer(r3);
+    assertNull(queue.offer(r3));
     assertEquals(r1, queue.peek());
-    queue.offer(r4);
+    assertNull(queue.offer(r4));
     assertEquals(r1, queue.peek());
-    assertEquals(false, queue.offer(r5));
+    // this offer will be rejected
+    assertEquals(r5, queue.offer(r5));
     assertEquals(r1, queue.take());
     assertEquals(r2, queue.take());
     assertEquals(r3, queue.take());
@@ -186,17 +133,18 @@ public class TestTaskExecutorService {
     r3 = new MockRequest(createRequest(3, 6), true, 1000000);
     r4 = new MockRequest(createRequest(4, 8), true, 1000000);
     r5 = new MockRequest(createRequest(5, 10), true, 1000000);
-    queue = new BoundedPriorityBlockingQueue(
+    queue = new EvictingPriorityBlockingQueue(
         new TaskExecutorService.WaitQueueComparator(), 4);
-    queue.offer(r1);
+    assertNull(queue.offer(r1));
     assertEquals(r1, queue.peek());
-    queue.offer(r2);
+    assertNull(queue.offer(r2));
     assertEquals(r1, queue.peek());
-    queue.offer(r3);
+    assertNull(queue.offer(r3));
     assertEquals(r1, queue.peek());
-    queue.offer(r4);
+    assertNull(queue.offer(r4));
     assertEquals(r1, queue.peek());
-    assertEquals(false, queue.offer(r5));
+    // this offer will be rejected
+    assertEquals(r5, queue.offer(r5));
     assertEquals(r1, queue.take());
     assertEquals(r2, queue.take());
     assertEquals(r3, queue.take());
@@ -207,84 +155,90 @@ public class TestTaskExecutorService {
     r3 = new MockRequest(createRequest(3, 1), true, 1000000);
     r4 = new MockRequest(createRequest(4, 1), false, 1000000);
     r5 = new MockRequest(createRequest(5, 10), true, 1000000);
-    queue = new BoundedPriorityBlockingQueue(
+    queue = new EvictingPriorityBlockingQueue(
         new TaskExecutorService.WaitQueueComparator(), 4);
-    queue.offer(r1);
+    assertNull(queue.offer(r1));
     assertEquals(r1, queue.peek());
-    queue.offer(r2);
+    assertNull(queue.offer(r2));
     assertEquals(r1, queue.peek());
-    queue.offer(r3);
-    assertEquals(r1, queue.peek());
-    queue.offer(r4);
-    assertEquals(r1, queue.peek());
-    assertEquals(false, queue.offer(r5));
-    assertEquals(r1, queue.take());
+    assertNull(queue.offer(r3));
+    // same priority with r1
+    assertEquals(r3, queue.peek());
+    // same priority with r2
+    assertNull(queue.offer(r4));
+    assertEquals(r3, queue.peek());
+    // offer accepted and r2 gets evicted
+    assertEquals(r2, queue.offer(r5));
     assertEquals(r3, queue.take());
+    assertEquals(r1, queue.take());
+    assertEquals(r5, queue.take());
     assertEquals(r4, queue.take());
-    assertEquals(r2, queue.take());
 
     r1 = new MockRequest(createRequest(1, 2), true, 100000);
     r2 = new MockRequest(createRequest(2, 4), false, 100000);
     r3 = new MockRequest(createRequest(3, 6), true, 1000000);
     r4 = new MockRequest(createRequest(4, 8), false, 1000000);
     r5 = new MockRequest(createRequest(5, 10), true, 1000000);
-    queue = new BoundedPriorityBlockingQueue(
+    queue = new EvictingPriorityBlockingQueue(
         new TaskExecutorService.WaitQueueComparator(), 4);
-    queue.offer(r1);
+    assertNull(queue.offer(r1));
     assertEquals(r1, queue.peek());
-    queue.offer(r2);
+    assertNull(queue.offer(r2));
     assertEquals(r1, queue.peek());
-    queue.offer(r3);
+    assertNull(queue.offer(r3));
     assertEquals(r1, queue.peek());
-    queue.offer(r4);
+    assertNull(queue.offer(r4));
     assertEquals(r1, queue.peek());
-    assertEquals(false, queue.offer(r5));
+    // offer accepted and r4 gets evicted
+    assertEquals(r4, queue.offer(r5));
     assertEquals(r1, queue.take());
     assertEquals(r3, queue.take());
+    assertEquals(r5, queue.take());
     assertEquals(r2, queue.take());
-    assertEquals(r4, queue.take());
 
     r1 = new MockRequest(createRequest(1, 2), true, 100000);
     r2 = new MockRequest(createRequest(2, 4), false, 100000);
     r3 = new MockRequest(createRequest(3, 6), false, 1000000);
     r4 = new MockRequest(createRequest(4, 8), false, 1000000);
     r5 = new MockRequest(createRequest(5, 10), true, 1000000);
-    queue = new BoundedPriorityBlockingQueue(
+    queue = new EvictingPriorityBlockingQueue(
         new TaskExecutorService.WaitQueueComparator(), 4);
-    queue.offer(r1);
+    assertNull(queue.offer(r1));
     assertEquals(r1, queue.peek());
-    queue.offer(r2);
+    assertNull(queue.offer(r2));
     assertEquals(r1, queue.peek());
-    queue.offer(r3);
+    assertNull(queue.offer(r3));
     assertEquals(r1, queue.peek());
-    queue.offer(r4);
+    assertNull(queue.offer(r4));
     assertEquals(r1, queue.peek());
-    assertEquals(false, queue.offer(r5));
+    // offer accepted and r4 gets evicted
+    assertEquals(r4, queue.offer(r5));
     assertEquals(r1, queue.take());
+    assertEquals(r5, queue.take());
     assertEquals(r2, queue.take());
     assertEquals(r3, queue.take());
-    assertEquals(r4, queue.take());
 
     r1 = new MockRequest(createRequest(1, 2), false, 100000);
     r2 = new MockRequest(createRequest(2, 4), true, 100000);
     r3 = new MockRequest(createRequest(3, 6), true, 1000000);
     r4 = new MockRequest(createRequest(4, 8), true, 1000000);
     r5 = new MockRequest(createRequest(5, 10), true, 1000000);
-    queue = new BoundedPriorityBlockingQueue(
+    queue = new EvictingPriorityBlockingQueue(
         new TaskExecutorService.WaitQueueComparator(), 4);
-    queue.offer(r1);
+    assertNull(queue.offer(r1));
     assertEquals(r1, queue.peek());
-    queue.offer(r2);
+    assertNull(queue.offer(r2));
     assertEquals(r2, queue.peek());
-    queue.offer(r3);
+    assertNull(queue.offer(r3));
     assertEquals(r2, queue.peek());
-    queue.offer(r4);
+    assertNull(queue.offer(r4));
     assertEquals(r2, queue.peek());
-    assertEquals(false, queue.offer(r5));
+    // offer accepted, r1 evicted
+    assertEquals(r1, queue.offer(r5));
     assertEquals(r2, queue.take());
     assertEquals(r3, queue.take());
     assertEquals(r4, queue.take());
-    assertEquals(r1, queue.take());
+    assertEquals(r5, queue.take());
   }
 
   @Test
