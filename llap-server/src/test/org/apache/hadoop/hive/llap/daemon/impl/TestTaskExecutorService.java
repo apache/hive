0fa35f932253c0e5302ada69dfb61c7c8a863e89
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-05-19 19:02:04 -0700

    HIVE-10764: LLAP: Wait queue scheduler goes into tight loop (Prasanth Jayachandran)

--- llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/TestTaskExecutorService.java
+++ llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/TestTaskExecutorService.java
@@ -60,7 +60,7 @@ public class TestTaskExecutorService {
 
     @Override
     protected TaskRunner2Result callInternal() {
-      System.out.println(requestId + " is executing..");
+      System.out.println(super.toString() + " is executing..");
       try {
         Thread.sleep(workTime);
       } catch (InterruptedException e) {
