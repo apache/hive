1d84c5f69758702d694f4c6a2ec249612eaa7f57
Eugene Koifman <ekoifman@hortonworks.com>
2015-05-20 10:05:31 -0700

    HIVE-10629 Dropping table in an encrypted zone does not drop warehouse directory(Eugene Koifman, reviewed by Sergio Pena)
    Conflicts:
    	metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
    	ql/src/test/results/clientpositive/encrypted/encryption_insert_partition_static.q.out

--- shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
+++ shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
@@ -1192,6 +1192,9 @@ public class Hadoop23Shims extends HadoopShimsSecure {
       } else {
         fullPath = path.getFileSystem(conf).makeQualified(path);
       }
+      if(!"hdfs".equalsIgnoreCase(path.toUri().getScheme())) {
+        return false;
+      }
       return (hdfsAdmin.getEncryptionZoneForPath(fullPath) != null);
     }
 
