120e33aa56f1a294bf6d2ad0cc7278272cf519b7
Ferdinand Xu <cheng.a.xu@intel.com>
2015-06-03 18:13:38 -0400

    HIVE-10904 Use beeline-log4j.properties for migrated CLI [beeline-cli Branch] (Chinna Rao Lalam, reviewed by Ferdinand Xu)

--- bin/ext/cli.sh
+++ bin/ext/cli.sh
@@ -21,6 +21,7 @@ updateCli() {
     CLASS=org.apache.hadoop.hive.cli.CliDriver
     JAR=hive-cli-*.jar
   else
+    export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Dlog4j.configuration=beeline-log4j.properties"
     CLASS=org.apache.hive.beeline.cli.HiveCli
     JAR=hive-beeline-*.jar
   fi
