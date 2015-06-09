1d77cb09f5003342c8eeed2a9ce48ecc6d696540
Thejas Nair <thejas@hortonworks.com>
2015-06-09 14:11:35 -0700

    HIVE-10967 : add mapreduce.job.tags to sql std authorization config whitelist (Thejas M Nair, reviewed by Jason Dere)

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -2914,6 +2914,7 @@ public class HiveConf extends Configuration {
     "mapred\\.min\\.split\\.size",
     "mapreduce\\.job\\.reduce\\.slowstart\\.completedmaps",
     "mapreduce\\.job\\.queuename",
+    "mapreduce\\.job\\.tags",
     "mapreduce\\.input\\.fileinputformat\\.split\\.minsize",
     "mapreduce\\.map\\..*",
     "mapreduce\\.reduce\\..*",
@@ -2923,6 +2924,7 @@ public class HiveConf extends Configuration {
     "tez\\.task\\..*",
     "tez\\.runtime\\..*",
     "tez.queue.name",
+
   };
 
 
