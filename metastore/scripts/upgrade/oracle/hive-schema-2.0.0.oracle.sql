670f57c188b6f02f2c7f55b758ff403501058285
Ferdinand Xu <cheng.a.xu@intel.com>
2015-06-14 13:44:40 -0400

    HIVE-10979: Fix failed tests in TestSchemaTool after the version number change in HIVE-10921(Ferdinand Xu, reviewed by Sergey Shelukhin)

--- metastore/scripts/upgrade/oracle/hive-schema-2.0.0.oracle.sql
+++ metastore/scripts/upgrade/oracle/hive-schema-2.0.0.oracle.sql
@@ -785,4 +785,4 @@ CREATE INDEX FUNC_RU_N49 ON FUNC_RU (FUNC_ID);
 -- -----------------------------------------------------------------
 -- Record schema version. Should be the last step in the init script
 -- -----------------------------------------------------------------
-INSERT INTO VERSION (VER_ID, SCHEMA_VERSION, VERSION_COMMENT) VALUES (1, '1.3.0', 'Hive release version 1.3.0');
+INSERT INTO VERSION (VER_ID, SCHEMA_VERSION, VERSION_COMMENT) VALUES (1, '2.0.0', 'Hive release version 2.0.0');
