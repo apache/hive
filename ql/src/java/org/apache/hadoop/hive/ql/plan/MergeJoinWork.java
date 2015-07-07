63b31c991350eb791a80ca34964d87d6e7de6ea2
Sergey Shelukhin <sershe@apache.org>
2015-07-07 14:46:58 -0700

    HIVE-11013 : MiniTez tez_join_hash test on the branch fails with NPE (initializeOp not called?) (Sergey Shelukhin, reviewed by Vikram Dixit K)

--- ql/src/java/org/apache/hadoop/hive/ql/plan/MergeJoinWork.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/MergeJoinWork.java
@@ -136,6 +136,11 @@ public class MergeJoinWork extends BaseWork {
   }
 
   @Override
+  public List<HashTableDummyOperator> getDummyOps() {
+    return getMainWork().getDummyOps();
+  }
+
+  @Override
   public void addDummyOp(HashTableDummyOperator dummyOp) {
     getMainWork().addDummyOp(dummyOp);
   }
