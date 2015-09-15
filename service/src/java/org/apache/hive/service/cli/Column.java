0c110bf92c1f0b9edaf243fd18b772ee0706c1f9
Sergio Pena <sergio.pena@cloudera.com>
2015-09-15 10:40:45 -0500

    HIVE-11802: Float-point numbers are displayed with different precision in Beeline/JDBC (Sergio Pena, reviewed by Carl Steinbach)

--- service/src/java/org/apache/hive/service/cli/Column.java
+++ service/src/java/org/apache/hive/service/cli/Column.java
@@ -40,6 +40,7 @@ import org.apache.hive.service.cli.thrift.TI16Column;
 import org.apache.hive.service.cli.thrift.TI32Column;
 import org.apache.hive.service.cli.thrift.TI64Column;
 import org.apache.hive.service.cli.thrift.TStringColumn;
+import sun.misc.FloatingDecimal;
 
 /**
  * Column.
@@ -349,7 +350,7 @@ public class Column extends AbstractList {
         break;
       case FLOAT_TYPE:
         nulls.set(size, field == null);
-        doubleVars()[size] = field == null ? 0 : ((Float)field).doubleValue();
+        doubleVars()[size] = field == null ? 0 : new FloatingDecimal((Float)field).doubleValue();
         break;
       case DOUBLE_TYPE:
         nulls.set(size, field == null);
