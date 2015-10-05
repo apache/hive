43ea568dee4ff2b91f1e4d6c8262c00a0916fe20
Xuefu Zhang <xzhang@Cloudera.com>
2015-10-05 05:42:20 -0700

    HIVE-11835: Type decimal(1,1) reads 0.0, 0.00, etc from text file as NULL (Reviewed by Szehon)

--- common/src/java/org/apache/hadoop/hive/common/type/HiveDecimal.java
+++ common/src/java/org/apache/hadoop/hive/common/type/HiveDecimal.java
@@ -271,9 +271,17 @@ public class HiveDecimal implements Comparable<HiveDecimal> {
     return bd;
   }
 
-  public static BigDecimal enforcePrecisionScale(BigDecimal bd, int maxPrecision, int maxScale) {
+  private static BigDecimal enforcePrecisionScale(BigDecimal bd, int maxPrecision, int maxScale) {
     if (bd == null) {
       return null;
+    }
+
+    /**
+     * Specially handling the case that bd=0, and we are converting it to a type where precision=scale,
+     * such as decimal(1, 1).
+     */
+    if (bd.compareTo(BigDecimal.ZERO) == 0 && bd.scale() == 0 && maxPrecision == maxScale) {
+      return bd.setScale(maxScale);
     }
 
     bd = trim(bd);
