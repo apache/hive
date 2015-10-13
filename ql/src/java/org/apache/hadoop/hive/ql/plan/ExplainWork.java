fb4622af8e7bc7620a534757da6278a66930bdc1
Gopal V <gopalv@apache.org>
2015-10-13 16:17:15 -0700

    HIVE-11578: Fix NPE in ExplainWork (Rajesh Balamohan, via Gopal V)

--- ql/src/java/org/apache/hadoop/hive/ql/plan/ExplainWork.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/ExplainWork.java
@@ -77,7 +77,9 @@ public class ExplainWork implements Serializable {
     this.fetchTask = fetchTask;
     this.astStringTree = astStringTree;
     this.analyzer = analyzer;
-    this.inputs = analyzer.getInputs();
+    if (analyzer != null) {
+      this.inputs = analyzer.getInputs();
+    }
     this.extended = extended;
     this.formatted = formatted;
     this.dependency = dependency;
