ab7794c0255cb2803eac7d9b92fb60567d7ac867
Chen Xin Yu <qihanycx@126.com>
2015-10-25 20:19:00 -0800

    HIVE-12263 : Hive SchemaTool does not tolerate leading spaces in JDBC url (Chen Xin Yu via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- beeline/src/java/org/apache/hive/beeline/HiveSchemaHelper.java
+++ beeline/src/java/org/apache/hive/beeline/HiveSchemaHelper.java
@@ -86,7 +86,7 @@ public class HiveSchemaHelper {
     if (confVarStr == null || confVarStr.isEmpty()) {
       throw new IOException("Empty " + confVar.varname);
     }
-    return confVarStr;
+    return confVarStr.trim();
   }
 
   public interface NestedScriptParser {
