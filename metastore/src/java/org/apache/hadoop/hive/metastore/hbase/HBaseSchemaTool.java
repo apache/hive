23932d97a7937d0982d7dc05c95a35725d6df4b1
Alan Gates <gates@hortonworks.com>
2015-10-28 12:38:16 -0700

    HIVE-11564 HBaseSchemaTool should be able to list objects (gates reviewed by Daniel Dai)

--- metastore/src/java/org/apache/hadoop/hive/metastore/hbase/HBaseSchemaTool.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/hbase/HBaseSchemaTool.java
@@ -18,27 +18,20 @@
  */
 package org.apache.hadoop.hive.metastore.hbase;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.metastore.api.ColumnStatistics;
-import org.apache.hadoop.hive.metastore.api.Database;
-import org.apache.hadoop.hive.metastore.api.Function;
-import org.apache.hadoop.hive.metastore.api.Partition;
-import org.apache.hadoop.hive.metastore.api.Role;
-import org.apache.hadoop.hive.metastore.api.Table;
-import org.apache.thrift.TBase;
-import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.protocol.TSimpleJSONProtocol;
-import org.apache.thrift.transport.TMemoryBuffer;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -47,29 +40,10 @@ import java.util.List;
  */
 public class HBaseSchemaTool {
 
-  private static String[] commands = {"db", "part", "parts", "role", "table", "function",
-                                      "install"};
+  static final private Log LOG = LogFactory.getLog(HBaseReadWrite.class.getName());
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     Options options = new Options();
-
-    options.addOption(OptionBuilder
-        .withLongOpt("column")
-        .withDescription("Comma separated list of column names")
-        .hasArg()
-        .create('c'));
-
-    options.addOption(OptionBuilder
-        .withLongOpt("db")
-        .withDescription("Database name")
-        .hasArg()
-        .create('d'));
-
-    options.addOption(OptionBuilder
-        .withLongOpt("function")
-        .withDescription("Function name")
-        .hasArg()
-        .create('f'));
 
     options.addOption(OptionBuilder
         .withLongOpt("help")
@@ -77,164 +51,146 @@ public class HBaseSchemaTool {
         .create('h'));
 
     options.addOption(OptionBuilder
-        .withLongOpt("role")
-        .withDescription("Role name")
+        .withLongOpt("install")
+        .withDescription("Install the schema onto an HBase cluster.")
+        .create('i'));
+
+    options.addOption(OptionBuilder
+        .withLongOpt("key")
+        .withDescription("Key to scan with.  This should be an exact key (not a regular expression")
+        .hasArg()
+        .create('k'));
+
+    options.addOption(OptionBuilder
+        .withLongOpt("list-tables")
+        .withDescription("List tables in HBase metastore")
+        .create('l'));
+
+    options.addOption(OptionBuilder
+        .withLongOpt("regex-key")
+        .withDescription("Regular expression to scan keys with.")
         .hasArg()
         .create('r'));
 
     options.addOption(OptionBuilder
-        .withLongOpt("partvals")
-        .withDescription("Comma separated list of partition values, in order of partition columns")
-        .hasArg()
-        .create('p'));
-
-    options.addOption(OptionBuilder
-        .withLongOpt("stats")
-        .withDescription("Get statistics rather than catalog object")
-        .create('s'));
-
-    options.addOption(OptionBuilder
         .withLongOpt("table")
-        .withDescription("Table name")
+        .withDescription("HBase metastore table to scan")
         .hasArg()
         .create('t'));
 
-    CommandLine cli = new GnuParser().parse(options, args);
-
-    if (cli.hasOption('h')) {
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.printHelp("hbaseschematool", options);
+    CommandLine cli = null;
+    try {
+      cli = new GnuParser().parse(options, args);
+    } catch (ParseException e) {
+      System.err.println("Parse Exception: " + e.getMessage());
+      usage(options);
       return;
     }
 
-    String[] cmds = cli.getArgs();
-    if (cmds.length != 1) {
-      System.err.print("Must include a cmd, valid cmds are: ");
-      for (int i = 0; i < commands.length; i++) {
-        if (i != 0) System.err.print(", ");
-        System.err.print(commands[i]);
-      }
-      System.err.println();
-      System.exit(1);
-    }
-    String cmd = cmds[0];
-
-    List<String> parts = null;
-    if (cli.hasOption('p')) {
-      parts = Arrays.asList(cli.getOptionValue('p').split(","));
+    if (cli.hasOption('h')) {
+      usage(options);
+      return;
     }
 
-    List<String> cols = null;
-    if (cli.hasOption('c')) {
-      cols = Arrays.asList(cli.getOptionValue('c').split(","));
+    Configuration conf = new Configuration();
+
+    if (cli.hasOption('i')) {
+      new HBaseSchemaTool().install(conf, System.err);
+      return;
     }
 
-    HBaseSchemaTool tool = new HBaseSchemaTool(cli.getOptionValue('d'), cli.getOptionValue('t'),
-        parts, cli.getOptionValue('f'), cli.getOptionValue('r'), cols, cli.hasOption('s'));
-    Method method = tool.getClass().getMethod(cmd);
-    method.invoke(tool);
+    String key = null;
+    if (cli.hasOption('k')) key = cli.getOptionValue('k');
+    String regex = null;
+    if (cli.hasOption('r')) regex = cli.getOptionValue('r');
+    if (key != null && regex != null) {
+      usage(options);
+      return;
+    }
+    if (key == null && regex == null) regex = ".*";
 
-
+    // I do this in the object rather than in the static main so that it's easier to test.
+    new HBaseSchemaTool().go(cli.hasOption('l'), cli.getOptionValue('t'), key, regex, conf,
+        System.out, System.err);
   }
 
-  private HBaseReadWrite hrw;
-  private String dbName;
-  private String funcName;
-  private String tableName;
-  private List<String> partVals;
-  private String roleName;
-  private List<String> colNames;
-  private boolean hasStats;
-
-  private HBaseSchemaTool(String dbname, String tn, List<String> pv, String fn, String rn,
-                          List<String> cn, boolean s) {
-    dbName = dbname;
-    tableName = tn;
-    partVals = pv;
-    funcName = fn;
-    roleName = rn;
-    colNames = cn;
-    hasStats = s;
-    HBaseReadWrite.setConf(new Configuration());
-    hrw = HBaseReadWrite.getInstance();
+  private static void usage(Options options) {
+    HelpFormatter formatter = new HelpFormatter();
+    String header = "This tool dumps contents of your hbase metastore.  You need to specify\n" +
+        "the table to dump.  You can optionally specify a regular expression on the key for\n" +
+        "the table.  Keep in mind that the key is often a compound.  For partitions regular\n" +
+        "expressions are not used because non-string values are\nstored in binary.  Instead for " +
+        "partition you can specify as much of the exact prefix as you want.  So you can give " +
+        "dbname.tablename or dbname.tablename.pval1...";
+    String footer = "If neither key or regex is provided a regex of .* will be assumed.  You\n" +
+        "cannot set both key and regex.";
+    formatter.printHelp("hbaseschematool", header, options, footer);
+    return;
   }
 
-  public void db() throws IOException, TException {
-    Database db = hrw.getDb(dbName);
-    if (db == null) System.err.println("No such database: " + db);
-    else dump(db);
-  }
-
-  public void install() throws IOException {
-    HBaseReadWrite.createTablesIfNotExist();
-  }
-
-  public void part() throws IOException, TException {
-    if (hasStats) {
-      Table table = hrw.getTable(dbName, tableName);
-      if (table == null) {
-        System.err.println("No such table: " + dbName + "." + tableName);
+  @VisibleForTesting void go(boolean listTables, String table, String key, String regex,
+                             Configuration conf, PrintStream out, PrintStream err) {
+    List<String> lines = new ArrayList<>();
+    if (listTables) {
+      lines = Arrays.asList(HBaseReadWrite.tableNames);
+    } else {
+      // If they've used '.' as a key separator we need to replace it with the separator used by
+      // HBaseUtils
+      if (key != null) key = key.replace('.', HBaseUtils.KEY_SEPARATOR);
+      try {
+        HBaseReadWrite.setConf(conf);
+        HBaseReadWrite hrw = HBaseReadWrite.getInstance();
+        if (table.equalsIgnoreCase(HBaseReadWrite.DB_TABLE)) {
+          if (key != null) lines.add(hrw.printDatabase(key));
+          else lines.addAll(hrw.printDatabases(regex));
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.FUNC_TABLE)) {
+          if (key != null) lines.add(hrw.printFunction(key));
+          else lines.addAll(hrw.printFunctions(regex));
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.GLOBAL_PRIVS_TABLE)) {
+          // Ignore whatever they passed, there's always only either one or zero global privileges
+          lines.add(hrw.printGlobalPrivs());
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.PART_TABLE)) {
+          if (key != null) lines.add(hrw.printPartition(key));
+          else lines.addAll(hrw.printPartitions(regex));
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.USER_TO_ROLE_TABLE)) {
+          if (key != null) lines.add(hrw.printRolesForUser(key));
+          else lines.addAll(hrw.printRolesForUsers(regex));
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.ROLE_TABLE)) {
+          if (key != null) lines.add(hrw.printRole(key));
+          else lines.addAll(hrw.printRoles(regex));
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.TABLE_TABLE)) {
+          if (key != null) lines.add(hrw.printTable(key));
+          else lines.addAll(hrw.printTables(regex));
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.SD_TABLE)) {
+          if (key != null) lines.add(hrw.printStorageDescriptor(Base64.decodeBase64(key)));
+          else lines.addAll(hrw.printStorageDescriptors());
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.SECURITY_TABLE)) {
+          // We always print all of security, we don't worry about finding particular entries.
+          lines.addAll(hrw.printSecurity());
+        } else if (table.equalsIgnoreCase(HBaseReadWrite.SEQUENCES_TABLE)) {
+          // We always print all of sequences, we don't worry about finding particular entries.
+          lines.addAll(hrw.printSequences());
+        } else {
+          err.println("Unknown table: " + table);
+          return;
+        }
+      } catch (Exception e) {
+        err.println("Caught exception " + e.getClass() + " with message: " + e.getMessage());
         return;
       }
-      String partName = HBaseStore.buildExternalPartName(table, partVals);
-      List<ColumnStatistics> stats = hrw.getPartitionStatistics(dbName, tableName,
-          Arrays.asList(partName), Arrays.asList(partVals), colNames);
-      if (stats == null) {
-        System.err.println("No stats for " + dbName + "." + tableName + "." +
-            StringUtils.join(partVals, ':'));
-      } else {
-        for (ColumnStatistics stat : stats) dump(stat);
-      }
-    } else {
-      Partition part = hrw.getPartition(dbName, tableName, partVals);
-      if (part == null) {
-        System.err.println("No such partition: " + dbName + "." + tableName + "." +
-            StringUtils.join(partVals, ':'));
-      } else {
-        dump(part);
-      }
+    }
+    for (String line : lines) out.println(line);
+  }
+
+  @VisibleForTesting void install(Configuration conf, PrintStream err) {
+    try {
+      // We need to set the conf because createTablesIfNotExist will get a thread local version
+      // which requires that the configuration object be set.
+      HBaseReadWrite.setConf(conf);
+      HBaseReadWrite.createTablesIfNotExist();
+    } catch (Exception e) {
+      err.println("Caught exception " + e.getClass() + " with message: " + e.getMessage());
+      return;
     }
   }
-
-  public void parts() throws IOException, TException {
-    List<Partition> parts = hrw.scanPartitionsInTable(dbName, tableName, -1);
-    if (parts == null) {
-      System.err.println("No such table: " + dbName + "." + tableName);
-    } else {
-      for (Partition p : parts) dump(p);
-    }
-  }
-
-  public void role() throws IOException, TException {
-    Role role = hrw.getRole(roleName);
-    if (role == null) System.err.println("No such role: " + roleName);
-    else dump(role);
-  }
-
-  public void table() throws IOException, TException {
-    if (hasStats) {
-      ColumnStatistics stats = hrw.getTableStatistics(dbName, tableName, colNames);
-      if (stats == null) System.err.println("No stats for " + dbName + "." + tableName);
-      else dump(stats);
-    } else {
-      Table table = hrw.getTable(dbName, tableName);
-      if (table == null) System.err.println("No such table: " + dbName + "." + tableName);
-      else dump(table);
-    }
-  }
-
-  public void function() throws IOException, TException {
-    Function func = hrw.getFunction(dbName, funcName);
-    if (func == null) System.err.println("No such function: " + dbName + "." + funcName);
-    else dump(func);
-  }
-
-  private void dump(TBase thriftObj) throws TException {
-    TMemoryBuffer buf = new TMemoryBuffer(1000);
-    TProtocol protocol = new TSimpleJSONProtocol(buf);
-    thriftObj.write(protocol);
-    System.out.println(new String(buf.getArray()));
-  }
-
-
 }
