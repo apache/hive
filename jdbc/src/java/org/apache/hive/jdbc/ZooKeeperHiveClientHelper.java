de1fe68b90ff9e29a21095035c7ed02dbbf35f26
Alan Gates <gates@hortonworks.com>
2015-11-02 16:01:03 -0800

    HIVE-11966 JDBC Driver parsing error when reading principal from ZooKeeper (Vaibhav Gumashta via Alan Gates)

--- jdbc/src/java/org/apache/hive/jdbc/ZooKeeperHiveClientHelper.java
+++ jdbc/src/java/org/apache/hive/jdbc/ZooKeeperHiveClientHelper.java
@@ -159,7 +159,7 @@ class ZooKeeperHiveClientHelper {
         }
         // KERBEROS
         // If delegation token is passed from the client side, do not set the principal
-        if (matcher.group(2).equalsIgnoreCase("hive.server2.authentication.kerberos.principal")
+        if (matcher.group(1).equalsIgnoreCase("hive.server2.authentication.kerberos.principal")
             && !(connParams.getSessionVars().containsKey(JdbcConnectionParams.AUTH_TYPE) && connParams
                 .getSessionVars().get(JdbcConnectionParams.AUTH_TYPE)
                 .equalsIgnoreCase(JdbcConnectionParams.AUTH_TOKEN))
