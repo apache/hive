7f4379ab0d9afc8ab305e7d5151984882b95cdcc
Ashutosh Chauhan <hashutosh@apache.org>
2015-11-17 10:46:45 -0800

    HIVE-12436 : Default hive.metastore.schema.verification to true (Ashutosh Chauhan via Sushanth Sowmyan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -540,7 +540,7 @@ public class HiveConf extends Configuration {
     METASTORE_AUTO_CREATE_SCHEMA("datanucleus.autoCreateSchema", false,
         "creates necessary schema on a startup if one doesn't exist. set this to false, after creating it once"),
     METASTORE_FIXED_DATASTORE("datanucleus.fixedDatastore", true, "Dictates whether to allow updates to schema or not."),
-    METASTORE_SCHEMA_VERIFICATION("hive.metastore.schema.verification", false,
+    METASTORE_SCHEMA_VERIFICATION("hive.metastore.schema.verification", true,
         "Enforce metastore schema version consistency.\n" +
         "True: Verify that version information stored in metastore matches with one from Hive jars.  Also disable automatic\n" +
         "      schema migration attempt. Users are required to manually migrate schema after Hive upgrade which ensures\n" +
