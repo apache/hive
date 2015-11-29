0a96201f268a8cf0efe71d960f8814592d30d756
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-11-29 16:53:20 -0600

    HIVE-12497: Remove HADOOP_CLIENT_OPTS from hive script (Prasanth Jayachandran reviewed by Gopal V)

--- bin/ext/version.sh
+++ bin/ext/version.sh
@@ -27,7 +27,7 @@ version () {
 
   # hadoop 20 or newer - skip the aux_jars option and hiveconf
   CLASS=org.apache.hive.common.util.HiveVersionInfo
-  exec $HADOOP jar $JAR $CLASS
+  exec $HADOOP jar $JAR $CLASS 2>> ${STDERR}
 }
 
 version_help () {
