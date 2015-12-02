cf09d6d2323763e1fcb079bf466113903e21cd3d
Mohammad Kamrul Islam <mislam77@yahoo.com>
2015-12-02 09:31:42 -0600

    CDH-45005: HIVE-12475: Parquet schema evolution within array<struct<>> doesn't work (Mohammad Kamrul Islam, reviewed by Sergio Pena)
    Change-Id: I69a1dbf2122ddb60cf867c1820877aaa8207692d

--- ql/src/java/org/apache/hadoop/hive/ql/io/parquet/serde/ArrayWritableObjectInspector.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/parquet/serde/ArrayWritableObjectInspector.java
@@ -136,10 +136,14 @@ public class ArrayWritableObjectInspector extends SettableStructObjectInspector 
     if (data == null) {
       return null;
     }
-
     if (data instanceof ArrayWritable) {
       final ArrayWritable arr = (ArrayWritable) data;
-      return arr.get()[((StructFieldImpl) fieldRef).getIndex()];
+      final StructFieldImpl structField = (StructFieldImpl) fieldRef;
+      if (structField.getIndex() < arr.get().length) {
+        return arr.get()[structField.getIndex()];
+      } else {
+        return null;
+      }
     }
 
     //since setStructFieldData and create return a list, getStructFieldData should be able to
