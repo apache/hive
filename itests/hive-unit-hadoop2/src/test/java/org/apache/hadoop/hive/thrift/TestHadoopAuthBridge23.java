0d968965742379e629328c56d8c3ed578eac2a82
Thejas Nair <thejas@hortonworks.com>
2015-12-17 15:57:20 -0800

    HIVE-12688 : HIVE-11826 makes hive unusable in properly secured cluster (Thejas Nair, reviewed by Ashutosh Chauhan)

--- itests/hive-unit-hadoop2/src/test/java/org/apache/hadoop/hive/thrift/TestHadoopAuthBridge23.java
+++ itests/hive-unit-hadoop2/src/test/java/org/apache/hadoop/hive/thrift/TestHadoopAuthBridge23.java
@@ -226,10 +226,6 @@ public class TestHadoopAuthBridge23 extends TestCase {
 
   public void testSaslWithHiveMetaStore() throws Exception {
     setup();
-
-    final String proxyUserName = UserGroupInformation.getCurrentUser().getShortUserName();
-    setGroupsInConf(new String[] {"*"}, proxyUserName);
-
     UserGroupInformation clientUgi = UserGroupInformation.getCurrentUser();
     obtainTokenAndAddIntoUGI(clientUgi, null);
     obtainTokenAndAddIntoUGI(clientUgi, "tokenForFooTablePartition");
--- shims/common/src/main/java/org/apache/hadoop/hive/thrift/HadoopThriftAuthBridge.java
+++ shims/common/src/main/java/org/apache/hadoop/hive/thrift/HadoopThriftAuthBridge.java
@@ -671,9 +671,6 @@ public class HadoopThriftAuthBridge {
           if (useProxy) {
             clientUgi = UserGroupInformation.createProxyUser(
                 endUser, UserGroupInformation.getLoginUser());
-
-            ProxyUsers.authorize(clientUgi, getRemoteAddress().getHostAddress(), null);
-
             remoteUser.set(clientUgi.getShortUserName());
             LOG.debug("Set remoteUser :" + remoteUser.get());
             return clientUgi.doAs(new PrivilegedExceptionAction<Boolean>() {
