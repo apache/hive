f3ea7773beda9267be71a803f56bbb8e1706ac65
Szehon Ho <szehon@cloudera.com>
2016-01-15 11:12:48 -0800

    HIVE-12868 : Fix empty operation-pool metrics (Szehon, reviewed by Jimmy Xiang)

--- service/src/java/org/apache/hive/service/server/HiveServer2.java
+++ service/src/java/org/apache/hive/service/server/HiveServer2.java
@@ -99,6 +99,15 @@ public class HiveServer2 extends CompositeService {
 
   @Override
   public synchronized void init(HiveConf hiveConf) {
+    //Initialize metrics first, as some metrics are for initialization stuff.
+    try {
+      if (hiveConf.getBoolVar(ConfVars.HIVE_SERVER2_METRICS_ENABLED)) {
+        MetricsFactory.init(hiveConf);
+      }
+    } catch (Throwable t) {
+      LOG.warn("Could not initiate the HiveServer2 Metrics system.  Metrics may not be reported.", t);
+    }
+
     cliService = new CLIService(this);
     addService(cliService);
     final HiveServer2 hiveServer2 = this;
@@ -506,9 +515,6 @@ public class HiveServer2 extends CompositeService {
         server.init(hiveConf);
         server.start();
 
-        if (hiveConf.getBoolVar(ConfVars.HIVE_SERVER2_METRICS_ENABLED)) {
-          MetricsFactory.init(hiveConf);
-        }
         try {
           JvmPauseMonitor pauseMonitor = new JvmPauseMonitor(hiveConf);
           pauseMonitor.start();
