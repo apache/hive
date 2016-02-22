5b0825f16911c0db7f641bc13d3e7d2c6d3a1101
Thejas Nair <thejas@hortonworks.com>
2016-02-22 08:50:40 -0800

    HIVE-13093 : hive metastore does not exit on start failure (Thejas Nair, reviewed by Daniel Dai)

--- metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -6246,7 +6246,8 @@ public class HiveMetaStore extends ThriftHiveMetastore {
         }
       }
     };
-
+    t.setDaemon(true);
+    t.setName("Metastore threads starter thread");
     t.start();
   }
 
