721aa7e4904a8a6069afe815af7cbf5ed3bde936
Josh Rosen <joshrosen@databricks.com>
2016-04-06 12:05:15 -0700

    Change groupId to org.spark-project.hive; keep relocated Kryo under Hive namespace.

--- publish-to-spark.sh
+++ publish-to-spark.sh
@@ -7,9 +7,14 @@ fi
 
 PUBLISH_PROFILES="-Phadoop-2 -DskipTests -Psources -Pjavadoc"
 
-echo "Replacing groupID with org.spark-project.hive"
-find . -name pom.xml | \
-  xargs -I {} sed -i -e "s/org.apache.hive/org.spark-project.hive/g" {}
+# Note: we don't want to do a blanket replacement of the groupid because
+# we want to relocate the shaded Kryo classes under the
+# org.apache.hive.com.esotericsoftware.kryo namespace in order to match the
+# the stock Hive 1.2.1 build.
+#
+# echo "Replacing groupID with org.spark-project.hive"
+# find . -name pom.xml | \
+ # xargs -I {} sed -i -e "s/org.apache.hive/org.spark-project.hive/g" {}
 
 echo "Publishing Spark to OSS Sonatype"
 mvn clean deploy -Dgpg.passphrase=$GPG_PASSPHRASE $PUBLISH_PROFILES
