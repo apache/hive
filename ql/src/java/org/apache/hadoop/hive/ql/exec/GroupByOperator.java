9a2bd0c48179f2b654b69adbbe88cd5485492658
Sergey Shelukhin <sershe@apache.org>
2016-04-22 10:55:53 -0700

    HIVE-13240 : GroupByOperator: Drop the hash aggregates when closing operator (Gopal V, reviewed by Sergey Shelukhin)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/GroupByOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/GroupByOperator.java
@@ -1095,6 +1095,7 @@ public class GroupByOperator extends Operator<GroupByDesc> {
         throw new HiveException(e);
       }
     }
+    hashAggregations = null;
   }
 
   // Group by contains the columns needed - no need to aggregate from children
