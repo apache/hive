ac4906d2b33e7b531e8b5c2295282964ef63dbdb
Owen O'Malley <omalley@apache.org>
2016-05-14 21:07:29 -0700

    CDH-48083 : HIVE-13763 : Update smart-apply-patch.sh with ability to use patches from git (Owen Omalley via Alan Gates)"
    Change-Id: I8f6b00707d931fb631e8cd9772c1c78bc1a00237
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- testutils/ptest2/src/main/resources/smart-apply-patch.sh
+++ testutils/ptest2/src/main/resources/smart-apply-patch.sh
@@ -83,6 +83,9 @@ elif $PATCH -p1 -E --dry-run < $PATCH_FILE 2>&1 > /dev/null; then
   PLEVEL=1
 elif $PATCH -p2 -E --dry-run < $PATCH_FILE 2>&1 > /dev/null; then
   PLEVEL=2
+elif git apply -p0 --check $PATCH_FILE 2>&1 > /dev/null; then
+  git apply -p0 $PATCH_FILE
+  cleanup $?
 else
   echo "The patch does not appear to apply with p0, p1, or p2";
   cleanup 1;
