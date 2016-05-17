c9fb65be92b60ddcf3e552557b5fb811dc4093e0
Jesus Camacho Rodriguez <jcamacho@apache.org>
2016-05-17 17:18:43 +0100

    HIVE-13767: Wrong type inferred in Semijoin condition leads to AssertionError (Jesus Camacho Rodriguez, reviewed by Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveCalciteUtil.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveCalciteUtil.java
@@ -181,6 +181,7 @@ public class HiveCalciteUtil {
       newRightFieldNames.add(field.getName());
     }
 
+    ImmutableBitSet.Builder origColEqCondsPosBuilder = ImmutableBitSet.builder();
     int newKeyCount = 0;
     List<Pair<Integer, Integer>> origColEqConds = new ArrayList<Pair<Integer, Integer>>();
     for (i = 0; i < leftKeyCount; i++) {
@@ -190,6 +191,7 @@ public class HiveCalciteUtil {
       if (leftKey instanceof RexInputRef && rightKey instanceof RexInputRef) {
         origColEqConds.add(Pair.of(((RexInputRef) leftKey).getIndex(),
             ((RexInputRef) rightKey).getIndex()));
+        origColEqCondsPosBuilder.set(i);
       } else {
         newLeftFields.add(leftKey);
         newLeftFieldNames.add(null);
@@ -198,11 +200,13 @@ public class HiveCalciteUtil {
         newKeyCount++;
       }
     }
+    ImmutableBitSet origColEqCondsPos = origColEqCondsPosBuilder.build();
 
     for (i = 0; i < origColEqConds.size(); i++) {
       Pair<Integer, Integer> p = origColEqConds.get(i);
-      RexNode leftKey = leftJoinKeys.get(i);
-      RexNode rightKey = rightJoinKeys.get(i);
+      int condPos = origColEqCondsPos.nth(i);
+      RexNode leftKey = leftJoinKeys.get(condPos);
+      RexNode rightKey = rightJoinKeys.get(condPos);
       leftKeys.add(p.left);
       rightKeys.add(p.right);
       RexNode cond = rexBuilder.makeCall(
