c64cff6f49c37cf4c3058bc9502e170d8db721c0
Rajat Khandelwal <prongs@apache.org>
2016-06-07 10:15:25 +0530

    HIVE-13903 : getFunctionInfo should register jars under a check (Reviewed by Amareshwari)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/Registry.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/Registry.java
@@ -556,7 +556,12 @@ public class Registry {
     // and the current thread may not be able to resolve the UDF. Test for this condition
     // and if necessary load the JARs in this thread.
     if (isNative && info != null && info.isPersistent()) {
-      return registerToSessionRegistry(qualifiedName, info);
+      try {
+        info.getFunctionClass();
+      } catch (Exception e) {
+        return registerToSessionRegistry(qualifiedName, info);
+      }
+      return info;
     }
     if (info != null || !isNative) {
       return info; // We have the UDF, or we are in the session registry (or both).
