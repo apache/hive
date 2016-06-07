5b4902c91530ec9ac804863cadbd0212d695a4a1
ctang <ctang@cloudera.com>
2016-06-07 14:20:03 -0400

    CDH-41196: HIVE-13953: Issues in HiveLockObject equals method (Chaoyu Tang, reviewed by Yongzhi Chen
    Change-Id: Ie2823f40e41c05b19470381257c93ffeac250093

--- ql/src/java/org/apache/hadoop/hive/ql/lockmgr/HiveLockObject.java
+++ ql/src/java/org/apache/hadoop/hive/ql/lockmgr/HiveLockObject.java
@@ -18,8 +18,7 @@
 
 package org.apache.hadoop.hive.ql.lockmgr;
 
-import java.util.Arrays;
-
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.hadoop.hive.ql.metadata.DummyPartition;
 import org.apache.hadoop.hive.ql.metadata.Partition;
@@ -253,9 +252,8 @@ public class HiveLockObject {
     }
 
     HiveLockObject tgt = (HiveLockObject) o;
-    return Arrays.equals(pathNames, tgt.pathNames) &&
-        data == null ? tgt.getData() == null :
-        tgt.getData() != null && data.equals(tgt.getData());
+    return StringUtils.equals(this.getName(), tgt.getName()) &&
+        (data == null ? tgt.getData() == null : data.equals(tgt.getData()));
   }
 
   @Override
