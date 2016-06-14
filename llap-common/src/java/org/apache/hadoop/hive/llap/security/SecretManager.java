efb35c57c6dddc6f535dd2b1c59782b2358db465
Sergey Shelukhin <sershe@apache.org>
2016-06-14 12:14:59 -0700

    HIVE-14008 : Duplicate line in LLAP SecretManager (Sergey Shelukhin, reviewed by Siddharth Seth)

--- llap-common/src/java/org/apache/hadoop/hive/llap/security/SecretManager.java
+++ llap-common/src/java/org/apache/hadoop/hive/llap/security/SecretManager.java
@@ -57,7 +57,6 @@ public class SecretManager extends ZKDelegationTokenSecretManager<LlapTokenIdent
   public SecretManager(Configuration conf, String clusterId) {
     super(conf);
     this.clusterId = clusterId;
-    checkForZKDTSMBug();
     this.conf = conf;
     checkForZKDTSMBug();
   }
