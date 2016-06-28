56b2eea51a0f44fba8526dce5209cffb53080dbc
Thejas Nair <thejas@hortonworks.com>
2016-06-27 19:00:05 -0700

    HIVE-14073 : update config whiltelist for sql std authorization (Thejas Nair, reviewed by Sushanth Sowmyan)

--- ql/src/test/org/apache/hadoop/hive/ql/security/authorization/plugin/sqlstd/TestSQLStdHiveAccessControllerHS2.java
+++ ql/src/test/org/apache/hadoop/hive/ql/security/authorization/plugin/sqlstd/TestSQLStdHiveAccessControllerHS2.java
@@ -88,7 +88,8 @@ public class TestSQLStdHiveAccessControllerHS2 {
     // create list with variables that match some of the regexes
     List<String> confVarRegexList = Arrays.asList("hive.convert.join.bucket.mapjoin.tez",
         "hive.optimize.index.filter.compact.maxsize", "hive.tez.dummy", "tez.task.dummy",
-        "hive.exec.dynamic.partition", "hive.exec.dynamic.partition.mode");
+        "hive.exec.dynamic.partition", "hive.exec.dynamic.partition.mode",
+        "oozie.HadoopAccessorService.created", "tez.queue.name");
 
     // combine two lists
     List<String> varList = new ArrayList<String>();
