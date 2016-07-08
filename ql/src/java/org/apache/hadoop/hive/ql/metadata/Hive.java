6588b84028b1d193ef930211480b3811f3274b7a
Gopal V <gopalv@apache.org>
2016-07-08 16:20:38 -0700

    HIVE-14178 : Hive::needsToCopy should reuse FileUtils::equalsFileSystem (Gopal V via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
@@ -2994,7 +2994,7 @@ private void constructOneLBLocationMap(FileStatus fSta,
    */
   static protected boolean needToCopy(Path srcf, Path destf, FileSystem srcFs, FileSystem destFs) throws HiveException {
     //Check if different FileSystems
-    if (!srcFs.getClass().equals(destFs.getClass())) {
+    if (!FileUtils.equalsFileSystem(srcFs, destFs)) {
       return true;
     }
 
