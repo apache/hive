b9e94a9d620a0f5844f5ab75651b55445d12243a
Sergey Shelukhin <sershe@apache.org>
2016-07-12 21:31:43 -0700

    CDH-36924 HIVE-14195 : HiveMetaStoreClient getFunction() does not throw NoSuchObjectException (Peter Vary, reviewed by Sergey Shelukhin)
    Change-Id: I59b566f0e204a16e21b6d2be51d56832faed5a5c

--- metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -5760,6 +5760,9 @@ public class HiveMetaStore extends ThriftHiveMetastore {
           throw new NoSuchObjectException(
               "Function " + dbName + "." + funcName + " does not exist");
         }
+      } catch (NoSuchObjectException e) {
+        ex = e;
+        rethrowException(e);
       } catch (Exception e) {
         ex = e;
         throw newMetaException(e);
