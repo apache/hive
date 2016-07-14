feedee4c922402339f2302bcc1c9be1ad9a47bba
Peter Vary <pvary@cloudera.com>
2016-07-14 11:27:18 -0500

    HIVE-14215: Displaying inconsistent CPU usage data with MR execution engine (Peter Vary, reviewed by Aihua Xu and Sergio Pena)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/mr/HadoopJobExecHelper.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/mr/HadoopJobExecHelper.java
@@ -375,6 +375,19 @@ public class HadoopJobExecHelper {
       reportTime = System.currentTimeMillis();
     }
 
+    Counters ctrs = th.getCounters();
+
+    if (ctrs != null) {
+      Counter counterCpuMsec = ctrs.findCounter("org.apache.hadoop.mapred.Task$Counter",
+              "CPU_MILLISECONDS");
+      if (counterCpuMsec != null) {
+        long newCpuMSec = counterCpuMsec.getValue();
+        if (newCpuMSec > cpuMsec) {
+          cpuMsec = newCpuMSec;
+        }
+      }
+    }
+
     if (cpuMsec > 0) {
       String status = "MapReduce Total cumulative CPU time: " + Utilities.formatMsecToStr(cpuMsec);
       console.printInfo(status);
@@ -383,7 +396,6 @@ public class HadoopJobExecHelper {
 
     boolean success;
 
-    Counters ctrs = th.getCounters();
     if (fatal) {
       success = false;
     } else {
@@ -398,17 +410,6 @@ public class HadoopJobExecHelper {
           ss.getHiveHistory().setTaskCounters(queryId, getId(), ctrs);
         }
         success = rj.isSuccessful();
-      }
-    }
-
-    if (ctrs != null) {
-      Counter counterCpuMsec = ctrs.findCounter("org.apache.hadoop.mapred.Task$Counter",
-          "CPU_MILLISECONDS");
-      if (counterCpuMsec != null) {
-        long newCpuMSec = counterCpuMsec.getValue();
-        if (newCpuMSec > cpuMsec) {
-          cpuMsec = newCpuMSec;
-        }
       }
     }
 
