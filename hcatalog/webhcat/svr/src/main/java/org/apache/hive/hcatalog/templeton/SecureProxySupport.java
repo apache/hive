13736517062e5b8efd87b18bee2da0c106634c5a
Niklaus Xiao <stone_xy@live.cn>
2016-07-18 23:04:40 -0700

    HIVE-13883 : WebHCat leaves token crc file never gets deleted (Niklaus Xiao via Thejas Nair)

--- hcatalog/webhcat/svr/src/main/java/org/apache/hive/hcatalog/templeton/SecureProxySupport.java
+++ hcatalog/webhcat/svr/src/main/java/org/apache/hive/hcatalog/templeton/SecureProxySupport.java
@@ -108,6 +108,16 @@ public class SecureProxySupport {
   public void close() {
     if (tokenPath != null) {
       new File(tokenPath.toUri()).delete();
+      String checksumStr = tokenPath.getParent() + File.separator + "." + tokenPath.getName() + ".crc";
+      File checksumFile = null;
+      try {
+        checksumFile = new File(new URI(checksumStr));
+        if (checksumFile.exists()) {
+          checksumFile.delete();
+        }
+      } catch (URISyntaxException e) {
+        LOG.error("Failed to delete token crc file.", e);
+      }
       tokenPath = null;
     }
   }
