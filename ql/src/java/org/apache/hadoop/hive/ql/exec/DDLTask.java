9938d45da0d6bbd74e0adf72ca3e19e58a03c9dd
Sahil Takiar <takiar.sahil@gmail.com>
2016-07-25 10:17:16 -0500

    HIVE-12646: beeline and HIVE CLI do not parse ; in quote properly (Sahil Takiar, reviewed by Sergio Pena)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
@@ -4346,7 +4346,7 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < str.length(); i ++) {
       char c = str.charAt(i);
-      if (c == '\'' || c == ';') {
+      if (c == '\'') {
         sb.append('\\');
       }
       sb.append(c);
