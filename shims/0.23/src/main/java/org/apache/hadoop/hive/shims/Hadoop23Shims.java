3c5cc149b211828821270d3a90f67355f85717f3
Mithun Radhakrishnan <mythrocks@gmail.com>
2016-08-03 10:57:42 -0500

    HIVE-14380: Queries on tables with remote HDFS paths fail in "encryption" checks. (Mithun Radhakrishnan, reviewed by Sergio Pena)
    Change-Id: I87cc42a9a52ebc53c9ac576a96db7c7ec991f5ef

--- shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
+++ shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
@@ -1192,11 +1192,11 @@ public class Hadoop23Shims extends HadoopShimsSecure {
 
     @Override
     public boolean arePathsOnSameEncryptionZone(Path path1, Path path2) throws IOException {
-      EncryptionZone zone1, zone2;
+      return equivalentEncryptionZones(hdfsAdmin.getEncryptionZoneForPath(path1),
+                                       hdfsAdmin.getEncryptionZoneForPath(path2));
+    }
 
-      zone1 = hdfsAdmin.getEncryptionZoneForPath(path1);
-      zone2 = hdfsAdmin.getEncryptionZoneForPath(path2);
-
+    private boolean equivalentEncryptionZones(EncryptionZone zone1, EncryptionZone zone2) {
       if (zone1 == null && zone2 == null) {
         return true;
       } else if (zone1 == null || zone2 == null) {
@@ -1204,6 +1204,19 @@ public class Hadoop23Shims extends HadoopShimsSecure {
       }
 
       return zone1.equals(zone2);
+    }
+
+    @Override
+    public boolean arePathsOnSameEncryptionZone(Path path1, Path path2,
+                                                HadoopShims.HdfsEncryptionShim encryptionShim2) throws IOException {
+      if (!(encryptionShim2 instanceof Hadoop23Shims.HdfsEncryptionShim)) {
+        LOG.warn("EncryptionShim for path2 (" + path2 + ") is of unexpected type: " + encryptionShim2.getClass()
+            + ". Assuming path2 is on the same EncryptionZone as path1(" + path1 + ").");
+        return true;
+      }
+
+      return equivalentEncryptionZones(hdfsAdmin.getEncryptionZoneForPath(path1),
+          ((HdfsEncryptionShim)encryptionShim2).hdfsAdmin.getEncryptionZoneForPath(path2));
     }
 
     @Override
