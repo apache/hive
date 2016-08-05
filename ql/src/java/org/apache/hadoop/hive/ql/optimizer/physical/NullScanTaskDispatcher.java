2cd04d814cb60cfb0d589e842347c18c560eeef9
niklaus xiao <stone_xy@live.cn>
2016-08-04 18:54:46 -0700

    HIVE-14411 : selecting Hive on Hbase table may cause FileNotFoundException (Niklaus Xiao via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/NullScanTaskDispatcher.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/NullScanTaskDispatcher.java
@@ -127,6 +127,10 @@ public class NullScanTaskDispatcher implements Dispatcher {
     for (TableScanOperator tso : tableScans) {
       // use LinkedHashMap<String, Operator<? extends OperatorDesc>>
       // getAliasToWork()
+	  // should not apply this for non-native table
+      if (tso.getConf().getTableMetadata().getStorageHandler() != null) {
+        continue;
+      }
       String alias = getAliasForTableScanOperator(work, tso);
       aliases.add(alias);
       tso.getConf().setIsMetadataOnly(true);
