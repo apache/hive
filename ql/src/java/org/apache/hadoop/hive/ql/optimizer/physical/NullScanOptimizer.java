ee7ad013ab6decbbff9eacc334cfc84d70d08268
Yongzhi Chen <ychena@apache.org>
2016-08-13 12:48:51 -0700

    CDH-43113 HIVE-14519 : Multi insert query bug (Ashutosh Chauhan via Yongzhi Chen)
    Change-Id: If1d91bfcd9e4d7445c27027a029b335b0f63b8ec
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/NullScanOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/NullScanOptimizer.java
@@ -27,6 +27,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.ql.exec.FilterOperator;
 import org.apache.hadoop.hive.ql.exec.LimitOperator;
+import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.TableScanOperator;
 import org.apache.hadoop.hive.ql.lib.DefaultGraphWalker;
 import org.apache.hadoop.hive.ql.lib.Dispatcher;
@@ -88,6 +89,17 @@ public class NullScanOptimizer implements PhysicalPlanResolver {
       ExprNodeConstantDesc c = (ExprNodeConstantDesc) condition;
       if (!Boolean.FALSE.equals(c.getValue())) {
         return null;
+      }
+
+      int numOfndPeers = 0;
+      if (filter.getParentOperators() != null) {
+        for (Operator<?> fParent : filter.getParentOperators()) {
+          if (fParent.getChildOperators() != null) {
+            numOfndPeers += fParent.getChildOperators().size();
+            if (numOfndPeers > 1)
+              return null;
+          }
+        }
       }
 
       WalkerCtx ctx = (WalkerCtx) procCtx;
