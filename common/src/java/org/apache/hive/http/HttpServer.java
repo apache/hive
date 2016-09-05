d17634d6437f212ffd07b7399a07ff2dc0b7b664
ctang <ctang@cloudera.com>
2016-09-05 10:50:58 -0400

    CDH-43944: HIVE-14697: Can not access kerberized HS2 Web UI (Chaoyu Tang, reviewed by Jimmy Xiang)
    Change-Id: I05302c023c2e5da04d841f5999b762fdf38ef080

--- common/src/java/org/apache/hive/http/HttpServer.java
+++ common/src/java/org/apache/hive/http/HttpServer.java
@@ -335,6 +335,7 @@ public class HttpServer {
     connector.setAcceptQueueSize(queueSize);
     connector.setResolveNames(false);
     connector.setUseDirectBuffers(false);
+    connector.setRequestHeaderSize(1024*64);
     connector.setReuseAddress(!Shell.WINDOWS);
     return connector;
   }
