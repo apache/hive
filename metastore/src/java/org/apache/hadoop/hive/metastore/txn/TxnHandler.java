185026a750c5cc2e5207403515c99fdf21e7dbfe
Eugene Koifman <ekoifman@hortonworks.com>
2016-11-30 16:25:11 -0800

    HIVE-15308 Create ACID table failed intermittently: due to Postgres (SQLState=25P02, ErrorCode=0) (Eugene Koifman, reviewed by Wei Zheng)

--- metastore/src/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
@@ -3260,6 +3260,11 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
             if (!isDuplicateKeyError(ex)) {
               throw new RuntimeException("Unable to lock " + quoteString(key) + " due to: " + getMessage(ex), ex);
             }
+            //if here, it means a concrurrent acquireLock() inserted the 'key'
+
+            //rollback is done for the benefit of Postgres which throws (SQLState=25P02, ErrorCode=0) if
+            //you attempt any stmt in a txn which had an error.
+            dbConn.rollback();
           }
           rs = stmt.executeQuery(sqlStmt);
           if (!rs.next()) {
