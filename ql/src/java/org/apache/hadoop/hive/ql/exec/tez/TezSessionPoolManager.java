4625964b5350343617e7eab45b24ef75d427c0d4
Siddharth Seth <sseth@apache.org>
2017-01-25 20:54:03 -0800

    HIVE-15731. sessions are not returned to the sessionPool in case of an interrupt. (Siddharth Seth, reviewed by Sergey Shelukhin)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/TezSessionPoolManager.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/TezSessionPoolManager.java
@@ -322,23 +322,39 @@ public class TezSessionPoolManager {
 
   public void returnSession(TezSessionState tezSessionState, boolean llap)
       throws Exception {
-    if (llap && (this.numConcurrentLlapQueries > 0)) {
-      llapQueue.release();
-    }
-    if (tezSessionState.isDefault() && tezSessionState instanceof TezSessionPoolSession) {
-      LOG.info("The session " + tezSessionState.getSessionId()
-          + " belongs to the pool. Put it back in");
-      SessionState sessionState = SessionState.get();
-      if (sessionState != null) {
-        sessionState.setTezSession(null);
+    // Ignore the interrupt status while returning the session, but set it back
+    // on the thread in case anything else needs to deal with it.
+    boolean isInterrupted = Thread.interrupted();
+
+    try {
+      if (isInterrupted) {
+        LOG.info("returnSession invoked with interrupt status set");
       }
-      TezSessionPoolSession poolSession = (TezSessionPoolSession)tezSessionState;
-      if (poolSession.returnAfterUse()) {
-        defaultQueuePool.put(poolSession);
+      if (llap && (this.numConcurrentLlapQueries > 0)) {
+        llapQueue.release();
+      }
+      if (tezSessionState.isDefault() &&
+          tezSessionState instanceof TezSessionPoolSession) {
+        LOG.info("The session " + tezSessionState.getSessionId()
+            + " belongs to the pool. Put it back in");
+        SessionState sessionState = SessionState.get();
+        if (sessionState != null) {
+          sessionState.setTezSession(null);
+        }
+        TezSessionPoolSession poolSession =
+            (TezSessionPoolSession) tezSessionState;
+        if (poolSession.returnAfterUse()) {
+          defaultQueuePool.put(poolSession);
+        }
+      }
+      // non default session nothing changes. The user can continue to use the existing
+      // session in the SessionState
+    } finally {
+      // Reset the interrupt status.
+      if (isInterrupted) {
+        Thread.currentThread().interrupt();
       }
     }
-    // non default session nothing changes. The user can continue to use the existing
-    // session in the SessionState
   }
 
   public static void closeIfNotDefault(
