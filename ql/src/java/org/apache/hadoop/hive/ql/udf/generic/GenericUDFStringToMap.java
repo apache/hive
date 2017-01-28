89c2e1925f6ca51b002eca217aec4abf9891b072
Alexander Pivovarov <pivovaa@amazon.com>
2017-01-27 23:58:23 -0800

    HIVE-15746: Fix default delimiter2 value in str_to_map UDF description

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFStringToMap.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFStringToMap.java
@@ -44,7 +44,7 @@ import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
     + "Creates a map by parsing text ", extended = "Split text into key-value pairs"
     + " using two delimiters. The first delimiter separates pairs, and the"
     + " second delimiter sperates key and value. If only one parameter is given, default"
-    + " delimiters are used: ',' as delimiter1 and '=' as delimiter2.")
+    + " delimiters are used: ',' as delimiter1 and ':' as delimiter2.")
 public class GenericUDFStringToMap extends GenericUDF {
   // Must be deterministic order map for consistent q-test output across Java versions - see HIVE-9161
   private final LinkedHashMap<Object, Object> ret = new LinkedHashMap<Object, Object>();
