bb486840708bfa0b2950a41e6071a407d578653d
Chaozhong Yang <yangchaozhong.2009@gmail.com>
2017-02-13 16:16:23 -0800

    HIVE-15872 : The PERCENTILE_APPROX UDAF does not work with empty set (Chaozhong Yang, reviewed by Wei Zheng)

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDAFPercentileApprox.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDAFPercentileApprox.java
@@ -312,6 +312,8 @@ public class GenericUDAFPercentileApprox extends AbstractGenericUDAFResolver {
           myagg.quantiles[i-1] = doi.get(partialHistogram.get(i));
         }
         partialHistogram.subList(0, nquantiles+1).clear();
+      } else {
+        partialHistogram.subList(0, 1).clear();
       }
 
       // merge histograms
