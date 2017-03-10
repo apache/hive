dcb2831d92ce3fe8406ba64b0f3a9f0e0af201a1
yunfei liu <liuyunfei@cmss.chinamobile.com>
2017-03-10 00:41:00 -0800

    HIVE-16174 : Update MetricsConstant.WAITING_COMPILE_OPS metric when we aquire lock failed in Driver (yunfei liu via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/Driver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Driver.java
@@ -1305,14 +1305,16 @@ public class Driver implements CommandProcessor {
 
     final ReentrantLock compileLock = tryAcquireCompileLock(isParallelEnabled,
       command);
+
+    if (metrics != null) {
+      metrics.decrementCounter(MetricsConstant.WAITING_COMPILE_OPS, 1);
+    }
+
     if (compileLock == null) {
       return ErrorMsg.COMPILE_LOCK_TIMED_OUT.getErrorCode();
     }
 
     try {
-      if (metrics != null) {
-        metrics.decrementCounter(MetricsConstant.WAITING_COMPILE_OPS, 1);
-      }
       ret = compile(command, true, deferClose);
     } finally {
       compileLock.unlock();
