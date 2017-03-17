1be6f15142e9c71b27391af259e93e56cfc21949
Vaibhav Gumashta <vgumashta@hortonworks.com>
2017-03-17 12:52:30 -0700

    HIVE-14210: ExecDriver should call jobclient.close() to trigger cleanup (Thomas Friedrich reviewed by Sergey Shelukhin)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/mr/ExecDriver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/mr/ExecDriver.java
@@ -211,6 +211,7 @@ public class ExecDriver extends Task<MapredWork> implements Serializable, Hadoop
     Context ctx = driverContext.getCtx();
     boolean ctxCreated = false;
     Path emptyScratchDir;
+    JobClient jc = null;
 
     MapWork mWork = work.getMapWork();
     ReduceWork rWork = work.getReduceWork();
@@ -398,7 +399,7 @@ public class ExecDriver extends Task<MapredWork> implements Serializable, Hadoop
         HiveConf.setVar(job, HiveConf.ConfVars.METASTOREPWD, "HIVE");
       }
       LOG.error(job.get("mapreduce.framework.name"));
-      JobClient jc = new JobClient(job);
+      jc = new JobClient(job);
       // make this client wait if job tracker is not behaving well.
       Throttle.checkJobTracker(job, LOG);
 
@@ -464,6 +465,9 @@ public class ExecDriver extends Task<MapredWork> implements Serializable, Hadoop
           }
           HadoopJobExecHelper.runningJobs.remove(rj);
           jobID = rj.getID().toString();
+        }
+        if (jc!=null) {
+          jc.close();
         }
       } catch (Exception e) {
       }
