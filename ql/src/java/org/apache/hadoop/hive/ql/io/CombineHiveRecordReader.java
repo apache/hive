difin/difin_hive

HIVE-15147 : LLAP: use LLAP cache for non-columnar formats in a somewhat general way (Sergey Shelukhin, reviewed by Gopal V)

ql/src/java/org/apache/hadoop/hive/ql/io/CombineHiveRecordReader.java
@@ -19,11 +19,20 @@
 package org.apache.hadoop.hive.ql.io;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.JavaUtils;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.exec.Utilities;
 import org.apache.hadoop.hive.ql.exec.mr.ExecMapper;
 import org.apache.hadoop.hive.ql.io.CombineHiveInputFormat.CombineHiveInputSplit;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.plan.MapWork;
+import org.apache.hadoop.hive.ql.plan.PartitionDesc;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapred.FileSplit;
@@ -42,6 +51,9 @@ import org.apache.hadoop.mapred.lib.CombineFileSplit;
  */
 public class CombineHiveRecordReader<K extends WritableComparable, V extends Writable>
     extends HiveContextAwareRecordReader<K, V> {
+  private org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(CombineHiveRecordReader.class);
+
+  private LinkedHashMap<Path, PartitionDesc> pathToPartInfo;
 
   public CombineHiveRecordReader(InputSplit split, Configuration conf,
       Reporter reporter, Integer partition, RecordReader preReader) throws IOException {
@@ -57,8 +69,27 @@ public class CombineHiveRecordReader<K extends WritableComparable, V extends Wri
       throw new IOException("CombineHiveRecordReader: class not found "
           + inputFormatClassName);
     }
-    InputFormat inputFormat = HiveInputFormat.getInputFormatFromCache(
-        inputFormatClass, jobConf);
+    InputFormat inputFormat = HiveInputFormat.getInputFormatFromCache(inputFormatClass, jobConf);
+    try {
+      // TODO: refactor this out
+      if (pathToPartInfo == null) {
+        MapWork mrwork;
+        if (HiveConf.getVar(conf, HiveConf.ConfVars.HIVE_EXECUTION_ENGINE).equals("tez")) {
+          mrwork = (MapWork) Utilities.getMergeWork(jobConf);
+          if (mrwork == null) {
+            mrwork = Utilities.getMapWork(jobConf);
+          }
+        } else {
+          mrwork = Utilities.getMapWork(jobConf);
+        }
+        pathToPartInfo = mrwork.getPathToPartitionInfo();
+      }
+
+      PartitionDesc part = extractSinglePartSpec(hsplit);
+      inputFormat = HiveInputFormat.wrapForLlap(inputFormat, jobConf, part);
+    } catch (HiveException e) {
+      throw new IOException(e);
+    }
 
     // create a split for the given partition
     FileSplit fsplit = new FileSplit(hsplit.getPaths()[partition], hsplit
@@ -77,6 +108,26 @@ public class CombineHiveRecordReader<K extends WritableComparable, V extends Wri
         this.setFooterBuffer(((CombineHiveRecordReader)preReader).getFooterBuffer());
     }
 
+  }
+
+  private PartitionDesc extractSinglePartSpec(CombineHiveInputSplit hsplit) throws IOException {
+    PartitionDesc part = null;
+    Map<Map<Path,PartitionDesc>, Map<Path,PartitionDesc>> cache = new HashMap<>();
+    for (Path path : hsplit.getPaths()) {
+      PartitionDesc otherPart = HiveFileFormatUtils.getPartitionDescFromPathRecursively(
+          pathToPartInfo, path, cache);
+      LOG.debug("Found spec for " + path + " " + otherPart + " from " + pathToPartInfo);
+      if (part == null) {
+        part = otherPart;
+      } else if (otherPart != part) { // Assume we should have the exact same object.
+        // TODO: we could also compare the schema and SerDe, and pass only those to the call
+        //       instead; most of the time these would be the same and LLAP IO can handle that.
+        LOG.warn("Multiple partitions found; not going to pass a part spec to LLAP IO: {"
+            + part.getPartSpec() + "} and {" + otherPart.getPartSpec() + "}");
+        return null;
+      }
+    }
+    return part;
   }
 
   @Override

apache/hive

Conflicts:
	llap-server/src/java/org/apache/hadoop/hive/llap/io/api/impl/LlapInputFormat.java
	llap-server/src/java/org/apache/hadoop/hive/llap/io/decode/ColumnVectorProducer.java
	llap-server/src/java/org/apache/hadoop/hive/llap/io/decode/OrcColumnVectorProducer.java
	llap-server/src/java/org/apache/hadoop/hive/llap/io/decode/ReadPipeline.java
	ql/src/java/org/apache/hadoop/hive/ql/io/HiveInputFormat.java
	ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcInputFormat.java
	ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/LlapDecider.java
	ql/src/java/org/apache/hadoop/hive/ql/plan/VectorPartitionDesc.java

ql/src/java/org/apache/hadoop/hive/ql/io/CombineHiveRecordReader.java
@@ -19,11 +19,20 @@
 package org.apache.hadoop.hive.ql.io;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.JavaUtils;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.exec.Utilities;
 import org.apache.hadoop.hive.ql.exec.mr.ExecMapper;
 import org.apache.hadoop.hive.ql.io.CombineHiveInputFormat.CombineHiveInputSplit;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.plan.MapWork;
+import org.apache.hadoop.hive.ql.plan.PartitionDesc;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapred.FileSplit;
@@ -41,6 +50,9 @@ import org.apache.hadoop.mapred.lib.CombineFileSplit;
  */
 public class CombineHiveRecordReader<K extends WritableComparable, V extends Writable>
     extends HiveContextAwareRecordReader<K, V> {
+  private org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(CombineHiveRecordReader.class);
+
+  private LinkedHashMap<String, PartitionDesc> pathToPartInfo;
 
   public CombineHiveRecordReader(InputSplit split, Configuration conf,
       Reporter reporter, Integer partition) throws IOException {
@@ -56,8 +68,27 @@ public class CombineHiveRecordReader<K extends WritableComparable, V extends Wri
       throw new IOException("CombineHiveRecordReader: class not found "
           + inputFormatClassName);
     }
-    InputFormat inputFormat = HiveInputFormat.getInputFormatFromCache(
-        inputFormatClass, jobConf);
+    InputFormat inputFormat = HiveInputFormat.getInputFormatFromCache(inputFormatClass, jobConf);
+    try {
+      // TODO: refactor this out
+      if (pathToPartInfo == null) {
+        MapWork mrwork;
+        if (HiveConf.getVar(conf, HiveConf.ConfVars.HIVE_EXECUTION_ENGINE).equals("tez")) {
+          mrwork = (MapWork) Utilities.getMergeWork(jobConf);
+          if (mrwork == null) {
+            mrwork = Utilities.getMapWork(jobConf);
+          }
+        } else {
+          mrwork = Utilities.getMapWork(jobConf);
+        }
+        pathToPartInfo = mrwork.getPathToPartitionInfo();
+      }
+
+      PartitionDesc part = extractSinglePartSpec(hsplit);
+      inputFormat = HiveInputFormat.wrapForLlap(inputFormat, jobConf, part);
+    } catch (HiveException e) {
+      throw new IOException(e);
+    }
 
     // create a split for the given partition
     FileSplit fsplit = new FileSplit(hsplit.getPaths()[partition], hsplit
@@ -67,6 +98,26 @@ public class CombineHiveRecordReader<K extends WritableComparable, V extends Wri
     this.setRecordReader(inputFormat.getRecordReader(fsplit, jobConf, reporter));
 
     this.initIOContext(fsplit, jobConf, inputFormatClass, this.recordReader);
+  }
+
+  private PartitionDesc extractSinglePartSpec(CombineHiveInputSplit hsplit) throws IOException {
+    PartitionDesc part = null;
+    Map<Map<String,PartitionDesc>, Map<String,PartitionDesc>> cache = new HashMap<>();
+    for (Path path : hsplit.getPaths()) {
+      PartitionDesc otherPart = HiveFileFormatUtils.getPartitionDescFromPathRecursively(
+          pathToPartInfo, path, cache);
+      LOG.debug("Found spec for " + path + " " + otherPart + " from " + pathToPartInfo);
+      if (part == null) {
+        part = otherPart;
+      } else if (otherPart != part) { // Assume we should have the exact same object.
+        // TODO: we could also compare the schema and SerDe, and pass only those to the call
+        //       instead; most of the time these would be the same and LLAP IO can handle that.
+        LOG.warn("Multiple partitions found; not going to pass a part spec to LLAP IO: {"
+            + part.getPartSpec() + "} and {" + otherPart.getPartSpec() + "}");
+        return null;
+      }
+    }
+    return part;
   }
 
   @Override