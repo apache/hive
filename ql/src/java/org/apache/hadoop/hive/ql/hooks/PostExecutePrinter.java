3c29f8ff5c23031e3e7b5cc0c9b099964162c899
Chaoyu Tang <ctang@cloudera.com>
2017-04-01 10:51:39 -0400

    HIVE-16308: PreExecutePrinter and PostExecutePrinter should log to INFO level instead of ERROR (Sahil Takiar via Chaoyu Tang)

--- ql/src/java/org/apache/hadoop/hive/ql/hooks/PostExecutePrinter.java
+++ ql/src/java/org/apache/hadoop/hive/ql/hooks/PostExecutePrinter.java
@@ -116,8 +116,8 @@ public class PostExecutePrinter implements ExecuteWithHookContext {
     }
 
     if (queryState != null) {
-      console.printError("POSTHOOK: query: " + queryState.getQueryString().trim());
-      console.printError("POSTHOOK: type: " + queryState.getCommandType());
+      console.printInfo("POSTHOOK: query: " + queryState.getQueryString().trim(), false);
+      console.printInfo("POSTHOOK: type: " + queryState.getCommandType(), false);
     }
 
     PreExecutePrinter.printEntities(console, inputs, "POSTHOOK: Input: ");
@@ -167,7 +167,7 @@ public class PostExecutePrinter implements ExecuteWithHookContext {
         }
         sb.append("]");
 
-        console.printError(sb.toString());
+        console.printInfo(sb.toString(), false);
       }
     }
   }
