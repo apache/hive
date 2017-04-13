a7004280301645ad4a5d750ba6f16b748e3fce2d
Naveen Gangam <ngangam@apache.org>
2017-04-13 11:28:27 -0400

    HIVE-16316 Add-on fix to change the upgrade path from 2.2-->2.3 to 2.2-->2.3-->3.0

---
+++ metastore/scripts/upgrade/mssql/upgrade-2.3.0-to-3.0.0.mssql.sql
@@ -0,0 +1,4 @@
+SELECT 'Upgrading MetaStore schema from 2.3.0 to 3.0.0' AS MESSAGE;
+
+UPDATE VERSION SET SCHEMA_VERSION='3.0.0', VERSION_COMMENT='Hive release version 3.0.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 2.3.0 to 3.0.0' AS MESSAGE;
