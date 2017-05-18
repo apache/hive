1dfe101a74af59aaa47f08399be76a798682d740
Siddharth Seth <sseth@apache.org>
2017-05-18 12:30:46 -0700

    HIVE-14052. Cleanup structures when external clients use LLAP.  (Siddharth Seth, reviewed by Jason Dere, Sergey Shelukhin)

--- llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/TaskExecutorTestHelpers.java
+++ llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/TaskExecutorTestHelpers.java
@@ -96,7 +96,7 @@ public class TaskExecutorTestHelpers {
         new QueryInfo(queryIdentifier, "fake_app_id_string", "fake_dag_id_string", "fake_dag_name",
             "fakeHiveQueryId", 1, "fakeUser",
             new ConcurrentHashMap<String, LlapDaemonProtocolProtos.SourceStateProto>(),
-            new String[0], null, "fakeUser", null, nodeId, null, null);
+            new String[0], null, "fakeUser", null, nodeId, null, null, false);
     return queryInfo;
   }
 
