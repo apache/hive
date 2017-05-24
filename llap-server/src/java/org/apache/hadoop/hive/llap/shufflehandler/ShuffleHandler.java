1bdc5413d480a0e8c407eeca92272d89727ef4d5
Prasanth Jayachandran <prasanthj@apache.org>
2017-05-23 23:32:43 -0700

    HIVE-16737: LLAP: Shuffle handler TCP listen queue overflows (Prasanth Jayachandran reviewed by Rajesh Balamohan)

--- llap-server/src/java/org/apache/hadoop/hive/llap/shufflehandler/ShuffleHandler.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/shufflehandler/ShuffleHandler.java
@@ -119,6 +119,8 @@ import org.jboss.netty.util.CharsetUtil;
 import org.jboss.netty.util.HashedWheelTimer;
 import org.jboss.netty.util.Timer;
 
+import io.netty.util.NetUtil;
+
 public class ShuffleHandler implements AttemptRegistrationListener {
 
   private static final Logger LOG = LoggerFactory.getLogger(ShuffleHandler.class);
@@ -327,6 +329,7 @@ public class ShuffleHandler implements AttemptRegistrationListener {
       throw new RuntimeException(ex);
     }
     bootstrap.setPipelineFactory(pipelineFact);
+    bootstrap.setOption("backlog", NetUtil.SOMAXCONN);
     port = conf.getInt(SHUFFLE_PORT_CONFIG_KEY, DEFAULT_SHUFFLE_PORT);
     Channel ch = bootstrap.bind(new InetSocketAddress(port));
     accepted.add(ch);
@@ -336,7 +339,8 @@ public class ShuffleHandler implements AttemptRegistrationListener {
     if (dirWatcher != null) {
       dirWatcher.start();
     }
-    LOG.info("LlapShuffleHandler" + " listening on port " + port);
+    LOG.info("LlapShuffleHandler" + " listening on port " + port + " (SOMAXCONN: " + bootstrap.getOption("backlog")
+      + ")");
   }
 
   public static void initializeAndStart(Configuration conf) throws Exception {
