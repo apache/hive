f572ce6d2c708ae0a08254d042ce5739c096ba18
Carl Steinbach <cws@apache.org>
2017-06-13 17:08:46 -0700

    HIVE-16844: Fix Connection leak in ObjectStore when new Conf object is used (Sunitha Beeram, reviewed by Carl Steinbach)

--- metastore/src/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -290,6 +290,8 @@ public class ObjectStore implements RawStore, Configurable {
       if (propsChanged) {
         if (pmf != null){
           clearOutPmfClassLoaderCache(pmf);
+          // close the underlying connection pool to avoid leaks
+          pmf.close();
         }
         pmf = null;
         prop = null;
