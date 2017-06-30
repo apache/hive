14733c7cfe4e2d09fd56ee0f279e3f168bb0cf57
Andrew Sherman <asherman@cloudera.com>
2017-06-30 14:19:41 -0500

    HIVE-16991: HiveMetaStoreClient needs a 2-arg constructor for backwards compatibility (Andrew Sherman, reviewed by Sergio Pena)

--- metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
@@ -128,6 +128,10 @@ public class HiveMetaStoreClient implements IMetaStoreClient, AutoCloseable {
     this(conf, null, true);
   }
 
+  public HiveMetaStoreClient(HiveConf conf, HiveMetaHookLoader hookLoader) throws MetaException {
+    this(conf, hookLoader, true);
+  }
+
   public HiveMetaStoreClient(HiveConf conf, HiveMetaHookLoader hookLoader, Boolean allowEmbedded)
     throws MetaException {
 
