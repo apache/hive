20186354bd0df54039dcb205ab9bace73da80805
Alan Gates <gates@hortonworks.com>
2017-07-27 16:19:56 -0700

    Moved DatabaseProduct

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/DatabaseProduct.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/DatabaseProduct.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -72,4 +72,4 @@ public enum DatabaseProduct {
   public static boolean hasJoinOperationOrderBug(DatabaseProduct dbType) {
     return dbType == DERBY || dbType == ORACLE;
   }
-};
+}
