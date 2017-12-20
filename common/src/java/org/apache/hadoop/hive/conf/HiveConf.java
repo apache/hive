846aa98f1eb4ffc87e40fdfecda5ac39b75c60d5
Janaki Lahorani <janaki@cloudera.com>
2017-12-19 17:11:20 -0800

    HIVE-18248: Clean up parameters (Janaki Lahorani, reviewed by Sahil Takiar)
    (cherry picked from commit 4ec47a6e6ffb6a070295348308e3a8bcbc246190)
    
    Change-Id: I1aea2d835766a76834c5d0066a4de192b2833e9e

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -3578,6 +3578,7 @@ public class HiveConf extends Configuration {
     HIVE_MM_AVOID_GLOBSTATUS_ON_S3("hive.mm.avoid.s3.globstatus", true,
         "Whether to use listFiles (optimized on S3) instead of globStatus when on S3."),
 
+    // If a parameter is added to the restricted list, add a test in TestRestrictedList.Java
     HIVE_CONF_RESTRICTED_LIST("hive.conf.restricted.list",
         "hive.security.authenticator.manager,hive.security.authorization.manager," +
         "hive.security.metastore.authorization.manager,hive.security.metastore.authenticator.manager," +
@@ -3606,7 +3607,9 @@ public class HiveConf extends Configuration {
             "bonecp.,"+
             "hive.druid.broker.address.default,"+
             "hive.druid.coordinator.address.default,"+
-            "hikari.",
+            "hikari.,"+
+            "hadoop.bin.path,"+
+            "yarn.bin.path",
         "Comma separated list of configuration options which are immutable at runtime"),
     HIVE_CONF_HIDDEN_LIST("hive.conf.hidden.list",
         METASTOREPWD.varname + "," + HIVE_SERVER2_SSL_KEYSTORE_PASSWORD.varname
@@ -4746,6 +4749,10 @@ public class HiveConf extends Configuration {
 
   public static void setHiveSiteLocation(URL location) {
     hiveSiteURL = location;
+  }
+
+  public static void setHivemetastoreSiteUrl(URL location) {
+    hivemetastoreSiteUrl = location;
   }
 
   public static URL getHiveSiteLocation() {
