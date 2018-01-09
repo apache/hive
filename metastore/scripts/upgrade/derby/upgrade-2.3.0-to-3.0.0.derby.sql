3c9a783edcd5273c6cca99362bb23f2c80887a63
Aihua Xu <aihuaxu@apache.org>
2018-01-09 15:46:04 -0800

    HIVE-18202: Automatically migrate hbase.table.name to hbase.mapreduce.hfileoutputformat.table.name for hbase-based table (Aihua Xu, reviewed by Naveen Gangam)
    (cherry picked from commit d37b8026ed25b4150fd3782331e6800459d40d7a)
    
    Change-Id: I947ff6e8bd8e3ac51e4f74b40f1067f5b8df821b

--- metastore/scripts/upgrade/derby/upgrade-2.3.0-to-3.0.0.derby.sql
+++ metastore/scripts/upgrade/derby/upgrade-2.3.0-to-3.0.0.derby.sql
@@ -5,5 +5,6 @@ RUN '043-HIVE-16922.derby.sql';
 RUN '044-HIVE-16997.derby.sql';
 RUN '045-HIVE-16886.derby.sql';
 RUN '046-HIVE-17566.derby.sql';
+RUN '047-HIVE-18202.derby.sql';
 
 UPDATE "APP".VERSION SET SCHEMA_VERSION='3.0.0', VERSION_COMMENT='Hive release version 3.0.0' where VER_ID=1;
--- metastore/scripts/upgrade/mysql/upgrade-2.3.0-to-3.0.0.mysql.sql
+++ metastore/scripts/upgrade/mysql/upgrade-2.3.0-to-3.0.0.mysql.sql
@@ -6,6 +6,7 @@ SOURCE 043-HIVE-16922.mysql.sql;
 SOURCE 044-HIVE-16997.mysql.sql;
 SOURCE 045-HIVE-16886.mysql.sql;
 SOURCE 046-HIVE-17566.mysql.sql;
+SOURCE 047-HIVE-18202.mysql.sql;
 
 UPDATE VERSION SET SCHEMA_VERSION='3.0.0', VERSION_COMMENT='Hive release version 3.0.0' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 2.3.0 to 3.0.0' AS ' ';
