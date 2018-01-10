d69601c24c04e6a065c2a0bcbe10935877aeb9c8
Prabhu Joseph <pjoseph@hortonworks.com>
2018-01-09 22:04:03 -0800

    HIVE-18353 : CompactorMR should call jobclient.close() to trigger cleanup (Prabhu Joseph via Thejas Nair)

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorMR.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorMR.java
@@ -333,13 +333,22 @@ public class CompactorMR {
       job.getJobName() + "' to " + job.getQueueName() + " queue.  " +
       "(current delta dirs count=" + curDirNumber +
       ", obsolete delta dirs count=" + obsoleteDirNumber + ". TxnIdRange[" + minTxn + "," + maxTxn + "]");
-    RunningJob rj = new JobClient(job).submitJob(job);
-    LOG.info("Submitted compaction job '" + job.getJobName() + "' with jobID=" + rj.getID() + " compaction ID=" + id);
-    txnHandler.setHadoopJobId(rj.getID().toString(), id);
-    rj.waitForCompletion();
-    if (!rj.isSuccessful()) {
-      throw new IOException(compactionType == CompactionType.MAJOR ? "Major" : "Minor" +
-          " compactor job failed for " + jobName + "! Hadoop JobId: " + rj.getID() );
+    JobClient jc = null;
+    try {
+      jc = new JobClient(job);
+      RunningJob rj = jc.submitJob(job);
+      LOG.info("Submitted compaction job '" + job.getJobName() +
+          "' with jobID=" + rj.getID() + " compaction ID=" + id);
+      txnHandler.setHadoopJobId(rj.getID().toString(), id);
+      rj.waitForCompletion();
+      if (!rj.isSuccessful()) {
+        throw new IOException(compactionType == CompactionType.MAJOR ? "Major" : "Minor" +
+               " compactor job failed for " + jobName + "! Hadoop JobId: " + rj.getID());
+      }
+    } finally {
+      if (jc!=null) {
+        jc.close();
+      }
     }
   }
   /**
