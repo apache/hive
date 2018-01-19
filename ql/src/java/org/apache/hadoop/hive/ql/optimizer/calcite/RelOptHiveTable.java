41a5c06597cfdc29324d3181d4c5e89a93b0aded
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-01-19 09:37:39 -0800

    HIVE-18482: Copy-paste error in the RelOptHiveTable (Jesus Camacho Rodriguez, reviewed by Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/RelOptHiveTable.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/RelOptHiveTable.java
@@ -191,8 +191,7 @@ public class RelOptHiveTable extends RelOptAbstractTable {
             break;
           }
         }
-        if (pkPos == rowType.getFieldNames().size()
-            || pkPos == rowType.getFieldNames().size()) {
+        if (pkPos == rowType.getFieldNames().size()) {
           LOG.error("Column for primary key definition " + pkColName + " not found");
           return ImmutableList.of();
         }
@@ -218,8 +217,7 @@ public class RelOptHiveTable extends RelOptAbstractTable {
             break;
           }
         }
-        if (ukPos == rowType.getFieldNames().size()
-            || ukPos == rowType.getFieldNames().size()) {
+        if (ukPos == rowType.getFieldNames().size()) {
           LOG.error("Column for unique constraint definition " + ukCol.colName + " not found");
           return ImmutableList.of();
         }
