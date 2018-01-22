15dd29451303bc0028203aee3d144bb6b28be773
Peter Vary <pvary@cloudera.com>
2018-01-22 11:02:24 +0100

    HIVE-18367: Describe Extended output is truncated on a table with an explicit row format containing tabs or newlines. (Andrew Sherman via Peter Vary)

--- itests/hive-unit/src/test/java/org/apache/hive/beeline/TestBeeLineWithArgs.java
+++ itests/hive-unit/src/test/java/org/apache/hive/beeline/TestBeeLineWithArgs.java
@@ -1130,4 +1130,19 @@ public class TestBeeLineWithArgs {
     argList.add("--outputformat=tsv2");
     testScriptFile(SCRIPT_TEXT, argList, EXPECTED_PATTERN, true);
   }
+
+  /**
+   * Test 'describe extended' on tables that have special white space characters in the row format.
+   */
+  @Test
+  public void testDescribeExtended() throws Throwable {
+    String SCRIPT_TEXT = "drop table if exists describeDelim;"
+        + "create table describeDelim (orderid int, orderdate string, customerid int)"
+        + " ROW FORMAT DELIMITED FIELDS terminated by '\\t' LINES terminated by '\\n';"
+        + "describe extended describeDelim;";
+    List<String> argList = getBaseArgs(miniHS2.getBaseJdbcURL());
+    testScriptFile(SCRIPT_TEXT, argList, OutStream.OUT, Arrays.asList(
+        new Tuple<>("Detailed Table Information.*line.delim=\\\\n", true),
+        new Tuple<>("Detailed Table Information.*field.delim=\\\\t", true)));
+  }
 }
