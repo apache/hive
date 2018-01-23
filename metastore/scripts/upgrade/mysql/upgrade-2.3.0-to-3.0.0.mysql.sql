7d9addd205d17e5274005772f643e572ed38b2f5
Sahil Takiar <takiar.sahil@gmail.com>
2018-01-23 08:30:41 -0800

    HIVE-18489: Automatically migration s3n URIs to s3a URIs (Sahil Takiar, reviewed by Aihua Xu)
    (cherry picked from commit ed256f35057b36ec25f806f69db490966ead8d0d)
    
    Change-Id: Ic24bee5a5446e035d541005b3451c02c6a8cd2a6

--- metastore/scripts/upgrade/mysql/upgrade-2.3.0-to-3.0.0.mysql.sql
+++ metastore/scripts/upgrade/mysql/upgrade-2.3.0-to-3.0.0.mysql.sql
@@ -8,6 +8,7 @@ SOURCE 045-HIVE-16886.mysql.sql;
 SOURCE 046-HIVE-17566.mysql.sql;
 SOURCE 047-HIVE-18202.mysql.sql;
 SOURCE 048-HIVE-14498.mysql.sql;
+SOURCE 049-HIVE-18489.mysql.sql;
 
 UPDATE VERSION SET SCHEMA_VERSION='3.0.0', VERSION_COMMENT='Hive release version 3.0.0' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 2.3.0 to 3.0.0' AS ' ';
