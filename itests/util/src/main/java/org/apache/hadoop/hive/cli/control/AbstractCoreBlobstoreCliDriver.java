e3dbaf9055204d485a292e4fdb322793fb4e870c
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2018-03-14 15:32:34 +0100

    HIVE-18560: qtests: QTestUtil refactor/split - QOutProcessor (Laszlo Bodor via Zoltan Haindrich)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>
    (cherry picked from commit db4fe384fdf57bb89d1f468b68ae6625e6f0ba77)
    
    Change-Id: Id59195935f52f298e90bfe759b0d0bd6b1faf647

--- itests/util/src/main/java/org/apache/hadoop/hive/cli/control/AbstractCoreBlobstoreCliDriver.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/AbstractCoreBlobstoreCliDriver.java
@@ -27,7 +27,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Map;
 
-import org.apache.hadoop.hive.cli.control.AbstractCliConfig.MetastoreType;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveVariableSource;
 import org.apache.hadoop.hive.conf.VariableSubstitution;
@@ -39,6 +38,8 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+
+import com.google.common.base.Strings;
 
 public abstract class AbstractCoreBlobstoreCliDriver extends CliAdapter {
 
@@ -180,6 +181,7 @@ public abstract class AbstractCoreBlobstoreCliDriver extends CliAdapter {
         + "-" + String.format("%03d", (int)(Math.random() * 999));
     testBlobstorePathUnique = testBlobstorePath + uid;
 
-    qt.addPatternWithMaskComment(testBlobstorePathUnique, String.format("### %s ###", HCONF_TEST_BLOBSTORE_PATH));
+    qt.getQOutProcessor().addPatternWithMaskComment(testBlobstorePathUnique,
+        String.format("### %s ###", HCONF_TEST_BLOBSTORE_PATH));
   }
 }
