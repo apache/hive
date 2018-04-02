fabfcaa39f075af529ec31e81042477fa6ba2d44
Prasanth Jayachandran <prasanthj@apache.org>
2018-04-01 22:42:28 -0700

    HIVE-19033: Provide an option to purge LLAP IO cache (Prasanth Jayachandran reviewed by Sergey Shelukhin)
    (cherry picked from commit 13f59a2267ed9c73ee729ba0f1925054e07d424b)
    
    Change-Id: I59b8e15b8ee3d3fee96b539bdd1a82792a6e4573

--- llap-server/src/java/org/apache/hadoop/hive/llap/cache/LowLevelFifoCachePolicy.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/cache/LowLevelFifoCachePolicy.java
@@ -24,6 +24,7 @@ import java.util.LinkedList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import org.apache.hadoop.hive.llap.LlapUtil;
 import org.apache.hadoop.hive.llap.cache.LowLevelCache.Priority;
 import org.apache.hadoop.hive.llap.io.api.impl.LlapIoImpl;
 
@@ -67,6 +68,13 @@ public class LowLevelFifoCachePolicy implements LowLevelCachePolicy {
   @Override
   public void setParentDebugDumper(LlapOomDebugDump dumper) {
     this.parentDebugDump = dumper;
+  }
+
+  @Override
+  public long purge() {
+    long evicted = evictSomeBlocks(Long.MAX_VALUE);
+    LlapIoImpl.LOG.info("PURGE: evicted {} from FIFO policy", LlapUtil.humanReadableByteCount(evicted));
+    return evicted;
   }
 
   @Override
