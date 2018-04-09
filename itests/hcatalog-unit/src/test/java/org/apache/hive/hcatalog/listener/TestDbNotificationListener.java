bd01805635495896d7ccb454f7f0836787da9682
Sergio Pena <sergio.pena@cloudera.com>
2018-04-09 10:04:43 -0500

    HIVE-18783: ALTER TABLE post-commit listener does not include the transactional listener responses (Sergio Pena, reviewed by Vihang Karajgaonkar)

--- itests/hcatalog-unit/src/test/java/org/apache/hive/hcatalog/listener/TestDbNotificationListener.java
+++ itests/hcatalog-unit/src/test/java/org/apache/hive/hcatalog/listener/TestDbNotificationListener.java
@@ -472,6 +472,7 @@ public class TestDbNotificationListener {
     assertEquals(TableType.MANAGED_TABLE.toString(), alterTableMessage.getTableType());
 
     // Verify the eventID was passed to the non-transactional listener
+    MockMetaStoreEventListener.popAndVerifyLastEventId(EventType.ALTER_TABLE, firstEventId + 2);
     MockMetaStoreEventListener.popAndVerifyLastEventId(EventType.CREATE_TABLE, firstEventId + 1);
 
     // When hive.metastore.transactional.event.listeners is set,
