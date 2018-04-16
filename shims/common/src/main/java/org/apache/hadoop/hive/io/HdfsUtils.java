64c1b5aec8bf6c53f380bb5e1b9f4c6eebe79b6b
Sahil Takiar <takiar.sahil@gmail.com>
2018-04-15 20:40:51 -0700

    HIVE-18319: Upgrade to Hadoop 3.0.0 (Sahil Takiar, reviewed by Ashutosh Chauhan)

--- shims/common/src/main/java/org/apache/hadoop/hive/io/HdfsUtils.java
+++ shims/common/src/main/java/org/apache/hadoop/hive/io/HdfsUtils.java
@@ -38,7 +38,6 @@ import org.apache.hadoop.fs.permission.AclEntryType;
 import org.apache.hadoop.fs.permission.AclStatus;
 import org.apache.hadoop.fs.permission.FsAction;
 import org.apache.hadoop.fs.permission.FsPermission;
-import org.apache.hadoop.hdfs.DistributedFileSystem;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
