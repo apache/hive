331dd57a0f9f8263d4e4a972042735d577d1240e
Antal Sinkovits <asinkovits@cloudera.com>
2018-04-27 13:02:20 -0500

    HIVE-18903: Lower Logging Level for ObjectStore (Antal Sinkovits, reviewed by Sahil Takiar)

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -392,7 +392,7 @@ public class ObjectStore implements RawStore, Configurable {
         throw new RuntimeException(
         "Unable to create persistence manager. Check dss.log for details");
       } else {
-        LOG.info("Initialized ObjectStore");
+        LOG.debug("Initialized ObjectStore");
       }
     } finally {
       pmfPropLock.unlock();
@@ -477,7 +477,7 @@ public class ObjectStore implements RawStore, Configurable {
    * @param dsProps
    */
   private void initializeHelper(Properties dsProps) {
-    LOG.info("ObjectStore, initialize called");
+    LOG.debug("ObjectStore, initialize called");
     prop = dsProps;
     pm = getPersistenceManager();
     try {
