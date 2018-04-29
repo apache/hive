a68ddd1481b013ea7d1f86cbd242204445c6f1c9
Matt McCline <mmccline@hortonworks.com>
2018-04-29 14:20:39 -0500

    HIVE-19352: Vectorization: Disable vectorization for org.apache.hive.jdbc.TestJdbcDriver2.testResultSetMetaData (Matt McCline, reviewed by Vineet Garg)
    (cherry picked from commit 0a7c9743b4c39bcfd3182f947704aeda95ffd1f2)
    
    Change-Id: Ib535c43885cea4eddffda3906a109dfb4e183196

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestJdbcDriver2.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestJdbcDriver2.java
@@ -1580,6 +1580,8 @@ public class TestJdbcDriver2 {
   public void testResultSetMetaData() throws SQLException {
     Statement stmt = con.createStatement();
 
+    stmt.execute("set " + HiveConf.ConfVars.HIVE_VECTORIZATION_ENABLED.varname + "=false");
+
     ResultSet res =
         stmt.executeQuery("select c1, c2, c3, c4, c5 as a, c6, c7, c8, c9, c10, c11, c12, "
             + "c1*2, sentences(null, null, null) as b, c17, c18, c20, c21, c22, c23, null as null_val from "
