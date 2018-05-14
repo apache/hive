e257d2bc9fd94fae9869c714762aad9233d4dae6
Yongzhi Chen <ychena@apache.org>
2018-05-14 14:56:15 -0400

    Revert "HIVE-13745: UDF current_date、current_timestamp、unix_timestamp NPE (Biao Wu, reviewed by Yongzhi Chen)"
    This reverts commit 9733539346c6552cac0ee8bff703321a00657f39.

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFCurrentDate.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFCurrentDate.java
@@ -18,12 +18,8 @@
 package org.apache.hadoop.hive.ql.udf.generic;
 
 import java.sql.Date;
-import java.sql.Timestamp;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.exec.Description;
-import org.apache.hadoop.hive.ql.exec.MapredContext;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
@@ -43,13 +39,6 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
 public class GenericUDFCurrentDate extends GenericUDF {
 
   protected DateWritable currentDate;
-  private Configuration conf;
-
-  @Override
-  public void configure(MapredContext context) {
-    super.configure(context);
-    conf = context.getJobConf();
-  }
 
   @Override
   public ObjectInspector initialize(ObjectInspector[] arguments)
@@ -61,21 +50,8 @@ public class GenericUDFCurrentDate extends GenericUDF {
     }
 
     if (currentDate == null) {
-      SessionState ss = SessionState.get();
-      Timestamp queryTimestamp;
-      if (ss == null) {
-        if (conf == null) {
-          queryTimestamp = new Timestamp(System.currentTimeMillis());
-        } else {
-          queryTimestamp = new Timestamp(
-                  HiveConf.getLongVar(conf, HiveConf.ConfVars.HIVE_QUERY_TIMESTAMP));
-        }
-      } else {
-        queryTimestamp = ss.getQueryCurrentTimestamp();
-      }
-
       Date dateVal =
-              Date.valueOf(queryTimestamp.toString().substring(0, 10));
+          Date.valueOf(SessionState.get().getQueryCurrentTimestamp().toString().substring(0, 10));
       currentDate = new DateWritable(dateVal);
     }
 
--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFCurrentTimestamp.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFCurrentTimestamp.java
@@ -17,12 +17,7 @@
  */
 package org.apache.hadoop.hive.ql.udf.generic;
 
-import java.sql.Timestamp;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.exec.Description;
-import org.apache.hadoop.hive.ql.exec.MapredContext;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
@@ -42,13 +37,6 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
 public class GenericUDFCurrentTimestamp extends GenericUDF {
 
   protected TimestampWritable currentTimestamp;
-  private Configuration conf;
-
-  @Override
-  public void configure(MapredContext context) {
-    super.configure(context);
-    conf = context.getJobConf();
-  }
 
   @Override
   public ObjectInspector initialize(ObjectInspector[] arguments)
@@ -60,19 +48,7 @@ public class GenericUDFCurrentTimestamp extends GenericUDF {
     }
 
     if (currentTimestamp == null) {
-      SessionState ss = SessionState.get();
-      Timestamp queryTimestamp;
-      if (ss == null) {
-        if (conf == null) {
-          queryTimestamp = new Timestamp(System.currentTimeMillis());
-        } else {
-          queryTimestamp = new Timestamp(
-                  HiveConf.getLongVar(conf, HiveConf.ConfVars.HIVE_QUERY_TIMESTAMP));
-        }
-      } else {
-        queryTimestamp = ss.getQueryCurrentTimestamp();
-      }
-      currentTimestamp = new TimestampWritable(queryTimestamp);
+      currentTimestamp = new TimestampWritable(SessionState.get().getQueryCurrentTimestamp());
     }
 
     return PrimitiveObjectInspectorFactory.writableTimestampObjectInspector;
