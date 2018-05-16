256a8954a56c9a263d47d77461c065f8f5e05ad5
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-05-16 11:53:29 -0700

    HIVE-19572: Add option to mask stats and data size in q files (Jesus Camacho Rodriguez, reviewed by Prasanth Jayachandran)
    (cherry picked from commit d02849db95e139b91e218b39f4702fbc3eaee231)
    
    Change-Id: I21094df7a76372adfc129ad1c26d779e5fd3a2a3

--- itests/util/src/main/java/org/apache/hadoop/hive/ql/QOutProcessor.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/ql/QOutProcessor.java
@@ -43,7 +43,7 @@ public class QOutProcessor {
   public static final String PATH_HDFS_WITH_DATE_USER_GROUP_REGEX =
       "([a-z]+) ([a-z]+)([ ]+)([0-9]+) ([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}) "
           + PATH_HDFS_REGEX;
-  
+
   public static final String HDFS_MASK = "### HDFS PATH ###";
   public static final String HDFS_DATE_MASK = "### HDFS DATE ###";
   public static final String HDFS_USER_MASK = "### USER ###";
@@ -51,6 +51,13 @@ public class QOutProcessor {
   
   public static final String MASK_PATTERN = "#### A masked pattern was here ####";
   public static final String PARTIAL_MASK_PATTERN = "#### A PARTIAL masked pattern was here ####";
+
+  private static final PatternReplacementPair MASK_STATS = new PatternReplacementPair(
+      Pattern.compile(" Num rows: [1-9][0-9]* Data size: [1-9][0-9]*"),
+      " Num rows: ###Masked### Data size: ###Masked###");
+  private static final PatternReplacementPair MASK_DATA_SIZE = new PatternReplacementPair(
+      Pattern.compile(" Data size: [1-9][0-9]*"),
+      " Data size: ###Masked###");
 
   private FsType fsType = FsType.local;
 
@@ -130,7 +137,7 @@ public class QOutProcessor {
     return patterns;
   }
 
-  public void maskPatterns(String fname) throws Exception {
+  public void maskPatterns(String fname, boolean maskStats, boolean maskDataSize) throws Exception {
     String line;
     BufferedReader in;
     BufferedWriter out;
@@ -145,7 +152,7 @@ public class QOutProcessor {
     boolean lastWasMasked = false;
 
     while (null != (line = in.readLine())) {
-      LineProcessingResult result = processLine(line);
+      LineProcessingResult result = processLine(line, maskStats, maskDataSize);
 
       if (result.line.equals(MASK_PATTERN)) {
         // We're folding multiple masked lines into one.
@@ -167,7 +174,7 @@ public class QOutProcessor {
     out.close();
   }
 
-  public LineProcessingResult processLine(String line) {
+  public LineProcessingResult processLine(String line, boolean maskStats, boolean maskDataSize) {
     LineProcessingResult result = new LineProcessingResult(line);
     
     Matcher matcher = null;
@@ -202,6 +209,22 @@ public class QOutProcessor {
           result.line = matcher.replaceAll(maskComment);
           result.partialMaskWasMatched = true;
           break;
+        }
+      }
+
+      if (!result.partialMaskWasMatched && maskStats) {
+        matcher = MASK_STATS.pattern.matcher(result.line);
+        if (matcher.find()) {
+          result.line = result.line.replaceAll(MASK_STATS.pattern.pattern(), MASK_STATS.replacement);
+          result.partialMaskWasMatched = true;
+        }
+      }
+
+      if (!result.partialMaskWasMatched && maskDataSize) {
+        matcher = MASK_DATA_SIZE.pattern.matcher(result.line);
+        if (matcher.find()) {
+          result.line = result.line.replaceAll(MASK_DATA_SIZE.pattern.pattern(), MASK_DATA_SIZE.replacement);
+          result.partialMaskWasMatched = true;
         }
       }
 
