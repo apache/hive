61dcea79ce4a113608aa4b65af29b03ef36726ec
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-05-18 18:07:06 -0700

    HIVE-19594: Add custom tmp folders to tests to avoid collisions (Jesus Camacho Rodriguez, reviewed by Prasanth Jayachandran)

--- hcatalog/core/src/test/java/org/apache/hive/hcatalog/mapreduce/HCatBaseTest.java
+++ hcatalog/core/src/test/java/org/apache/hive/hcatalog/mapreduce/HCatBaseTest.java
@@ -20,6 +20,7 @@
 package org.apache.hive.hcatalog.mapreduce;
 
 import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.cli.CliSessionState;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
@@ -78,6 +79,16 @@ public abstract class HCatBaseTest {
    */
   protected void setUpHiveConf() {
     hiveConf = new HiveConf(this.getClass());
+    Path workDir = new Path(System.getProperty("test.tmp.dir",
+        "target" + File.separator + "test" + File.separator + "tmp"));
+    hiveConf.set("mapred.local.dir", workDir + File.separator + this.getClass().getSimpleName()
+        + File.separator + "mapred" + File.separator + "local");
+    hiveConf.set("mapred.system.dir", workDir + File.separator + this.getClass().getSimpleName()
+        + File.separator + "mapred" + File.separator + "system");
+    hiveConf.set("mapreduce.jobtracker.staging.root.dir", workDir + File.separator + this.getClass().getSimpleName()
+        + File.separator + "mapred" + File.separator + "staging");
+    hiveConf.set("mapred.temp.dir", workDir + File.separator + this.getClass().getSimpleName()
+        + File.separator + "mapred" + File.separator + "temp");
     hiveConf.setVar(HiveConf.ConfVars.PREEXECHOOKS, "");
     hiveConf.setVar(HiveConf.ConfVars.POSTEXECHOOKS, "");
     hiveConf.setBoolVar(HiveConf.ConfVars.HIVE_SUPPORT_CONCURRENCY, false);
@@ -107,6 +118,17 @@ public abstract class HCatBaseTest {
   public static PigServer createPigServer(boolean stopOnFailure) throws ExecException {
     if(stopOnFailure) {
       Properties p = new Properties();
+      Path workDir = new Path(System.getProperty("test.tmp.dir",
+          "target" + File.separator + "test" + File.separator + "tmp"));
+      String testId = "HCatBaseTest_" + System.currentTimeMillis();
+      p.put("mapred.local.dir", workDir + File.separator + testId
+          + File.separator + "mapred" + File.separator + "local");
+      p.put("mapred.system.dir", workDir + File.separator + testId
+          + File.separator + "mapred" + File.separator + "system");
+      p.put("mapreduce.jobtracker.staging.root.dir", workDir + File.separator + testId
+          + File.separator + "mapred" + File.separator + "staging");
+      p.put("mapred.temp.dir", workDir + File.separator + testId
+          + File.separator + "mapred" + File.separator + "temp");
       p.put("stop.on.failure", Boolean.TRUE.toString());
       return new PigServer(ExecType.LOCAL, p);
     }
