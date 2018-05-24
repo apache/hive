402a443072710e1db34ede4bab6bab934fc8d9fc
sergey <sershe@apache.org>
2018-05-24 14:32:00 -0700

    HIVE-19631 : reduce epic locking in AbstractService (Sergey Shelukhin, reviewed by Thejas M Nair)

--- service/src/java/org/apache/hive/service/server/HiveServer2.java
+++ service/src/java/org/apache/hive/service/server/HiveServer2.java
@@ -772,7 +772,7 @@ public class HiveServer2 extends CompositeService {
       LOG.info("Started/Reconnected tez sessions.");
 
       // resolve futures used for testing
-      if (HiveConf.getBoolVar(hiveServer2.hiveConf, ConfVars.HIVE_IN_TEST)) {
+      if (HiveConf.getBoolVar(hiveServer2.getHiveConf(), ConfVars.HIVE_IN_TEST)) {
         hiveServer2.isLeaderTestFuture.set(true);
         hiveServer2.resetNotLeaderTestFuture();
       }
@@ -787,7 +787,7 @@ public class HiveServer2 extends CompositeService {
       LOG.info("Stopped/Disconnected tez sessions.");
 
       // resolve futures used for testing
-      if (HiveConf.getBoolVar(hiveServer2.hiveConf, ConfVars.HIVE_IN_TEST)) {
+      if (HiveConf.getBoolVar(hiveServer2.getHiveConf(), ConfVars.HIVE_IN_TEST)) {
         hiveServer2.notLeaderTestFuture.set(true);
         hiveServer2.resetIsLeaderTestFuture();
       }
@@ -802,14 +802,15 @@ public class HiveServer2 extends CompositeService {
       try {
         resourcePlan = sessionHive.getActiveResourcePlan();
       } catch (HiveException e) {
-        if (!HiveConf.getBoolVar(hiveConf, ConfVars.HIVE_IN_TEST_SSL)) {
+        if (!HiveConf.getBoolVar(getHiveConf(), ConfVars.HIVE_IN_TEST_SSL)) {
           throw new RuntimeException(e);
         } else {
           resourcePlan = null; // Ignore errors in SSL tests where the connection is misconfigured.
         }
       }
 
-      if (resourcePlan == null && HiveConf.getBoolVar(hiveConf, ConfVars.HIVE_IN_TEST)) {
+      if (resourcePlan == null && HiveConf.getBoolVar(
+          getHiveConf(), ConfVars.HIVE_IN_TEST)) {
         LOG.info("Creating a default resource plan for test");
         resourcePlan = createTestResourcePlan();
       }
@@ -825,6 +826,7 @@ public class HiveServer2 extends CompositeService {
       // will be invoked anyway in TezTask. Doing it early to initialize triggers for non-pool tez session.
       LOG.info("Initializing tez session pool manager");
       tezSessionPoolManager = TezSessionPoolManager.getInstance();
+      HiveConf hiveConf = getHiveConf();
       if (hiveConf.getBoolVar(ConfVars.HIVE_SERVER2_TEZ_INITIALIZE_DEFAULT_SESSIONS)) {
         tezSessionPoolManager.setupPool(hiveConf);
       } else {
@@ -842,7 +844,7 @@ public class HiveServer2 extends CompositeService {
       // Initialize workload management.
       LOG.info("Initializing workload management");
       try {
-        wm = WorkloadManager.create(wmQueue, hiveConf, resourcePlan);
+        wm = WorkloadManager.create(wmQueue, getHiveConf(), resourcePlan);
         wm.start();
         LOG.info("Workload manager initialized.");
       } catch (Exception e) {
