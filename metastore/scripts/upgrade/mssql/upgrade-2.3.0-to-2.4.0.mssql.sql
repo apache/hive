977ea45599cc0948b74825fe086a768bc133e3ff
Sergio Pena <sergio.pena@cloudera.com>
2018-05-24 09:52:24 -0500

    HIVE-19527: Preparing for 2.4 development (Sergio Pena, reviewed by Peter Vary)

---
+++ metastore/scripts/upgrade/mssql/upgrade-2.3.0-to-2.4.0.mssql.sql
@@ -0,0 +1,4 @@
+SELECT 'Upgrading MetaStore schema from 2.3.0 to 2.4.0' AS MESSAGE;
+
+UPDATE VERSION SET SCHEMA_VERSION='2.4.0', VERSION_COMMENT='Hive release version 2.4.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 2.3.0 to 2.4.0' AS MESSAGE;
---
+++ metastore/scripts/upgrade/oracle/upgrade-2.3.0-to-2.4.0.oracle.sql
@@ -0,0 +1,6 @@
+SELECT 'Upgrading MetaStore schema from 2.3.0 to 2.4.0' AS Status from dual;
+
+@040-HIVE-16399.oracle.sql;
+
+UPDATE VERSION SET SCHEMA_VERSION='2.4.0', VERSION_COMMENT='Hive release version 2.4.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 2.3.0 to 2.4.0' AS Status from dual;
