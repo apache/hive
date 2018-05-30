801399d8a68504862554e3a15c8bc372b34389ca
Vihang Karajgaonkar <vihang@cloudera.com>
2018-05-30 10:20:20 -0700

    HIVE-19605 : TAB_COL_STATS table has no index on db/table name (Vihang Karajgaonkar reviewed by Yongzhi Chen)

--- standalone-metastore/src/main/sql/postgres/hive-schema-3.1.0.postgres.sql
+++ standalone-metastore/src/main/sql/postgres/hive-schema-3.1.0.postgres.sql
@@ -1282,6 +1282,11 @@ CREATE INDEX "TBLS_N50" ON "TBLS" USING btree ("SD_ID");
 
 CREATE INDEX "TBL_COL_PRIVS_N49" ON "TBL_COL_PRIVS" USING btree ("TBL_ID");
 
+--
+-- Name: TAB_COL_STATS_IDX; Type: INDEX; Schema: public; Owner: hiveuser; Tablespace:
+--
+
+CREATE INDEX "TAB_COL_STATS_IDX" ON "TAB_COL_STATS" USING btree ("CAT_NAME", "DB_NAME","TABLE_NAME","COLUMN_NAME");
 
 --
 -- Name: TBL_PRIVS_N49; Type: INDEX; Schema: public; Owner: hiveuser; Tablespace:
