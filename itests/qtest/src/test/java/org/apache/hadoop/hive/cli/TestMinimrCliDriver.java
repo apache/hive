ec2e2ad6c6775d4aae0efdab52419b5c12c9fffa
Prasanth Jayachandran <prasanthj@apache.org>
2018-06-03 09:23:57 -0700

    HIVE-19633 : Remove/Migrate Minimr tests (Prasanth J via Sergey Shelukhin)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- itests/qtest/src/test/java/org/apache/hadoop/hive/cli/TestMinimrCliDriver.java
+++ itests/qtest/src/test/java/org/apache/hadoop/hive/cli/TestMinimrCliDriver.java
@@ -23,6 +23,7 @@ import java.util.List;
 import org.apache.hadoop.hive.cli.control.CliAdapter;
 import org.apache.hadoop.hive.cli.control.CliConfigs;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
@@ -54,6 +55,7 @@ public class TestMinimrCliDriver {
     this.qfile = qfile;
   }
 
+  @Ignore
   @Test
   public void testCliDriver() throws Exception {
     adapter.runTest(name, qfile);
