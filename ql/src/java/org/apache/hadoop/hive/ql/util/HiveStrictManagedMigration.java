4c5ec6a23385ce928e8c2690d76a8b5db3d4779d
Jason Dere <jdere@hortonworks.com>
2018-06-24 10:09:43 -0700

    HIVE-19753: Strict managed tables mode in Hive (Jason Dere, reviewed by Eugene Koifman)

--- ql/src/java/org/apache/hadoop/hive/ql/util/HiveStrictManagedMigration.java
+++ ql/src/java/org/apache/hadoop/hive/ql/util/HiveStrictManagedMigration.java
@@ -47,6 +47,7 @@ import org.apache.hadoop.hive.metastore.TableType;
 import org.apache.hadoop.hive.metastore.TransactionalValidationListener;
 import org.apache.hadoop.hive.metastore.Warehouse;
 import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
+import org.apache.hadoop.hive.metastore.utils.HiveStrictManagedUtils;
 import org.apache.hadoop.hive.metastore.utils.MetaStoreUtils;
 import org.apache.hadoop.hive.ql.DriverFactory;
 import org.apache.hadoop.hive.ql.IDriver;
