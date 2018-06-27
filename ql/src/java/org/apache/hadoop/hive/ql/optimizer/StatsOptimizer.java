f9622c960a58c47d3e5456177ddd83ed06cbcc86
sergey <sershe@apache.org>
2018-06-26 19:11:17 -0700

    HIVE-19532 : fix tests for master-txnstats branch (Sergey Shelukhin) ADDENDUM

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/StatsOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/StatsOptimizer.java
@@ -919,7 +919,7 @@ public class StatsOptimizer extends Transform {
         partNames.add(part.getName());
       }
       AcidUtils.TableSnapshot tableSnapshot =
-          AcidUtils.getTableSnapshot(hive.getConf(), tbl, true);
+          AcidUtils.getTableSnapshot(hive.getConf(), tbl);
 
       Map<String, List<ColumnStatisticsObj>> result = hive.getMSC().getPartitionColumnStatistics(
           tbl.getDbName(), tbl.getTableName(), partNames, Lists.newArrayList(colName),
