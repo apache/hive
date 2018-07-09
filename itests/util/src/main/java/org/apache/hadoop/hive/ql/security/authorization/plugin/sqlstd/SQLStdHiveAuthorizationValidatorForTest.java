77f31e62a15f01929e5e99aca5b52bdd33d57f28
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-07-09 12:35:10 -0700

    BUG-107146/HIVE-20123: Fix masking tests after HIVE-19617
    Change-Id: Ibab442ad3ed54b116a27a70b32a54923b5909c92

--- itests/util/src/main/java/org/apache/hadoop/hive/ql/security/authorization/plugin/sqlstd/SQLStdHiveAuthorizationValidatorForTest.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/ql/security/authorization/plugin/sqlstd/SQLStdHiveAuthorizationValidatorForTest.java
@@ -119,7 +119,7 @@ public class SQLStdHiveAuthorizationValidatorForTest extends SQLStdHiveAuthoriza
       List<HivePrivilegeObject> privObjs) throws SemanticException {
     List<HivePrivilegeObject> needRewritePrivObjs = new ArrayList<>(); 
     for (HivePrivilegeObject privObj : privObjs) {
-      if (privObj.getObjectName().equals("masking_test")) {
+      if (privObj.getObjectName().equals("masking_test") || privObj.getObjectName().startsWith("masking_test_n")) {
         privObj.setRowFilterExpression("key % 2 = 0 and key < 10");
         List<String> cellValueTransformers = new ArrayList<>();
         for (String columnName : privObj.getColumns()) {
@@ -131,7 +131,7 @@ public class SQLStdHiveAuthorizationValidatorForTest extends SQLStdHiveAuthoriza
         }
         privObj.setCellValueTransformers(cellValueTransformers);
         needRewritePrivObjs.add(privObj);
-      } else if (privObj.getObjectName().equals("masking_test_view")) {
+      } else if (privObj.getObjectName().equals("masking_test_view") || privObj.getObjectName().startsWith("masking_test_view_n")) {
         privObj.setRowFilterExpression("key > 6");
         List<String> cellValueTransformers = new ArrayList<>();
         for (String columnName : privObj.getColumns()) {
@@ -143,14 +143,14 @@ public class SQLStdHiveAuthorizationValidatorForTest extends SQLStdHiveAuthoriza
         }
         privObj.setCellValueTransformers(cellValueTransformers);
         needRewritePrivObjs.add(privObj);
-      } else if (privObj.getObjectName().equals("masking_test_subq")) {
+      } else if (privObj.getObjectName().equals("masking_test_subq") || privObj.getObjectName().startsWith("masking_test_subq_n")) {
         privObj
-            .setRowFilterExpression("key in (select key from src where src.key = masking_test_subq.key)");
+            .setRowFilterExpression("key in (select key from src where src.key = " + privObj.getObjectName() + ".key)");
         needRewritePrivObjs.add(privObj);
-      } else if (privObj.getObjectName().equals("masking_acid_no_masking")) {
+      } else if (privObj.getObjectName().equals("masking_acid_no_masking") || privObj.getObjectName().startsWith("masking_acid_no_masking_n")) {
         // testing acid usage when no masking/filtering is present
         needRewritePrivObjs.add(privObj);
-      } else if (privObj.getObjectName().equals("masking_test_druid")) {
+      } else if (privObj.getObjectName().equals("masking_test_druid") || privObj.getObjectName().startsWith("masking_test_druid_n")) {
         // testing druid queries row filtering is present
         privObj.setRowFilterExpression("key > 10");
         needRewritePrivObjs.add(privObj);
