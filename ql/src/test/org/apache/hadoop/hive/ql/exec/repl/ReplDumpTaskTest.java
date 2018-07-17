75818146196e230845756519514c29a1571d9fcb
Anishek Agarwal <anishek@gmail.com>
2018-07-17 11:41:54 +0530

    HIVE-20152: reset db state, when repl dump fails, so rename table can be done. Fixing the checkstyle issues and license issues

--- ql/src/test/org/apache/hadoop/hive/ql/exec/repl/ReplDumpTaskTest.java
+++ ql/src/test/org/apache/hadoop/hive/ql/exec/repl/ReplDumpTaskTest.java
@@ -1,10 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hive.ql.exec.repl;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.metadata.Hive;
-import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.parse.repl.dump.Utils;
-import org.apache.thrift.TException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -21,7 +36,6 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.mock;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -42,30 +56,30 @@ public class ReplDumpTaskTest {
     }
 
     @Override
-    long currentNotificationId(Hive hiveDb) throws TException {
+    long currentNotificationId(Hive hiveDb) {
       return Long.MAX_VALUE;
     }
 
     @Override
-    String getValidTxnListForReplDump(Hive hiveDb) throws HiveException {
+    String getValidTxnListForReplDump(Hive hiveDb) {
       return "";
     }
 
     @Override
-    void dumpFunctionMetadata(String dbName, Path dumpRoot) throws Exception {
+    void dumpFunctionMetadata(String dbName, Path dumpRoot) {
     }
 
     @Override
-    Path dumpDbMetadata(String dbName, Path dumpRoot, long lastReplId) throws Exception {
+    Path dumpDbMetadata(String dbName, Path dumpRoot, long lastReplId) {
       return Mockito.mock(Path.class);
     }
 
     @Override
-    void dumpConstraintMetadata(String dbName, String tblName, Path dbRoot) throws Exception {
+    void dumpConstraintMetadata(String dbName, String tblName, Path dbRoot) {
     }
   }
 
-  static class TestException extends Exception {
+  private static class TestException extends Exception {
   }
 
   @Test(expected = TestException.class)
