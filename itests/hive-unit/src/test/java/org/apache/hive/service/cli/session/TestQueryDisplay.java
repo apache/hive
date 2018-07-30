096627cd0fb9a87326dbc968cda0907759e3971a
sergey <sershe@apache.org>
2018-07-30 12:16:47 -0700

    HIVE-19568 : Active/Passive HS2 HA: Disallow direct connection to passive HS2 instance (Sergey Shelukhin, reviewed by Prasanth Jayachandran)

--- itests/hive-unit/src/test/java/org/apache/hive/service/cli/session/TestQueryDisplay.java
+++ itests/hive-unit/src/test/java/org/apache/hive/service/cli/session/TestQueryDisplay.java
@@ -48,7 +48,7 @@ public class TestQueryDisplay {
     conf = new HiveConf();
     conf.set("hive.support.concurrency", "false");
 
-    sessionManager = new SessionManager(null);
+    sessionManager = new SessionManager(null, true);
     sessionManager.init(conf);
   }
 
