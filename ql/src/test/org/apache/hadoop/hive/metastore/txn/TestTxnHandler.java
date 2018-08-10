cfe439a25752f57597d16c0e058351957ff6468f
Sankar Hariappan <sankarh@apache.org>
2018-08-10 22:26:24 +0530

    HIVE-20264: Bootstrap repl dump with concurrent write and drop of ACID table makes target inconsistent (Sankar Hariappan, reviewed by Mahesh Kumar Behera, Anishek Agarwal)

--- ql/src/test/org/apache/hadoop/hive/metastore/txn/TestTxnHandler.java
+++ ql/src/test/org/apache/hadoop/hive/metastore/txn/TestTxnHandler.java
@@ -1667,12 +1667,15 @@ public class TestTxnHandler {
     allocMsg = new AllocateTableWriteIdsRequest("destdb", "tbl2");
     allocMsg.setReplPolicy("destdb.*");
     allocMsg.setSrcTxnToWriteIdList(srcTxnToWriteId);
+
+    // This is an idempotent case when repl flow forcefully allocate write id if it doesn't match
+    // the next write id.
     try {
       txnHandler.allocateTableWriteIds(allocMsg).getTxnToWriteIds();
     } catch (IllegalStateException e) {
       failed = true;
     }
-    assertTrue(failed);
+    assertFalse(failed);
 
     replAbortTxnForTest(srcTxnIdList, "destdb.*");
 
