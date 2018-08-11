2d34d5b1d81411f639744bdc58bf3e22e8cefb55
Sankar Hariappan <sankarh@apache.org>
2018-08-11 08:52:20 +0530

    HIVE-20264: Bootstrap repl dump with concurrent write and drop of ACID table makes target inconsistent (Sankar Hariappan, reviewed by Mahesh Kumar Behera, Anishek Agarwal)

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenariosAcidTables.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenariosAcidTables.java
@@ -364,6 +364,85 @@ public class TestReplicationScenariosAcidTables {
   }
 
   @Test
+  public void testAcidTablesBootstrapWithConcurrentDropTable() throws Throwable {
+    HiveConf primaryConf = primary.getConf();
+    primary.run("use " + primaryDbName)
+            .run("create table t1 (id int) clustered by(id) into 3 buckets stored as orc " +
+                    "tblproperties (\"transactional\"=\"true\")")
+            .run("insert into t1 values(1)");
+
+    // Perform concurrent write + drop on the acid table t1 when bootstrap dump in progress. Bootstrap
+    // won't dump the table but the subsequent incremental repl with new table with same name should be seen.
+    BehaviourInjection<CallerArguments, Boolean> callerInjectedBehavior
+            = new BehaviourInjection<CallerArguments, Boolean>() {
+      @Nullable
+      @Override
+      public Boolean apply(@Nullable CallerArguments args) {
+        if (injectionPathCalled) {
+          nonInjectedPathCalled = true;
+        } else {
+          // Insert another row to t1 and drop the table from another txn when bootstrap dump in progress.
+          injectionPathCalled = true;
+          Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+              LOG.info("Entered new thread");
+              IDriver driver = DriverFactory.newDriver(primaryConf);
+              SessionState.start(new CliSessionState(primaryConf));
+              CommandProcessorResponse ret = driver.run("insert into " + primaryDbName + ".t1 values(2)");
+              boolean success = (ret.getException() == null);
+              assertTrue(success);
+              ret = driver.run("drop table " + primaryDbName + ".t1");
+              success = (ret.getException() == null);
+              assertTrue(success);
+              LOG.info("Exit new thread success - {}", success, ret.getException());
+            }
+          });
+          t.start();
+          LOG.info("Created new thread {}", t.getName());
+          try {
+            t.join();
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        }
+        return true;
+      }
+    };
+
+    InjectableBehaviourObjectStore.setCallerVerifier(callerInjectedBehavior);
+    WarehouseInstance.Tuple bootstrapDump = null;
+    try {
+      bootstrapDump = primary.dump(primaryDbName, null);
+      callerInjectedBehavior.assertInjectionsPerformed(true, true);
+    } finally {
+      InjectableBehaviourObjectStore.resetCallerVerifier(); // reset the behaviour
+    }
+
+    // Bootstrap dump has taken latest list of tables and hence won't see table t1 as it is dropped.
+    replica.load(replicatedDbName, bootstrapDump.dumpLocation)
+            .run("use " + replicatedDbName)
+            .run("repl status " + replicatedDbName)
+            .verifyResult(bootstrapDump.lastReplicationId)
+            .run("show tables")
+            .verifyResult(null);
+
+    // Create another ACID table with same name and insert a row. It should be properly replicated.
+    WarehouseInstance.Tuple incrementalDump = primary.run("use " + primaryDbName)
+            .run("create table t1 (id int) clustered by(id) into 3 buckets stored as orc " +
+                    "tblproperties (\"transactional\"=\"true\")")
+            .run("insert into t1 values(100)")
+            .dump(primaryDbName, bootstrapDump.lastReplicationId);
+
+    replica.load(replicatedDbName, incrementalDump.dumpLocation)
+            .run("use " + replicatedDbName)
+            .run("repl status " + replicatedDbName)
+            .verifyResult(incrementalDump.lastReplicationId)
+            .run("select id from t1 order by id")
+            .verifyResult("100");
+  }
+
+  @Test
   public void testOpenTxnEvent() throws Throwable {
     String tableName = testName.getMethodName();
     WarehouseInstance.Tuple bootStrapDump = primary.dump(primaryDbName, null);
