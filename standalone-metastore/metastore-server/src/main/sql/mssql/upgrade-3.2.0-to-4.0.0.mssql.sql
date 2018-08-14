5f993e24ce7410fd95873b3d39a1a4b846d9d827
sergey <sershe@apache.org>
2018-08-14 13:36:57 -0700

    HIVE-20298 : Illegal null value in column `TBLS`.`WRITE_ID` (Sergey Shelukhin, reviewed by Jason Dere)

--- standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-3.2.0-to-4.0.0.mssql.sql
+++ standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-3.2.0-to-4.0.0.mssql.sql
@@ -1,8 +1,8 @@
 SELECT 'Upgrading MetaStore schema from 3.2.0 to 4.0.0' AS MESSAGE;
 
 -- HIVE-19416
-ALTER TABLE TBLS ADD WRITE_ID bigint NULL;
-ALTER TABLE PARTITIONS ADD WRITE_ID bigint NULL;
+ALTER TABLE TBLS ADD WRITE_ID bigint  NOT NULL DEFAULT 0;
+ALTER TABLE PARTITIONS ADD WRITE_ID bigint  NOT NULL DEFAULT 0;
 
 -- These lines need to be last.  Insert any changes above.
 UPDATE VERSION SET SCHEMA_VERSION='4.0.0', VERSION_COMMENT='Hive release version 4.0.0' where VER_ID=1;
