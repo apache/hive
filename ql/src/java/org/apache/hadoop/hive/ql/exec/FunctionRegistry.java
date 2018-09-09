4c06079b1ffb5950ec978dd7588a88eedbd9264c
Deepak Jaiswal <djaiswal@apache.org>
2018-09-09 00:06:39 -0700

    HIVE-20510 : Vectorization : Support loading bucketed tables using sorted dynamic partition optimizer (Deepak Jaiswal, reviewed by Gopal Vijayarahavan, Matt Mccline, and Thejas Nair)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/FunctionRegistry.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/FunctionRegistry.java
@@ -514,6 +514,8 @@ public final class FunctionRegistry {
     system.registerGenericUDF("internal_interval", GenericUDFInternalInterval.class);
 
     system.registerGenericUDF("to_epoch_milli", GenericUDFEpochMilli.class);
+    system.registerGenericUDF("bucket_number", GenericUDFBucketNumber.class);
+
     // Generic UDTF's
     system.registerGenericUDTF("explode", GenericUDTFExplode.class);
     system.registerGenericUDTF("replicate_rows", GenericUDTFReplicateRows.class);
