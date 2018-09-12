9643f333f166dea825c2fde66540b0b7401573ab
Daniel Dai <daijyc@gmail.com>
2018-09-12 12:05:33 -0700

    Revert "BUG-110674: Make webhcat work on HDP3 (Add HIVEQUERYTAG to whitelist)"
    This reverts commit 5630865d7bcd7215a97de1fac86e09ce77544203.
    
    Change-Id: I80fa96604733829900404c57ab2a54d8e2333c31

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -5405,7 +5405,6 @@ public class HiveConf extends Configuration {
     ConfVars.SHOW_JOB_FAIL_DEBUG_INFO.varname,
     ConfVars.TASKLOG_DEBUG_TIMEOUT.varname,
     ConfVars.HIVEQUERYID.varname,
-    ConfVars.HIVEQUERYTAG.varname,
   };
 
   /**
