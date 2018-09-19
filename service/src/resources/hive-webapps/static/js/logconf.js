208e202f5b849ce62a12434da3ec648d949d40d6
Zoltan Chovan <zchovan@cloudera.com>
2018-09-19 09:57:52 -0700

    HIVE-20267: Expanding WebUI to include form to dynamically config log levels (Zoltan Chovan reviewed by Prasanth Jayachandran)

---
+++ service/src/resources/hive-webapps/static/js/logconf.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    // init the table with the current loggers
+    loadLoggers();
+
+    // set up event handler for submitting the form
+    $("#log-level-submit").click(function(e) {
+        setLoggersWithLevel(e);
+    });
+});
+
+function setLoggersWithLevel(e) {
+    console.log("handler called");
+    var loggerName = $("#logger-name").val();
+    var logLevel = $("#log-level").val();
+    var data = JSON.stringify( { "loggers" : [ { "logger" : loggerName, "level" : logLevel } ] } );
+
+    $.post('conflog', data, function() {
+        loadLoggers();
+    });
+}
+
+function loadLoggers() {
+    // clear the current content
+    $("#current-logs").html("");
+
+    // load and render the new
+    $.getJSON('conflog', function (data) {
+        var loggers = data.loggers;
+
+        $.each(loggers, function(i, logger) {
+            var logger_information = "<tr>\n" +
+                "                        <td>" + logger.logger + "</td>\n" +
+                "                        <td>" + logger.level + "</td>\n" +
+                "                    </tr>";
+            $("#current-logs").append(logger_information);
+        });
+
+    });
+}
