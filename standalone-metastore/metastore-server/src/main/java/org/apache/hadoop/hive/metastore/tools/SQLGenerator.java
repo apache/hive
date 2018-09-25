6137ee5dd23f6a0317489248bfd2a2a97d20a04c
Sankar Hariappan <sankarh@apache.org>
2018-09-25 23:56:51 +0530

    HIVE-20607: TxnHandler should use PreparedStatement to execute direct SQL queries (Sankar Hariappan, reviewed by Daniel Dai)

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/tools/SQLGenerator.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/tools/SQLGenerator.java
@@ -26,6 +26,9 @@ import org.apache.hadoop.hive.metastore.conf.MetastoreConf.ConfVars;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -47,18 +50,80 @@ public final class SQLGenerator {
   }
 
   /**
-   * Genereates "Insert into T(a,b,c) values(1,2,'f'),(3,4,'c')" for appropriate DB
+   * Generates "Insert into T(a,b,c) values(1,2,'f'),(3,4,'c')" for appropriate DB
+   *
+   * @param tblColumns   e.g. "T(a,b,c)"
+   * @param rows         e.g. list of Strings like 3,4,'d'
+   * @param paramsList   List of parameters which in turn is list of Strings to be set in PreparedStatement object
+   * @return List PreparedStatement objects for fully formed INSERT INTO ... statements
+   */
+  public List<PreparedStatement> createInsertValuesPreparedStmt(Connection dbConn,
+                                                                String tblColumns, List<String> rows,
+                                                                List<List<String>> paramsList)
+          throws SQLException {
+    if (rows == null || rows.size() == 0) {
+      return Collections.emptyList();
+    }
+    assert((paramsList == null) || (rows.size() == paramsList.size()));
+
+    List<Integer> rowsCountInStmts = new ArrayList<>();
+    List<String> insertStmts = createInsertValuesStmt(tblColumns, rows, rowsCountInStmts);
+    assert(insertStmts.size() == rowsCountInStmts.size());
+
+    List<PreparedStatement> preparedStmts = new ArrayList<>();
+    int paramsListFromIdx = 0;
+    try {
+      for (int stmtIdx = 0; stmtIdx < insertStmts.size(); stmtIdx++) {
+        String sql = insertStmts.get(stmtIdx);
+        PreparedStatement pStmt = prepareStmtWithParameters(dbConn, sql, null);
+        if (paramsList != null) {
+          int paramIdx = 1;
+          int paramsListToIdx = paramsListFromIdx + rowsCountInStmts.get(stmtIdx);
+          for (int paramsListIdx = paramsListFromIdx; paramsListIdx < paramsListToIdx; paramsListIdx++) {
+            List<String> params = paramsList.get(paramsListIdx);
+            for (int i = 0; i < params.size(); i++, paramIdx++) {
+              pStmt.setString(paramIdx, params.get(i));
+            }
+          }
+          paramsListFromIdx = paramsListToIdx;
+        }
+        preparedStmts.add(pStmt);
+      }
+    } catch (SQLException e) {
+      for (PreparedStatement pst : preparedStmts) {
+        pst.close();
+      }
+      throw e;
+    }
+    return preparedStmts;
+  }
+
+  /**
+   * Generates "Insert into T(a,b,c) values(1,2,'f'),(3,4,'c')" for appropriate DB
    *
    * @param tblColumns e.g. "T(a,b,c)"
    * @param rows       e.g. list of Strings like 3,4,'d'
    * @return fully formed INSERT INTO ... statements
    */
   public List<String> createInsertValuesStmt(String tblColumns, List<String> rows) {
+    return createInsertValuesStmt(tblColumns, rows, null);
+  }
+
+  /**
+   * Generates "Insert into T(a,b,c) values(1,2,'f'),(3,4,'c')" for appropriate DB
+   *
+   * @param tblColumns e.g. "T(a,b,c)"
+   * @param rows       e.g. list of Strings like 3,4,'d'
+   * @param rowsCountInStmts Output the number of rows in each insert statement returned.
+   * @return fully formed INSERT INTO ... statements
+   */
+  private List<String> createInsertValuesStmt(String tblColumns, List<String> rows, List<Integer> rowsCountInStmts) {
     if (rows == null || rows.size() == 0) {
       return Collections.emptyList();
     }
     List<String> insertStmts = new ArrayList<>();
     StringBuilder sb = new StringBuilder();
+    int numRowsInCurrentStmt = 0;
     switch (dbProduct) {
     case ORACLE:
       if (rows.size() > 1) {
@@ -69,15 +134,23 @@ public final class SQLGenerator {
             if (numRows > 0) {
               sb.append(" select * from dual");
               insertStmts.add(sb.toString());
+              if (rowsCountInStmts != null) {
+                rowsCountInStmts.add(numRowsInCurrentStmt);
+              }
+              numRowsInCurrentStmt = 0;
             }
             sb.setLength(0);
             sb.append("insert all ");
           }
           sb.append("into ").append(tblColumns).append(" values(").append(rows.get(numRows))
               .append(") ");
+          numRowsInCurrentStmt++;
         }
         sb.append("select * from dual");
         insertStmts.add(sb.toString());
+        if (rowsCountInStmts != null) {
+          rowsCountInStmts.add(numRowsInCurrentStmt);
+        }
         return insertStmts;
       }
       //fall through
@@ -89,13 +162,21 @@ public final class SQLGenerator {
         if (numRows % MetastoreConf.getIntVar(conf, ConfVars.DIRECT_SQL_MAX_ELEMENTS_VALUES_CLAUSE) == 0) {
           if (numRows > 0) {
             insertStmts.add(sb.substring(0, sb.length() - 1));//exclude trailing comma
+            if (rowsCountInStmts != null) {
+              rowsCountInStmts.add(numRowsInCurrentStmt);
+            }
+            numRowsInCurrentStmt = 0;
           }
           sb.setLength(0);
           sb.append("insert into ").append(tblColumns).append(" values");
         }
         sb.append('(').append(rows.get(numRows)).append("),");
+        numRowsInCurrentStmt++;
       }
       insertStmts.add(sb.substring(0, sb.length() - 1));//exclude trailing comma
+      if (rowsCountInStmts != null) {
+        rowsCountInStmts.add(numRowsInCurrentStmt);
+      }
       return insertStmts;
     default:
       String msg = "Unrecognized database product name <" + dbProduct + ">";
@@ -169,6 +250,33 @@ public final class SQLGenerator {
       LOG.error(msg);
       throw new MetaException(msg);
     }
+  }
+
+  /**
+   * Make PreparedStatement object with list of String type parameters to be set.
+   * It is assumed the input sql string have the number of "?" equal to number of parameters
+   * passed as input.
+   * @param dbConn - Connection object
+   * @param sql - SQL statement with "?" for input parameters.
+   * @param parameters - List of String type parameters to be set in PreparedStatement object
+   * @return PreparedStatement type object
+   * @throws SQLException
+   */
+  public PreparedStatement prepareStmtWithParameters(Connection dbConn, String sql, List<String> parameters)
+          throws SQLException {
+    PreparedStatement pst = dbConn.prepareStatement(addEscapeCharacters(sql));
+    if ((parameters == null) || parameters.isEmpty()) {
+      return pst;
+    }
+    try {
+      for (int i = 1; i <= parameters.size(); i++) {
+        pst.setString(i, parameters.get(i - 1));
+      }
+    } catch (SQLException e) {
+      pst.close();
+      throw e;
+    }
+    return pst;
   }
 
   public DatabaseProduct getDbProduct() {
