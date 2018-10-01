457080760e6be7afb12100fac03d99fc45419c95
Zoltan Haindrich <kirk@rxd.hu>
2018-10-01 08:52:36 +0200

    HIVE-20618: During join selection BucketMapJoin might be choosen for non bucketed tables (Zoltan Haindrich reviewed by Ashutosh Chauhan, Deepak Jaiswal)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/ConvertJoinMapJoin.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/ConvertJoinMapJoin.java
@@ -1429,7 +1429,7 @@ public class ConvertJoinMapJoin implements NodeProcessor {
             parentOp.getOpTraits().getNumBuckets() : numBuckets;
       }
 
-      if (parentOp instanceof ReduceSinkOperator) {
+      if (!useOpTraits && parentOp instanceof ReduceSinkOperator) {
         ReduceSinkOperator rs = (ReduceSinkOperator) parentOp;
         estimatedBuckets = (estimatedBuckets < rs.getConf().getNumReducers()) ?
             rs.getConf().getNumReducers() : estimatedBuckets;
