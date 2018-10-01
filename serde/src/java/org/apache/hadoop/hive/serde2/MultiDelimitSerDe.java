03b9da850b6b40252120e3cea688d3f6bb343262
Naveen Gangam <ngangam@apache.org>
2018-10-01 09:37:59 -0400

    HIVE-20619: Include MultiDelimitSerDe in HiveServer2 By Default (Alice Fan, reviewed by Naveen Gangam)
    (cherry picked from commit 2ad65f7e3b002eefddfc47edce151d6376a89481)

--- serde/src/java/org/apache/hadoop/hive/serde2/MultiDelimitSerDe.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/MultiDelimitSerDe.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hive.contrib.serde2;
+package org.apache.hadoop.hive.serde2;
 
 import java.io.IOException;
 import java.util.List;
