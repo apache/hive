24f7d2473c64053247c539af41288cf0e9110917
Ashutosh Bapat <abapat@hortonworks.com>
2018-10-12 07:28:21 -0500

    HIVE-20644 : Avoid exposing sensitive infomation through a Hive Runtime exception (Ashutosh Bapat reviewed by Thejas Nair, Sankar Hariappan)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/MapOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/MapOperator.java
@@ -562,9 +562,15 @@ public class MapOperator extends AbstractMapOperator {
         }
         if (row == null) {
           deserialize_error_count.set(deserialize_error_count.get() + 1);
-          throw new HiveException("Hive Runtime Error while processing writable " + message, e);
+          LOG.trace("Hive Runtime Error while processing writable " + message);
+          throw new HiveException("Hive Runtime Error while processing writable", e);
         }
-        throw new HiveException("Hive Runtime Error while processing row " + message, e);
+
+        // Log the contents of the row that caused exception so that it's available for debugging. But
+        // when exposed through an error message it can leak sensitive information, even to the
+        // client application.
+        LOG.trace("Hive Runtime Error while processing row " + message);
+        throw new HiveException("Hive Runtime Error while processing row", e);
       }
     }
     rowsForwarded(childrenDone, 1);
