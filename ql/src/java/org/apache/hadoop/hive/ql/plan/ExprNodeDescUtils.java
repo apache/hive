4c5087b42ee206199d37e90bc3adfc4fc39cd686
Jason Dere <jdere@hortonworks.com>
2018-11-05 13:43:29 -0800

    HIVE-20839: "Cannot find field" error during dynamically partitioned hash join (Jason Dere, reviewed by Vineet Garg)

--- ql/src/java/org/apache/hadoop/hive/ql/plan/ExprNodeDescUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/ExprNodeDescUtils.java
@@ -432,7 +432,7 @@ public class ExprNodeDescUtils {
     // Find the key/value where the ExprNodeDesc value matches the column we are searching for.
     // The key portion of the entry will be the internal column name for the join key expression.
     for (Map.Entry<String, ExprNodeDesc> mapEntry : reduceSinkOp.getColumnExprMap().entrySet()) {
-      if (mapEntry.getValue().isSame(source)) {
+      if (mapEntry.getValue().equals(source)) {
         String columnInternalName = mapEntry.getKey();
         if (source instanceof ExprNodeColumnDesc) {
           // The join key is a table column. Create the ExprNodeDesc based on this column.
