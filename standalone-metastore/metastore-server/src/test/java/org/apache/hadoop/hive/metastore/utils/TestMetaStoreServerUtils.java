39b4f9434f4fc5e1dca0df8ee3d98d86347efcf3
Laszlo Pinter <lpinter@cloudera.com>
2018-11-05 16:20:23 +0100

    HIVE-20796: jdbc URL can contain sensitive information that should not be logged (Laszlo Pinter, reviewed by Andrew Sherman, Denys Kuzmenko and Peter Vary)

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/utils/TestMetaStoreServerUtils.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/utils/TestMetaStoreServerUtils.java
@@ -64,6 +64,7 @@ import static org.apache.hadoop.hive.common.StatsSetupConst.TOTAL_SIZE;
 import static org.apache.hadoop.hive.metastore.utils.MetaStoreUtils.filterMapkeys;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -812,5 +813,35 @@ public class TestMetaStoreServerUtils {
     assertThat(partition.getSd().getLocation(), is("/a/b"));
     assertThat(partition.getValues(), is(Collections.singletonList("val2")));
   }
+
+  @Test
+  public void testAnonymizeConnectionURL() {
+    String connectionURL = null;
+    String expectedConnectionURL = null;
+    String result = MetaStoreServerUtils.anonymizeConnectionURL(connectionURL);
+    assertEquals(expectedConnectionURL, result);
+
+    connectionURL = "jdbc:mysql://localhost:1111/db?user=user&password=password";
+    expectedConnectionURL = "jdbc:mysql://localhost:1111/db?user=****&password=****";
+    result = MetaStoreServerUtils.anonymizeConnectionURL(connectionURL);
+    assertEquals(expectedConnectionURL, result);
+
+    connectionURL = "jdbc:derby:sample;user=jill;password=toFetchAPail";
+    expectedConnectionURL = "jdbc:derby:sample;user=****;password=****";
+    result = MetaStoreServerUtils.anonymizeConnectionURL(connectionURL);
+    assertEquals(expectedConnectionURL, result);
+
+    connectionURL = "jdbc:mysql://[(host=myhost1,port=1111,user=sandy,password=secret)," +
+            "(host=myhost2,port=2222,user=finn,password=secret)]/db";
+    expectedConnectionURL = "jdbc:mysql://[(host=myhost1,port=1111,user=****,password=****)," +
+            "(host=myhost2,port=2222,user=****,password=****)]/db";
+    result = MetaStoreServerUtils.anonymizeConnectionURL(connectionURL);
+    assertEquals(expectedConnectionURL, result);
+
+    connectionURL = "jdbc:derby:memory:${test.tmp.dir}/junit_metastore_db;create=true";
+    result = MetaStoreServerUtils.anonymizeConnectionURL(connectionURL);
+    assertEquals(connectionURL, result);
+  }
+
 }
 
