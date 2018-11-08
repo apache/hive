abf8cb1dc3dcd76473a1c17c1216647314c5b42d
Vihang Karajgaonkar <vihangk1@apache.org>
2018-11-08 10:49:27 -0800

    HIVE-16839 : HIVE-22365 : CDPD-4403 : Unbalanced calls to openTransaction/commitTransaction when alter the same partition concurrently (Guang Yang, reviewed by Karthik Manamcheri and Vihang Karajgaonkar)
    Change-Id: I0b9ae56eaa48bce4b4f48b950d7ad31117ac0695

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -2584,36 +2584,41 @@ public class ObjectStore implements RawStore, Configurable {
                                 List<String> part_vals,
                                 String validWriteIds)
       throws NoSuchObjectException, MetaException {
+    boolean committed = false;
     openTransaction();
-    MTable table = this.getMTable(catName, dbName, tableName);
-    MPartition mpart = getMPartition(catName, dbName, tableName, part_vals);
-    Partition part = convertToPart(mpart);
-    commitTransaction();
-    if(part == null) {
-      throw new NoSuchObjectException("partition values="
-          + part_vals.toString());
-    }
-    part.setValues(part_vals);
-    // If transactional table partition, check whether the current version partition
-    // statistics in the metastore comply with the client query's snapshot isolation.
-    long statsWriteId = mpart.getWriteId();
-    if (TxnUtils.isTransactionalTable(table.getParameters())) {
-      if (!areTxnStatsSupported) {
-        // Do not make persistent the following state since it is query specific (not global).
-        StatsSetupConst.setBasicStatsState(part.getParameters(), StatsSetupConst.FALSE);
-        LOG.info("Removed COLUMN_STATS_ACCURATE from Partition object's parameters.");
-      } else if (validWriteIds != null) {
-        if (isCurrentStatsValidForTheQuery(part, statsWriteId, validWriteIds, false)) {
-          part.setIsStatsCompliant(true);
-        } else {
-          part.setIsStatsCompliant(false);
+    try {
+      MTable table = this.getMTable(catName, dbName, tableName);
+      MPartition mpart = getMPartition(catName, dbName, tableName, part_vals);
+      Partition part = convertToPart(mpart);
+      committed = commitTransaction();
+      if (part == null) {
+        throw new NoSuchObjectException("partition values="
+                + part_vals.toString());
+      }
+      part.setValues(part_vals);
+      // If transactional table partition, check whether the current version partition
+      // statistics in the metastore comply with the client query's snapshot isolation.
+      long statsWriteId = mpart.getWriteId();
+      if (TxnUtils.isTransactionalTable(table.getParameters())) {
+        if (!areTxnStatsSupported) {
           // Do not make persistent the following state since it is query specific (not global).
           StatsSetupConst.setBasicStatsState(part.getParameters(), StatsSetupConst.FALSE);
           LOG.info("Removed COLUMN_STATS_ACCURATE from Partition object's parameters.");
+        } else if (validWriteIds != null) {
+          if (isCurrentStatsValidForTheQuery(part, statsWriteId, validWriteIds, false)) {
+            part.setIsStatsCompliant(true);
+          } else {
+            part.setIsStatsCompliant(false);
+            // Do not make persistent the following state since it is query specific (not global).
+            StatsSetupConst.setBasicStatsState(part.getParameters(), StatsSetupConst.FALSE);
+            LOG.info("Removed COLUMN_STATS_ACCURATE from Partition object's parameters.");
+          }
         }
       }
+      return part;
+    } finally {
+      rollbackAndCleanup(committed, (Query)null);
     }
-    return part;
   }
 
   /**
