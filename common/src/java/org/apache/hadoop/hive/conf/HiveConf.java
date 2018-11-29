073c7abb196b475d23e4b09305ecb6821af42d4e
Daniel Dai <daijyc@gmail.com>
2018-11-28 23:46:52 -0800

    HIVE-20978: "hive.jdbc.*" should add to sqlStdAuthSafeVarNameRegexes (Daniel Dai, reviewed by Jesus Camacho Rodriguez)
    Signed-off-by: Jesus Camacho Rodriguez <jcamacho@apache.org>

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -5576,6 +5576,7 @@ public class HiveConf extends Configuration {
     "hive\\.index\\..*",
     "hive\\.index\\..*",
     "hive\\.intermediate\\..*",
+    "hive\\.jdbc\\..*",
     "hive\\.join\\..*",
     "hive\\.limit\\..*",
     "hive\\.log\\..*",
