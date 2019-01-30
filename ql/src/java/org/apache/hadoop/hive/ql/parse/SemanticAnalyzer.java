fdc424e69e10c8f0cc342835f1dd6b3aa534e879
Vineet Garg <vgarg@apache.org>
2019-01-30 13:15:53 -0800

    BUG-111137/HIVE-21182 :  Avoid s3 interaction during query planning
    Change-Id: I033a7129be2fc56f963f64a6f3875ca3b9bc10bb

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -2544,7 +2544,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
         stagingPath = ctx.getMRTmpPath(tablePath.toUri());
       }
     } else {
-      stagingPath = ctx.getMRTmpPath();
+      stagingPath = ctx.getMRTmpPath(false);
     }
 
     return stagingPath;
