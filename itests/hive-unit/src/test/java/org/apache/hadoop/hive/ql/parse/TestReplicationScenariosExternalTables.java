fb7ffe6f69a70e17f3f4a00abc68184f597d3d85
Sankar Hariappan <mailtosankarh@gmail.com>
2019-02-15 10:20:39 +0530

    BUG-117715 :: HIVE-21261: Incremental replication adds redundant COPY and MOVE tasks for external table events (Sankar Hariappan, reviewed by Mahesh Kumar Behera)
    Change-Id: Iad4d00df5a6a990c8bbc50672573ef5c89804272

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenariosExternalTables.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenariosExternalTables.java
@@ -553,7 +553,7 @@ public class TestReplicationScenariosExternalTables extends BaseReplicationAcros
       tableNames.add(components[0]);
       assertTrue(components[1].length() > 0);
     }
-    assertTrue(expected.containsAll(tableNames));
+    assertTrue(tableNames.containsAll(expected));
     reader.close();
   }
 }
