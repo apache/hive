3600fe442a60a3f3eb7e6a5ad5343e28ae04267c
Slim Bouguerra <bslim@apache.org>
2019-02-27 09:05:36 -0800

    [HIVE-21332] Purge the non locked buffers instead of locked ones
    Change-Id: Iadc5067e03ad3ce545d02080fa4b4f0da0aa7988

--- llap-server/src/java/org/apache/hadoop/hive/llap/cache/MemoryManager.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/cache/MemoryManager.java
@@ -20,9 +20,42 @@ package org.apache.hadoop.hive.llap.cache;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Memory Manager is an accountant over a fixed size of memory.
+ * It does is the following.
+ * 1 - tracks the amount of memory (bytes) reserved out of a given maximum size to be shared between IO Threads.
+ * 2 - when a reservation can not be fulfilled form the current free space it has to notify Evictor to free up some
+ * space.
+ * <p>
+ * Note that it does not know about the actual shape, content or owners of memory, all it cares about is bytes usage.
+ */
 public interface MemoryManager {
+
+  /**
+   * Signals to the Memory manager the release of some memory bytes that are free to be used.
+   *
+   * @param memUsage amount of memory bytes that are released to be added to the ledger as free.
+   */
   void releaseMemory(long memUsage);
+
+  /**
+   * Sets the amount of bytes that the memory manager is managing.
+   *
+   * @param maxSize total amount of available bytes to be allocated.
+   */
   void updateMaxSize(long maxSize);
+
+  /**
+   * Reserves some amount of bytes within the managed pool of memory.
+   * <p>
+   * Callers expect that the memory manager will always fulfill the request by notifying the Evictor about how much
+   * need to be evicted to accommodate the reserve request.
+   * Note that this method will block until reservation is fulfilled.
+   *
+   * @param memoryToReserve Amount of bytes to reserve.
+   * @param isStopped       Caller state to indicate if it is still running while the memory manager is trying to
+   *                        allocate the space.
+   */
   void reserveMemory(long memoryToReserve, AtomicBoolean isStopped);
 
   /**
