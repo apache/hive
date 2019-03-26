10c14ce2f0fd32e06afc9ce05aaadadc9931f933
miklosgergely <miki.gergely@gmail.com>
2019-03-26 18:08:46 +0100

    BUG-119092/HIVE-21423 Do not check for whitespace issues in generated code
    Change-Id: Ie672f74d7a313e6154584f6f83c758c5ad716818

--- dev-support/hive-personality.sh
+++ dev-support/hive-personality.sh
@@ -38,9 +38,9 @@ function personality_globals
   #shellcheck disable=SC2034
   FINDBUGS_SKIP_MAVEN_SOURCE_CHECK=true
   #shellcheck disable=SC2034
-  WHITESPACE_EOL_IGNORE_LIST='.*.q.out'
+  WHITESPACE_EOL_IGNORE_LIST=('.*.q.out' '.*/gen/thrift/gen-javabean/.*')
   #shellcheck disable=SC2034
-  WHITESPACE_TABS_IGNORE_LIST='.*.q.out'
+  WHITESPACE_TABS_IGNORE_LIST=('.*.q.out' '.*/gen/thrift/gen-javabean/.*')
 }
 
 ## @description  Queue up modules for this personality
