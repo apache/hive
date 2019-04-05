881405ff7ee27aa9f22e2de0ac113312c0643b5c
Prasanth Jayachandran <prasanthj@apache.org>
2019-04-05 12:46:10 -0700

    Revert "BUG-119129: Backport HIVE-21427: Syslog storage handler"
    This reverts commit 36a59c57cf8a7c1cfc3d50dcc0ca7127745f1962.

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/tools/schematool/SchemaToolCommandLine.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/tools/schematool/SchemaToolCommandLine.java
@@ -76,6 +76,7 @@ public class SchemaToolCommandLine {
         .hasArg()
         .withDescription("Create table for Hive warehouse/compute logs")
         .create("createLogsTable");
+
     OptionGroup optGroup = new OptionGroup();
     optGroup
       .addOption(help)
@@ -169,9 +170,6 @@ public class SchemaToolCommandLine {
         .withDescription("Database a moving table is going to.  This is " +
             "required if you are moving a table.")
         .create("toDatabase");
-    Option retentionPeriod = OptionBuilder.hasArg()
-      .withDescription("Specify logs table retention period")
-      .create("retentionPeriod");
 
     Options options = new Options();
     options.addOption(help);
@@ -197,7 +195,6 @@ public class SchemaToolCommandLine {
     options.addOption(hivePasswdOpt);
     options.addOption(hiveDbOpt);
     options.addOption(yesOpt);
-    options.addOption(retentionPeriod);
     if (additionalOptions != null) options.addOptionGroup(additionalOptions);
 
     return options;
