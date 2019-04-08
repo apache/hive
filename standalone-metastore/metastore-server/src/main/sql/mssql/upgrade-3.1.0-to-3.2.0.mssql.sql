84be5e94b0d3bc8b746abac1539a612bf2db7258
David Lavati <david.lavati@gmail.com>
2019-04-08 17:19:23 +0200

    HIVE-21404: MSSQL upgrade script alters the wrong column (David Lavati via Zoltan Haindrich, Ashutosh Bapat)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>

--- standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-3.1.0-to-3.2.0.mssql.sql
+++ standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-3.1.0-to-3.2.0.mssql.sql
@@ -18,7 +18,45 @@ INSERT INTO SEQUENCE_TABLE (SEQUENCE_NAME, NEXT_VAL) VALUES ('org.apache.hadoop.
 
 
 -- HIVE-20221
-ALTER TABLE "SERDE_PARAMS" ALTER COLUMN "PARAM_VALUE" nvarchar(MAX);
+
+-- We can not change the datatype of a column with default value. Hence we first drop the default constraint
+-- and then change the datatype. We wrap the code to drop the default constraint in a stored procedure to avoid
+-- code duplicate. We create temporary stored procedures since we do not need them during normal
+-- metastore operation.
+CREATE PROCEDURE #DROP_DEFAULT_CONSTRAINT @TBL_NAME sysname, @COL_NAME sysname
+AS
+BEGIN
+	DECLARE @constraintname sysname
+	SELECT @constraintname = default_constraints.name
+		FROM sys.all_columns INNER JOIN sys.tables ON all_columns.object_id = tables.object_id
+			INNER JOIN sys.schemas ON tables.schema_id = schemas.schema_id
+			INNER JOIN sys.default_constraints ON all_columns.default_object_id = default_constraints.object_id
+		WHERE schemas.name = 'dbo' AND tables.name = @TBL_NAME AND all_columns.name = @COL_NAME
+
+	IF (@constraintname IS NOT NULL)
+	BEGIN
+		DECLARE @sql nvarchar(max) = 'ALTER TABLE [dbo].' + QUOTENAME(@TBL_NAME) + ' DROP CONSTRAINT ' + QUOTENAME(@constraintname)
+		EXEC(@sql)
+	END
+END;
+
+-- Similarly for primary key constraint
+CREATE PROCEDURE #DROP_PRIMARY_KEY_CONSTRAINT @TBL_NAME sysname
+AS
+BEGIN
+	DECLARE @constraintname sysname
+	SELECT @constraintname = constraint_name
+		FROM information_schema.table_constraints
+		WHERE constraint_type = 'PRIMARY KEY' AND table_schema = 'dbo' AND table_name = @TBL_NAME
+	IF @constraintname IS NOT NULL
+	BEGIN
+	    DECLARE @sql_pk nvarchar(max) = 'ALTER TABLE [dbo].' + QUOTENAME(@TBL_NAME) + ' DROP CONSTRAINT ' + @constraintname
+	    EXEC(@sql_pk)
+	end
+END;
+
+EXEC #DROP_DEFAULT_CONSTRAINT "PARTITION_PARAMS", "PARAM_VALUE";
+ALTER TABLE "PARTITION_PARAMS" ALTER COLUMN "PARAM_VALUE" varchar(max);
 
 -- HIVE-21077
 ALTER TABLE DBS ADD CREATE_TIME INT;
