5be2694eb37a2e88980d5860ecbdf04118d97b16
Ashutosh Bapat <abapat@cloudera.com>
2019-04-16 18:48:58 +0530

    HIVE-21529: Hive support bootstrap of ACID/MM tables on an existing policy (Ashutosh Bapat, reviewed by Sankar Hariappan)
    Signed-off-by: Sankar Hariappan <sankarh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/parse/repl/dump/events/AllocWriteIdHandler.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/repl/dump/events/AllocWriteIdHandler.java
@@ -17,8 +17,10 @@
  */
 package org.apache.hadoop.hive.ql.parse.repl.dump.events;
 
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.api.NotificationEvent;
 import org.apache.hadoop.hive.metastore.messaging.AllocWriteIdMessage;
+import org.apache.hadoop.hive.ql.exec.repl.util.ReplUtils;
 import org.apache.hadoop.hive.ql.parse.repl.DumpType;
 import org.apache.hadoop.hive.ql.parse.repl.load.DumpMetaData;
 
@@ -35,6 +37,18 @@ class AllocWriteIdHandler extends AbstractEventHandler<AllocWriteIdMessage> {
   @Override
   public void handle(Context withinContext) throws Exception {
     LOG.info("Processing#{} ALLOC_WRITE_ID message : {}", fromEventId(), eventMessageAsJSON);
+
+    // If we are bootstrapping ACID table during an incremental dump, the events corresponding to
+    // these ACID tables are not dumped. Hence we do not need to allocate any writeId on the
+    // target and hence we do not need to dump these events.
+    if (withinContext.hiveConf.getBoolVar(HiveConf.ConfVars.REPL_BOOTSTRAP_ACID_TABLES)) {
+      return;
+    }
+
+    if (!ReplUtils.includeAcidTableInDump(withinContext.hiveConf)) {
+      return;
+    }
+
     DumpMetaData dmd = withinContext.createDmd(this);
     dmd.setPayload(eventMessageAsJSON);
     dmd.write();
