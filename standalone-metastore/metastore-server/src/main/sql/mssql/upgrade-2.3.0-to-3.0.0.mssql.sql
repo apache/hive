49c67a1d6ee330252f5459932d3e2b34548a2f59
Alan Gates <gates@hortonworks.com>
2019-05-28 12:48:21 -0700

    HIVE-21739 Make metastore DB backward compatible with pre-catalog versions of hive (Aditya Shah via Alan Gates)

--- standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-2.3.0-to-3.0.0.mssql.sql
+++ standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-2.3.0-to-3.0.0.mssql.sql
@@ -271,6 +271,9 @@ UPDATE DBS
 -- Add the not null constraint
 ALTER TABLE DBS ALTER COLUMN CTLG_NAME nvarchar(256) NOT NULL;
 
+-- Add the default as 'hive'
+ALTER TABLE DBS ADD DEFAULT 'hive' FOR CTLG_NAME;
+
 -- Put back the unique index
 CREATE UNIQUE INDEX UNIQUEDATABASE ON DBS ("NAME", "CTLG_NAME");
 
