68017e89db37c90a2b4768eb573c8df600932c1b
Naveen Gangam <ngangam@apache.org>
2019-06-07 12:29:51 -0400

    DWX-273: HIVE-21816: HMS Translation: Refactor tests to work with ACID tables (Naveen Gangam, reviewed by Daniel Dai)
    Change-Id: Ifc4f7c792508444d640d056446daa98aab22e7cd

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -3143,11 +3143,15 @@ public class HiveMetaStore extends ThriftHiveMetastore {
         if (tables.size() > 0) {
           tObjects = getMS().getTableObjectsByName(catalog, database, tables);
           LOG.debug("get_tables_ext:getTableObjectsByName() returned " + tObjects.size());
-          if (processorCapabilities != null && processorCapabilities.size() > 0) {
+          if (processorCapabilities == null || processorCapabilities.size() == 0 ||
+                processorCapabilities.contains("MANAGERAWMETADATA")) {
+            LOG.info("Skipping translation for processor with " + processorId);
+          } else {
             if (transformer != null) {
               Map<Table, List<String>> retMap = transformer.transform(tObjects, processorCapabilities, processorId);
 
               for (Map.Entry<Table, List<String>> entry : retMap.entrySet())  {
+                LOG.debug("Table " + entry.getKey().getTableName() + " requires " + Arrays.toString((entry.getValue()).toArray()));
                 ret.add(convertTableToExtendedTable(entry.getKey(), entry.getValue(), req.getRequestedFields()));
               }
             } else {
@@ -3216,15 +3220,21 @@ public class HiveMetaStore extends ThriftHiveMetastore {
               "insert-only tables", "get_table_req");
         }
 
-        if (processorCapabilities != null && processorCapabilities.size() > 0 && transformer != null) {
-          List<Table> tList = new ArrayList<>();
-          tList.add(t);
-          Map<Table, List<String>> ret = transformer.transform(tList, processorCapabilities, processorId);
-          if (ret.size() > 1) {
-            LOG.warn("Unexpected resultset size:" + ret.size());
-            throw new MetaException("Unexpected result from metadata transformer:return list size=" + ret.size());
+        if (processorCapabilities == null || processorCapabilities.size() == 0 ||
+              processorCapabilities.contains("MANAGERAWMETADATA")) {
+          LOG.info("Skipping translation for processor with " + processorId);
+        } else {
+          if (transformer != null) {
+            List<Table> tList = new ArrayList<>();
+            tList.add(t);
+            Map<Table, List<String>> ret = transformer.transform(tList, processorCapabilities, processorId);
+            if (ret.size() > 1) {
+              LOG.warn("Unexpected resultset size:" + ret.size());
+              throw new MetaException("Unexpected result from metadata transformer:return list size is " + ret.size());
+            }
+            t = (Table)(ret.keySet().iterator().next());
+            LOG.debug("Table " + t.getTableName() + " requires " + Arrays.toString((ret.get(t)).toArray()));
           }
-          t = (Table)(ret.keySet().iterator().next());
         }
 
         firePreEvent(new PreReadTableEvent(t, this));
@@ -6625,8 +6635,11 @@ public class HiveMetaStore extends ThriftHiveMetastore {
           }
         }
 
-        if (transformer != null) {
-          if (processorCapabilities != null && processorCapabilities.size() > 0) {
+        if (processorCapabilities == null || processorCapabilities.size() == 0 ||
+              processorCapabilities.contains("MANAGERAWMETADATA")) {
+          LOG.info("Skipping translation for processor with " + processorId);
+        } else {
+          if (transformer != null) {
             ret = transformer.transformPartitions(ret, processorCapabilities, processorId);
           }
         }
