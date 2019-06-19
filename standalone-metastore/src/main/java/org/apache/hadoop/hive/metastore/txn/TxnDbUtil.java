3ea39f92279273615a03a4bc9fa16dbb5c3d14e8
Csaba Ringhofer <csringhofer@cloudera.com>
2019-06-19 21:58:32 +0200

    Revert "SUST-2363 HIVE-20833: package.jdo needs to be updated to conform with HIVE-20221 changes (Vineet Garg, reviewed by Jason Dere)"
    This reverts commit 629d068d388055fa6b94e5a07b42cf31b1f15f46.
    
    The commit broke some Impala unit tests, see CDH-80709 for details.
    
    Change-Id: I0aa935c3a6448b03d430619b8d9b81f1a80b9a07

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnDbUtil.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnDbUtil.java
@@ -269,7 +269,7 @@ public final class TxnDbUtil {
       try {
         stmt.execute("CREATE TABLE \"APP\".\"PARTITION_PARAMS\" (" +
             " \"PART_ID\" BIGINT NOT NULL, \"PARAM_KEY\" VARCHAR(256) NOT NULL, " +
-            " \"PARAM_VALUE\" CLOB, " +
+            " \"PARAM_VALUE\" VARCHAR(4000), " +
             " PRIMARY KEY (PART_ID, PARAM_KEY))"
         );
       } catch (SQLException e) {
