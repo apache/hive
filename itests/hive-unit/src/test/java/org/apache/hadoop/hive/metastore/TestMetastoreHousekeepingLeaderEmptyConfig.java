c3d05022f79ed8ef6c61ca9c088f6a8a992d6e8d
Ashutosh Bapat <abapat@cloudera.com>
2019-06-20 09:48:36 +0530

    HIVE-21841 : Leader election in HMS to run housekeeping tasks. (Ashutosh Bapat reviewed by Mahesh Kumar Behera)
    Signed-off-by: mbehera <mahesh@apache.org>

---
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestMetastoreHousekeepingLeaderEmptyConfig.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.metastore;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * Test for specifying empty HMS leader.
+ */
+public class TestMetastoreHousekeepingLeaderEmptyConfig extends MetastoreHousekeepingLeaderTestBase {
+  private static final Logger LOG =
+          LoggerFactory.getLogger(TestMetastoreHousekeepingLeaderEmptyConfig.class);
+
+  @Before
+  public void setUp() throws Exception {
+    // Empty string for leader indicates that the HMS is leader.
+    internalSetup("");
+  }
+
+  @Test
+  public void testHouseKeepingThreadExistence() throws Exception {
+    searchHousekeepingThreads();
+
+    // Verify existence of threads
+    for (Map.Entry<String, Boolean> entry : threadNames.entrySet()) {
+      if (entry.getValue()) {
+        LOG.info("Found thread with name " + entry.getKey());
+      }
+      Assert.assertTrue("No thread with name " + entry.getKey() + " found.", entry.getValue());
+    }
+
+    for (Map.Entry<Class, Boolean> entry : threadClasses.entrySet()) {
+      if (entry.getValue()) {
+        LOG.info("Found thread for " + entry.getKey().getSimpleName());
+      }
+      Assert.assertTrue("No thread found for class " + entry.getKey().getSimpleName(),
+              entry.getValue());
+    }
+  }
+}
