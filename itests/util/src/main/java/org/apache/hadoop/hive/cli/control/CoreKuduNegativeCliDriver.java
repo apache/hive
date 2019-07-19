b254a7dc6c66f5b752b2dba5622e5bcd7e3142df
Grant Henke <ghenke@cloudera.com>
2019-07-18 21:25:38 -0500

    HIVE-12971: Add Support for Kudu Tables
    This patch adds an initial integration for Apache Kudu backed tables
    by supporting the creation of external tables pointed at existing
    underlying Kudu tables.
    
    SELECT queries can read from the tables including pushing most
    predicates/filters into the Kudu scanners. Future work should
    complete support for Kudu predicates.
    
    INSERT queries can write to the tables. However, they currently
    use Kudu UPSERT operations when writing. Future work should
    complete support for INSERT, UPDATE, and DELETE.
    
    Note: The table properties and class names match the values used by
    Apache Impala when creating HMS entries for Kudu tables. This
    means tables created by Impala can be used by Hive and vice versa.

---
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CoreKuduNegativeCliDriver.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.cli.control;
+
+import org.apache.hadoop.hive.kudu.KuduTestSetup;
+import org.apache.hadoop.hive.ql.QTestArguments;
+import org.apache.hadoop.hive.ql.QTestProcessExecResult;
+import org.apache.hadoop.hive.ql.QTestUtil;
+import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * The Kudu negative CliAdapter implementation.
+ */
+public class CoreKuduNegativeCliDriver extends CliAdapter {
+
+  private QTestUtil qt;
+
+  public CoreKuduNegativeCliDriver(AbstractCliConfig cliConfig) {
+    super(cliConfig);
+  }
+
+  @Override
+  @BeforeClass
+  public void beforeClass() {
+    try {
+      qt = new QTestUtil(QTestArguments.QTestArgumentsBuilder.instance()
+          .withOutDir(cliConfig.getResultsDir())
+          .withLogDir(cliConfig.getLogDir())
+          .withClusterType(cliConfig.getClusterType())
+          .withConfDir(cliConfig.getHiveConfDir())
+          .withInitScript(cliConfig.getInitScript())
+          .withCleanupScript(cliConfig.getCleanupScript())
+          .withLlapIo(true)
+          .withQTestSetup(new KuduTestSetup())
+          .build());
+
+      // do a one time initialization
+      qt.newSession();
+      qt.cleanUp();
+      qt.createSources();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in setUp", e);
+    }
+  }
+
+  @Override
+  @AfterClass
+  public void shutdown() {
+    try {
+      qt.shutdown();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in tearDown", e);
+    }
+  }
+
+  @Override
+  @Before
+  public void setUp() {
+    try {
+      qt.newSession();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in setup");
+    }
+  }
+
+  @Override
+  @After
+  public void tearDown() {
+    try {
+      qt.clearPostTestEffects();
+      qt.clearTestSideEffects();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in tearDown");
+    }
+  }
+
+  @Override
+  public void runTest(String tname, String fname, String fpath) {
+    long startTime = System.currentTimeMillis();
+    try {
+      System.err.println("Begin query: " + fname);
+      qt.addFile(fpath);
+      qt.cliInit(new File(fpath));
+      int ecode = qt.executeClient(fname).getResponseCode();
+      if (ecode == 0) {
+        qt.failed(fname, null);
+      }
+
+      QTestProcessExecResult result = qt.checkCliDriverResults(fname);
+      if (result.getReturnCode() != 0) {
+        qt.failedDiff(result.getReturnCode(), fname, result.getCapturedOutput());
+      }
+
+    } catch (Exception e) {
+      qt.failedWithException(e, fname, null);
+    }
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    System.err.println("Done query: " + fname + " elapsedTime=" + elapsedTime / 1000 + "s");
+    assertTrue("Test passed", true);
+  }
+}
+
---
+++ kudu-handler/src/java/org/apache/hadoop/hive/kudu/KuduStorageHandler.java
@@ -0,0 +1,216 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.kudu;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.kudu.KuduOutputFormat.KuduRecordWriter;
+import org.apache.hadoop.hive.metastore.HiveMetaHook;
+import org.apache.hadoop.hive.metastore.api.MetaException;
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hadoop.hive.ql.exec.Utilities;
+import org.apache.hadoop.hive.ql.metadata.DefaultStorageHandler;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.metadata.HiveStoragePredicateHandler;
+import org.apache.hadoop.hive.ql.metadata.StorageHandlerInfo;
+import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
+import org.apache.hadoop.hive.ql.plan.TableDesc;
+import org.apache.hadoop.hive.ql.security.authorization.DefaultHiveAuthorizationProvider;
+import org.apache.hadoop.hive.ql.security.authorization.HiveAuthorizationProvider;
+import org.apache.hadoop.hive.serde2.AbstractSerDe;
+import org.apache.hadoop.hive.serde2.Deserializer;
+import org.apache.hadoop.mapred.InputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.OutputFormat;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.util.StringUtils;
+import org.apache.kudu.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Provides a HiveStorageHandler implementation for Apache Kudu.
+ */
+public class KuduStorageHandler extends DefaultStorageHandler implements HiveStoragePredicateHandler {
+
+  private static final Logger LOG = LoggerFactory.getLogger(KuduStorageHandler.class);
+
+  private static final String KUDU_PROPERTY_PREFIX = "kudu.";
+
+  /** Table Properties. Used in the hive table definition when creating a new table. */
+  public static final String KUDU_TABLE_ID_KEY = KUDU_PROPERTY_PREFIX + "table_id";
+  public static final String KUDU_TABLE_NAME_KEY = KUDU_PROPERTY_PREFIX + "table_name";
+  public static final String KUDU_MASTER_ADDRS_KEY = KUDU_PROPERTY_PREFIX + "master_addresses";
+  public static final List<String> KUDU_TABLE_PROPERTIES =
+      Arrays.asList(KUDU_TABLE_ID_KEY, KUDU_TABLE_NAME_KEY, KUDU_MASTER_ADDRS_KEY);
+
+  private Configuration conf;
+
+  @Override
+  public Class<? extends InputFormat> getInputFormatClass() {
+    return KuduInputFormat.class;
+  }
+
+  @Override
+  public Class<? extends OutputFormat> getOutputFormatClass() {
+    return KuduOutputFormat.class;
+  }
+
+  @Override
+  public Class<? extends AbstractSerDe> getSerDeClass() {
+    return KuduSerDe.class;
+  }
+
+  @Override
+  public HiveMetaHook getMetaHook() {
+    return null;
+  }
+
+  @Override
+  public HiveAuthorizationProvider getAuthorizationProvider() throws HiveException {
+    return new DefaultHiveAuthorizationProvider();
+  }
+
+  @Override
+  public Configuration getConf() {
+    return conf;
+  }
+
+  @Override
+  public void setConf(Configuration conf) {
+    this.conf = conf;
+  }
+
+  @Override
+  public void configureInputJobProperties(TableDesc tableDesc,
+      Map<String, String> jobProperties) {
+    configureJobProperties(tableDesc, jobProperties);
+  }
+
+  @Override
+  public void configureOutputJobProperties(TableDesc tableDesc,
+      Map<String, String> jobProperties) {
+    configureJobProperties(tableDesc, jobProperties);
+  }
+
+  @Override
+  public void configureTableJobProperties(TableDesc tableDesc,
+      Map<String, String> jobProperties) {
+    configureJobProperties(tableDesc, jobProperties);
+  }
+
+  @Override
+  public void configureJobConf(TableDesc tableDesc, JobConf jobConf) {
+    // Copied from the DruidStorageHandler.
+    if (UserGroupInformation.isSecurityEnabled()) {
+      // AM can not do Kerberos Auth so will do the input split generation in the HS2
+      LOG.debug("Setting {} to {} to enable split generation on HS2",
+          HiveConf.ConfVars.HIVE_AM_SPLIT_GENERATION.toString(),
+          Boolean.FALSE.toString());
+      jobConf.set(HiveConf.ConfVars.HIVE_AM_SPLIT_GENERATION.toString(), Boolean.FALSE.toString());
+    }
+    try {
+      addDependencyJars(jobConf, KuduRecordWriter.class);
+    } catch (IOException e) {
+      Throwables.propagate(e);
+    }
+  }
+
+  // Copied from the DruidStorageHandler.
+  private static void addDependencyJars(Configuration conf, Class<?>... classes)
+      throws IOException {
+    FileSystem localFs = FileSystem.getLocal(conf);
+    Set<String> jars = new HashSet<>(conf.getStringCollection("tmpjars"));
+    for (Class<?> clazz : classes) {
+      if (clazz == null) {
+        continue;
+      }
+      final String path = Utilities.jarFinderGetJar(clazz);
+      if (path == null) {
+        throw new RuntimeException("Could not find jar for class " + clazz +
+            " in order to ship it to the cluster.");
+      }
+      if (!localFs.exists(new Path(path))) {
+        throw new RuntimeException("Could not validate jar file " + path + " for class " + clazz);
+      }
+      jars.add(path);
+    }
+    if (jars.isEmpty()) {
+      return;
+    }
+    //noinspection ToArrayCallWithZeroLengthArrayArgument
+    conf.set("tmpjars", StringUtils.arrayToString(jars.toArray(new String[jars.size()])));
+  }
+
+  private void configureJobProperties(TableDesc tableDesc,
+      Map<String, String> jobProperties) {
+
+    Properties tblProps = tableDesc.getProperties();
+    copyPropertiesFromTable(jobProperties, tblProps);
+  }
+
+  private void copyPropertiesFromTable(Map<String, String> jobProperties, Properties tblProps) {
+    for (String propToCopy : KUDU_TABLE_PROPERTIES) {
+      if (tblProps.containsKey(propToCopy)) {
+        String value = tblProps.getProperty(propToCopy);
+        conf.set(propToCopy, value);
+        jobProperties.put(propToCopy, value);
+      }
+    }
+  }
+
+  /**
+   * Gives the storage handler a chance to decompose a predicate.
+   * The storage handler should analyze the predicate and return the portion of it which
+   * cannot be evaluated during table access.
+   *
+   * @param jobConf contains a job configuration matching the one that will later be passed
+   *               to getRecordReader and getSplits
+   * @param deserializer deserializer which will be used when fetching rows
+   * @param predicate predicate to be decomposed
+   * @return decomposed form of predicate, or null if no pushdown is possible at all
+   */
+  @Override
+  public DecomposedPredicate decomposePredicate(JobConf jobConf, Deserializer deserializer,
+                                                ExprNodeDesc predicate) {
+    Preconditions.checkArgument(deserializer instanceof KuduSerDe);
+    KuduSerDe serDe = (KuduSerDe) deserializer;
+    Schema schema = serDe.getSchema();
+    return KuduPredicateHandler.decompose(predicate, schema);
+  }
+
+  /**
+   * Used to fetch runtime information about storage handler during DESCRIBE EXTENDED statement.
+   */
+  @Override
+  public StorageHandlerInfo getStorageHandlerInfo(Table table) throws MetaException {
+    return null;
+  }
+}
