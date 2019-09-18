03205cc25347a9ff6c7cd00f92fa93af922f4ed0
Zoltan Haindrich <kirk@rxd.hu>
2019-09-18 08:43:11 +0200

    HIVE-22210: Vectorization may reuse computation output columns involved in filtering (Zoltan Haindrich reviewed by Laszlo Bodor)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizationContext.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizationContext.java
@@ -2125,9 +2125,7 @@ import com.google.common.annotations.VisibleForTesting;
       vectorExpression.setChildExpressions(children.toArray(new VectorExpression[0]));
     }
 
-    for (VectorExpression ve : children) {
-      ocm.freeOutputColumn(ve.getOutputColumnNum());
-    }
+    freeNonColumns(children.toArray(new VectorExpression[0]));
 
     return vectorExpression;
   }
