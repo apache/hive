fe575ef3a0c3f069458b73d623c0bc6bcb81222f
Vineet Garg <vgarg@apache.org>
2019-09-23 11:15:20 -0700

    HIVE-22145: Avoid optimizations for analyze compute statistics (Vineet Garg, reviewed by Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/parse/TaskCompiler.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/TaskCompiler.java
@@ -156,7 +156,10 @@ public abstract class TaskCompiler {
       return;
     }
 
-    optimizeOperatorPlan(pCtx, inputs, outputs);
+    if (!pCtx.getQueryProperties().isAnalyzeCommand()) {
+      LOG.debug("Skipping optimize operator plan for analyze command.");
+      optimizeOperatorPlan(pCtx, inputs, outputs);
+    }
 
     /*
      * In case of a select, use a fetch task instead of a move task.
