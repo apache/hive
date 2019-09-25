bc6c8e43d9004bdb23852073b17ac9c202202b07
Zoltan Haindrich <kirk@rxd.hu>
2019-09-25 16:55:21 +0200

    cleanup

--- itests/util/src/main/java/org/apache/hadoop/hive/ql/feat/QTestFeatDispatcher.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/ql/feat/QTestFeatDispatcher.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hive.ql.feat;
 
 import java.io.BufferedReader;
@@ -27,21 +44,7 @@ public class QTestFeatDispatcher {
 
   public void process(File file) {
     synchronized (QTestUtil.class) {
-
       parse(file);
-
-      //      Set<String> missingDatasets = datasets.getTables();
-      //      missingDatasets.removeAll(getSrcTables());
-      //      if (missingDatasets.isEmpty()) {
-      //        return;
-      //      }
-      //      qt.newSession(true);
-      //      for (String table : missingDatasets) {
-      //        if (initDataset(table, cliDriver)) {
-      //          addSrcTable(table);
-      //        }
-      //      }
-      //      qt.newSession(true);
     }
   }
 
