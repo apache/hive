difin/difin_hive

HIVE-12971: Hive Support for Kudu (Grant Henke, reviewed by Jesus Camacho Rodriguez)
Close apache/hive#733

Signed-off-by: Jesus Camacho Rodriguez <jcamacho@apache.org>

itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
@@ -778,4 +778,50 @@ public class CliConfigs {
     }
   }
 
+  /**
+   * The CliConfig implementation for Kudu.
+   */
+  public static class KuduCliConfig extends AbstractCliConfig {
+    public KuduCliConfig() {
+      super(CoreKuduCliDriver.class);
+      try {
+        setQueryDir("kudu-handler/src/test/queries/positive");
+
+        setResultsDir("kudu-handler/src/test/results/positive");
+        setLogDir("itests/qtest/target/qfile-results/kudu/positive");
+
+        setInitScript("q_test_init_src.sql");
+        setCleanupScript("q_test_cleanup_src.sql");
+
+        setHiveConfDir("data/conf/llap");
+        setClusterType(MiniClusterType.TEZ_LOCAL);
+        setMetastoreType(MetastoreType.sql);
+        setFsType(QTestMiniClusters.FsType.LOCAL);
+      } catch (Exception e) {
+        throw new RuntimeException("can't construct cliconfig", e);
+      }
+    }
+  }
+
+  public static class KuduNegativeCliConfig extends AbstractCliConfig {
+    public KuduNegativeCliConfig() {
+      super(CoreKuduNegativeCliDriver.class);
+      try {
+        setQueryDir("kudu-handler/src/test/queries/negative");
+
+        setResultsDir("kudu-handler/src/test/results/negative");
+        setLogDir("itests/qtest/target/qfile-results/kudu/negative");
+
+        setInitScript("q_test_init_src.sql");
+        setCleanupScript("q_test_cleanup_src.sql");
+
+        setHiveConfDir("data/conf/llap");
+        setClusterType(MiniClusterType.TEZ_LOCAL);
+        setMetastoreType(MetastoreType.sql);
+        setFsType(QTestMiniClusters.FsType.LOCAL);
+      } catch (Exception e) {
+        throw new RuntimeException("can't construct cliconfig", e);
+      }
+    }
+  }
 }

itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CoreKuduCliDriver.java
@@ -1,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.cli.control;
+
+import org.apache.hadoop.hive.kudu.KuduTestSetup;
+import org.apache.hadoop.hive.ql.QTestArguments;
+import org.apache.hadoop.hive.ql.QTestProcessExecResult;
+import org.apache.hadoop.hive.ql.QTestUtil;
+import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * The Kudu CliAdapter implementation.
+ */
+public class CoreKuduCliDriver extends CliAdapter {
+
+  private QTestUtil qt;
+
+  public CoreKuduCliDriver(AbstractCliConfig cliConfig) {
+    super(cliConfig);
+  }
+
+  @Override
+  @BeforeClass
+  public void beforeClass() {
+    try {
+      qt = new QTestUtil(QTestArguments.QTestArgumentsBuilder.instance()
+          .withOutDir(cliConfig.getResultsDir())
+          .withLogDir(cliConfig.getLogDir())
+          .withClusterType(cliConfig.getClusterType())
+          .withConfDir(cliConfig.getHiveConfDir())
+          .withInitScript(cliConfig.getInitScript())
+          .withCleanupScript(cliConfig.getCleanupScript())
+          .withLlapIo(true)
+          .withQTestSetup(new KuduTestSetup())
+          .build());
+
+      // do a one time initialization
+      qt.newSession();
+      qt.cleanUp();
+      qt.createSources();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in setUp", e);
+    }
+  }
+
+  @Override
+  @AfterClass
+  public void shutdown() {
+    try {
+      qt.shutdown();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in tearDown", e);
+    }
+  }
+
+  @Override
+  @Before
+  public void setUp() {
+    try {
+      qt.newSession();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in setup");
+    }
+  }
+
+  @Override
+  @After
+  public void tearDown() {
+    try {
+      qt.clearPostTestEffects();
+      qt.clearTestSideEffects();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in tearDown");
+    }
+  }
+
+  @Override
+  public void runTest(String tname, String fname, String fpath) {
+    long startTime = System.currentTimeMillis();
+    try {
+      System.err.println("Begin query: " + fname);
+
+      qt.addFile(fpath);
+      qt.cliInit(new File(fpath));
+
+      CommandProcessorResponse response = qt.executeClient(fname);
+      if (response.getResponseCode() != 0) {
+        qt.failedQuery(response.getException(), response.getResponseCode(), fname, null);
+      }
+
+      QTestProcessExecResult result = qt.checkCliDriverResults(fname);
+      if (result.getReturnCode() != 0) {
+        qt.failedDiff(result.getReturnCode(), fname, result.getCapturedOutput());
+      }
+      qt.clearPostTestEffects();
+
+    } catch (Exception e) {
+      qt.failedWithException(e, fname, null);
+    }
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    System.err.println("Done query: " + fname + " elapsedTime=" + elapsedTime/1000 + "s");
+    assertTrue("Test passed", true);
+  }
+}
+

itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CoreKuduNegativeCliDriver.java
@@ -1,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.cli.control;
+
+import org.apache.hadoop.hive.kudu.KuduTestSetup;
+import org.apache.hadoop.hive.ql.QTestArguments;
+import org.apache.hadoop.hive.ql.QTestProcessExecResult;
+import org.apache.hadoop.hive.ql.QTestUtil;
+import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * The Kudu negative CliAdapter implementation.
+ */
+public class CoreKuduNegativeCliDriver extends CliAdapter {
+
+  private QTestUtil qt;
+
+  public CoreKuduNegativeCliDriver(AbstractCliConfig cliConfig) {
+    super(cliConfig);
+  }
+
+  @Override
+  @BeforeClass
+  public void beforeClass() {
+    try {
+      qt = new QTestUtil(QTestArguments.QTestArgumentsBuilder.instance()
+          .withOutDir(cliConfig.getResultsDir())
+          .withLogDir(cliConfig.getLogDir())
+          .withClusterType(cliConfig.getClusterType())
+          .withConfDir(cliConfig.getHiveConfDir())
+          .withInitScript(cliConfig.getInitScript())
+          .withCleanupScript(cliConfig.getCleanupScript())
+          .withLlapIo(true)
+          .withQTestSetup(new KuduTestSetup())
+          .build());
+
+      // do a one time initialization
+      qt.newSession();
+      qt.cleanUp();
+      qt.createSources();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in setUp", e);
+    }
+  }
+
+  @Override
+  @AfterClass
+  public void shutdown() {
+    try {
+      qt.shutdown();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in tearDown", e);
+    }
+  }
+
+  @Override
+  @Before
+  public void setUp() {
+    try {
+      qt.newSession();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in setup");
+    }
+  }
+
+  @Override
+  @After
+  public void tearDown() {
+    try {
+      qt.clearPostTestEffects();
+      qt.clearTestSideEffects();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in tearDown");
+    }
+  }
+
+  @Override
+  public void runTest(String tname, String fname, String fpath) {
+    long startTime = System.currentTimeMillis();
+    try {
+      System.err.println("Begin query: " + fname);
+      qt.addFile(fpath);
+      qt.cliInit(new File(fpath));
+      int ecode = qt.executeClient(fname).getResponseCode();
+      if (ecode == 0) {
+        qt.failed(fname, null);
+      }
+
+      QTestProcessExecResult result = qt.checkCliDriverResults(fname);
+      if (result.getReturnCode() != 0) {
+        qt.failedDiff(result.getReturnCode(), fname, result.getCapturedOutput());
+      }
+
+    } catch (Exception e) {
+      qt.failedWithException(e, fname, null);
+    }
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    System.err.println("Done query: " + fname + " elapsedTime=" + elapsedTime / 1000 + "s");
+    assertTrue("Test passed", true);
+  }
+}
+

apache/hive

(cherry picked from commit f7f7c586d7c8a8142223ae95c8e3add0873c2c86)
Change-Id: I5f9df270471619a47854294e98d9cc0b1a2866cb

itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
@@ -747,4 +747,50 @@ public class CliConfigs {
     }
   }
 
+  /**
+   * The CliConfig implementation for Kudu.
+   */
+  public static class KuduCliConfig extends AbstractCliConfig {
+    public KuduCliConfig() {
+      super(CoreKuduCliDriver.class);
+      try {
+        setQueryDir("kudu-handler/src/test/queries/positive");
+
+        setResultsDir("kudu-handler/src/test/results/positive");
+        setLogDir("itests/qtest/target/qfile-results/kudu/positive");
+
+        setInitScript("q_test_init_src.sql");
+        setCleanupScript("q_test_cleanup_src.sql");
+
+        setHiveConfDir("");
+        setClusterType(MiniClusterType.none);
+        setMetastoreType(MetastoreType.sql);
+        setFsType(QTestUtil.FsType.local);
+      } catch (Exception e) {
+        throw new RuntimeException("can't construct cliconfig", e);
+      }
+    }
+  }
+
+  public static class KuduNegativeCliConfig extends AbstractCliConfig {
+    public KuduNegativeCliConfig() {
+      super(CoreKuduNegativeCliDriver.class);
+      try {
+        setQueryDir("kudu-handler/src/test/queries/negative");
+
+        setResultsDir("kudu-handler/src/test/results/negative");
+        setLogDir("itests/qtest/target/qfile-results/kudu/negative");
+
+        setInitScript("q_test_init_src.sql");
+        setCleanupScript("q_test_cleanup_src.sql");
+
+        setHiveConfDir("");
+        setClusterType(MiniClusterType.none);
+        setMetastoreType(MetastoreType.sql);
+        setFsType(QTestUtil.FsType.local);
+      } catch (Exception e) {
+        throw new RuntimeException("can't construct cliconfig", e);
+      }
+    }
+  }
 }

itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CoreKuduCliDriver.java
@@ -1,0 +1,137 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.cli.control;
+
+import org.apache.hadoop.hive.kudu.KuduTestSetup;
+import org.apache.hadoop.hive.ql.QTestArguments;
+import org.apache.hadoop.hive.ql.QTestProcessExecResult;
+import org.apache.hadoop.hive.ql.QTestUtil;
+import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * The Kudu CliAdapter implementation.
+ */
+public class CoreKuduCliDriver extends CliAdapter {
+
+  private QTestUtil qt;
+
+  public CoreKuduCliDriver(AbstractCliConfig cliConfig) {
+    super(cliConfig);
+  }
+
+  @Override
+  @BeforeClass
+  public void beforeClass() {
+    try {
+      qt = new QTestUtil(QTestArguments.QTestArgumentsBuilder.instance()
+          .withOutDir(cliConfig.getResultsDir())
+          .withLogDir(cliConfig.getLogDir())
+          .withClusterType(cliConfig.getClusterType())
+          .withConfDir(cliConfig.getHiveConfDir())
+          .withInitScript(cliConfig.getInitScript())
+          .withCleanupScript(cliConfig.getCleanupScript())
+          .withLlapIo(true)
+          .withQTestSetup(new KuduTestSetup())
+          .build());
+
+      // do a one time initialization
+      qt.newSession();
+      qt.cleanUp();
+      qt.createSources();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in setUp", e);
+    }
+  }
+
+  @Override
+  @AfterClass
+  public void shutdown() {
+    try {
+      qt.shutdown();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in tearDown", e);
+    }
+  }
+
+  @Override
+  @Before
+  public void setUp() {
+    try {
+      qt.newSession();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in setup");
+    }
+  }
+
+  @Override
+  @After
+  public void tearDown() {
+    try {
+      qt.clearPostTestEffects();
+      qt.clearTestSideEffects();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in tearDown");
+    }
+  }
+
+  @Override
+  public void runTest(String tname, String fname, String fpath) {
+    long startTime = System.currentTimeMillis();
+    try {
+      System.err.println("Begin query: " + fname);
+
+      qt.addFile(fpath);
+      qt.cliInit(new File(fpath));
+
+      int ecode = qt.executeClient(fname);
+      if (ecode != 0) {
+        qt.failed(ecode, fname, null);
+      }
+
+      QTestProcessExecResult result = qt.checkCliDriverResults(fname);
+      if (result.getReturnCode() != 0) {
+        qt.failedDiff(result.getReturnCode(), fname, result.getCapturedOutput());
+      }
+
+    } catch (Exception e) {
+      qt.failed(e, fname, null);
+    }
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    System.err.println("Done query: " + fname + " elapsedTime=" + elapsedTime/1000 + "s");
+    assertTrue("Test passed", true);
+  }
+}
+

itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CoreKuduNegativeCliDriver.java
@@ -1,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.cli.control;
+
+import org.apache.hadoop.hive.kudu.KuduTestSetup;
+import org.apache.hadoop.hive.ql.QTestArguments;
+import org.apache.hadoop.hive.ql.QTestProcessExecResult;
+import org.apache.hadoop.hive.ql.QTestUtil;
+import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * The Kudu negative CliAdapter implementation.
+ */
+public class CoreKuduNegativeCliDriver extends CliAdapter {
+
+  private QTestUtil qt;
+
+  public CoreKuduNegativeCliDriver(AbstractCliConfig cliConfig) {
+    super(cliConfig);
+  }
+
+  @Override
+  @BeforeClass
+  public void beforeClass() {
+    try {
+      qt = new QTestUtil(QTestArguments.QTestArgumentsBuilder.instance()
+          .withOutDir(cliConfig.getResultsDir())
+          .withLogDir(cliConfig.getLogDir())
+          .withClusterType(cliConfig.getClusterType())
+          .withConfDir(cliConfig.getHiveConfDir())
+          .withInitScript(cliConfig.getInitScript())
+          .withCleanupScript(cliConfig.getCleanupScript())
+          .withLlapIo(true)
+          .withQTestSetup(new KuduTestSetup())
+          .build());
+
+      // do a one time initialization
+      qt.newSession();
+      qt.cleanUp();
+      qt.createSources();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in setUp", e);
+    }
+  }
+
+  @Override
+  @AfterClass
+  public void shutdown() {
+    try {
+      qt.shutdown();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected exception in tearDown", e);
+    }
+  }
+
+  @Override
+  @Before
+  public void setUp() {
+    try {
+      qt.newSession();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in setup");
+    }
+  }
+
+  @Override
+  @After
+  public void tearDown() {
+    try {
+      qt.clearPostTestEffects();
+      qt.clearTestSideEffects();
+
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+      System.err.flush();
+      fail("Unexpected exception in tearDown");
+    }
+  }
+
+  @Override
+  public void runTest(String tname, String fname, String fpath) {
+    long startTime = System.currentTimeMillis();
+    try {
+      System.err.println("Begin query: " + fname);
+      qt.addFile(fpath);
+      qt.cliInit(new File(fpath));
+      int ecode = qt.executeClient(fname);
+      if (ecode == 0) {
+        qt.failed(fname, null);
+      }
+
+      QTestProcessExecResult result = qt.checkCliDriverResults(fname);
+      if (result.getReturnCode() != 0) {
+        qt.failedDiff(result.getReturnCode(), fname, result.getCapturedOutput());
+      }
+
+    } catch (Exception e) {
+      qt.failed(e, fname, null);
+    }
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    System.err.println("Done query: " + fname + " elapsedTime=" + elapsedTime / 1000 + "s");
+    assertTrue("Test passed", true);
+  }
+}
+