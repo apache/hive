d0bd0712b957d1c053539689892909b0fe2bd376
Krisztian Kasa <kkasa@cloudera.com>
2019-11-05 20:12:13 -0800

    HIVE-22292: Implement Hypothetical-Set Aggregate Functions (Krisztian Kasa, reviewed by Jesus Camacho Rodriguez)

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -255,6 +255,7 @@ import org.apache.hadoop.hive.ql.udf.generic.GenericUDFSurrogateKey;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTFInline;
 import org.apache.hadoop.hive.ql.util.DirectionUtils;
+import org.apache.hadoop.hive.ql.util.NullOrdering;
 import org.apache.hadoop.hive.ql.util.ResourceDownloader;
 import org.apache.hadoop.hive.serde.serdeConstants;
 import org.apache.hadoop.hive.serde2.DelimitedJSONSerDe;
@@ -948,17 +949,35 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
 
   private void transformWithinGroup(ASTNode expressionTree, Tree withinGroupNode) throws SemanticException {
     Tree functionNameNode = expressionTree.getChild(0);
-    if (!FunctionRegistry.supportsWithinGroup(functionNameNode.getText())) {
+    if (!FunctionRegistry.isOrderedAggregate(functionNameNode.getText())) {
       throw new SemanticException(ErrorMsg.WITHIN_GROUP_NOT_ALLOWED, functionNameNode.getText());
     }
 
-    Tree tabSortColNameNode = withinGroupNode.getChild(0);
-    ASTNode sortKey = (ASTNode) tabSortColNameNode.getChild(0).getChild(0);
-    expressionTree.deleteChild(withinGroupNode.getChildIndex());
-    // backward compatibility: the sortkey is the first paramater of the percentile_cont and percentile_disc functions
-    expressionTree.insertChild(1, sortKey);
-    expressionTree.addChild(ASTBuilder.createAST(HiveParser.NumberLiteral,
-            Integer.toString(DirectionUtils.tokenToCode(tabSortColNameNode.getType()))));
+    List<Tree> parameters = new ArrayList<>(expressionTree.getChildCount() - 2);
+    for (int i = 1; i < expressionTree.getChildCount() - 1; ++i) {
+      parameters.add(expressionTree.getChild(i));
+    }
+    while (expressionTree.getChildCount() > 1) {
+      expressionTree.deleteChild(1);
+    }
+
+    Tree orderByNode = withinGroupNode.getChild(0);
+    if (parameters.size() != orderByNode.getChildCount()) {
+      throw new SemanticException(ErrorMsg.WITHIN_GROUP_PARAMETER_MISMATCH,
+              Integer.toString(parameters.size()), Integer.toString(orderByNode.getChildCount()));
+    }
+
+    for (int i = 0; i < orderByNode.getChildCount(); ++i) {
+      expressionTree.addChild(parameters.get(i));
+      Tree tabSortColNameNode = orderByNode.getChild(i);
+      Tree nullsNode = tabSortColNameNode.getChild(0);
+      ASTNode sortKey = (ASTNode) tabSortColNameNode.getChild(0).getChild(0);
+      expressionTree.addChild(sortKey);
+      expressionTree.addChild(ASTBuilder.createAST(HiveParser.NumberLiteral,
+              Integer.toString(DirectionUtils.tokenToCode(tabSortColNameNode.getType()))));
+      expressionTree.addChild(ASTBuilder.createAST(HiveParser.NumberLiteral,
+              Integer.toString(NullOrdering.fromToken(nullsNode.getType()).getCode())));
+    }
   }
 
   private List<ASTNode> doPhase1GetDistinctFuncExprs(Map<String, ASTNode> aggregationTrees) {
