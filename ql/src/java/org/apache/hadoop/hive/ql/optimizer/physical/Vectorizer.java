18cf60d3968078d1694dcccbd78e5f812ef21641
Panagiotis Garefalakis <panos@cloudera.com>
2019-11-25 17:35:13 -0800

    DWX-1711 HIVE-22505: ClassCastException caused by wrong Vectorized operator selection (Panagiotis Garefalakis, reviewed by Jesus Camacho Rodriguez)
    (cherry picked from commit bacd9d4c60c2a0f2659315db8c1342589206b5df)
    Change-Id: I3189bcf8843ee600f5b4d1b673a3c072decb5000

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/Vectorizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/Vectorizer.java
@@ -5173,6 +5173,7 @@ public class Vectorizer implements PhysicalPlanResolver {
             if (op instanceof MapJoinOperator) {
 
               MapJoinDesc desc = (MapJoinDesc) op.getConf();
+              int joinType = desc.getConds()[0].getType();
 
               VectorMapJoinDesc vectorMapJoinDesc = new VectorMapJoinDesc();
               boolean specialize =
@@ -5189,6 +5190,10 @@ public class Vectorizer implements PhysicalPlanResolver {
                 if (!isOuterAndFiltered) {
                   opClass = VectorMapJoinOperator.class;
                 } else {
+                  if (joinType == JoinDesc.FULL_OUTER_JOIN) {
+                    setOperatorIssue("Vectorized & filtered full-outer joins not supported");
+                    throw new VectorizerCannotVectorizeException();
+                  }
                   opClass = VectorMapJoinOuterFilteredOperator.class;
                 }
 
