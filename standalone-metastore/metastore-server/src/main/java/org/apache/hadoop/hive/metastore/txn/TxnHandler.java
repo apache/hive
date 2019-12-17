ebb60a36dc6616a9cb369f0a6f8b2a78f4c2f753
David Lavati <dlavati@hortonworks.com>
2019-12-17 16:22:02 +0000

    Use proper signatures of abbreviate/NotImplementedException

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
@@ -5425,7 +5425,7 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
   }
   public void acquireLock(String key, LockHandle handle) {
     //the idea is that this will use LockHandle.dbConn
-    throw new NotImplementedException();
+    throw new NotImplementedException("acquireLock(String, LockHandle) is not implemented");
   }
   private static final class LockHandleImpl implements LockHandle {
     private final Connection dbConn;
@@ -5447,7 +5447,7 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
     void addKey(String key) {
       //keys.add(key);
       //would need a list of (stmt,rs) pairs - 1 for each key
-      throw new NotImplementedException();
+      throw new NotImplementedException("addKey(String) is not implemented, would require a list of (stmt,rs) pairs / key");
     }
     
     @Override
