24c135d145b260d3cfdb78b711b5ca8e77ec603f
Rajesh Balamohan <rbalamohan@apache.org>
2020-01-17 14:46:52 -0800

    HIVE-22724 : Reduce number of DB calls in ObjectStore, TxnHandler (Rajesh Balamohan via Gopal V)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -1843,7 +1843,7 @@ public class ObjectStore implements RawStore, Configurable {
   private List<FieldSchema> convertToFieldSchemas(List<MFieldSchema> mkeys) {
     List<FieldSchema> keys = null;
     if (mkeys != null) {
-      keys = new ArrayList<>(mkeys.size());
+      keys = new ArrayList<>();
       for (MFieldSchema part : mkeys) {
         keys.add(new FieldSchema(part.getName(), part.getType(), part
             .getComment()));
@@ -1855,7 +1855,7 @@ public class ObjectStore implements RawStore, Configurable {
   private List<MOrder> convertToMOrders(List<Order> keys) {
     List<MOrder> mkeys = null;
     if (keys != null) {
-      mkeys = new ArrayList<>(keys.size());
+      mkeys = new ArrayList<>();
       for (Order part : keys) {
         mkeys.add(new MOrder(normalizeIdentifier(part.getCol()), part.getOrder()));
       }
@@ -1866,7 +1866,7 @@ public class ObjectStore implements RawStore, Configurable {
   private List<Order> convertToOrders(List<MOrder> mkeys) {
     List<Order> keys = null;
     if (mkeys != null) {
-      keys = new ArrayList<>(mkeys.size());
+      keys = new ArrayList<>();
       for (MOrder part : mkeys) {
         keys.add(new Order(part.getCol(), part.getOrder()));
       }
@@ -1953,7 +1953,7 @@ public class ObjectStore implements RawStore, Configurable {
   private List<List<String>> convertToSkewedValues(List<MStringList> mLists) {
     List<List<String>> lists = null;
     if (mLists != null) {
-      lists = new ArrayList<>(mLists.size());
+      lists = new ArrayList<>();
       for (MStringList element : mLists) {
         lists.add(new ArrayList<>(element.getInternalList()));
       }
@@ -1978,7 +1978,7 @@ public class ObjectStore implements RawStore, Configurable {
   private Map<List<String>, String> covertToSkewedMap(Map<MStringList, String> mMap) {
     Map<List<String>, String> map = null;
     if (mMap != null) {
-      map = new HashMap<>(mMap.size());
+      map = new HashMap<>();
       Set<MStringList> keys = mMap.keySet();
       for (MStringList key : keys) {
         map.put(new ArrayList<>(key.getInternalList()), mMap.get(key));
@@ -1993,7 +1993,7 @@ public class ObjectStore implements RawStore, Configurable {
   private Map<MStringList, String> covertToMapMStringList(Map<List<String>, String> mMap) {
     Map<MStringList, String> map = null;
     if (mMap != null) {
-      map = new HashMap<>(mMap.size());
+      map = new HashMap<>();
       Set<List<String>> keys = mMap.keySet();
       for (List<String> key : keys) {
         map.put(new MStringList(key), mMap.get(key));
