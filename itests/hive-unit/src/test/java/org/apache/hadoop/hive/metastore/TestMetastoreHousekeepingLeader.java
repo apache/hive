b9ecc5b261eb474364931a90d238b03f741aa14e
miklosgergely <mgergely@cloudera.com>
2020-03-12 13:24:05 +0100

    CDPD-9033 Ignore TestMetastoreHousekeepingLeader.testHouseKeepingThreadExistence as it is flaky
    Change-Id: I54d69efe60b495abe24dd5c94923d65dee5fe6ae

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestMetastoreHousekeepingLeader.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestMetastoreHousekeepingLeader.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.hive.metastore;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +38,7 @@ public class TestMetastoreHousekeepingLeader extends MetastoreHousekeepingLeader
     internalSetup("localhost");
   }
 
+  @Ignore("CDPD-9033 Ignore TestMetastoreHousekeepingLeader.testHouseKeepingThreadExistence as it is flaky")
   @Test
   public void testHouseKeepingThreadExistence() throws Exception {
     searchHousekeepingThreads();
