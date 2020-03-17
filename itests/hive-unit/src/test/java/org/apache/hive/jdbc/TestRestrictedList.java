a6a0ba59dff73f7ad27bf02a20da1ce06085d0b2
Daniel Voros <daniel.voros@gmail.com>
2020-03-17 17:00:17 +0000

    HIVE-22901: Variable substitution can lead to OOM on circular references (Daniel Voros via Zoltan Haindrich)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestRestrictedList.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestRestrictedList.java
@@ -93,6 +93,7 @@ public class TestRestrictedList {
     addToExpectedRestrictedMap("hive.spark.client.rpc.server.address");
     addToExpectedRestrictedMap("hive.spark.client.rpc.server.port");
     addToExpectedRestrictedMap("hive.spark.client.rpc.sasl.mechanisms");
+    addToExpectedRestrictedMap("hive.query.max.length");
     addToExpectedRestrictedMap("bonecp.test");
     addToExpectedRestrictedMap("hive.druid.broker.address.default");
     addToExpectedRestrictedMap("hive.druid.coordinator.address.default");
