22475fb4d0420afcb07f95cb8f6e03f377aad6af
miklosgergely <mgergely@cloudera.com>
2020-03-18 20:56:55 +0100

    CDPD-8493 : HIVE-23049 Constraint name uniqueness query should set Long as parameter instead of long (Miklos Gergely, reviewed by Jesus Camacho Rodriguez)
    Change-Id: Icccc22bddc77fe459b228d923686b146d1d1b55f

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -4794,7 +4794,9 @@ public class ObjectStore implements RawStore, Configurable {
       constraintExistsQuery.declareParameters("java.lang.Long parentTableP, java.lang.String constraintNameP");
       constraintExistsQuery.setUnique(true);
       constraintExistsQuery.setResult("constraintName");
-      constraintNameIfExists = (String) constraintExistsQuery.executeWithArray(table.getId(), constraintName);
+      // table id must be converted to Long as Oracle 19 can not handle primitive types
+      constraintNameIfExists = (String) constraintExistsQuery.executeWithArray(Long.valueOf(table.getId()),
+          constraintName);
       commited = commitTransaction();
     } finally {
       rollbackAndCleanup(commited, constraintExistsQuery);
