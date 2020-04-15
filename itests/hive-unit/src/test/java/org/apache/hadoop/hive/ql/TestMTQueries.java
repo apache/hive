7c86d4e60d528a6a53b22b4ef0f7344559e0bec0
miklosgergely <mgergely@cloudera.com>
2020-04-15 17:36:37 +0200

    HIVE-23205 ADDENDUM Do not run TestMTQueries until HIVE-23138 is finished (Miklos Gergely, reviewed by Jesus Camacho Rodriguez)

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/TestMTQueries.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/TestMTQueries.java
@@ -19,12 +19,15 @@
 package org.apache.hadoop.hive.ql;
 
 import java.io.File;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.fail;
 
 /**
  * Suite for testing running of queries in multi-threaded mode.
  */
+@Ignore("Ignore until HIVE-23138 is finished")
 public class TestMTQueries extends BaseTestQueries {
 
   public TestMTQueries() {
