2bcd672a3ef69b1f5e2e91a712f8986fe57846ed
Zoltan Haindrich <zhaindrich@cloudera.com>
2020-04-20 11:03:54 +0000

    throw it out

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestJdbcWithMiniLlapArrow.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestJdbcWithMiniLlapArrow.java
@@ -384,6 +384,9 @@ public class TestJdbcWithMiniLlapArrow extends BaseJdbcWithMiniLlap {
         tKillHolder.throwable = e;
       }
     }
+    if (count >= 10) {
+      throw new RuntimeException("test timed-out", stmtHolder.throwable);
+    }
 
     tExecute.join();
     try {
