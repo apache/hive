658962d99fc78f3d1f757a77b6fd8d5af70cb479
Aasha Medhi <amedhi@cloudera.com>
2020-04-23 16:25:26 +0530

    Avoid overwrite in case of incremental replication

--- ql/src/java/org/apache/hadoop/hive/ql/exec/ReplCopyTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/ReplCopyTask.java
@@ -221,7 +221,7 @@ public class ReplCopyTask extends Task<ReplCopyWork> implements Serializable {
       }
       // Copy the files from different source file systems to one destination directory
       CopyUtils copyUtils = new CopyUtils(rwork.distCpDoAsUser(), conf, dstFs);
-      copyUtils.copyAndVerify(toPath, srcFiles, fromPath);
+      copyUtils.copyAndVerify(toPath, srcFiles, fromPath, true);
 
       // If a file is copied from CM path, then need to rename them using original source file name
       // This is needed to avoid having duplicate files in target if same event is applied twice
