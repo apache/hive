7257d225fb53fdfe945458d10275ece34b70d95b
Zoltan Haindrich <zhaindrich@cloudera.com>
2020-05-04 18:53:42 +0000

    cleanup

--- itests/util/src/test/java/org/apache/hadoop/hive/cli/control/TestSplitSupport.java
+++ itests/util/src/test/java/org/apache/hadoop/hive/cli/control/TestSplitSupport.java
@@ -21,13 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import org.junit.Test;
-
-import com.google.common.collect.Sets;
 
 public class TestSplitSupport {
 
