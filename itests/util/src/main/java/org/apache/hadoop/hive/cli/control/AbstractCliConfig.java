3abe763fd350c897b52fee9ca33581a78bb0b258
Zoltan Haindrich <zhaindrich@cloudera.com>
2020-05-13 10:08:01 +0000

    excludequery is private

--- itests/util/src/main/java/org/apache/hadoop/hive/cli/control/AbstractCliConfig.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/AbstractCliConfig.java
@@ -130,7 +130,7 @@ public abstract class AbstractCliConfig {
     }
   }
 
-  protected void excludeQuery(String qFile) {
+  private void excludeQuery(String qFile) {
     excludedQueryFileNames.add(qFile);
   }
 
