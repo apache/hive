d842d9fda11ae2a33bf906dcb617e1ac0db47576
Adesh Kumar Rao <adrao@microsoft.com>
2020-06-24 10:49:41 +0530

    Fix comments

--- ql/src/java/org/apache/hadoop/hive/ql/reexec/ReExecuteLostAMQueryPlugin.java
+++ ql/src/java/org/apache/hadoop/hive/ql/reexec/ReExecuteLostAMQueryPlugin.java
@@ -26,50 +26,50 @@ import org.apache.hadoop.hive.ql.plan.mapper.PlanMapper;
 import java.util.regex.Pattern;
 
 public class ReExecuteLostAMQueryPlugin implements IReExecutionPlugin {
-    private boolean retryPossible;
+  private boolean retryPossible;
 
-    // Lost am container have exit code -100, due to node failures.
-    private Pattern lostAMContainerErrorPattern = Pattern.compile(".*AM Container for .* exited .* exitCode: -100.*");
+  // Lost am container have exit code -100, due to node failures.
+  private Pattern lostAMContainerErrorPattern = Pattern.compile(".*AM Container for .* exited .* exitCode: -100.*");
 
-    class LocalHook implements ExecuteWithHookContext {
+  class LocalHook implements ExecuteWithHookContext {
 
-        @Override
-        public void run(HookContext hookContext) throws Exception {
-            if (hookContext.getHookType() == HookContext.HookType.ON_FAILURE_HOOK) {
-                Throwable exception = hookContext.getException();
+    @Override
+    public void run(HookContext hookContext) throws Exception {
+      if (hookContext.getHookType() == HookContext.HookType.ON_FAILURE_HOOK) {
+        Throwable exception = hookContext.getException();
 
-                if (exception != null && exception.getMessage() != null
-                        && lostAMContainerErrorPattern.matcher(exception.getMessage()).matches()) {
-                    retryPossible = true;
-                }
-            }
+        if (exception != null && exception.getMessage() != null
+            && lostAMContainerErrorPattern.matcher(exception.getMessage()).matches()) {
+          retryPossible = true;
         }
+      }
     }
+  }
 
-    @Override
-    public void initialize(Driver driver) {
-        driver.getHookRunner().addOnFailureHook(new LocalHook());
-    }
+  @Override
+  public void initialize(Driver driver) {
+    driver.getHookRunner().addOnFailureHook(new LocalHook());
+  }
 
-    @Override
-    public void beforeExecute(int executionIndex, boolean explainReOptimization) {
-    }
+  @Override
+  public void beforeExecute(int executionIndex, boolean explainReOptimization) {
+  }
 
-    @Override
-    public boolean shouldReExecute(int executionNum) {
-        return retryPossible;
-    }
+  @Override
+  public boolean shouldReExecute(int executionNum) {
+    return retryPossible;
+  }
 
-    @Override
-    public void prepareToReExecute() {
-    }
+  @Override
+  public void prepareToReExecute() {
+  }
 
-    @Override
-    public boolean shouldReExecute(int executionNum, PlanMapper oldPlanMapper, PlanMapper newPlanMapper) {
-        return retryPossible;
-    }
+  @Override
+  public boolean shouldReExecute(int executionNum, PlanMapper oldPlanMapper, PlanMapper newPlanMapper) {
+    return retryPossible;
+  }
 
-    @Override
-    public void afterExecute(PlanMapper planMapper, boolean successfull) {
-    }
+  @Override
+  public void afterExecute(PlanMapper planMapper, boolean successfull) {
+  }
 }
