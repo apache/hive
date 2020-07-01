e09f450740662df5f7b89e5e47b7eae91f234864
Anishek Agarwal <anishek@gmail.com>
2020-07-01 09:07:40 +0530

    HIVE-23611: Mandate fully qualified absolute path for external table base dir during REPL operation (Pravin Kumar Sinha, reviewed by Aasha Medhi)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplExternalTables.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplExternalTables.java
@@ -24,6 +24,7 @@ import org.apache.hadoop.hive.common.FileUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.TableType;
 import org.apache.hadoop.hive.metastore.api.NoSuchObjectException;
+import org.apache.hadoop.hive.metastore.utils.StringUtils;
 import org.apache.hadoop.hive.ql.ErrorMsg;
 import org.apache.hadoop.hive.ql.metadata.Hive;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
@@ -40,6 +41,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.StringWriter;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.HashSet;
@@ -65,13 +67,23 @@ public final class ReplExternalTables {
   private ReplExternalTables(){}
 
   public static String externalTableLocation(HiveConf hiveConf, String location) throws SemanticException {
-    String baseDir = hiveConf.get(HiveConf.ConfVars.REPL_EXTERNAL_TABLE_BASE_DIR.varname);
-    Path basePath = new Path(baseDir);
+    Path basePath = getExternalTableBaseDir(hiveConf);
     Path currentPath = new Path(location);
     Path dataLocation = externalTableDataPath(hiveConf, basePath, currentPath);
 
     LOG.info("Incoming external table location: {} , new location: {}", location, dataLocation.toString());
     return dataLocation.toString();
+  }
+
+  public static Path getExternalTableBaseDir(HiveConf hiveConf) throws SemanticException {
+    String baseDir = hiveConf.get(HiveConf.ConfVars.REPL_EXTERNAL_TABLE_BASE_DIR.varname);
+    URI baseDirUri  = StringUtils.isEmpty(baseDir) ? null : new Path(baseDir).toUri();
+    if (baseDirUri == null || baseDirUri.getScheme() == null || baseDirUri.getAuthority() == null) {
+      throw new SemanticException(
+              String.format("Fully qualified path for 'hive.repl.replica.external.table.base.dir' is required %s",
+                      baseDir == null ? "" : "- ('" + baseDir + "')"));
+    }
+    return new Path(baseDirUri);
   }
 
   public static Path externalTableDataPath(HiveConf hiveConf, Path basePath, Path sourcePath)
