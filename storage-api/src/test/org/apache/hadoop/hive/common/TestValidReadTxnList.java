d1d0c11ea99fda7272edde9ac28b0921f6fa2533
Peter Varga <pvarga@cloudera.com>
2020-07-08 17:59:33 +0200

    HIVE-23819: Use ranges in ValidReadTxnList serialization

--- storage-api/src/test/org/apache/hadoop/hive/common/TestValidReadTxnList.java
+++ storage-api/src/test/org/apache/hadoop/hive/common/TestValidReadTxnList.java
@@ -106,4 +106,20 @@ public class TestValidReadTxnList {
     Assert.assertTrue(txnList.isTxnAborted(8L));
     Assert.assertFalse(txnList.isTxnAborted(10L));
   }
+
+  @Test
+  public void testAbortedTxnRange() throws Exception {
+    long[] exceptions = {2L, 4L, 5L, 6L, 8L, 9L, 10L};
+    BitSet bitSet = new BitSet(exceptions.length);
+    bitSet.set(0);  // mark txn "2L" aborted
+    bitSet.set(5);  // mark txn "9L" aborted, do not create range 8-10
+    ValidTxnList txnList = new ValidReadTxnList(exceptions, bitSet, 11, 4L);
+    String str = txnList.writeToString();
+    Assert.assertEquals("11:4:4-6,8,10:2,9", str);
+    Assert.assertTrue(txnList.isTxnAborted(2L));
+    Assert.assertFalse(txnList.isTxnAborted(4L));
+    Assert.assertFalse(txnList.isTxnAborted(6L));
+    Assert.assertTrue(txnList.isTxnAborted(9L));
+    Assert.assertFalse(txnList.isTxnAborted(10L));
+  }
 }
