b508f76d628d94f4b1b8f3c3e6245156fa19c858
Hunter Logan <hunterlogan@outlook.com>
2020-07-14 13:59:33 -0400

    Small whitespace issue and triggering CI builds again

--- beeline/src/test/org/apache/hive/beeline/TestJSONFileOutputFormat.java
+++ beeline/src/test/org/apache/hive/beeline/TestJSONFileOutputFormat.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.hive.beeline;
 
 import static org.mockito.ArgumentMatchers.anyInt;
