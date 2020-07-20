6827cd6310db1291c5337938e441cd0f0882440c
Rajesh Balamohan <rbalamohan@apache.org>
2020-07-21 04:57:40 +0530

    Revert "HIVE-23843: Improve key evictions in VectorGroupByOperator"
    This reverts commit c54dce886322c4b922b88bce210714b037e6b3b9.

--- ql/src/test/org/apache/hadoop/hive/ql/exec/vector/TestVectorGroupByOperator.java
+++ ql/src/test/org/apache/hadoop/hive/ql/exec/vector/TestVectorGroupByOperator.java
@@ -89,13 +89,11 @@ import org.apache.hadoop.io.Text;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 /**
  * Unit test for the vectorized GROUP BY operator.
  */
-@RunWith(Parameterized.class)
 public class TestVectorGroupByOperator {
 
   HiveConf hconf = new HiveConf();
