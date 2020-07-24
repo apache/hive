1d2ae748085598dd518256d40c330339cf533c36
dh20 <65940415+dh20@users.noreply.github.com>
2020-07-24 16:54:31 +0800

    Update UDFArgumentException.java

--- udf/src/java/org/apache/hadoop/hive/ql/exec/UDFArgumentException.java
+++ udf/src/java/org/apache/hadoop/hive/ql/exec/UDFArgumentException.java
@@ -61,7 +61,10 @@ public class UDFArgumentException extends SemanticException {
    * @param methods
    *          All potential matches.
    */
-  public UDFArgumentException(String message, List<Method> methods) {
+  public UDFArgumentException(String message,
+      Class<?> funcClass,
+      List<TypeInfo> argTypeInfos,
+      List<Method> methods) {
     super(getMessage(message, methods));
     this.funcClass = funcClass;
     this.argTypeInfos = argTypeInfos;
