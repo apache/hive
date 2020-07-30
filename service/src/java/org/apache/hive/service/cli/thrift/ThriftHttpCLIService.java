f911c2ad1b4a3ffacd820fbc15e9259038c4a836
Kevin Risden <krisden@apache.org>
2020-07-30 12:24:25 -0400

    HIVE-23958: HiveServer2 should support additional keystore/truststores types besides JKS

--- service/src/java/org/apache/hive/service/cli/thrift/ThriftHttpCLIService.java
+++ service/src/java/org/apache/hive/service/cli/thrift/ThriftHttpCLIService.java
@@ -18,6 +18,7 @@
 
 package org.apache.hive.service.cli.thrift;
 
+import java.security.KeyStore;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.SynchronousQueue;
@@ -143,6 +144,7 @@ public class ThriftHttpCLIService extends ThriftCLIService {
             + Arrays.toString(sslContextFactory.getExcludeProtocols()));
         sslContextFactory.setKeyStorePath(keyStorePath);
         sslContextFactory.setKeyStorePassword(keyStorePassword);
+        sslContextFactory.setKeyStoreType(KeyStore.getDefaultType());
         connector = new ServerConnector(server, sslContextFactory, http);
       } else {
         connector = new ServerConnector(server, http);
