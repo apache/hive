4f315ca27a5d701062458c8087755c6a300da655
dh20 <65940415+dh20@users.noreply.github.com>
2020-08-06 14:40:19 +0800

    Update UDFArgumentException.java

--- udf/src/java/org/apache/hadoop/hive/ql/exec/UDFArgumentException.java
+++ udf/src/java/org/apache/hadoop/hive/ql/exec/UDFArgumentException.java
@@ -70,7 +70,6 @@ public class UDFArgumentException extends SemanticException {
     this.argTypeInfos = argTypeInfos;
     this.methods = methods;
   }
-  //HIVE-23896 remove unnecessary parameter
   private static String getMessage(String message, List<Method> methods) {
     StringBuilder sb = new StringBuilder();
     sb.append(message);
