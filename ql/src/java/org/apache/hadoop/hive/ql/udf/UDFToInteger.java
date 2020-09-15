74259859b4ed1b22f5445e2a0ad0c5adb5ff5987
Zoltan Haindrich <kirk@rxd.hu>
2020-09-15 10:45:20 +0000

    ts->num

--- ql/src/java/org/apache/hadoop/hive/ql/udf/UDFToInteger.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/UDFToInteger.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.hive.ql.udf;
 
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.UDFMethodResolver;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedExpressions;
 import org.apache.hadoop.hive.ql.exec.vector.expressions.CastDecimalToLong;
 import org.apache.hadoop.hive.ql.exec.vector.expressions.gen.CastDoubleToLong;
@@ -68,6 +69,11 @@ public class UDFToInteger extends UDF {
   private final IntWritable intWritable = new IntWritable();
 
   public UDFToInteger() {
+  }
+
+  @Override
+  public UDFMethodResolver getResolver() {
+    return new TimestampCastRestrictorResolver(super.getResolver());
   }
 
   /**
