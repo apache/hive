ad991f46ce7722907576ca4eac5f80d46df8d19f
Peter Varga <pvarga@cloudera.com>
2020-09-16 13:50:03 +0200

    DWX-5280: HIVE-24162: Query based compaction looses bloom filter (Peter Varga, reviewed by Laszlo Pinter and Karen Coppage)
    Closes #1498.
    
    (cherry picked from commit 4bf3c2d6b30b378a7128bdd440e7971e3828ac60)
    Change-Id: I7bbec84fc6678e134adf45e75b49b2938f070dca

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorOnTezTest.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorOnTezTest.java
@@ -33,6 +33,7 @@ import org.junit.Before;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
@@ -125,12 +126,17 @@ public class CompactorOnTezTest {
 
     void createFullAcidTable(String tblName, boolean isPartitioned, boolean isBucketed)
         throws Exception {
-      createFullAcidTable(null, tblName, isPartitioned, isBucketed);
+      createFullAcidTable(null, tblName, isPartitioned, isBucketed, null);
     }
 
     void createFullAcidTable(String dbName, String tblName, boolean isPartitioned, boolean isBucketed)
         throws Exception {
-      createTable(dbName, tblName, isPartitioned, isBucketed, false, "orc");
+      createFullAcidTable(dbName, tblName, isPartitioned, isBucketed, null);
+    }
+
+    void createFullAcidTable(String dbName, String tblName, boolean isPartitioned, boolean isBucketed,
+        Map<String, String> additionalTblProperties) throws Exception {
+      createTable(dbName, tblName, isPartitioned, isBucketed, false, "orc", additionalTblProperties);
     }
 
     void createMmTable(String tblName, boolean isPartitioned, boolean isBucketed)
@@ -145,11 +151,11 @@ public class CompactorOnTezTest {
 
     void createMmTable(String dbName, String tblName, boolean isPartitioned, boolean isBucketed, String fileFormat)
         throws Exception {
-      createTable(dbName, tblName, isPartitioned, isBucketed, true, fileFormat);
+      createTable(dbName, tblName, isPartitioned, isBucketed, true, fileFormat, null);
     }
 
     private void createTable(String dbName, String tblName, boolean isPartitioned, boolean isBucketed,
-        boolean insertOnly, String fileFormat) throws Exception {
+        boolean insertOnly, String fileFormat, Map<String, String> additionalTblProperties) throws Exception {
       if (dbName != null) {
         tblName = dbName + "." + tblName;
       }
@@ -164,6 +170,11 @@ public class CompactorOnTezTest {
       }
       query.append(" stored as ").append(fileFormat);
       query.append(" TBLPROPERTIES('transactional'='true',");
+      if (additionalTblProperties != null) {
+        for (Map.Entry<String, String> e : additionalTblProperties.entrySet()) {
+          query.append("'").append(e.getKey()).append("'='").append(e.getValue()).append("', ");
+        }
+      }
       if (insertOnly) {
         query.append(" 'transactional_properties'='insert_only')");
       } else {
