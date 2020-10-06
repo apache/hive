2c6fcf0bf8d68773767ab50c0edef4c683945b74
Panagiotis Garefalakis <pgaref@cloudera.com>
2020-10-06 10:55:53 +0100

    HIVE-24225: Revert HIVE-23158 HIVE-23393 Optimize S3A recordReader policy for Random IO formats" (#1547) (Panagiotis Garefalakis reviewed by Laszlo Bodor)
    Change-Id: Icae85d2f6ece7ad3e18ab55ddca098684382173f

--- ql/src/java/org/apache/hadoop/hive/ql/io/HiveInputFormat.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/HiveInputFormat.java
@@ -24,8 +24,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.s3a.S3AFileSystem;
-import org.apache.hadoop.fs.s3a.S3AInputPolicy;
 import org.apache.hadoop.hive.common.FileUtils;
 import org.apache.hadoop.hive.common.StringInternUtils;
 import org.apache.hadoop.hive.common.ValidTxnWriteIdList;
@@ -40,8 +38,6 @@ import org.apache.hadoop.hive.ql.exec.SerializationUtilities;
 import org.apache.hadoop.hive.ql.exec.TableScanOperator;
 import org.apache.hadoop.hive.ql.exec.Utilities;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
-import org.apache.hadoop.hive.ql.io.orc.OrcInputFormat;
-import org.apache.hadoop.hive.ql.io.parquet.VectorizedParquetInputFormat;
 import org.apache.hadoop.hive.ql.log.PerfLogger;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.metadata.HiveStoragePredicateHandler;
@@ -93,7 +89,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import static java.lang.Integer.min;
-import static org.apache.hadoop.hive.common.FileUtils.isS3a;
 
 /**
  * HiveInputFormat is a parameterized InputFormat which looks at the path name
@@ -384,19 +379,6 @@ public class HiveInputFormat<K extends WritableComparable, V extends Writable>
     return instance;
   }
 
-  /**
-   * Returns true if the inputFormat performs random seek+read
-   * @param inputFormat
-   * @return
-   */
-  public static boolean isRandomAccessInputFormat(InputFormat inputFormat) {
-    if (inputFormat instanceof OrcInputFormat ||
-        inputFormat instanceof VectorizedParquetInputFormat) {
-      return true;
-    }
-    return false;
-  }
-
   @Override
   public RecordReader getRecordReader(InputSplit split, JobConf job,
       Reporter reporter) throws IOException {
@@ -449,13 +431,6 @@ public class HiveInputFormat<K extends WritableComparable, V extends Writable>
       innerReader = HiveIOExceptionHandlerUtil
           .handleRecordReaderCreationException(e, job);
     }
-
-    FileSystem splitFileSystem = splitPath.getFileSystem(job);
-    if (isS3a(splitFileSystem) && isRandomAccessInputFormat(inputFormat)) {
-      LOG.debug("Changing S3A input policy to RANDOM");
-      ((S3AFileSystem) splitFileSystem).setInputPolicy(S3AInputPolicy.Random);
-    }
-
     HiveRecordReader<K,V> rr = new HiveRecordReader(innerReader, job);
     rr.initIOContext(hsplit, job, inputFormatClass, innerReader);
     return rr;
