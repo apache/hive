3c2e16c9fe090ffe35485f3d3040eb903639b5d9
kkasa <kkasa@cloudera.com>
2020-10-28 07:36:30 +0100

    enable unparsing if query based mv rewrite is enabled

--- ql/src/java/org/apache/hadoop/hive/ql/Context.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Context.java
@@ -336,7 +336,8 @@ public class Context {
     opContext = new CompilationOpContext();
 
     viewsTokenRewriteStreams = new HashMap<>();
-    enableUnparse = HiveConf.getBoolVar(conf, HiveConf.ConfVars.HIVE_QUERY_RESULTS_CACHE_ENABLED);
+    enableUnparse =
+        HiveConf.getBoolVar(conf, HiveConf.ConfVars.HIVE_MATERIALIZED_VIEW_ENABLE_AUTO_REWRITING_QUERY_TEXT);
   }
 
   protected Context(Context ctx) {
