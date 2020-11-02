a6f4d86a007e26e968df68ad9c2befb35f69864d
Chao Sun <sunchao@apache.org>
2020-11-02 13:54:20 -0800

    Fix parsing precision & scale

--- serde/src/java/org/apache/hadoop/hive/serde2/avro/SchemaToTypeInfo.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/avro/SchemaToTypeInfo.java
@@ -136,8 +136,14 @@ class SchemaToTypeInfo {
       int precision = 0;
       int scale = 0;
       try {
-        precision = AvroSerdeUtils.getIntFromSchema(schema, AvroSerDe.AVRO_PROP_PRECISION);
-        scale = AvroSerdeUtils.getIntFromSchema(schema, AvroSerDe.AVRO_PROP_SCALE);
+        Object o = schema.getObjectProp(AvroSerDe.AVRO_PROP_PRECISION);
+        if (o instanceof Integer) {
+          precision = (int) o;
+        }
+        o = schema.getObjectProp(AvroSerDe.AVRO_PROP_SCALE);
+        if (o instanceof Integer) {
+          scale = (int) o;
+        }
       } catch (Exception ex) {
         throw new AvroSerdeException("Failed to obtain scale value from file schema: " + schema, ex);
       }
