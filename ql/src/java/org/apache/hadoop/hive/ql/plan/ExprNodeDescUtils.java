98656304df2c7b09124c362648fa0debc5916dad
Zoltan Haindrich <kirk@rxd.hu>
2020-11-13 08:56:57 +0100

    HIVE-24365: SWO should not create complex and redundant filter expressions (#1661)  (Zoltan Haindrich reviewed by Jesus Camacho Rodriguez)

--- ql/src/java/org/apache/hadoop/hive/ql/plan/ExprNodeDescUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/ExprNodeDescUtils.java
@@ -1067,6 +1067,15 @@ public class ExprNodeDescUtils {
     for (ExprNodeDesc e : inputExpr) {
       conjunctiveDecomposition(e, operands);
     }
+    for (int i = 0; i < operands.size(); i++) {
+      ExprNodeDesc curr = operands.get(i);
+      if (isOr(curr)) {
+        if (deterministicIntersection(curr.getChildren(), operands)) {
+          operands.remove(i);
+          i--;
+        }
+      }
+    }
 
     if (operands.isEmpty()) {
       return null;
@@ -1076,6 +1085,23 @@ public class ExprNodeDescUtils {
     } else {
       return operands.get(0);
     }
+  }
+
+  /**
+   * Checks wether the two expression sets have a common deterministic intersection.
+   */
+  private static boolean deterministicIntersection(List<ExprNodeDesc> li1, List<ExprNodeDesc> li2) {
+    for (ExprNodeDesc e1 : li1) {
+      if (!isDeterministic(e1)) {
+        continue;
+      }
+      for (ExprNodeDesc e2 : li2) {
+        if (e1.isSame(e2)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   private static void conjunctiveDecomposition(ExprNodeDesc expr, List<ExprNodeDesc> operands) {
@@ -1198,6 +1224,21 @@ public class ExprNodeDescUtils {
       }
     }
     return expr;
+  }
+
+  public static void replaceTabAlias(Map<String, ExprNodeDesc> exprMap, String oldAlias, String newAlias) {
+    if (exprMap != null) {
+      ExprNodeDescUtils.replaceTabAlias(exprMap.values(), oldAlias, newAlias);
+    }
+  }
+
+  public static void replaceTabAlias(Collection<ExprNodeDesc> exprs, String oldAlias, String newAlias) {
+    if (exprs != null) {
+      for (ExprNodeDesc expr : exprs) {
+        replaceTabAlias(expr, oldAlias, newAlias);
+      }
+    }
+
   }
 
 }
