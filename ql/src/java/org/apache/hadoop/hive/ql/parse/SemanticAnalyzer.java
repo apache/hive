4e94a79b0ee30b09138e4ad7b01c29fb396ccbab
Naveen Gangam <ngangam@cloudera.com>
2020-11-25 17:28:14 -0500

    Retain case on table names during query processing

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -1112,7 +1112,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
 
     ASTNode tableTree = (ASTNode) (tabref.getChild(0));
 
-    String tabIdName = getUnescapedName(tableTree).toLowerCase();
+    String tabIdName = getUnescapedName(tableTree);
 
     String alias = findSimpleTableName(tabref, aliasIndex);
 
