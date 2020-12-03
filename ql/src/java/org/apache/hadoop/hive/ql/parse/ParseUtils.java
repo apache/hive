ca366695ebcd5c96df390217774c4adf54a7c3e7
Zoltan Haindrich <kirk@rxd.hu>
2020-12-03 12:06:00 +0100

    HIVE-21843: UNION query with regular expressions for column name does not work (#1684) (Zoltan Haindrich reviewed by Krisztian Kasa)

--- ql/src/java/org/apache/hadoop/hive/ql/parse/ParseUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/ParseUtils.java
@@ -79,9 +79,6 @@ import org.slf4j.LoggerFactory;
 public final class ParseUtils {
   /** Parses the Hive query. */
   private static final Logger LOG = LoggerFactory.getLogger(ParseUtils.class);
-  public static ASTNode parse(String command) throws ParseException {
-    return parse(command, null);
-  }
 
   /** Parses the Hive query. */
   public static ASTNode parse(String command, Context ctx) throws ParseException {
@@ -105,7 +102,7 @@ public final class ParseUtils {
     }
     ASTNode tree = parseResult.getTree();
     tree = findRootNonNullToken(tree);
-    handleSetColRefs(tree);
+    handleSetColRefs(tree, ctx);
     return tree;
   }
 
@@ -355,13 +352,13 @@ public final class ParseUtils {
     }
 
 
-    private static void handleSetColRefs(ASTNode tree) {
+    private static void handleSetColRefs(ASTNode tree, Context ctx) {
       CalcitePlanner.ASTSearcher astSearcher = new CalcitePlanner.ASTSearcher();
       while (true) {
         astSearcher.reset();
         ASTNode setCols = astSearcher.depthFirstSearch(tree, HiveParser.TOK_SETCOLREF);
         if (setCols == null) break;
-        processSetColsNode(setCols, astSearcher);
+        processSetColsNode(setCols, astSearcher, ctx);
       }
     }
 
@@ -374,7 +371,7 @@ public final class ParseUtils {
      * @param setCols TOK_SETCOLREF ASTNode.
      * @param searcher AST searcher to reuse.
      */
-    private static void processSetColsNode(ASTNode setCols, ASTSearcher searcher) {
+    private static void processSetColsNode(ASTNode setCols, ASTSearcher searcher, Context ctx) {
       searcher.reset();
       CommonTree rootNode = setCols;
       while (rootNode != null && rootNode.getType() != HiveParser.TOK_INSERT) {
@@ -460,8 +457,8 @@ public final class ParseUtils {
         switch (child.getType()) {
         case HiveParser.TOK_SETCOLREF:
           // We have a nested setcolref. Process that and start from scratch TODO: use stack?
-          processSetColsNode((ASTNode)child, searcher);
-          processSetColsNode(setCols, searcher);
+          processSetColsNode((ASTNode)child, searcher, ctx);
+          processSetColsNode(setCols, searcher, ctx);
           return;
         case HiveParser.TOK_ALLCOLREF:
           // We should find an alias of this insert and do (alias).*. This however won't fix e.g.
@@ -472,13 +469,13 @@ public final class ParseUtils {
         case HiveParser.TOK_TABLE_OR_COL:
           Tree idChild = child.getChild(0);
           assert idChild.getType() == HiveParser.Identifier : idChild;
-          if (!createChildColumnRef(idChild, alias, newChildren, aliases)) {
+          if (!createChildColumnRef(idChild, alias, newChildren, aliases, ctx)) {
             setCols.token.setType(HiveParser.TOK_ALLCOLREF);
             return;
           }
           break;
         case HiveParser.Identifier:
-          if (!createChildColumnRef(child, alias, newChildren, aliases)) {
+          if (!createChildColumnRef(child, alias, newChildren, aliases, ctx)) {
             setCols.token.setType(HiveParser.TOK_ALLCOLREF);
             return;
           }
@@ -486,7 +483,7 @@ public final class ParseUtils {
         case HiveParser.DOT: {
           Tree colChild = child.getChild(child.getChildCount() - 1);
           assert colChild.getType() == HiveParser.Identifier : colChild;
-          if (!createChildColumnRef(colChild, alias, newChildren, aliases)) {
+          if (!createChildColumnRef(colChild, alias, newChildren, aliases, ctx)) {
             setCols.token.setType(HiveParser.TOK_ALLCOLREF);
             return;
           }
@@ -514,8 +511,12 @@ public final class ParseUtils {
     }
 
     private static boolean createChildColumnRef(Tree child, String alias,
-        List<ASTNode> newChildren, HashSet<String> aliases) {
+        List<ASTNode> newChildren, HashSet<String> aliases, Context ctx) {
       String colAlias = child.getText();
+      if (SemanticAnalyzer.isRegex(colAlias, (HiveConf)ctx.getConf())) {
+        LOG.debug("Skip creating child column reference because of regexp used as alias: " + colAlias);
+        return false;
+      }
       if (!aliases.add(colAlias)) {
         // TODO: if a side of the union has 2 columns with the same name, noone on the higher
         //       level can refer to them. We could change the alias in the original node.
