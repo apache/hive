fd8cc61dc2b047585392cdf2aa2a829c11cc3caf
Zoltan Haindrich <kirk@rxd.hu>
2020-12-07 15:46:04 +0000

    remove file writes

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
@@ -148,13 +148,6 @@ public class SharedWorkOptimizer extends Transform {
       LOG.debug("Before SharedWorkOptimizer:\n" + Operator.toString(pctx.getTopOps().values()));
     }
 
-    try {
-      new OperatorGraph(pctx).toDot(new File("/tmp/0.full.dot"));
-      new OperatorGraph(pctx).implode().toDot(new File("/tmp/0.joins.dot"));
-    } catch (Exception e1) {
-      throw new RuntimeException(e1);
-    }
-
     // We enforce a certain order when we do the reutilization.
     // In particular, we use size of table x number of reads to
     // rank the tables.
@@ -231,13 +224,6 @@ public class SharedWorkOptimizer extends Transform {
         }
       }
 
-    }
-
-    try {
-      new OperatorGraph(pctx).toDot(new File("/tmp/1.full.dot"));
-      new OperatorGraph(pctx).implode().toDot(new File("/tmp/1.joins.dot"));
-    } catch (Exception e1) {
-      throw new RuntimeException(e1);
     }
 
     if(pctx.getConf().getBoolVar(ConfVars.HIVE_SHARED_WORK_REUSE_MAPJOIN_CACHE)) {
