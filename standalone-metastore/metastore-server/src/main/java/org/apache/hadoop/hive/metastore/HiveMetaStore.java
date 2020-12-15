b77c8a75b477b3b604ea1d5a5ac033d03cddaa40
Zoltan Haindrich <kirk@rxd.hu>
2020-12-15 12:43:13 +0000

    en-back hms  changes

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -3942,8 +3942,8 @@ public class HiveMetaStore extends ThriftHiveMetastore {
       List<ColumnStatistics> partsColStats = new ArrayList<>(parts.size());
       List<Long> partsWriteIds = new ArrayList<>(parts.size());
 
-//      Lock tableLock = tablelocks.get(dbName + "." + tblName);
-//      tableLock.lock();
+      Lock tableLock = tablelocks.get(dbName + "." + tblName);
+      tableLock.lock();
       try {
         ms.openTransaction();
         tbl = ms.getTable(catName, dbName, tblName, null);
@@ -3984,11 +3984,7 @@ public class HiveMetaStore extends ThriftHiveMetastore {
             existingParts.add(part);
           }
         }
-try{
-	Thread.sleep(1000);
-}catch(Exception e){
-throw new RuntimeException(e);
-}
+
         newParts.addAll(createPartitionFolders(partitionsToAdd, tbl, addedPartitions));
 
         if (!newParts.isEmpty()) {
@@ -4055,7 +4051,7 @@ throw new RuntimeException(e);
           }
           }
         } finally {
-//          tableLock.unlock();
+          tableLock.unlock();
         }
       }
 
@@ -4339,8 +4335,8 @@ throw new RuntimeException(e);
       Table tbl = null;
       Map<String, String> transactionalListenerResponses = Collections.emptyMap();
       Database db = null;
-//      Lock tableLock = tablelocks.get(dbName + tblName);
-//      tableLock.lock();
+      Lock tableLock = tablelocks.get(dbName + tblName);
+      tableLock.lock();
       try {
         ms.openTransaction();
         tbl = ms.getTable(catName, dbName, tblName, null);
@@ -4393,7 +4389,7 @@ throw new RuntimeException(e);
                                                   transactionalListenerResponses, ms);
           }
         } finally {
-//          tableLock.unlock();
+          tableLock.unlock();
         }
       }
     }
