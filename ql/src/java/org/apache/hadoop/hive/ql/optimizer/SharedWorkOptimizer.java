ef3fcacefbf3df4ef57e4cd60ea1230e046e4b96
Zoltan Haindrich <kirk@rxd.hu>
2021-01-15 12:59:59 +0000

    Revert "remove og printout"
    This reverts commit bb7c447e4fce8348992523de7fcb98aea58d245a.

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
@@ -146,6 +146,13 @@ public class SharedWorkOptimizer extends Transform {
       return pctx;
     }
 
+    try {
+      new OperatorGraph(pctx).toDot(new java.io.File("/tmp/0.full.dot"));
+      new OperatorGraph(pctx).implode().toDot(new java.io.File("/tmp/0.joins.dot"));
+    } catch (Exception e1) {
+      throw new RuntimeException(e1);
+    }
+
     if (LOG.isDebugEnabled()) {
       LOG.debug("Before SharedWorkOptimizer:\n" + Operator.toString(pctx.getTopOps().values()));
     }
@@ -301,6 +308,13 @@ public class SharedWorkOptimizer extends Transform {
           }
         }
       }
+    }
+
+    try {
+      new OperatorGraph(pctx).toDot(new java.io.File("/tmp/1.full.dot"));
+      new OperatorGraph(pctx).implode().toDot(new java.io.File("/tmp/1.joins.dot"));
+    } catch (Exception e1) {
+      throw new RuntimeException(e1);
     }
 
     return pctx;
