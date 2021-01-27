74b167e2f3fef3142798c8f6a458eef5219dc7eb
ArkoSharma <37324791+ArkoSharma@users.noreply.github.com>
2021-01-27 19:19:22 +0530

    HIVE-24675: Handle external table replication for HA with same NS and… (Arko Sharma, reviewed by Aasha )
    * HIVE-24675: Handle external table replication for HA with same NS and lazy copy.
    
    * HIVE-24675: Fixed test errors
    
    * HIVE-24675: Fix review comments

--- ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplExternalTables.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplExternalTables.java
@@ -35,6 +35,7 @@ import org.apache.hadoop.hive.ql.metadata.Partition;
 import org.apache.hadoop.hive.ql.metadata.Table;
 import org.apache.hadoop.hive.ql.parse.SemanticException;
 import org.apache.hadoop.hive.ql.parse.repl.PathBuilder;
+import org.apache.hadoop.hive.ql.parse.repl.dump.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -183,6 +184,22 @@ public final class ReplExternalTables {
             throws HiveException {
       Path basePath = getExternalTableBaseDir(conf);
       Path targetPath = externalTableDataPath(conf, basePath, sourcePath);
+      //Here, when src and target are HA clusters with same NS, then sourcePath would have the correct host
+      //whereas the targetPath would have an host that refers to the target cluster. This is fine for
+      //data-copy running during dump as the correct logical locations would be used. But if data-copy runs during
+      //load, then the remote location needs to point to the src cluster from where the data would be copied and
+      //the common original NS would suffice for targetPath.
+      if(hiveConf.getBoolVar(HiveConf.ConfVars.REPL_HA_DATAPATH_REPLACE_REMOTE_NAMESERVICE) &&
+              hiveConf.getBoolVar(HiveConf.ConfVars.REPL_RUN_DATA_COPY_TASKS_ON_TARGET)) {
+        String remoteNS = hiveConf.get(HiveConf.ConfVars.REPL_HA_DATAPATH_REPLACE_REMOTE_NAMESERVICE_NAME.varname);
+        if (StringUtils.isEmpty(remoteNS)) {
+          throw new SemanticException(ErrorMsg.REPL_INVALID_CONFIG_FOR_SERVICE
+                  .format("Configuration 'hive.repl.ha.datapath.replace.remote.nameservice.name' is not valid "
+                          + remoteNS == null ? "null" : remoteNS, ReplUtils.REPL_HIVE_SERVICE));
+        }
+        targetPath = new Path(Utils.replaceHost(targetPath.toString(), sourcePath.toUri().getHost()));
+        sourcePath = new Path(Utils.replaceHost(sourcePath.toString(), remoteNS));
+      }
       fileList.add(new DirCopyWork(sourcePath, targetPath).convertToString());
     }
 
