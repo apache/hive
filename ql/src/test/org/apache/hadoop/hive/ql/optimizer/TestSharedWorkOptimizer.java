7042ee4f0d10200b5f9efec1f55c8ac11f76ced6
Zoltan Haindrich <kirk@rxd.hu>
2021-02-02 16:56:00 +0100

    HIVE-24295: Apply schema merge to all shared work optimizations (#1662) (Zoltan Haindrich reviewed by Krisztian Kasa)

--- ql/src/test/org/apache/hadoop/hive/ql/optimizer/TestSharedWorkOptimizer.java
+++ ql/src/test/org/apache/hadoop/hive/ql/optimizer/TestSharedWorkOptimizer.java
@@ -18,10 +18,29 @@
 
 package org.apache.hadoop.hive.ql.optimizer;
 
+import org.apache.hadoop.hive.ql.CompilationOpContext;
+import org.apache.hadoop.hive.ql.exec.Operator;
+import org.apache.hadoop.hive.ql.exec.OperatorFactory;
+import org.apache.hadoop.hive.ql.exec.TableScanOperator;
+import org.apache.hadoop.hive.ql.metadata.Table;
+import org.apache.hadoop.hive.ql.optimizer.SharedWorkOptimizer.TSComparator;
+import org.apache.hadoop.hive.ql.plan.ExprNodeColumnDesc;
+import org.apache.hadoop.hive.ql.plan.ExprNodeConstantDesc;
+import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
+import org.apache.hadoop.hive.ql.plan.ExprNodeGenericFuncDesc;
 import org.apache.hadoop.hive.ql.plan.ReduceSinkDesc;
+import org.apache.hadoop.hive.ql.plan.Statistics;
+import org.apache.hadoop.hive.ql.plan.TableScanDesc;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDFConcat;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 
 import static org.apache.hadoop.hive.ql.plan.ReduceSinkDesc.ReducerTraits.AUTOPARALLEL;
 import static org.apache.hadoop.hive.ql.plan.ReduceSinkDesc.ReducerTraits.FIXED;
@@ -123,4 +142,62 @@ public class TestSharedWorkOptimizer {
     // UNIFORM and AUTOPARALLEL
     ensureDeduplicate(uniformAutoparallel, 1, uniformAutoparallel, 2, uniformAutoparallel, 2);
   }
+
+  @Test
+  public void testTSCmp() {
+
+    ArrayList<TableScanOperator> li = Lists.newArrayList(addFilter(getTsOp(), 1), getTsOp());
+    li.sort(new TSComparator());
+    assertNull(li.get(0).getConf().getFilterExpr());
+
+  }
+
+  @Test
+  public void testTSCmpOrdersById() {
+    TableScanOperator ts1 = getTsOp();
+    TableScanOperator ts2 = getTsOp();
+    ArrayList<TableScanOperator> li1 = Lists.newArrayList(ts1, ts2);
+    ArrayList<TableScanOperator> li2 = Lists.newArrayList(ts2, ts1);
+    li1.sort(new TSComparator());
+    li2.sort(new TSComparator());
+    assertTrue(li1.get(0) == li2.get(0));
+  }
+
+  @Test
+  public void testTSCmpOrdersByDataSizeDesc() {
+    TableScanOperator ts1 = getTsOp();
+    TableScanOperator ts2 = getTsOp();
+    TableScanOperator ts3 = getTsOp();
+    ts1.setStatistics(new Statistics(100, 100, 1, 1));
+    ts2.setStatistics(new Statistics(1000, 1000, 1, 1));
+    ts3.setStatistics(new Statistics(10, 10, 1, 1));
+    ArrayList<TableScanOperator> li1 = Lists.newArrayList(ts1, ts3, ts2);
+    li1.sort(new TSComparator());
+
+    assertTrue(li1.get(0).getStatistics().getDataSize() == 1000);
+    assertTrue(li1.get(1).getStatistics().getDataSize() == 100);
+    assertTrue(li1.get(2).getStatistics().getDataSize() == 10);
+  }
+
+
+  CompilationOpContext cCtx = new CompilationOpContext();
+
+  private TableScanOperator getTsOp() {
+    Table tblMetadata = new Table("db", "table");
+    TableScanDesc desc = new TableScanDesc("alias_" + cCtx.nextOperatorId(), tblMetadata);
+    Operator<TableScanDesc> ts = OperatorFactory.get(cCtx, desc);
+    return (TableScanOperator) ts;
+  }
+
+  private TableScanOperator addFilter(TableScanOperator ts, int i) {
+    TableScanDesc desc = ts.getConf();
+    List<ExprNodeDesc> as =
+        Lists.newArrayList(new ExprNodeConstantDesc(TypeInfoFactory.intTypeInfo, Integer.valueOf(i)),
+            new ExprNodeColumnDesc(TypeInfoFactory.intTypeInfo, "c1", "aa", false));
+    GenericUDF udf = new GenericUDFConcat();
+    ExprNodeGenericFuncDesc f1 = new ExprNodeGenericFuncDesc(TypeInfoFactory.intTypeInfo, udf, as);
+    desc.setFilterExpr(f1);
+    return ts;
+  }
+
 }
