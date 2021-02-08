56d4f542a6ba710768b3d4bafcc3bb45661ccca8
xiepengjie <xiepengjie@didiglobal.com>
2021-02-08 10:35:33 +0800

    add unit-test

---
+++ ql/src/test/org/apache/hadoop/hive/ql/udf/TestExitUDF.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "import org.apache.hadoop.hive.ql.exec.FunctionInfo;
+AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.ql.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hive.http.security.DisableExitSecurityManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TestExitUDF
+ */
+public class TestExitUDF extends UDF {
+  public String evaluate(){
+    try {
+      System.exit(1);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return "ERROR";
+  }
+
+  @Before
+  public void before() throws Exception {
+    DisableExitSecurityManager.disableExit();
+  }
+
+  @After
+  public void after() throws Exception {
+    DisableExitSecurityManager.enableExit();
+  }
+
+  @Test
+  public void test() throws Exception {
+    before();
+    TestExitUDF exitUDF = new TestExitUDF();
+    String ex = exitUDF.evaluate();
+    assertEquals("ERROR", ex);
+    after();
+  }
+}
