08a67806081e2ab69cb58b6a5f3a1ca9de1fb0cb
jphelps <jphelps@cloudera.com>
2021-02-17 09:13:56 -0500

    HIVE-24782-001

--- hcatalog/core/src/main/java/org/apache/hive/hcatalog/cli/HCatCli.java
+++ hcatalog/core/src/main/java/org/apache/hive/hcatalog/cli/HCatCli.java
@@ -178,12 +178,12 @@ public class HCatCli {
     SessionState.start(ss);
 
     // remove the leading and trailing quotes. hcatalog can miss on some cases.
-    if (execString.length() > 1 && execString.startsWith("\"") && execString.endsWith("\"")) {
-      execString = execString.substring(1, execString.length() - 1);
-    }
 
     // all done parsing, let's run stuff!
     if (execString != null) {
+      if (execString.length() > 1 && execString.startsWith("\"") && execString.endsWith("\"")) {
+        execString = execString.substring(1, execString.length() - 1);
+      }
       sysExit(ss, processLine(execString));
     }
 
