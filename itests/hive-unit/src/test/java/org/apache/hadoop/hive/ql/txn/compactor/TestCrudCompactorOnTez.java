1068a5e0683e7cbc58359e16fe5f8b44f86d0ece
kasakrisz <kasakrisz2@gmail.com>
2021-02-26 17:00:05 +0100

    add test case for compaction

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/txn/compactor/TestCrudCompactorOnTez.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/txn/compactor/TestCrudCompactorOnTez.java
@@ -68,6 +68,7 @@ import org.apache.tez.dag.history.logging.proto.ProtoMessageReader;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.apache.hadoop.hive.ql.txn.compactor.TestCompactor.execSelectAndDumpData;
 import static org.apache.hadoop.hive.ql.txn.compactor.TestCompactor.executeStatementOnDriver;
 import static org.apache.hadoop.hive.ql.txn.compactor.CompactorTestUtil.executeStatementOnDriverAndReturnResults;
 import static org.mockito.Mockito.doAnswer;
@@ -75,6 +76,32 @@ import static org.mockito.Mockito.mock;
 
 @SuppressWarnings("deprecation")
 public class TestCrudCompactorOnTez extends CompactorOnTezTest {
+
+  @Test
+  public void testIncrementalMVRebuild() throws Exception {
+    System.out.println("hive.metastore.warehouse.dir:\n" + conf.get("hive.metastore.warehouse.dir"));
+
+
+    executeStatementOnDriver("create table t1(a int, b varchar(128), c float) stored as orc TBLPROPERTIES ('transactional'='true')", driver);
+    executeStatementOnDriver("insert into t1(a,b, c) values (1, 'one', 1.1), (2, 'two', 2.2), (NULL, NULL, NULL)", driver);
+    executeStatementOnDriver("create materialized view mat1 stored as orc TBLPROPERTIES ('transactional'='true') as " +
+            "select a,b,c from t1 where a > 0 or a is null", driver);
+
+    executeStatementOnDriver("delete from t1 where a = 1", driver);
+
+//    CompactorTestUtil.runCompaction(conf, "default", "t1", CompactionType.MAJOR, true);
+//    CompactorTestUtil.runCleaner(conf);
+//    verifySuccessfulCompaction(1);
+
+    executeStatementOnDriver("alter materialized view mat1 rebuild", driver);
+
+    List<String> result = execSelectAndDumpData("select * from mat1", driver, "");
+    Assert.assertEquals(Arrays.asList("2\ttwo\t2.2", "NULL\tNULL\tNULL"), result);
+
+    result = execSelectAndDumpData("select * from t1", driver, "");
+    Assert.assertEquals(Arrays.asList("2\ttwo\t2.2", "NULL\tNULL\tNULL"), result);
+  }
+
 
   @Test
   public void testMajorCompactionNotPartitionedWithoutBuckets() throws Exception {
