e0cd15a7462cf25c929392bbc88187eabe97c1fd
aasha <aasha.medhi2004@gmail.com>
2021-02-26 15:22:23 +0530

    Revert "HIVE-24624: Repl Load should detect the compatible staging dir (Pratyushotpal Madhukar, reviewed by Aasha Medhi, Pravin Kumar Sinha)"
    This reverts commit 76732ad27e139fbdef25b820a07cf35934771083.

--- ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplDumpTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplDumpTask.java
@@ -115,7 +115,6 @@ import static org.apache.hadoop.hive.conf.Constants.SCHEDULED_QUERY_SCHEDULENAME
 import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.REPL_BOOTSTRAP_DUMP_ABORT_WRITE_TXN_AFTER_TIMEOUT;
 import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.REPL_DUMP_METADATA_ONLY;
 import static org.apache.hadoop.hive.metastore.ReplChangeManager.getReplPolicyIdString;
-import static org.apache.hadoop.hive.ql.exec.Utilities.MIN_VERSION_FOR_NEW_DUMP_FORMAT;
 import static org.apache.hadoop.hive.ql.exec.repl.ReplExternalTables.Writer;
 import static org.apache.hadoop.hive.ql.exec.repl.ReplAck.LOAD_ACKNOWLEDGEMENT;
 import static org.apache.hadoop.hive.ql.exec.repl.util.ReplUtils.RANGER_AUTHORIZER;
@@ -185,7 +184,6 @@ public class ReplDumpTask extends Task<ReplDumpWork> implements Serializable {
             initiateAuthorizationDumpTask();
           }
           DumpMetaData dmd = new DumpMetaData(hiveDumpRoot, conf);
-          dmd.setDumpFormatVersion(MIN_VERSION_FOR_NEW_DUMP_FORMAT);
           // Initialize ReplChangeManager instance since we will require it to encode file URI.
           ReplChangeManager.getInstance(conf);
           Path cmRoot = new Path(conf.getVar(HiveConf.ConfVars.REPLCMDIR));
