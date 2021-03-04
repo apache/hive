1813e9981d2a83190301c6df623b5a86921302c1
David Mollitor <dmollitor@apache.org>
2021-03-04 11:48:42 -0500

    HIVE-24846: Log When HS2 Goes OOM

--- service/src/java/org/apache/hive/service/server/HiveServer2OomHookRunner.java
+++ service/src/java/org/apache/hive/service/server/HiveServer2OomHookRunner.java
@@ -21,10 +21,13 @@ package org.apache.hive.service.server;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.hooks.HookContext.HookType;
 import org.apache.hadoop.hive.ql.hooks.HookUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class HiveServer2OomHookRunner implements Runnable {
+  private static final Logger LOG = LoggerFactory.getLogger(HiveServer2OomHookRunner.class);
   private final HiveServer2 hiveServer2;
 
   HiveServer2OomHookRunner(HiveServer2 hiveServer2) {
@@ -34,6 +37,7 @@ public class HiveServer2OomHookRunner implements Runnable {
   @Override
   public synchronized void run() {
     try {
+      LOG.warn("HiveServer2 is being directed to shutdown because an OOM was detected.");
       HiveConf hiveConf = hiveServer2.getHiveConf();
       List<Runnable> hooks = HookUtils.readHooksFromConf(hiveConf, HookType.HIVE_SERVER2_OOM_HOOKS);
       for (Runnable runnable : hooks) {
