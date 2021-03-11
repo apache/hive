f7d6a8f6388ddea6651a0a0b89704b7cbc31da71
Krisztian Kasa <kkasa@cloudera.com>
2021-03-11 18:40:43 +0100

    HIVE-24865: Implement Respect/Ignore Nulls in first/last_value (Krisztian Kasa, reviewed by Jesus Camacho Rodriguez)

--- ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
@@ -4533,7 +4533,7 @@ public class CalcitePlanner extends SemanticAnalyzer {
 
         w = cluster.getRexBuilder().makeOver(calciteAggFnRetType, calciteAggFn, calciteAggFnArgs,
             partitionKeys, ImmutableList.<RexFieldCollation> copyOf(orderKeys), lowerBound,
-            upperBound, isRows, true, false, hiveAggInfo.isDistinct());
+            upperBound, isRows, true, false, hiveAggInfo.isDistinct(), wndSpec.ignoreNulls());
       } else {
         // TODO: Convert to Semantic Exception
         throw new RuntimeException("Unsupported window Spec");
