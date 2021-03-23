b7ff28a777d3b7f3c2783eb46820bfa957ac1c0e
zeroflag <amagyar@hortonworks.com>
2021-03-23 14:08:32 +0100

    HIVE-24315. Improve validation and error handling in HPL/SQL (amagyar)

--- hplsql/src/main/java/org/apache/hive/hplsql/SyntaxErrorReporter.java
+++ hplsql/src/main/java/org/apache/hive/hplsql/SyntaxErrorReporter.java
@@ -18,14 +18,9 @@
 
 package org.apache.hive.hplsql;
 
-import java.util.BitSet;
-
 import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
 
 public class SyntaxErrorReporter extends BaseErrorListener {
   private final Console console;
@@ -37,14 +32,5 @@ public class SyntaxErrorReporter extends BaseErrorListener {
   @Override
   public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
     console.printError("Syntax error at line " + line + ":" + charPositionInLine + " " + msg);
-  }
-
-  public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
-  }
-
-  public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
-  }
-
-  public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
   }
 }
