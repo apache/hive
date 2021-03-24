69e893f326e0273dc5e45b137d75beced3a72124
Soumyakanti Das <soumyakanti.das.08@gmail.com>
2021-03-24 17:21:33 -0400

    Annotate UDF to be non deterministic, update q.out files

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFSQCountCheck.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFSQCountCheck.java
@@ -22,17 +22,18 @@ import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.UDFType;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters.Converter;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.io.BooleanWritable;
-import org.apache.hadoop.io.LongWritable;
 
 /**
  * GenericUDFSQCountCheck.
  *
  */
+@UDFType(deterministic = false)
 @Description(name = "sq_count_check",
     value = "_FUNC_(x) - Internal check on scalar subquery expression to make sure atmost one row is returned",
     extended = "For internal use only")
