975cd87bb47f12c101b826487f101fb98b4172a9
Zoltan Haindrich <kirk@rxd.hu>
2021-04-01 15:36:58 +0000

    Revert "add"
    This reverts commit ef436690cfe1671bb30bde2cb0250b84495f2720.

--- itests/util/src/main/java/org/apache/hadoop/hive/ql/QTestUtil.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/ql/QTestUtil.java
@@ -78,7 +78,6 @@ import org.apache.hadoop.hive.ql.processors.CommandProcessorFactory;
 import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
 import org.apache.hadoop.hive.ql.processors.HiveCommand;
 import org.apache.hadoop.hive.ql.qoption.QTestAuthorizerHandler;
-import org.apache.hadoop.hive.ql.qoption.QTestConfHandler;
 import org.apache.hadoop.hive.ql.qoption.QTestDisabledHandler;
 import org.apache.hadoop.hive.ql.qoption.QTestOptionDispatcher;
 import org.apache.hadoop.hive.ql.qoption.QTestReplaceHandler;
@@ -231,7 +230,6 @@ public class QTestUtil {
     dispatcher.register("timezone", new QTestTimezoneHandler());
     dispatcher.register("authorizer", new QTestAuthorizerHandler());
     dispatcher.register("disabled", new QTestDisabledHandler());
-    dispatcher.register("conf", new QTestConfHandler());
 
     String scriptsDir = getScriptsDir();
 
