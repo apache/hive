d7eea01f8cdce495c0f79cd19ca79f5544e67230
Marton Bod <marton.bod@gmail.com>
2021-04-27 12:10:36 +0200

    HIVE-25007: Implement insert overwrite for Iceberg tables (#2178) (Marton Bod, reviewed by Peter Vary and Laszlo Pinter)

--- iceberg/iceberg-handler/src/test/java/org/apache/iceberg/mr/hive/TestTables.java
+++ iceberg/iceberg-handler/src/test/java/org/apache/iceberg/mr/hive/TestTables.java
@@ -196,21 +196,26 @@ abstract class TestTables {
         "'" + InputFormatConfig.CATALOG_NAME + "'='" + Catalogs.ICEBERG_DEFAULT_CATALOG_NAME + "')");
 
     if (records != null && !records.isEmpty()) {
-      StringBuilder query = new StringBuilder().append("INSERT INTO " + identifier + " VALUES ");
-
-      records.forEach(record -> {
-        query.append("(");
-        query.append(record.struct().fields().stream()
-                .map(field -> getStringValueForInsert(record.getField(field.name()), field.type()))
-                .collect(Collectors.joining(",")));
-        query.append("),");
-      });
-      query.setLength(query.length() - 1);
-
-      shell.executeStatement(query.toString());
+      String query = getInsertQuery(records, identifier, false);
+      shell.executeStatement(query);
     }
 
     return loadTable(identifier);
+  }
+
+  public String getInsertQuery(List<Record> records, TableIdentifier identifier, boolean isOverwrite) {
+    StringBuilder query = new StringBuilder(String.format("INSERT %s %s VALUES ",
+        isOverwrite ? "OVERWRITE TABLE" : "INTO", identifier));
+
+    records.forEach(record -> {
+      query.append("(");
+      query.append(record.struct().fields().stream()
+              .map(field -> getStringValueForInsert(record.getField(field.name()), field.type()))
+              .collect(Collectors.joining(",")));
+      query.append("),");
+    });
+    query.setLength(query.length() - 1);
+    return query.toString();
   }
 
   /**
