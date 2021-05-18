75eb3447348d3c87ce31b3c1b9f969a39ea92b8d
Antal Sinkovits <asinkovits@cloudera.com>
2021-05-18 21:34:53 +0200

    HIVE-25080: Create metric about oldest entry in "ready for cleaning" state
    Change-Id: Ifd0e038d593a8530b3b614d91c6a31a4050aab5f

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
@@ -295,7 +295,9 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
       "SELECT COUNT(*), ({0} - MIN(\"HL_ACQUIRED_AT\"))/1000 FROM \"HIVE_LOCKS\") \"HL\" CROSS JOIN (" +
       "SELECT COUNT(*) FROM (SELECT COUNT(\"TXN_ID\"), \"T2W_DATABASE\", \"T2W_TABLE\" FROM \"TXN_TO_WRITE_ID\" " +
           "INNER JOIN \"TXNS\" ON \"T2W_TXNID\" = \"TXN_ID\" WHERE \"TXN_STATE\"='" + TxnStatus.ABORTED + "' " +
-          "GROUP BY \"T2W_DATABASE\", \"T2W_TABLE\" HAVING COUNT(\"TXN_ID\") > ?) \"L\") \"L\"";
+          "GROUP BY \"T2W_DATABASE\", \"T2W_TABLE\" HAVING COUNT(\"TXN_ID\") > ?) \"L\") \"L\" CROSS JOIN (" +
+      "SELECT ({0} - MIN(\"CQ_ENQUEUE_TIME\"))/1000 from \"COMPACTION_QUEUE\" WHERE " +
+          "\"CQ_STATE\"=''" + Character.toString(READY_FOR_CLEANING) + "'') OLDEST_CLEAN";
 
 
 
@@ -3780,6 +3782,7 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
             metrics.setLocksCount(rs.getInt(12));
             metrics.setOldestLockAge(rs.getInt(13));
             metrics.setTablesWithXAbortedTxns(rs.getInt(14));
+            metrics.setOldestLockAge(rs.getInt(15));
           }
         }
         return metrics;
