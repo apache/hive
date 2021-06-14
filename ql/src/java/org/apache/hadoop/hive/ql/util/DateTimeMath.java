88048e7d2c35e9046679131e4dab3bde73bdef87
Ashish Kumar Sharma <ashishkumarsharma20@gmail.com>
2021-06-14 19:26:36 +0530

    HIVE-25093: date_format() UDF is returning output in UTC time zone only (Ashish Sharma, reviewed by Adesh Rao, Nikhil Gupta, Sankar Hariappan)
    Signed-off-by: Sankar Hariappan <sankarh@apache.org>
    Closes (# 2252)

--- ql/src/java/org/apache/hadoop/hive/ql/util/DateTimeMath.java
+++ ql/src/java/org/apache/hadoop/hive/ql/util/DateTimeMath.java
@@ -21,9 +21,12 @@ import org.apache.hadoop.hive.common.type.Date;
 import org.apache.hadoop.hive.common.type.HiveIntervalDayTime;
 import org.apache.hadoop.hive.common.type.HiveIntervalYearMonth;
 import org.apache.hadoop.hive.common.type.Timestamp;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.session.SessionState;
 import org.apache.hadoop.hive.serde2.io.DateWritableV2;
 import org.apache.hive.common.util.DateUtils;
 
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
@@ -611,6 +614,24 @@ public class DateTimeMath {
   public static Calendar getProlepticGregorianCalendarUTC() {
     GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC".intern()));
     calendar.setGregorianChange(new java.util.Date(Long.MIN_VALUE));
+    return calendar;
+  }
+
+  /**
+   * TODO - this is a temporary fix for handling Julian calendar dates.
+   * Returns a Gregorian calendar that can be used from year 0+ instead of default 1582.10.15.
+   * This is desirable for some UDFs that work on dates which normally would use Julian calendar.
+   * Julian calendar only works with UTC time zone only.
+   * @return the calendar
+   */
+  public static Calendar getTimeZonedProlepticGregorianCalendar() {
+    ZoneId id = SessionState.get() == null ? new HiveConf().getLocalTimeZone() : SessionState.get().getConf()
+        .getLocalTimeZone();
+    GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone(id));
+
+    if (id.getId().equals("UTC"))
+      calendar.setGregorianChange(new java.util.Date(Long.MIN_VALUE));
+
     return calendar;
   }
 }
