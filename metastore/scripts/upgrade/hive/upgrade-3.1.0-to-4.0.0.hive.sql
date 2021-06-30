e920daa687b9b86d0930ac8cdce849c9d1e956e5
Ayush Saxena <ayushsaxena@apache.org>
2021-06-30 20:15:51 +0530

    HIVE-25301. Expose notification log table through sys db.

--- metastore/scripts/upgrade/hive/upgrade-3.1.0-to-4.0.0.hive.sql
+++ metastore/scripts/upgrade/hive/upgrade-3.1.0-to-4.0.0.hive.sql
@@ -542,6 +542,34 @@ TBLPROPERTIES (
 FROM \"REPLICATION_METRICS\""
 );
 
+CREATE EXTERNAL TABLE IF NOT EXISTS `NOTIFICATION_LOG` (
+  `NL_ID` bigint,
+  `EVENT_ID` bigint,
+  `EVENT_TIME` int,
+  `EVENT_TYPE` varchar(32),
+  `CAT_NAME` varchar(256),
+  `DB_NAME` varchar(128),
+  `TBL_NAME` varchar(256),
+  `MESSAGE` string,
+  `MESSAGE_FORMAT` varchar(16)
+)
+STORED BY 'org.apache.hive.storage.jdbc.JdbcStorageHandler'
+TBLPROPERTIES (
+"hive.sql.database.type" = "METASTORE",
+"hive.sql.query" =
+"SELECT
+    \"NL_ID\",
+    \"EVENT_ID\",
+    \"EVENT_TIME\",
+    \"EVENT_TYPE\",
+    \"CAT_NAME\",
+    \"DB_NAME\",
+    \"TBL_NAME\",
+    \"MESSAGE\",
+    \"MESSAGE_FORMAT\"
+FROM \"NOTIFICATION_LOG\""
+);
+
 DROP TABLE IF EXISTS `VERSION`;
 
 CREATE OR REPLACE VIEW `VERSION` AS SELECT 1 AS `VER_ID`, '4.0.0' AS `SCHEMA_VERSION`,
