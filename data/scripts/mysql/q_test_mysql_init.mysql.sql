3142b07fa12c3039bc1525a91336566f7696b7d7
Dantong Dong <dantong.dong@cloudera.com>
2021-07-13 18:33:11 -0700

    add external table init

---
+++ data/scripts/mysql/q_test_mysql_init.mysql.sql
@@ -0,0 +1,25 @@
+create table country (name varchar(30) not null);
+create table state (name varchar(30) not null,
+                    country_code int not null);
+create table city (name varchar(30) not null,
+                   state_code int not null);
+
+insert into country (name) values ('India');
+insert into country (name) values ('Russia');
+insert into country (name) values ('USA');
+
+insert into state (name,country_code) values ('Maharashtra',1);
+insert into state (name,country_code) values ('Madhya Pradesh',1);
+insert into state (name,country_code) values ('Moscow',3);
+insert into state (name,country_code) values ('Something',4);
+insert into state (name,country_code) values ('Florida',4);
+insert into state (name,country_code) values ('Texas',4);
+
+insert into city (name,state_code) values('Mumbai',1);
+insert into city (name,state_code) values('Pune',1);
+insert into city (name,state_code) values('Bhopal',2);
+insert into city (name,state_code) values('Indore',2);
+insert into city (name,state_code) values('Klin',3);
+insert into city (name,state_code) values('Los Angeles',5);
+insert into city (name,state_code) values('Plant City',5);
+insert into city (name,state_code) values('Arlington',6);
