380ee750b695ec21efa31c787ef791d939ef7e82
Robbie Zhang <rzhang@cloudera.com>
2021-07-15 10:51:10 +1000

    HIVE-25329: CTAS creates a managed table as non-ACID table

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -13379,6 +13379,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
         break;
       case HiveParser.KW_MANAGED:
         isManaged = true;
+        isTransactional = true;
         break;
       case HiveParser.KW_TEMPORARY:
         isTemporary = true;
