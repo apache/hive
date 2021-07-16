e93bd463e81bbf5673be0141f96b84bbb42e35ed
kkasa <kkasa@cloudera.com>
2021-07-16 15:12:40 +0200

    fix package header

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/views/HiveAggregatePartitionIncrementalRewritingRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/views/HiveAggregatePartitionIncrementalRewritingRule.java
@@ -1,4 +1,5 @@
-package org.apache.hadoop.hive.ql.optimizer.calcite.rules.views;/*
+package org.apache.hadoop.hive.ql.optimizer.calcite.rules.views;
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -119,7 +120,7 @@ public class HiveAggregatePartitionIncrementalRewritingRule extends RelOptRule {
       }
     }
 
-    if (partitionColumnCount == 0 || partitionColumnIndexes.size() != partitionColumnCount) {
+    if (partitionColumnCount <= 0 || partitionColumnIndexes.size() != partitionColumnCount) {
       LOG.debug("Could not find all partition column lineages, bail out.");
       return;
     }
