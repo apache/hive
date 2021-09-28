a336f5ecb649173de1c3d9a0c0525c372cf13419
Zoltan Haindrich <kirk@rxd.hu>
2021-09-28 16:41:40 +0200

    HIVE-25569: Enable table definition over a single file(SFS)
    Change-Id: I6e8afa3463951c5b4e032df390df06a0d634fde7

---
+++ ql/src/java/org/apache/hadoop/hive/ql/io/SingleFileSystem.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.ql.io;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.util.Progressable;
+
+/**
+ * Implements an abstraction layer to show files in a single directory.
+ *
+ * Suppose the filesystem has a directory in which there are multiple files:
+ * file://somedir/f1.txt
+ * file://somedir/f2.txt
+ *
+ * In case of Hive the content of a directory may be inside a table.
+ * To give a way to show a single file as a single file in a directory it could be specified:
+ *
+ * sfs+file://somedir/f1.txt/SINGLEFILE
+ *
+ * This will be a directory containing only the f1.txt and nothing else.
+ *
+ */
+public abstract class SingleFileSystem extends FileSystem {
+
+  public static class HDFS extends SingleFileSystem {
+  }
+
+  public static class S3A extends SingleFileSystem {
+  }
+
+  public static class ABFS extends SingleFileSystem {
+  }
+
+  public static class ABFSS extends SingleFileSystem {
+  }
+
+  public static class ADL extends SingleFileSystem {
+  }
+
+  public static class GS extends SingleFileSystem {
+  }
+
+  public static class O3FS extends SingleFileSystem {
+  }
+
+  public static class PFILE extends SingleFileSystem {
+  }
+
+  public static class FILE extends SingleFileSystem {
+  }
+
+  private static final String SINGLEFILE = "SINGLEFILE";
+
+  private URI uri;
+  private Configuration conf;
+  private Path workDir;
+
+  public String getScheme() {
+    return "sfs+" + getClass().getSimpleName().toLowerCase();
+  }
+
+  @Override
+  public void initialize(URI uri, Configuration conf) throws IOException {
+    super.initialize(uri, conf);
+    this.uri = uri;
+    this.conf = conf;
+  }
+
+  @Override
+  public URI getUri() {
+    return uri;
+  }
+
+  @Override
+  public FSDataInputStream open(Path upperPath, int bufferSize) throws IOException {
+    SfsInfo info = new SfsInfo(upperPath);
+    switch (info.type) {
+    case LEAF_FILE:
+      return info.lowerTargetPath.getFileSystem(conf).open(info.lowerTargetPath, bufferSize);
+    default:
+      throw unsupported("open:" + upperPath);
+    }
+  }
+
+  @Override
+  public FileStatus getFileStatus(Path upperPath) throws IOException {
+    SfsInfo info = new SfsInfo(upperPath);
+    switch (info.type) {
+    case LEAF_FILE:
+      return info.getTargetFileStatus();
+    case DIR_MODE:
+    case SINGLEFILE_DIR:
+      return makeDirFileStatus(upperPath, info.lowerTargetPath);
+    default:
+      throw unsupported("fileStatus:" + upperPath);
+    }
+  }
+
+  enum SfsInodeType {
+    LEAF_FILE, SINGLEFILE_DIR, DIR_MODE, NONEXISTENT,
+  }
+
+  class SfsInfo {
+
+    private String[] parts;
+    private URI uri;
+    private SfsInodeType type;
+    private Path lowerTargetPath;
+    private Path upperTargetPath;
+
+    public SfsInfo(Path upperPath) {
+      uri = upperPath.toUri();
+      parts = uri.getPath().split(Path.SEPARATOR);
+      type = SfsInodeType.DIR_MODE;
+      lowerTargetPath = upperPath;
+
+      int n = parts.length;
+      if (n >= 1 && parts[n - 1].equals(SINGLEFILE)) {
+        type = SfsInodeType.SINGLEFILE_DIR;
+        lowerTargetPath = upperPath.getParent();
+        upperTargetPath = new Path(uri.getScheme(), uri.getAuthority(), uri.getPath() + "/" + parts[n - 2]);
+      }
+      if (n >= 2 && parts[n - 2].equals(SINGLEFILE)) {
+        if (n >= 3 && !parts[n - 3].equals(parts[n - 1])) {
+          type = SfsInodeType.NONEXISTENT;
+        } else {
+          type = SfsInodeType.LEAF_FILE;
+          lowerTargetPath = upperPath.getParent().getParent();
+          upperTargetPath = upperPath;
+        }
+      }
+      if (lowerTargetPath != null) {
+        URI u = lowerTargetPath.toUri();
+        lowerTargetPath = new Path(removeSfsScheme(u.getScheme()), u.getAuthority(), u.getPath());
+      }
+    }
+
+    private String removeSfsScheme(String scheme) {
+      if (scheme.startsWith("sfs+")) {
+        return scheme.substring(4);
+      }
+      if (scheme.equals("sfs")) {
+        return null;
+      }
+      throw new RuntimeException("Unexpected scheme: " + scheme);
+    }
+
+    public FileStatus getTargetFileStatus() throws IOException {
+      return makeFileStatus(upperTargetPath, lowerTargetPath);
+    }
+
+    public FileStatus[] listStatus(Path upperPath, Path lowerPath) throws IOException {
+      FileSystem fs = lowerPath.getFileSystem(conf);
+      FileStatus status = fs.getFileStatus(lowerPath);
+      List<FileStatus> ret = new ArrayList<>();
+      if (status.isDirectory()) {
+        FileStatus[] statusList = fs.listStatus(lowerTargetPath);
+        for (FileStatus fileStatus : statusList) {
+          ret.add(makeDirFileStatus(fileStatus));
+        }
+      } else {
+        FileStatus dirStat = makeDirFileStatus(new Path(upperPath, SINGLEFILE), lowerPath);
+        ret.add(dirStat);
+      }
+      return ret.toArray(new FileStatus[0]);
+    }
+  }
+
+  @Override
+  public FileStatus[] listStatus(Path upperPath) throws FileNotFoundException, IOException {
+    SfsInfo info = new SfsInfo(upperPath);
+    switch (info.type) {
+    case DIR_MODE:
+      return info.listStatus(upperPath, info.lowerTargetPath);
+    case LEAF_FILE:
+    case SINGLEFILE_DIR:
+      return new FileStatus[] { info.getTargetFileStatus() };
+    default:
+      throw unsupported("listStatus: " + upperPath);
+    }
+  }
+
+  @Override
+  public void setWorkingDirectory(Path new_dir) {
+    workDir = new_dir;
+  }
+
+  @Override
+  public Path getWorkingDirectory() {
+    return workDir;
+  }
+
+  public FileStatus makeFileStatus(Path upperPath, Path lowerPath) throws IOException {
+    FileStatus status = lowerPath.getFileSystem(conf).getFileStatus(lowerPath);
+    status = new FileStatus(status);
+    status.setPath(upperPath);
+    return status;
+  }
+
+  private static FileStatus makeDirFileStatus(FileStatus lowerStatus) throws IOException {
+    return makeDirFileStatus(makeSfsPath(lowerStatus.getPath()), lowerStatus);
+  }
+
+  private static Path makeSfsPath(Path path) throws IOException {
+    URI oldUri = path.toUri();
+    if (oldUri.getScheme().startsWith("sfs+")) {
+      throw new IOException("unexpected path");
+    }
+    return new Path("sfs+" + oldUri.getScheme(), oldUri.getAuthority(), oldUri.getPath());
+  }
+
+  public FileStatus makeDirFileStatus(Path upperPath, Path lowerPath) throws IOException {
+    FileStatus status = lowerPath.getFileSystem(conf).getFileStatus(lowerPath);
+    return makeDirFileStatus(upperPath, status);
+  }
+
+  public static FileStatus makeDirFileStatus(Path upperPath, FileStatus status) throws IOException {
+    FileStatus newStatus = new FileStatus(status.getLen(), true, status.getReplication(), status.getBlockSize(),
+        status.getModificationTime(), status.getAccessTime(), addExecute(status.getPermission()), status.getOwner(),
+        status.getGroup(), (status.isSymlink() ? status.getSymlink() : null), status.getPath());
+    newStatus.setPath(upperPath);
+    return newStatus;
+  }
+
+  private static FsPermission addExecute(FsPermission permission) {
+    return new FsPermission(permission.toShort() | 1 | (1 << 3) | (1 << 6));
+  }
+
+  @Override
+  public FSDataOutputStream create(Path f, FsPermission permission, boolean overwrite, int bufferSize,
+      short replication, long blockSize, Progressable progress) throws IOException {
+    throw unsupported("create");
+  }
+
+  @Override
+  public FSDataOutputStream append(Path f, int bufferSize, Progressable progress) throws IOException {
+    throw unsupported("append");
+
+  }
+
+  @Override
+  public boolean rename(Path src, Path dst) throws IOException {
+    throw unsupported("rename");
+  }
+
+  @Override
+  public boolean delete(Path f, boolean recursive) throws IOException {
+    throw unsupported("delete");
+
+  }
+
+  @Override
+  public boolean mkdirs(Path f, FsPermission permission) throws IOException {
+    throw unsupported("mkdirs");
+  }
+
+  private IOException unsupported(String str) {
+    return new IOException("Unsupported operation on sfs filesystem! (" + str + ")");
+  }
+}
