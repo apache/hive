6379b3ac1d0c9c488a2710caf1c1d6d11784bdbe
Marton Bod <mbod@cloudera.com>
2021-10-07 14:53:31 +0200

    move to right package

--- iceberg/iceberg-handler/src/test/java/org/apache/iceberg/mr/hive/TestHiveIcebergStorageHandler.java
+++ iceberg/iceberg-handler/src/test/java/org/apache/iceberg/mr/hive/TestHiveIcebergStorageHandler.java
@@ -1,10 +1,9 @@
-package org.apache.iceberg.mr.hive.serde.objectinspector;
+package org.apache.iceberg.mr.hive;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
 import org.apache.iceberg.mr.Catalogs;
-import org.apache.iceberg.mr.hive.HiveIcebergStorageHandler;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
 import org.junit.Test;
