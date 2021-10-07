1cd4343fdfc85ff364039dec492703f4e10ef381
Marton Bod <mbod@cloudera.com>
2021-10-07 14:44:47 +0200

    move test

---
+++ iceberg/iceberg-handler/src/test/java/org/apache/iceberg/mr/hive/serde/objectinspector/TestHiveIcebergStorageHandler.java
@@ -0,0 +1,26 @@
+package org.apache.iceberg.mr.hive.serde.objectinspector;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import org.apache.iceberg.mr.Catalogs;
+import org.apache.iceberg.mr.hive.HiveIcebergStorageHandler;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestHiveIcebergStorageHandler {
+
+  @Test
+  public void testAuthzURI() throws URISyntaxException {
+    Map<String, String> props = ImmutableMap.of(
+        Catalogs.LOCATION, "hdfs://abcd/123/"
+    );
+
+    HiveIcebergStorageHandler storageHandler = new HiveIcebergStorageHandler();
+    URI uriForAuth = storageHandler.getURIForAuth(props);
+
+    Assert.assertEquals("iceberg://hdfs://abcd/123/", uriForAuth.toString());
+  }
+
+}
