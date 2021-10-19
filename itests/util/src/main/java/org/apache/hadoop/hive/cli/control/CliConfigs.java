96a6243a11240ad72b892d9f17c975f6525d6cbb
Stamatis Zampetakis <zabetak@gmail.com>
2021-10-19 18:35:11 +0200

    HIVE-25625: Drop TestCompareCliDriver and related code from tests
    The driver has been introduced back in 2015 aiming to run queries with
    vectorization on/off and comparing the results. However it didn't
    receive much attention since then and currently only two queries are run
    with this driver. The majority of tests aiming to ensure vectorization
    works correctly use the TestMiniLlapLocalCliDriver and run a query
    twice switching on/off the necessary properties.
    
    Summing up having the CoreCompareCliDriver in the repo leads to extra
    code maintenance cost without much benefit.

--- itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
@@ -352,26 +352,6 @@ public class CliConfigs {
     }
   }
 
-  public static class CompareCliConfig extends AbstractCliConfig {
-    public CompareCliConfig() {
-      super(CoreCompareCliDriver.class);
-      try {
-        setQueryDir("ql/src/test/queries/clientcompare");
-
-        setResultsDir("ql/src/test/results/clientcompare");
-        setLogDir("itests/qtest/target/qfile-results/clientcompare");
-
-        setInitScript("q_test_init_compare.sql");
-        setCleanupScript("q_test_cleanup_compare.sql");
-
-        setHiveConfDir("");
-        setClusterType(MiniClusterType.NONE);
-      } catch (Exception e) {
-        throw new RuntimeException("can't construct cliconfig", e);
-      }
-    }
-  }
-
   public static class NegativeLlapLocalCliConfig extends AbstractCliConfig {
     public NegativeLlapLocalCliConfig() {
       super(CoreNegativeCliDriver.class);
--- ql/src/test/queries/clientpositive/llap_0.q
+++ ql/src/test/queries/clientpositive/llap_0.q
@@ -11,3 +11,14 @@ WHERE    (cbigint > -23)
            AND ((cdouble != 988888)
                 OR (cint > -863.257))
 ORDER BY cbigint, cfloat;
+SET hive.vectorized.execution.enabled=false;
+SELECT   cfloat,
+         cint,
+         cdouble,
+         cbigint,
+         cstring1
+FROM     alltypesorc
+WHERE    (cbigint > -23)
+  AND ((cdouble != 988888)
+    OR (cint > -863.257))
+ORDER BY cbigint, cfloat;
