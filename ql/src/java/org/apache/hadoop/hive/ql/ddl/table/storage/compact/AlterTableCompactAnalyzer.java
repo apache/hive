7ea8d10c64b72335d4cde60e48403c91f2baf16c
saihemanth <saihemanth@cloudera.com>
2021-10-20 09:23:01 -0700

    Qfile output  changes

--- ql/src/java/org/apache/hadoop/hive/ql/ddl/table/storage/compact/AlterTableCompactAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/ddl/table/storage/compact/AlterTableCompactAnalyzer.java
@@ -79,17 +79,18 @@ public class AlterTableCompactAnalyzer extends AbstractAlterTableAnalyzer {
     }
 
     AlterTableCompactDesc desc = new AlterTableCompactDesc(tableName, partitionSpec, type, isBlocking, poolName, mapProp);
-    Table table = getTable(tableName);
-    WriteEntity.WriteType writeType = null;
-    if (AcidUtils.isTransactionalTable(table)) {
-      setAcidDdlDesc(desc);
-      writeType = WriteType.DDL_EXCLUSIVE;
-    } else {
-      writeType = WriteEntity.determineAlterTableWriteType(AlterTableType.COMPACT);
-    }
-    inputs.add(new ReadEntity(table));
-    WriteEntity alterTableOutput = new WriteEntity(table, writeType);
-    outputs.add(alterTableOutput);
+//    Table table = getTable(tableName);
+//    WriteEntity.WriteType writeType = null;
+//    if (AcidUtils.isTransactionalTable(table)) {
+//      setAcidDdlDesc(desc);
+//      writeType = WriteType.DDL_EXCLUSIVE;
+//    } else {
+//      writeType = WriteEntity.determineAlterTableWriteType(AlterTableType.COMPACT);
+//    }
+//    inputs.add(new ReadEntity(table));
+//    WriteEntity alterTableOutput = new WriteEntity(table, writeType);
+//    outputs.add(alterTableOutput);
+    addInputsOutputsAlterTable(tableName, null, desc, desc.getType(), false);
     rootTasks.add(TaskFactory.get(new DDLWork(getInputs(), getOutputs(), desc)));
   }
 }
