fce5b60a728006c26ad3ae980a92b6776a52db32
kkasa <kkasa@cloudera.com>
2021-10-26 09:50:20 +0200

    update UT

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/txn/compactor/TestMaterializedViewRebuild.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/txn/compactor/TestMaterializedViewRebuild.java
@@ -48,7 +48,7 @@ public class TestMaterializedViewRebuild extends CompactorOnTezTest {
   private static final List<String> INCREMENTAL_REBUILD_PLAN = Arrays.asList(
       "CBO PLAN:",
       "HiveProject(a=[$0], b=[$1], c=[$2])",
-      "  HiveFilter(condition=[AND(<(2, $5.writeid), OR(>($0, 0), IS NULL($0)))])",
+      "  HiveFilter(condition=[AND(<(1, $5.writeid), OR(>($0, 0), IS NULL($0)))])",
       "    HiveTableScan(table=[[default, t1]], table:alias=[t1])",
       ""
   );
@@ -91,51 +91,7 @@ public class TestMaterializedViewRebuild extends CompactorOnTezTest {
     txnHandler.cleanTxnToWriteIdTable();
 
     List<String> result = execSelectAndDumpData("explain cbo alter materialized view " + MV1 + " rebuild", driver, "");
-    Assert.assertEquals(FULL_REBUILD_PLAN, result);
-    executeStatementOnDriver("alter materialized view " + MV1 + " rebuild", driver);
-
-    result = execSelectAndDumpData("select * from " + MV1 , driver, "");
-    assertResult(EXPECTED_RESULT, result);
-
-    result = execSelectAndDumpData("explain cbo select a,b,c from " + TABLE1 + " where a > 0 or a is null", driver, "");
-    Assert.assertEquals(Arrays.asList("CBO PLAN:", "HiveTableScan(table=[[default, " + MV1 + "]], table:alias=[default." + MV1 + "])", ""), result);
-  }
-
-  @Test
-  public void testSecondRebuildCanBeIncrementalAfterMajorCompaction() throws Exception {
-
-    executeStatementOnDriver("insert into " + TABLE1 + "(a,b,c) values (3, 'three', 3.3)", driver);
-
-    CompactorTestUtil.runCompaction(conf, "default",  TABLE1 , CompactionType.MAJOR, true);
-    CompactorTestUtil.runCleaner(conf);
-    verifySuccessfulCompaction(1);
-
-    executeStatementOnDriver("alter materialized view " + MV1 + " rebuild", driver);
-
-    // Insert after first rebuild.
-    executeStatementOnDriver("insert into " + TABLE1 + "(a,b,c) values (4, 'four', 4.4)", driver);
-
-    List<String> result = execSelectAndDumpData("explain cbo alter materialized view " + MV1 + " rebuild", driver, "");
     Assert.assertEquals(INCREMENTAL_REBUILD_PLAN, result);
-    executeStatementOnDriver("alter materialized view " + MV1 + " rebuild", driver);
-
-    result = execSelectAndDumpData("select * from " + MV1 , driver, "");
-    List<String> expected = new ArrayList(EXPECTED_RESULT);
-    expected.add("4\tfour\t4.4");
-    assertResult(expected, result);
-
-    result = execSelectAndDumpData("explain cbo select a,b,c from " + TABLE1 + " where a > 0 or a is null", driver, "");
-    Assert.assertEquals(Arrays.asList("CBO PLAN:", "HiveTableScan(table=[[default, " + MV1 + "]], table:alias=[default." + MV1 + "])", ""), result);
-  }
-
-  @Test
-  public void testWhenCleanUpOfMajorCompactionHasNotFinishedIncrementalMVRebuildNotUsed() throws Exception {
-    executeStatementOnDriver("insert into " + TABLE1 + "(a,b,c) values (3, 'three', 3.3)", driver);
-
-    CompactorTestUtil.runCompaction(conf, "default",  TABLE1 , CompactionType.MAJOR, true);
-
-    List<String> result = execSelectAndDumpData("explain cbo alter materialized view " + MV1 + " rebuild", driver, "");
-    Assert.assertEquals(FULL_REBUILD_PLAN, result);
     executeStatementOnDriver("alter materialized view " + MV1 + " rebuild", driver);
 
     result = execSelectAndDumpData("select * from " + MV1 , driver, "");
