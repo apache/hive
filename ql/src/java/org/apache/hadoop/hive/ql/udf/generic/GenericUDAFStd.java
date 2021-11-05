bd0c074fe91f41bc43afa4c16c28f09e26804608
ashish-kumar-sharma <ashishkumarsharma20@gmail.com>
2021-11-05 04:47:29 +0000

    HIVE-25653: fix tests

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDAFStd.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDAFStd.java
@@ -28,6 +28,7 @@ import org.apache.hadoop.hive.serde2.typeinfo.PrimitiveTypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 /**
  * Compute the standard deviation by extending GenericUDAFVariance and
@@ -92,8 +93,9 @@ public class GenericUDAFStd extends GenericUDAFVariance {
      * use it, etc.
      */
     public static double calculateStdResult(double variance, long count) {
+      // TODO: BigDecimal.sqrt() is introduced in java 9. So change the below calculation once hive upgraded to java 9 or above.
       BigDecimal bvariance = new BigDecimal(variance);
-      BigDecimal result = bvariance.divide(new BigDecimal(count));
+      BigDecimal result = bvariance.divide(new BigDecimal(count), MathContext.DECIMAL128);
       return Math.sqrt(result.doubleValue());
     }
 
