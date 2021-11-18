06a8fca0fde18a574d7d92dae4ae5eb720031c20
Zoltan Haindrich <kirk@rxd.hu>
2021-11-18 09:41:52 +0000

    Revert "count lines"
    This reverts commit 67e1147bd614cf021e4a1ea1484a26e4ce2902a4.

--- itests/util/src/main/java/org/apache/hadoop/hive/ql/externalDB/AbstractExternalDB.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/ql/externalDB/AbstractExternalDB.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * The class is in charge of connecting and populating dockerized databases for qtest.
@@ -95,15 +94,12 @@ public abstract class AbstractExternalDB {
         }
         BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
         final StringBuilder lines = new StringBuilder();
-        final AtomicInteger i=new AtomicInteger();
-        final AtomicInteger j=new AtomicInteger();
-        reader.lines().forEach(s -> { i.incrementAndGet();lines.append(s).append('\n');});
+        reader.lines().forEach(s -> lines.append(s).append('\n'));
 
         reader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
         final StringBuilder errLines = new StringBuilder();
-        reader.lines().forEach(s -> { errLines.append(s).append('\n');j.incrementAndGet();});
+        reader.lines().forEach(s -> errLines.append(s).append('\n'));
         LOG.info("Result size: " + lines.length() + ";" + errLines.length());
-        LOG.info("Result lines: " + i.get() + ";" + j.get());
         return new ProcessResults(lines.toString(), errLines.toString(), proc.exitValue());
     }
 
