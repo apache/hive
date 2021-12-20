692a447573041c607b8bb14cdaaae65759474c9a
Zoltan Haindrich <kirk@rxd.hu>
2021-12-20 12:13:26 +0000

    undo change

--- data/files/datasets/in5/load.hive.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE join_1to1_1(key1 int, key2 int, value int);
-LOAD DATA LOCAL INPATH '${hiveconf:test.data.dir}/in5.txt' INTO TABLE join_1to1_1;
-
--- data/files/datasets/in6/load.psql.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE join_1to1_1(key1 int, key2 int, value int);
-COPY join_1to1_2 FROM '/tmp/in6.txt' ( FORMAT CSV, DELIMITER(E'\1') );
-
