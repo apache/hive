5c1779fa2d02b0d2e289a794bf460d531575f6a6
kasakrisz <kasakrisz2@gmail.com>
2022-02-08 10:39:43 +0100

    disable rewrite when creation a view

--- ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
@@ -2083,7 +2083,8 @@ public class CalcitePlanner extends SemanticAnalyzer {
               mvRebuildMode == MaterializationRebuildMode.NONE &&
               !rootQB.isMaterializedView() && !ctx.isLoadingMaterializedView() && !rootQB.isCTAS() &&
               rootQB.getIsQuery() &&
-              rootQB.hasTableDefined();
+              rootQB.hasTableDefined() &&
+              !forViewCreation;
     }
 
     private RelNode applyMaterializedViewRewritingByText(
