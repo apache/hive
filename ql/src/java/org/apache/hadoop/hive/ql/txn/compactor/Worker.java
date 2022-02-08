8a718a78626355cf21420e5d62ac0a5d9cc3856a
veghlaci05 <90267982+veghlaci05@users.noreply.github.com>
2022-02-08 15:11:49 +0100

    HIVE-25898: Compaction txn heartbeating after Worker timeout (Laszlo Vegh, reviewed by Denys Kuzmenko, Stamatis Zampetakis)
    Closes #2981

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Worker.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Worker.java
@@ -19,6 +19,8 @@ package org.apache.hadoop.hive.ql.txn.compactor;
 
 import com.google.common.annotations.VisibleForTesting;
 import java.util.concurrent.ScheduledExecutorService;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.ValidCompactorWriteIdList;
@@ -202,26 +204,16 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
     }
   }
 
-  static final class CompactionHeartbeater extends Thread {
+  static final class CompactionHeartbeater implements Runnable {
     static final private Logger LOG = LoggerFactory.getLogger(CompactionHeartbeater.class);
     private final CompactionTxn compactionTxn;
     private final String tableName;
     private final HiveConf conf;
-    private final AtomicBoolean errorLogEnabled;
 
     public CompactionHeartbeater(CompactionTxn compactionTxn, String tableName, HiveConf conf) {
       this.tableName = Objects.requireNonNull(tableName);
       this.compactionTxn = Objects.requireNonNull(compactionTxn);
       this.conf = Objects.requireNonNull(conf);
-      this.errorLogEnabled = new AtomicBoolean(true);
-
-      setDaemon(true);
-      setPriority(MIN_PRIORITY);
-      setName("CompactionHeartbeater-" + compactionTxn.getTxnId());
-    }
-
-    public void shouldLogError(boolean shouldLogError) {
-      this.errorLogEnabled.set(shouldLogError);
     }
 
     @Override
@@ -233,9 +225,7 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
         msc = HiveMetaStoreUtils.getHiveMetastoreClient(conf);
         msc.heartbeat(compactionTxn.getTxnId(), compactionTxn.getLockId());
       } catch (Exception e) {
-        if (errorLogEnabled.get()) {
           LOG.error("Error while heartbeating transaction id {} for table: {}", compactionTxn, tableName, e);
-        }
       } finally {
         if (msc != null) {
           msc.close();
@@ -334,15 +324,19 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
     CompactionInfo ci = null;
     boolean computeStats = false;
     Table t1 = null;
-    try (CompactionTxn compactionTxn = new CompactionTxn()) {
-      if (msc == null) {
-        try {
-          msc = HiveMetaStoreUtils.getHiveMetastoreClient(conf);
-        } catch (Exception e) {
-          LOG.error("Failed to connect to HMS", e);
-          return false;
-        }
+
+    // If an exception is thrown in the try-with-resources block below, msc is closed and nulled, so a new instance
+    // is need to be obtained here.
+    if (msc == null) {
+      try {
+        msc = HiveMetaStoreUtils.getHiveMetastoreClient(conf);
+      } catch (Exception e) {
+        LOG.error("Failed to connect to HMS", e);
+        return false;
       }
+    }
+
+    try (CompactionTxn compactionTxn = new CompactionTxn()) {
 
       FindNextCompactRequest findNextCompactRequest = new FindNextCompactRequest();
       findNextCompactRequest.setWorkerId(workerName);
@@ -663,7 +657,6 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
 
     private TxnStatus status = TxnStatus.UNKNOWN;
     private boolean succeessfulCompaction = false;
-    private CompactionHeartbeater heartbeater;
     private ScheduledExecutorService heartbeatExecutor;
 
     /**
@@ -671,10 +664,6 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
      * @throws TException
      */
     void open(CompactionInfo ci) throws TException {
-      if (msc == null) {
-        LOG.error("Metastore client was null. Could not open a new transaction.");
-        return;
-      }
       this.txnId = msc.openTxn(ci.runAs, TxnType.COMPACTION);
       status = TxnStatus.OPEN;
 
@@ -685,11 +674,18 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
             + "}, status {" + res.getState() + "}, reason {" + res.getErrorMessage() + "}");
       }
       lockId = res.getLockid();
-
-      heartbeatExecutor = Executors.newSingleThreadScheduledExecutor();
+      heartbeatExecutor = Executors.newSingleThreadScheduledExecutor(new ThreadFactoryBuilder()
+                .setPriority(Thread.MIN_PRIORITY)
+                .setDaemon(true)
+                .setNameFormat("CompactionTxnHeartbeater-" + txnId)
+                .build());
       long txnTimeout = MetastoreConf.getTimeVar(conf, MetastoreConf.ConfVars.TXN_TIMEOUT, TimeUnit.MILLISECONDS);
-      heartbeater = new CompactionHeartbeater(this, TxnUtils.getFullTableName(ci.dbname, ci.tableName), conf);
-      heartbeatExecutor.scheduleAtFixedRate(heartbeater, txnTimeout / 4, txnTimeout / 2, TimeUnit.MILLISECONDS);
+      heartbeatExecutor.scheduleAtFixedRate(
+              new CompactionHeartbeater(this, TxnUtils.getFullTableName(ci.dbname, ci.tableName), conf),
+              txnTimeout / 4,
+              txnTimeout / 2,
+              TimeUnit.MILLISECONDS
+      );
     }
 
     /**
@@ -704,16 +700,15 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
      * @throws Exception
      */
     @Override public void close() throws Exception {
+      //the transaction is about to close, we can stop heartbeating regardless of it's state
+      shutdownHeartbeater();
       if (status != TxnStatus.UNKNOWN) {
-        // turn off error logging in heartbeater in case of race condition between commit/abort and heartbeating
-        heartbeater.shouldLogError(false);
         if (succeessfulCompaction) {
           commit();
         } else {
           abort();
         }
       }
-      shutdownHeartbeater();
     }
 
     private void shutdownHeartbeater() {
@@ -721,11 +716,12 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
         heartbeatExecutor.shutdownNow();
         try {
           if (!heartbeatExecutor.awaitTermination(5, TimeUnit.SECONDS)) {
-            heartbeatExecutor.shutdownNow();
+            LOG.warn("Heartbeating for transaction {} did not stop in 5 seconds, do not wait any longer.", this);
           }
-          LOG.debug("Successfully stopped heartbeating for transaction {}", this);
         } catch (InterruptedException ex) {
-          heartbeatExecutor.shutdownNow();
+          //Caller thread was interrupted while waiting for heartbeater to terminate.
+          //Nothing to do, just restore the interrupted state.
+          Thread.currentThread().interrupt();
         }
       }
     }
@@ -745,37 +741,22 @@ public class Worker extends RemoteCompactorThread implements MetaStoreThread {
     /**
      * Commit the txn if open.
      */
-    private void commit() {
-      if (msc == null) {
-        LOG.error("Metastore client was null. Could not commit txn " + this);
-        return;
-      }
+    private void commit() throws TException {
       if (status == TxnStatus.OPEN) {
-        try {
-          msc.commitTxn(txnId);
-          status = TxnStatus.COMMITTED;
-        } catch (TException e) {
-          LOG.error("Caught an exception while committing compaction txn in worker " + workerName, e);
-        }
+        msc.commitTxn(txnId);
+        status = TxnStatus.COMMITTED;
       }
     }
 
     /**
      * Abort the txn if open.
      */
-    private void abort() {
-      if (msc == null) {
-        LOG.error("Metastore client was null. Could not abort txn " + this);
-        return;
-      }
+    private void abort() throws TException {
       if (status == TxnStatus.OPEN) {
-        try {
-          msc.abortTxns(Collections.singletonList(txnId));
-          status = TxnStatus.ABORTED;
-        } catch (TException e) {
-          LOG.error("Caught an exception while aborting compaction txn in worker " + workerName, e);
-        }
+        msc.abortTxns(Collections.singletonList(txnId));
+        status = TxnStatus.ABORTED;
       }
     }
   }
+
 }
