1e853b92b88a107414872927761cb126db084ea6
Zoltan Haindrich <kirk@rxd.hu>
2022-02-17 18:12:06 +0100

    HIVE-21152: Rewrite if expression to case and recognize simple case as an if (#2791) (Zoltan Haindrich reviewed by Krisztian Kasa)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/SqlFunctionConverter.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/SqlFunctionConverter.java
@@ -398,6 +398,7 @@ public class SqlFunctionConverter {
       registerDuplicateFunction("<=>", SqlStdOperatorTable.IS_NOT_DISTINCT_FROM, hToken(HiveParser.EQUAL_NS, "<=>"));
       registerFunction("when", SqlStdOperatorTable.CASE, hToken(HiveParser.Identifier, "when"));
       registerDuplicateFunction("case", SqlStdOperatorTable.CASE, hToken(HiveParser.Identifier, "when"));
+      registerDuplicateFunction("if", SqlStdOperatorTable.CASE, hToken(HiveParser.Identifier, "when"));
       registerFunction("coalesce", SqlStdOperatorTable.COALESCE, hToken(HiveParser.Identifier, "coalesce"));
 
       // timebased
