dd8ec7a54014dddc2ebcd2c2b9c0bb101f16dd67
Steve Carlin <scarlin@cloudera.com>
2022-03-08 12:29:04 -0800

    HIVE-26015: URLEncode the URI for Ranger auth for HBase
    Fields in the Serde Properties can have a hash tag in it,
    so the URI needs to be URLEncoded.

--- hbase-handler/src/java/org/apache/hadoop/hive/hbase/HBaseStorageHandler.java
+++ hbase-handler/src/java/org/apache/hadoop/hive/hbase/HBaseStorageHandler.java
@@ -21,6 +21,7 @@ package org.apache.hadoop.hive.hbase;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -296,7 +297,8 @@ public class HBaseStorageHandler extends DefaultStorageHandler
     String hbase_host = tableProperties.containsKey(HBASE_HOST_NAME)? tableProperties.get(HBASE_HOST_NAME) : hbaseConf.get(HBASE_HOST_NAME);
     String hbase_port = tableProperties.containsKey(HBASE_CLIENT_PORT)? tableProperties.get(HBASE_CLIENT_PORT) : hbaseConf.get(HBASE_CLIENT_PORT);
     String table_name = tableProperties.getOrDefault(HBaseSerDe.HBASE_TABLE_NAME, null);
-    String column_family = tableProperties.getOrDefault(HBaseSerDe.HBASE_COLUMNS_MAPPING, null);
+    String column_family =
+        URLEncoder.encode(tableProperties.getOrDefault(HBaseSerDe.HBASE_COLUMNS_MAPPING, null));
     if (column_family != null)
       return new URI(HBASE_PREFIX+"//"+hbase_host+":"+hbase_port+"/"+table_name+"/"+column_family);
     else
