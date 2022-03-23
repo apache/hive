6817fd9ac2f6824c1e21c7b7c9d1771106682b03
Alessandro Solimando <alessandro.solimando@gmail.com>
2022-03-23 10:38:37 +0100

    Added hive.stats.kll.k config property in HiveConf

--- common/src/java/org/apache/hadoop/hive/common/HiveStatsUtils.java
+++ common/src/java/org/apache/hadoop/hive/common/HiveStatsUtils.java
@@ -81,10 +81,13 @@ public class HiveStatsUtils {
     return Lists.newArrayList(fs.globStatus(pathPattern, FileUtils.HIDDEN_FILES_PATH_FILTER));
   }
 
-  public static int getKParamForKllSketch(Configuration conf) throws Exception {
-    // TODO: AS - read from config and validate
-    // is in [8, 65535], default value is 200
-    return 200;
+  public static int getKParamForKllSketch(Configuration conf) {
+    int k = HiveConf.getIntVar(conf, HiveConf.ConfVars.HIVE_STATS_KLL_K);
+
+    if (k < 8 || k > 65535) {
+      throw new IllegalArgumentException("hive.stats.kll.k must be in the range [8, 65535], got " + k);
+    }
+    return k;
   }
 
   public static int getNumBitVectorsForNDVEstimation(Configuration conf) throws Exception {
