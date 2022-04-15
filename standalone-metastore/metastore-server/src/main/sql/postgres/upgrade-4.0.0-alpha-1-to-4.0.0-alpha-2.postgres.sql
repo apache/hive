271dc26e4e2d94fe34c9c8b31ce9e7c3a2591037
Janos Kovacs <jkovacs@cloudera.com>
2022-04-15 21:10:36 +0200

    Add missing keys/indexes for high concurrency workload

--- standalone-metastore/metastore-server/src/main/sql/postgres/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.postgres.sql
+++ standalone-metastore/metastore-server/src/main/sql/postgres/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.postgres.sql
@@ -1,5 +1,9 @@
 SELECT 'Upgrading MetaStore schema from 4.0.0-alpha-1 to 4.0.0-alpha-2';
 
+-- HIVE-26144
+ALTER TABLE "TXN_COMPONENTS" ADD "TC_ID" bigserial PRIMARY KEY;
+ALTER TABLE "COMPLETED_TXN_COMPONENTS" ADD "CTC_ID" bigserial PRIMARY KEY;
+CREATE INDEX IND_WNL_ID ON "TXN_WRITE_NOTIFICATION_LOG" ("WNL_ID");
 
 -- These lines need to be last. Insert any changes above.
 UPDATE "VERSION" SET "SCHEMA_VERSION"='4.0.0-alpha-2', "VERSION_COMMENT"='Hive release version 4.0.0-alpha-2' where "VER_ID"=1;
