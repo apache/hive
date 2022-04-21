aebaf75ec6213a583ef079cdb3251a1cfab96f60
kasakrisz <kasakrisz2@gmail.com>
2022-04-21 09:38:06 +0200

    fix package and licence header

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveSubQueryVisitor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveSubQueryVisitor.java
@@ -1,4 +1,4 @@
-package org.apache.hadoop.hive.ql.optimizer.calcite;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,6 +15,8 @@ package org.apache.hadoop.hive.ql.optimizer.calcite;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.hadoop.hive.ql.optimizer.calcite;
 
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.RelVisitor;
