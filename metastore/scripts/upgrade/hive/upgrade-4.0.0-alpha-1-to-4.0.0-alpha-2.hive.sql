3d76c66caccf959a0befbfcf368210e471652477
SimhadriG <simhadri.govindappa@cloudera.com>
2022-05-10 03:32:47 +0530

    HIVE-26215: Expose the MIN_HISTORY_LEVEL table through Hive sys database

--- metastore/scripts/upgrade/hive/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.hive.sql
+++ metastore/scripts/upgrade/hive/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.hive.sql
@@ -2,5 +2,18 @@ SELECT 'Upgrading MetaStore schema from 4.0.0-alpha-1 to 4.0.0-alpha-2';
 
 USE SYS;
 
+CREATE EXTERNAL TABLE IF NOT EXISTS `MIN_HISTORY_LEVEL` (
+    `MHL_TXNID` bigint,
+    `MHL_MIN_OPEN_TXNID` bigint
+)
+STORED BY 'org.apache.hive.storage.jdbc.JdbcStorageHandler'
+TBLPROPERTIES (
+"hive.sql.database.type" = "METASTORE",
+"hive.sql.query" =
+"SELECT
+    \"MHL_TXNID\",
+    \"MHL_MIN_OPEN_TXNID\",
+FROM \"MIN_HISTORY_LEVEL\""
+);
 
 SELECT 'Finished upgrading MetaStore schema from 4.0.0-alpha-1 to 4.0.0-alpha-2';
