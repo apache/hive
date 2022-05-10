1127ea4629771abf4151e2aff6e2ff7afbe4674b
kasakrisz <kasakrisz2@gmail.com>
2022-05-10 09:18:54 +0200

    handle default in set clause

--- parser/src/java/org/apache/hadoop/hive/ql/parse/HiveParser.g
+++ parser/src/java/org/apache/hadoop/hive/ql/parse/HiveParser.g
@@ -2777,8 +2777,15 @@ deleteStatement
 /*SET <columName> = (3 + col2)*/
 columnAssignmentClause
    :
-   tableOrColumn EQUAL^ precedencePlusExpression
+   | tableOrColumn EQUAL^ precedencePlusExpressionOrDefault
    ;
+
+precedencePlusExpressionOrDefault
+    :
+    (KW_DEFAULT (~DOT|EOF)) => defaultValue
+    | precedencePlusExpression
+    ;
+
 
 /*SET col1 = 5, col2 = (4 + col4), ...*/
 setColumnsClause
--- parser/src/test/org/apache/hadoop/hive/ql/parse/TestParseDefault.java
+++ parser/src/test/org/apache/hadoop/hive/ql/parse/TestParseDefault.java
@@ -30,53 +30,79 @@ public class TestParseDefault {
   @Test
   public void testParseDefaultKeywordInInsert() throws Exception {
     ASTNode tree = parseDriver.parse(
-        "INSERT INTO TABLE t1 values(DEFAULT, deFaUlt)", null).getTree();
+            "INSERT INTO TABLE t1 values(DEFAULT, deFaUlt)", null).getTree();
 
-    assertTrue(tree.toStringTree().contains(
+    assertTrue(tree.dump(), tree.toStringTree().contains(
             "(tok_table_or_col tok_default_value) (tok_table_or_col tok_default_value)"));
   }
 
   @Test
   public void testParseDefaultKeywordInUpdate() throws Exception {
     ASTNode tree = parseDriver.parse(
-        "update t1 set b = default where a = 1", null).getTree();
+            "update t1 set b = default", null).getTree();
 
-    assertTrue(tree.toStringTree().contains(
+    assertTrue(tree.dump(), tree.toStringTree().contains(
             "(tok_table_or_col tok_default_value)"));
 
   }
 
   @Test
+  public void testParseDefaultKeywordInUpdateWithWhere() throws Exception {
+    ASTNode tree = parseDriver.parse(
+            "update t1 set b = default where a = 10", null).getTree();
+
+    assertTrue(tree.dump(), tree.toStringTree().contains(
+            "(tok_table_or_col tok_default_value)"));
+
+  }
+
+  @Test
+  public void testParseStructFieldNamedDefaultInSetClause() throws Exception {
+    ASTNode tree = parseDriver.parse(
+            "update t1 set b = default.field0\n", null).getTree();
+
+    assertFalse(tree.dump(), tree.toStringTree().contains("tok_default_value"));
+  }
+
+  @Test
+  public void testParseStructFieldNamedDefaultInBeginningOdSetClause() throws Exception {
+    ASTNode tree = parseDriver.parse(
+            "update t1 set b = default.field0, a = 10\n", null).getTree();
+
+    assertFalse(tree.dump(), tree.toStringTree().contains("tok_default_value"));
+  }
+
+  @Test
   public void testParseDefaultKeywordInMerge() throws Exception {
     ASTNode tree = parseDriver.parse(
-        "MERGE INTO t1 AS t USING t2 as s ON t.a = s.a\n" +
-                "WHEN MATCHED THEN UPDATE SET b = defauLt " +
-                "WHEN NOT MATCHED THEN INSERT VALUES (s.a, DEFAuLT, DEFAULT)", null).getTree();
+            "MERGE INTO t1 AS t USING t2 as s ON t.a = s.a\n" +
+                    "WHEN MATCHED THEN UPDATE SET b = defauLt " +
+                    "WHEN NOT MATCHED THEN INSERT VALUES (s.a, DEFAuLT, DEFAULT)", null).getTree();
 
-    assertEquals(3, StringUtils.countMatches(tree.toStringTree(), "(tok_table_or_col tok_default_value)"));
+    assertEquals(tree.dump(), 3, StringUtils.countMatches(tree.toStringTree(), "(tok_table_or_col tok_default_value)"));
   }
 
   @Test
   public void testParseStructNamedDefault() throws Exception {
     ASTNode tree = parseDriver.parse(
-        "select default.src.`end`.key from s_n1\n", null).getTree();
+            "select default.src.`end`.key from s_n1\n", null).getTree();
 
-    assertFalse(tree.toStringTree().contains("tok_default_value"));
+    assertFalse(tree.dump(), tree.toStringTree().contains("tok_default_value"));
   }
 
   @Test
   public void testParseStructFieldNamedDefault() throws Exception {
     ASTNode tree = parseDriver.parse(
-        "select col0.default.key from s_n1\n", null).getTree();
+            "select col0.default.key from s_n1\n", null).getTree();
 
-    assertFalse(tree.toStringTree().contains("tok_default_value"));
+    assertFalse(tree.dump(), tree.toStringTree().contains("tok_default_value"));
   }
 
   @Test
   public void testSelectColumNamedDefault() throws Exception {
     ASTNode tree = parseDriver.parse(
-        "select default from s_n1\n", null).getTree();
+            "select default from s_n1\n", null).getTree();
 
-    assertFalse(tree.toStringTree().contains("tok_default_value"));
+    assertFalse(tree.dump(), tree.toStringTree().contains("tok_default_value"));
   }
 }
