78858d04d5f22a02a6ee0d55052ab3e0bdcbb5d9
Steve Carlin <scarlin@cloudera.com>
2022-06-06 12:03:53 -0700

    HIVE-26294: Allow substr to take bigint params
    Impala allows a bigint as an argument for the substr function.
    This will allow Hive substr function to take bigint arguments too.

--- ql/src/java/org/apache/hadoop/hive/ql/udf/UDFSubstr.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/UDFSubstr.java
@@ -33,6 +33,7 @@ import org.apache.hadoop.hive.ql.stats.estimator.StatEstimator;
 import org.apache.hadoop.hive.ql.stats.estimator.StatEstimatorProvider;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
 /**
@@ -64,6 +65,27 @@ public class UDFSubstr extends UDF implements StatEstimatorProvider {
     r = new Text();
   }
 
+  public Text evaluate(Text t, LongWritable pos, LongWritable len) {
+
+    if ((t == null) || (pos == null) || (len == null)) {
+      return null;
+    }
+
+    r.clear();
+    if ((len.get() <= 0)) {
+      return r;
+    }
+
+    String s = t.toString();
+    int[] index = makeIndex(pos.get(), len.get(), s.length());
+    if (index == null) {
+      return r;
+    }
+
+    r.set(s.substring(index[0], index[1]));
+    return r;
+  }
+
   public Text evaluate(Text t, IntWritable pos, IntWritable len) {
 
     if ((t == null) || (pos == null) || (len == null)) {
@@ -83,6 +105,31 @@ public class UDFSubstr extends UDF implements StatEstimatorProvider {
 
     r.set(s.substring(index[0], index[1]));
     return r;
+  }
+
+  private int[] makeIndex(long pos, long len, int inputLen) {
+    if ((Math.abs(pos) > inputLen)) {
+      return null;
+    }
+
+    long start, end;
+
+    if (pos > 0) {
+      start = pos - 1;
+    } else if (pos < 0) {
+      start = inputLen + pos;
+    } else {
+      start = 0;
+    }
+
+    if ((inputLen - start) < len) {
+      end = inputLen;
+    } else {
+      end = start + len;
+    }
+    index[0] = (int) start;
+    index[1] = (int) end;
+    return index;
   }
 
   private int[] makeIndex(int pos, int len, int inputLen) {
@@ -112,8 +159,32 @@ public class UDFSubstr extends UDF implements StatEstimatorProvider {
 
   private final IntWritable maxValue = new IntWritable(Integer.MAX_VALUE);
 
+  private final LongWritable maxLongValue = new LongWritable(Long.MAX_VALUE);
+
   public Text evaluate(Text s, IntWritable pos) {
     return evaluate(s, pos, maxValue);
+  }
+
+  public Text evaluate(Text s, LongWritable pos) {
+    return evaluate(s, pos, maxLongValue);
+  }
+
+  public BytesWritable evaluate(BytesWritable bw, LongWritable pos, LongWritable len) {
+
+    if ((bw == null) || (pos == null) || (len == null)) {
+      return null;
+    }
+
+    if ((len.get() <= 0)) {
+      return new BytesWritable();
+    }
+
+    int[] index = makeIndex(pos.get(), len.get(), bw.getLength());
+    if (index == null) {
+      return new BytesWritable();
+    }
+
+    return new BytesWritable(Arrays.copyOfRange(bw.getBytes(), index[0], index[1]));
   }
 
   public BytesWritable evaluate(BytesWritable bw, IntWritable pos, IntWritable len) {
@@ -136,6 +207,10 @@ public class UDFSubstr extends UDF implements StatEstimatorProvider {
 
   public BytesWritable evaluate(BytesWritable bw, IntWritable pos){
     return evaluate(bw, pos, maxValue);
+  }
+
+  public BytesWritable evaluate(BytesWritable bw, LongWritable pos){
+    return evaluate(bw, pos, maxLongValue);
   }
 
   @Override
