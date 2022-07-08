edc341c0b9a3d3f02458c8783e89f5ee93aafa37
Alessandro Solimando <alessandro.solimando@gmail.com>
2022-07-08 10:52:06 +0200

    HIVE-26378: Improve error message for masking over complex data types (#3421).  (Alessandro Solimando reviewed by Ayush Saxena)

--- common/src/java/org/apache/hadoop/hive/ql/ErrorMsg.java
+++ common/src/java/org/apache/hadoop/hive/ql/ErrorMsg.java
@@ -384,6 +384,8 @@ public enum ErrorMsg {
   MASKING_FILTERING_ON_MATERIALIZED_VIEWS_SOURCES(10288,
       "Querying directly materialized view contents is not supported since we detected {0}.{1} " +
           "used by materialized view has row masking/column filtering enabled", true),
+  MASKING_COMPLEX_TYPE_NOT_SUPPORTED(10289,
+      "Masking complex types is not supported, found a masking expression {0} over column {1}:{2}", true),
 
   UPDATEDELETE_PARSE_ERROR(10290, "Encountered parse error while parsing rewritten merge/update or " +
       "delete query"),
