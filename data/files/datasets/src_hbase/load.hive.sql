92217ed6035e40a1af65ccca893949c795acc49e
Ayush Saxena <ayushsaxena@apache.org>
2022-08-04 15:09:28 +0530

    Fix Hbase Tests, by Using Shaded Protobuf.

--- data/files/datasets/src_hbase/load.hive.sql
+++ data/files/datasets/src_hbase/load.hive.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS src_hbase_tmp;
+DROP table if exists src_hbase_tmp;
 
 CREATE TABLE src_hbase_tmp (key STRING COMMENT 'default', value STRING COMMENT 'default') STORED AS TEXTFILE;
 
@@ -8,7 +8,7 @@ ANALYZE TABLE src_hbase_tmp COMPUTE STATISTICS;
 
 ANALYZE TABLE src_hbase_tmp COMPUTE STATISTICS FOR COLUMNS key,value;
 
-DROP TABLE IF EXISTS src_hbase;
+DROP table if exists src_hbase;
 
 CREATE TABLE src_hbase (key INT, value STRING)
 STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
