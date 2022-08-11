18501dd2e6f0c68038600b4dc69b7197a7827cbd
Ayush Saxena <ayushsaxena@apache.org>
2022-08-11 10:59:25 +0530

    HIVE-24483: aarch64 Support: Bump protobuf version to 3.21.4 (#3498). (Ayush Saxena, reviewed by Stamatis Zampetakis and  Sylwester Lachiewicz)

--- ql/src/java/org/apache/tez/dag/history/logging/proto/ProtoMessageWritable.java
+++ ql/src/java/org/apache/tez/dag/history/logging/proto/ProtoMessageWritable.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import com.google.protobuf.ExtensionRegistryLite;
 import org.apache.hadoop.io.Writable;
 
 import com.google.protobuf.CodedInputStream;
@@ -96,6 +97,6 @@ public class ProtoMessageWritable<T extends MessageLite> implements Writable {
       cin.setSizeLimit(Integer.MAX_VALUE);
     }
     din.in = in;
-    message = cin.readMessage(parser, null);
+    message = cin.readMessage(parser, ExtensionRegistryLite.newInstance());
   }
 }
