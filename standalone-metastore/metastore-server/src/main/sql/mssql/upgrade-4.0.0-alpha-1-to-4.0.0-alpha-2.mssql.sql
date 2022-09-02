36f9e6296b8fd67b2275a8757b58f10f7b4d3a97
Soubhik Chakraborty <shoubhik.chakraborty@cloudera.com>
2022-09-02 14:10:13 -0400

    [BASE] metastore upgrade scripts

--- standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.mssql.sql
+++ standalone-metastore/metastore-server/src/main/sql/mssql/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.mssql.sql
@@ -8,6 +8,9 @@ ALTER TABLE COMPLETED_COMPACTIONS ADD CC_COMMIT_TIME bigint NULL;
 -- HIVE-26324
 ALTER TABLE NOTIFICATION_SEQUENCE ADD CONSTRAINT ONE_ROW_CONSTRAINT CHECK (NNI_ID = 1);
 
+ALTER TABLE TAB_COL_STATS ADD FREQ_ITEMS varbinary(max);
+ALTER TABLE PART_COL_STATS ADD FREQ_ITEMS varbinary(max);
+
 -- These lines need to be last.  Insert any changes above.
 UPDATE VERSION SET SCHEMA_VERSION='4.0.0-alpha-2', VERSION_COMMENT='Hive release version 4.0.0-alpha-2' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 4.0.0-alpha-1 to 4.0.0-alpha-2' AS MESSAGE;
--- standalone-metastore/metastore-server/src/main/sql/postgres/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.postgres.sql
+++ standalone-metastore/metastore-server/src/main/sql/postgres/upgrade-4.0.0-alpha-1-to-4.0.0-alpha-2.postgres.sql
@@ -8,6 +8,9 @@ ALTER TABLE "COMPLETED_COMPACTIONS" ADD "CC_COMMIT_TIME" bigint;
 -- HIVE-26324
 ALTER TABLE "NOTIFICATION_SEQUENCE" ADD CONSTRAINT "ONE_ROW_CONSTRAINT" CHECK ("NNI_ID" = 1);
 
+ALTER TABLE "TAB_COL_STATS" ADD "FREQ_ITEMS" bytea;
+ALTER TABLE "PART_COL_STATS" ADD "FREQ_ITEMS" bytea;
+
 -- These lines need to be last. Insert any changes above.
 UPDATE "VERSION" SET "SCHEMA_VERSION"='4.0.0-alpha-2', "VERSION_COMMENT"='Hive release version 4.0.0-alpha-2' where "VER_ID"=1;
 SELECT 'Finished upgrading MetaStore schema from 4.0.0-alpha-1 to 4.0.0-alpha-2';
