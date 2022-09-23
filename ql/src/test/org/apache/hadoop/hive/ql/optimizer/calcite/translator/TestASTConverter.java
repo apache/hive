2f041d5cc8f4c9ac69a28073e823745e8ec9448a
kasakrisz <kasakrisz2@gmail.com>
2022-09-23 09:12:59 +0200

    format TestASTConverter

--- ql/src/test/org/apache/hadoop/hive/ql/optimizer/calcite/translator/TestASTConverter.java
+++ ql/src/test/org/apache/hadoop/hive/ql/optimizer/calcite/translator/TestASTConverter.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.hive.ql.optimizer.calcite.translator;
 
 import org.apache.calcite.rel.type.RelDataType;
@@ -20,35 +38,19 @@ import static org.apache.hadoop.hive.ql.optimizer.calcite.translator.ASTConverte
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-public class TestASTConverter {
+class TestASTConverter {
   @Test
-  public void testEmptyPlanWhenInputSchemaIsEmpty() {
+  void testEmptyPlanWhenInputSchemaIsEmpty() {
+    RelRecordType dataType = new RelRecordType(Collections.emptyList());
     IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-      emptyPlan(new RelRecordType(Collections.emptyList()));
+      emptyPlan(dataType);
     });
 
     Assertions.assertTrue(thrown.getMessage().contains("Schema is empty"));
   }
 
   @Test
-  public void testEmptyPlan() {
+  void testEmptyPlan() {
     List<RelDataTypeField> fields = asList(
             new RelDataTypeFieldImpl("a", 0, new BasicSqlType(new HiveTypeSystemImpl(), SqlTypeName.INTEGER)),
             new RelDataTypeFieldImpl("b", 1, new BasicSqlType(new HiveTypeSystemImpl(), SqlTypeName.CHAR, 30)),
