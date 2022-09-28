109c0c1753cd50e7195174b2318a34dac5942e12
Steve Carlin <scarlin@cloudera.com>
2022-09-28 10:33:02 -0700

    HIVE-26573: Fix ClassCastException issues for Decimal64 vectorization.
    A ClassCastException error was being thrown while deserializing a Decimal64Vector.
    
    It is unnecessary for the code to check the DataTypePhysicalVariation property within
    VectorDeserializeRow.  There is already enough information to determine that the binding
    should bedone with the Decimal64Vector. So the code now just checks for the instance
    when deciding on the binding.
    
    Another bug was found while implementing this fix. The Lazy reads were always converting
    the decimal into its decimal64 form which isn't necessary when there is no decimal64
    binding. This code is now pushed into VectorDeserializeRow which is the only place in
    the repository which references this variable.

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorDeserializeRow.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorDeserializeRow.java
@@ -1061,8 +1061,8 @@ public final class VectorDeserializeRow<T extends DeserializeRead> {
     @Override
     void store(ColumnVector colVector, Field field, int batchIndex, boolean canRetainByteRef)
             throws IOException {
-      if (field.getDataTypePhysicalVariation() == DataTypePhysicalVariation.DECIMAL_64) {
-        ((Decimal64ColumnVector) colVector).vector[batchIndex] = deserializeRead.currentDecimal64;
+      if (colVector instanceof Decimal64ColumnVector) {
+        ((Decimal64ColumnVector) colVector).set(batchIndex, deserializeRead.currentHiveDecimalWritable);
       } else {
         // The DecimalColumnVector set method will quickly copy the deserialized decimal writable fields.
         ((DecimalColumnVector) colVector).set(
