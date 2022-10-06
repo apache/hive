59d0c078bb6efe45fdfb24b214c049d680432c5c
Stamatis Zampetakis <zabetak@gmail.com>
2022-10-06 16:27:57 +0200

    Compilation error in HiveFilterJoinRule due to RelRule.Config.EMPTY
    RelRule.Config.EMPTY has been removed in CALCITE-4839 which is a known
    breaking change.
    
    Refactor HiveFilterJoinRule using Config interface instead of
    inheritance to parameterize Hive variants.
    
    The refactored version does not use RelRule.Config.EMPTY so it solves
    the compilation problem.

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/jdbc/JDBCFilterJoinRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/jdbc/JDBCFilterJoinRule.java
@@ -17,31 +17,37 @@
  */
 package org.apache.hadoop.hive.ql.optimizer.calcite.rules.jdbc;
 
-import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.rel.core.Filter;
 import org.apache.calcite.rel.core.Join;
+import org.apache.calcite.rel.rules.FilterJoinRule;
 import org.apache.hadoop.hive.ql.optimizer.calcite.HiveRelFactories;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveFilter;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.jdbc.HiveJdbcConverter;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveJoin;
-import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveFilterJoinRule;
 
 /**
  * Rule that tries to push filter expressions into a join condition and into
  * the inputs of the join.
  */
 
-public class JDBCFilterJoinRule extends HiveFilterJoinRule {
+public class JDBCFilterJoinRule extends FilterJoinRule.FilterIntoJoinRule {
 
+  // @formatter:off
+  private static final FilterJoinRule.FilterIntoJoinRule.FilterIntoJoinRuleConfig DEFAULT =
+      (FilterIntoJoinRuleConfig) FilterIntoJoinRuleConfig.DEFAULT
+          .withOperandSupplier(
+              b0 -> b0.operand(HiveFilter.class).oneInput(
+                  b1 -> b1.operand(HiveJoin.class).inputs(
+                      b2 -> b2.operand(HiveJdbcConverter.class).anyInputs(),
+                      b3 -> b3.operand(HiveJdbcConverter.class).anyInputs())))
+          .withRelBuilderFactory(HiveRelFactories.HIVE_BUILDER)
+          .withDescription("JDBCFilterJoinRule");
+  // @formatter:on
   public static final JDBCFilterJoinRule INSTANCE = new JDBCFilterJoinRule();
 
   public JDBCFilterJoinRule() {
-    super(RelOptRule.operand(HiveFilter.class,
-            RelOptRule.operand(HiveJoin.class,
-              RelOptRule.operand(HiveJdbcConverter.class, RelOptRule.any()),
-              RelOptRule.operand(HiveJdbcConverter.class, RelOptRule.any()))),
-        "JDBCFilterJoinRule", true, HiveRelFactories.HIVE_BUILDER);
+    super(DEFAULT);
   }
 
   @Override
@@ -62,10 +68,4 @@ public class JDBCFilterJoinRule extends HiveFilterJoinRule {
     return false;
   }
 
-  @Override
-  public void onMatch(RelOptRuleCall call) {
-    Filter filter = call.rel(0);
-    Join join = call.rel(1);
-    super.perform(call, filter, join);
-  }
 }
