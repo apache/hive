4ee393adbae70b589110b06fdab5aa2f8c03927c
Sourabh Badhya <sbadhya@cloudera.com>
2022-10-10 09:16:30 +0530

    Remove redundant imports

--- ql/src/test/org/apache/hadoop/hive/ql/TestTxnCommands2.java
+++ ql/src/test/org/apache/hadoop/hive/ql/TestTxnCommands2.java
@@ -86,7 +86,6 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
