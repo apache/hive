718df0a7e4f5423e9ea4aeb4088e956635a1567a
Bodor Laszlo <bodorlaszlo0202@gmail.com>
2022-10-18 18:07:15 +0200

    HIVE-26639: ConstantVectorExpression shouldn't rely on default charset (#3675) (Laszlo Bodor reviewed by Ayush Saxena)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/ConstantVectorExpression.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/ConstantVectorExpression.java
@@ -205,7 +205,7 @@ public class ConstantVectorExpression extends VectorExpression {
               outputColumnNum, (HiveDecimal) constantValue, outputTypeInfo);
         case STRING:
           return new ConstantVectorExpression(
-              outputColumnNum, ((String) constantValue).getBytes(), outputTypeInfo);
+              outputColumnNum, ((String) constantValue).getBytes(StandardCharsets.UTF_8), outputTypeInfo);
         case VARCHAR:
           return new ConstantVectorExpression(
               outputColumnNum, ((HiveVarchar) constantValue), outputTypeInfo);
