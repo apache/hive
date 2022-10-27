c4cfc1a1a0a5bf7fd58817dabd7d79dcd8aea8b6
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2022-10-27 20:12:38 +0200

    HIVE-26670: Track every single HTTP request between beeline and hs2

---
+++ jdbc/src/java/org/apache/hive/jdbc/HttpDefaultResponseInterceptor.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hive.jdbc;
+
+import java.io.IOException;
+
+import org.apache.hadoop.hive.conf.Constants;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.protocol.HttpContext;
+
+public class HttpDefaultResponseInterceptor extends HttpResponseInterceptorBase {
+
+  @Override
+  public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
+    String trackHeader = (String) context.getAttribute(Constants.HTTP_HEADER_REQUEST_TRACK);
+    if (trackHeader == null) {
+      return;
+    }
+    long elapsed = System.currentTimeMillis() - (long) context.getAttribute(trackHeader + "_TIME");
+    LOG.info("Response to {} in {} ms", trackHeader, elapsed);
+    context.removeAttribute(Constants.HTTP_HEADER_REQUEST_TRACK);
+    context.removeAttribute(trackHeader + "_TIME");
+  }
+}
