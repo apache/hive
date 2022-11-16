c799bffda9726d912d0f04497352299df2495330
Adam Szita <40628386+szlta@users.noreply.github.com>
2022-11-16 09:46:39 +0100

    HIVE-26740: HS2 makes direct connections to HMS backend DB due to Compaction/StatsUpdater (#3765) (Adam Szita, reviewed by Zhihua Deng)

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/StatsUpdater.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/StatsUpdater.java
@@ -74,7 +74,6 @@ public final class StatsUpdater {
             }
             sb.append(" compute statistics");
             LOG.info(ci + ": running '" + sb + "'");
-            statusUpdaterConf.setVar(HiveConf.ConfVars.METASTOREURIS, "");
             if (compactionQueueName != null && compactionQueueName.length() > 0) {
                 statusUpdaterConf.set(TezConfiguration.TEZ_QUEUE_NAME, compactionQueueName);
             }
