e654f0bc22dd07147095063e919be6297d6ec43e
TaoZex <45089228+TaoZex@users.noreply.github.com>
2023-01-28 16:49:42 +0800

    Remove redundant import

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFConvertCharset.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFConvertCharset.java
@@ -31,14 +31,11 @@ import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ConstantObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector.Category;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping;
 
 @Description(name = "convertCharset", value = "_FUNC_(str, str, str) - Converts the first argument from the second argument character set to the third argument character set", extended =
     "Possible options for the character set are 'US-ASCII', 'ISO-8859-1',\n"
@@ -55,31 +52,13 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
       throw new UDFArgumentLengthException("ConvertCharset() requires exactly three arguments");
     }
 
-    if (arguments[0].getCategory() != ObjectInspector.Category.PRIMITIVE
-        || PrimitiveObjectInspectorUtils.PrimitiveGrouping.STRING_GROUP
-        != PrimitiveObjectInspectorUtils.getPrimitiveGrouping(
-        ((PrimitiveObjectInspector) arguments[0]).getPrimitiveCategory())) {
-      throw new UDFArgumentTypeException(0, "The first argument to ConvertCharset() must be a string/varchar");
-    }
-
+    checkInputArgument(arguments, 0);
     stringOI = (PrimitiveObjectInspector) arguments[0];
 
-    if (arguments[1].getCategory() != ObjectInspector.Category.PRIMITIVE
-        || PrimitiveObjectInspectorUtils.PrimitiveGrouping.STRING_GROUP
-        != PrimitiveObjectInspectorUtils.getPrimitiveGrouping(
-        ((PrimitiveObjectInspector) arguments[1]).getPrimitiveCategory())) {
-      throw new UDFArgumentTypeException(1, "The second argument to ConvertCharset() must be a string/varchar");
-    }
-
+    checkInputArgument(arguments, 1);
     fromCharsetOI = (PrimitiveObjectInspector) arguments[1];
 
-    if (arguments[2].getCategory() != ObjectInspector.Category.PRIMITIVE
-        || PrimitiveObjectInspectorUtils.PrimitiveGrouping.STRING_GROUP
-        != PrimitiveObjectInspectorUtils.getPrimitiveGrouping(
-        ((PrimitiveObjectInspector) arguments[2]).getPrimitiveCategory())) {
-      throw new UDFArgumentTypeException(2, "The third argument to ConvertCharset() must be a string/varchar");
-    }
-
+    checkInputArgument(arguments, 2);
     toCharsetOI = (PrimitiveObjectInspector) arguments[2];
 
     // If the character set for encoding is constant, we can optimize that
@@ -134,5 +113,14 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
   @Override public String getDisplayString(String[] children) {
     assert (children.length == 3);
     return getStandardDisplayString("convertCharset", children, ",");
+  }
+
+  private void checkInputArgument(ObjectInspector[] arguments, int index) throws UDFArgumentTypeException {
+    if (arguments[index].getCategory() != ObjectInspector.Category.PRIMITIVE
+        || PrimitiveObjectInspectorUtils.PrimitiveGrouping.STRING_GROUP
+        != PrimitiveObjectInspectorUtils.getPrimitiveGrouping(
+        ((PrimitiveObjectInspector) arguments[index]).getPrimitiveCategory())) {
+      throw new UDFArgumentTypeException(index, "The argument to ConvertCharset() must be a string/varchar");
+    }
   }
 }
