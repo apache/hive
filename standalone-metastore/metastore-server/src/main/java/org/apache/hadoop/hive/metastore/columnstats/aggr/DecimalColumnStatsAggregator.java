50eb7c38042e9baa9f87741c55556f83052c6f7d
Alessandro Solimando <alessandro.solimando@gmail.com>
2023-02-04 15:04:35 +0100

    HIVE-27000: Improve the modularity of the *ColumnStatsMerger classes (#3997). (Alessandro Solimando, reviewed by Ayush Saxena, Akshat Mathur)

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/columnstats/aggr/DecimalColumnStatsAggregator.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/columnstats/aggr/DecimalColumnStatsAggregator.java
@@ -115,11 +115,13 @@ public class DecimalColumnStatsAggregator extends ColumnStatsAggregator implemen
         if (aggregateData == null) {
           aggregateData = newData.deepCopy();
         } else {
-          merger.setLowValue(aggregateData, newData);
-          merger.setHighValue(aggregateData, newData);
+          aggregateData.setLowValue(merger.mergeLowValue(
+              merger.getLowValue(aggregateData), merger.getLowValue(newData)));
+          aggregateData.setHighValue(merger.mergeHighValue(
+              merger.getHighValue(aggregateData), merger.getHighValue(newData)));
 
-          aggregateData.setNumNulls(aggregateData.getNumNulls() + newData.getNumNulls());
-          aggregateData.setNumDVs(Math.max(aggregateData.getNumDVs(), newData.getNumDVs()));
+          aggregateData.setNumNulls(merger.mergeNumNulls(aggregateData.getNumNulls(), newData.getNumNulls()));
+          aggregateData.setNumDVs(merger.mergeNumDVs(aggregateData.getNumDVs(), newData.getNumDVs()));
         }
       }
       if (areAllNDVEstimatorsMergeable && ndvEstimator != null) {
