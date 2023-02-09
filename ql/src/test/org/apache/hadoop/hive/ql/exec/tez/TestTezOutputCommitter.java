5f0775b78c00d591bc40da711fa5d96eced3e0b8
Yi Zhang <yigress@google.com>
2023-02-09 13:33:21 -0800

    fix test case

--- ql/src/test/org/apache/hadoop/hive/ql/exec/tez/TestTezOutputCommitter.java
+++ ql/src/test/org/apache/hadoop/hive/ql/exec/tez/TestTezOutputCommitter.java
@@ -111,6 +111,7 @@ public class TestTezOutputCommitter {
     HiveConf conf = ENVIRONMENT.getTestCtx().hiveConf;
     conf.setVar(HiveConf.ConfVars.HIVE_AUTHORIZATION_MANAGER,
         "org.apache.hadoop.hive.ql.security.authorization.plugin.sqlstd.SQLStdHiveAuthorizerFactory");
+    conf.setVar(HiveConf.ConfVars.TEZ_MAPREDUCE_OUTPUT_COMMITTER, "org.apache.tez.mapreduce.committer.MROutputCommitter");
     conf.setBoolVar(HiveConf.ConfVars.HIVE_SUPPORT_CONCURRENCY, false);
     conf.setInt("tez.am.task.max.failed.attempts", MAX_TASK_ATTEMPTS);
     conf.setBoolVar(HiveConf.ConfVars.HIVESTATSCOLAUTOGATHER, false);
