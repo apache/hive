ca0ed83797a39e8925f31f2e104559c7078b8735
Stamatis Zampetakis <zabetak@gmail.com>
2023-02-23 15:54:11 +0100

    StackOverflowError while running cbo_query13.q
    The query runs for a very long time and eventually fails with StackOverflowError.
    
    jstack (attached at the end) shows that the query is stuck in a very deep call to RelMdPredicates.getPredicates.
    
    Analysing the CBO logs we can see that the only rule that fires is HivePreFilteringRule.
    The rule constantly adds a new HiveFilter till the query plan becomes huge and eventually crashes due to a very deep stack.
    
    Turns out that a simplification that was added in CALCITE-5036 turns OR expressions into SEARCH is triggering the problem.
    
    Example:
    OR(=($76, 3), =($76, 1)) => SEARCH($76, Sarg[1, 3])
    
    In HiveCalciteUtil#getPredsNotPushedAlready, we compare simplified with not simplified predicates and thus fail to detect the equivalence between OR and SEARCH.
    
    A quick workaround, not sure if it is going be the final solution, is to also simplify the input to getPredsNotPushedAlready. This is sufficient to overcome the StackOverflowError reported here.
    
    Worth noting that the HivePreFilteringRule seems in general buggy so there might be more and better solutions to this problem.
    
       java.lang.Thread.State: RUNNABLE
    	at java.util.TreeMap.compare(TreeMap.java:1294)
    	at java.util.TreeMap.put(TreeMap.java:538)
    	at org.apache.hive.com.google.common.collect.TreeRangeSet.replaceRangeWithSameLowerBound(TreeRangeSet.java:272)
    	at org.apache.hive.com.google.common.collect.TreeRangeSet.add(TreeRangeSet.java:222)
    	at org.apache.hive.com.google.common.collect.RangeSet.addAll(RangeSet.java:225)
    	at org.apache.hive.com.google.common.collect.AbstractRangeSet.addAll(AbstractRangeSet.java:64)
    	at org.apache.hive.com.google.common.collect.TreeRangeSet.addAll(TreeRangeSet.java:41)
    	at org.apache.calcite.rex.RexSimplify$RexSargBuilder.addSarg(RexSimplify.java:3056)
    	at org.apache.calcite.rex.RexSimplify$SargCollector.accept2b(RexSimplify.java:2894)
    	at org.apache.calcite.rex.RexSimplify$SargCollector.accept2(RexSimplify.java:2812)
    	at org.apache.calcite.rex.RexSimplify$SargCollector.accept_(RexSimplify.java:2793)
    	at org.apache.calcite.rex.RexSimplify$SargCollector.accept(RexSimplify.java:2778)
    	at org.apache.calcite.rex.RexSimplify$SargCollector.access$400(RexSimplify.java:2761)
    	at org.apache.calcite.rex.RexSimplify.lambda$simplifyAnd$3(RexSimplify.java:1488)
    	at org.apache.calcite.rex.RexSimplify$$Lambda$1099/1247334493.accept(Unknown Source)
    	at java.util.ArrayList.forEach(ArrayList.java:1259)
    	at org.apache.calcite.rex.RexSimplify.simplifyAnd(RexSimplify.java:1488)
    	at org.apache.calcite.rex.RexSimplify.simplify(RexSimplify.java:279)
    	at org.apache.calcite.rex.RexSimplify.simplifyUnknownAs(RexSimplify.java:248)
    	at org.apache.calcite.rex.RexSimplify.simplify(RexSimplify.java:223)
    	at org.apache.calcite.rel.metadata.RelMdPredicates.getPredicates(RelMdPredicates.java:299)
    	at org.apache.calcite.rel.metadata.janino.GeneratedMetadata_PredicatesHandler.getPredicates_$(Unknown Source)
    	at org.apache.calcite.rel.metadata.janino.GeneratedMetadata_PredicatesHandler.getPredicates(Unknown Source)
    	at org.apache.calcite.rel.metadata.RelMetadataQuery.getPulledUpPredicates(RelMetadataQuery.java:841)
    	at org.apache.calcite.rel.metadata.RelMdPredicates.getPredicates(RelMdPredicates.java:292)
    	...
    	at org.apache.calcite.rel.metadata.janino.GeneratedMetadata_PredicatesHandler.getPredicates_$(Unknown Source)
    	at org.apache.calcite.rel.metadata.janino.GeneratedMetadata_PredicatesHandler.getPredicates(Unknown Source)
    	at org.apache.calcite.rel.metadata.RelMetadataQuery.getPulledUpPredicates(RelMetadataQuery.java:841)
    	at org.apache.calcite.rel.metadata.RelMdPredicates.getPredicates(RelMdPredicates.java:292)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveCalciteUtil.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveCalciteUtil.java
@@ -27,6 +27,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptPredicateList;
 import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.plan.RelOptUtil.InputFinder;
@@ -45,6 +46,7 @@ import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexCall;
 import org.apache.calcite.rex.RexCorrelVariable;
 import org.apache.calcite.rex.RexDynamicParam;
+import org.apache.calcite.rex.RexExecutor;
 import org.apache.calcite.rex.RexFieldAccess;
 import org.apache.calcite.rex.RexInputRef;
 import org.apache.calcite.rex.RexLiteral;
@@ -53,6 +55,7 @@ import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexOver;
 import org.apache.calcite.rex.RexPatternFieldRef;
 import org.apache.calcite.rex.RexShuttle;
+import org.apache.calcite.rex.RexSimplify;
 import org.apache.calcite.rex.RexTableInputRef;
 import org.apache.calcite.rex.RexRangeRef;
 import org.apache.calcite.rex.RexSubQuery;
@@ -656,7 +659,9 @@ public class HiveCalciteUtil {
     }
     // Build map to not convert multiple times, further remove already included predicates
     Map<String,RexNode> stringToRexNode = Maps.newLinkedHashMap();
+    RexSimplify simplify = new RexSimplify(inp.getCluster().getRexBuilder(), RelOptPredicateList.EMPTY, RexUtil.EXECUTOR);
     for (RexNode r : predsToPushDown) {
+      r = simplify.simplify(r);
       String rexNodeString = r.toString();
       if (predicatesToExclude.add(rexNodeString)) {
         stringToRexNode.put(rexNodeString, r);
