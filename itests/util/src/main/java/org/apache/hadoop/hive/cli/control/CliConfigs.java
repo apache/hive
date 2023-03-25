108e65414724ae4c8713fc19756765957b917cb0
amanraj2520 <rajaman@microsoft.com>
2023-03-25 11:10:07 +0530

    Disabled sysdb.q

--- itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
@@ -226,7 +226,7 @@ public class CliConfigs {
         includesFrom(testConfigProps, "minillaplocal.shared.query.files");
         excludeQuery("bucket_map_join_tez1.q"); // Disabled in HIVE-19509
         excludeQuery("special_character_in_tabnames_1.q"); // Disabled in HIVE-19509
-        excludeQuery("sysdb.q"); // Disabled in HIVE-19509
+        excludeQuery("sysdb.q"); // Disabled in HIVE-HIVE-27174. To be fixed in HIVE-27057
         excludeQuery("tez_smb_1.q"); // Disabled in HIVE-19509
         excludeQuery("union_fast_stats.q"); // Disabled in HIVE-19509
         excludeQuery("schema_evol_orc_acidvec_part.q"); // Disabled in HIVE-19509
