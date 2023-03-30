d4eaef83eb666ca3bb24b30898a7a2e1425a1df7
Aman Raj <104416558+amanraj2520@users.noreply.github.com>
2023-03-30 11:29:30 +0530

    HIVE-21755: Backport HIVE-21462 to branch-3 (Aman Raj, reviewed by Vihang Karajgaonkar)
    (#4021)

--- standalone-metastore/src/main/sql/mssql/upgrade-2.1.0-to-2.2.0.mssql.sql
+++ standalone-metastore/src/main/sql/mssql/upgrade-2.1.0-to-2.2.0.mssql.sql
@@ -1,14 +1,53 @@
 SELECT 'Upgrading MetaStore schema from 2.1.0 to 2.2.0' AS MESSAGE;
 
+-- We can not change the datatype of a column with default value. Hence we first drop the default constraint
+-- and then change the datatype. We wrap the code to drop the default constraint in a stored procedure to avoid
+-- code duplicate. We create temporary stored procedures since we do not need them during normal
+-- metastore operation.
+CREATE PROCEDURE #DROP_DEFAULT_CONSTRAINT @TBL_NAME sysname, @COL_NAME sysname
+AS
+BEGIN
+	DECLARE @constraintname sysname
+	SELECT @constraintname = default_constraints.name
+		FROM sys.all_columns INNER JOIN sys.tables ON all_columns.object_id = tables.object_id
+			INNER JOIN sys.schemas ON tables.schema_id = schemas.schema_id
+			INNER JOIN sys.default_constraints ON all_columns.default_object_id = default_constraints.object_id
+		WHERE schemas.name = 'dbo' AND tables.name = @TBL_NAME AND all_columns.name = @COL_NAME
+
+	IF (@constraintname IS NOT NULL)
+	BEGIN
+		DECLARE @sql nvarchar(max) = 'ALTER TABLE [dbo].' + QUOTENAME(@TBL_NAME) + ' DROP CONSTRAINT ' + QUOTENAME(@constraintname)
+		EXEC(@sql)
+	END
+END;
+
+-- Similarly for primary key constraint
+CREATE PROCEDURE #DROP_PRIMARY_KEY_CONSTRAINT @TBL_NAME sysname
+AS
+BEGIN
+	DECLARE @constraintname sysname
+	SELECT @constraintname = constraint_name
+		FROM information_schema.table_constraints
+		WHERE constraint_type = 'PRIMARY KEY' AND table_schema = 'dbo' AND table_name = @TBL_NAME
+	IF @constraintname IS NOT NULL
+	BEGIN
+	    DECLARE @sql_pk nvarchar(max) = 'ALTER TABLE [dbo].' + QUOTENAME(@TBL_NAME) + ' DROP CONSTRAINT ' + @constraintname
+	    EXEC(@sql_pk)
+	end
+END;
+
 --:r 022-HIVE-14496.mssql.sql
-ALTER TABLE TBLS ADD IS_REWRITE_ENABLED bit NOT NULL DEFAULT(0);
+ALTER TABLE TBLS ADD IS_REWRITE_ENABLED bit NOT NULL CONSTRAINT DEFAULT_IS_REWRITE_ENABLED DEFAULT(0);
 
 --:r 023-HIVE-10562.mssql.sql
 ALTER TABLE NOTIFICATION_LOG ADD MESSAGE_FORMAT nvarchar(16);
 
 --:r 024-HIVE-12274.mssql.sql
+EXEC #DROP_DEFAULT_CONSTRAINT "SERDE_PARAMS", "PARAM_VALUE";
 ALTER TABLE "SERDE_PARAMS" ALTER COLUMN "PARAM_VALUE" nvarchar(MAX);
+EXEC #DROP_DEFAULT_CONSTRAINT "TABLE_PARAMS", "PARAM_VALUE";
 ALTER TABLE "TABLE_PARAMS" ALTER COLUMN "PARAM_VALUE" nvarchar(MAX);
+EXEC #DROP_DEFAULT_CONSTRAINT "SD_PARAMS", "PARAM_VALUE";
 ALTER TABLE "SD_PARAMS" ALTER COLUMN "PARAM_VALUE" nvarchar(MAX);
 
 ALTER TABLE "TBLS" ALTER COLUMN "TBL_NAME" nvarchar(256);
@@ -20,8 +59,10 @@ ALTER TABLE "COMPLETED_TXN_COMPONENTS" ALTER COLUMN "CTC_TABLE" nvarchar(256);
 
 
 -- A number of indices and constraints reference COLUMN_NAME.  These have to be dropped before the not null constraint
--- can be added.
-ALTER TABLE COLUMNS_V2 DROP CONSTRAINT COLUMNS_PK;
+-- can be added. Earlier versions may not have created named constraints, so use IF EXISTS and also
+-- the stored procedure.
+ALTER TABLE COLUMNS_V2 DROP CONSTRAINT IF EXISTS COLUMNS_PK;
+EXEC #DROP_PRIMARY_KEY_CONSTRAINT COLUMNS_V2;
 DROP INDEX PARTITIONCOLUMNPRIVILEGEINDEX ON PART_COL_PRIVS;
 DROP INDEX TABLECOLUMNPRIVILEGEINDEX ON TBL_COL_PRIVS;
 DROP INDEX PCS_STATS_IDX ON PART_COL_STATS;
