07e58dcf1f147f2877471e9fa9367e042a716f86
saihemanth <saihemanth@cloudera.com>
2023-04-05 08:24:18 +0530

    Address test failures

--- standalone-metastore/metastore-common/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
+++ standalone-metastore/metastore-common/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
@@ -1381,6 +1381,9 @@ public class HiveMetaStoreClient implements IMetaStoreClient, AutoCloseable {
     if (!db.isSetCatalogName()) {
       db.setCatalogName(getDefaultCatalog(conf));
     }
+    if (db.getName() == null) {
+      throw new MetaException("DbName cannot be null");
+    }
     CreateDatabaseRequest req = new CreateDatabaseRequest(db.getName());
     if (db.isSetDescription()) {
       req.setDescription(db.getDescription());
@@ -2672,7 +2675,9 @@ public class HiveMetaStoreClient implements IMetaStoreClient, AutoCloseable {
   @Override
   public Partition getPartition(String catName, String dbName, String tblName,
                                 List<String> partVals) throws TException {
-
+    if (dbName == null || tblName == null || partVals == null) {
+      throw new MetaException("DbName/TableName/PartitionValues cannot be null");
+    }
     GetPartitionRequest getPartitionRequest = new GetPartitionRequest(dbName, tblName, partVals);
     getPartitionRequest.setCatName(catName);
     GetPartitionResponse res = client.get_partition_req(getPartitionRequest);
@@ -3187,6 +3192,9 @@ public class HiveMetaStoreClient implements IMetaStoreClient, AutoCloseable {
 
   protected List<String> listPartitionNamesInternal(String catName, String dbName, String tableName,
       int maxParts) throws TException {
+    if (dbName == null || tableName == null) {
+      throw new MetaException("DbName/TableName cannot be null");
+    }
     PartitionsRequest partitionReq = new PartitionsRequest(dbName, tableName);
     partitionReq.setCatName(catName);
     partitionReq.setMaxParts(shrinkMaxtoShort(maxParts));
@@ -3210,6 +3218,9 @@ public class HiveMetaStoreClient implements IMetaStoreClient, AutoCloseable {
 
   protected List<String> listPartitionNamesInternal(String catName, String db_name, String tbl_name,
       List<String> part_vals, int max_parts) throws MetaException, TException, NoSuchObjectException {
+    if (db_name == null || tbl_name == null) {
+      throw new MetaException("DbName/TableName cannot be null");
+    }
     GetPartitionNamesPsRequest getPartitionNamesPsRequest = new GetPartitionNamesPsRequest(db_name, tbl_name);
     getPartitionNamesPsRequest.setCatName(catName);
     getPartitionNamesPsRequest.setPartValues(part_vals);
