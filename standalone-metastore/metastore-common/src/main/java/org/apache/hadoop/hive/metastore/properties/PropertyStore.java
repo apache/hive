bab4a10bd0d53479e2b7fdd817b84d85eede3105
Henri Biestro <hbiestro@cloudera.com>
2023-04-13 09:18:00 +0200

    HIVE-27186: fix Javadoc;

--- standalone-metastore/metastore-common/src/main/java/org/apache/hadoop/hive/metastore/properties/PropertyStore.java
+++ standalone-metastore/metastore-common/src/main/java/org/apache/hadoop/hive/metastore/properties/PropertyStore.java
@@ -27,7 +27,7 @@ import java.util.function.Predicate;
 
 /**
  * The PropertyStore is the persistent container of property maps.
- * Maps are addressed in the store by their key -  their name prepended by their manager&quote;s namespace.
+ * Maps are addressed in the store by their key -  their name prepended by their manager&quot;s namespace.
  */
 
 public abstract class PropertyStore {
