c0beec8042c9c439bc821f2bdb1adf73c26030fd
Henri Biestro <hbiestro@cloudera.com>
2023-04-24 14:43:09 +0200

    HIVE-27186: refactored unit tests;

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/properties/HMSTestBase.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/properties/HMSTestBase.java
@@ -21,14 +21,11 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.jexl3.JxltEngine;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.HMSHandler;
-import org.apache.hadoop.hive.metastore.HiveMetaStore;
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
-import org.apache.hadoop.hive.metastore.JdoPropertyStore;
 import org.apache.hadoop.hive.metastore.MetaStoreTestUtils;
 import org.apache.hadoop.hive.metastore.ObjectStore;
 import org.apache.hadoop.hive.metastore.TestObjectStore;
 import org.apache.hadoop.hive.metastore.Warehouse;
-import org.apache.hadoop.hive.metastore.api.InvalidInputException;
 import org.apache.hadoop.hive.metastore.api.InvalidObjectException;
 import org.apache.hadoop.hive.metastore.api.InvalidOperationException;
 import org.apache.hadoop.hive.metastore.api.MaintenanceOpStatus;
@@ -37,13 +34,17 @@ import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.client.builder.DatabaseBuilder;
 import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
 import org.apache.hadoop.hive.metastore.security.HadoopThriftAuthBridge;
+import org.apache.thrift.TException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 
 import static org.apache.hadoop.hive.metastore.properties.HMSPropertyManager.JEXL;
@@ -56,28 +57,43 @@ import static org.apache.hadoop.hive.metastore.properties.PropertyType.INTEGER;
 import static org.apache.hadoop.hive.metastore.properties.PropertyType.JSON;
 import static org.apache.hadoop.hive.metastore.properties.PropertyType.STRING;
 
-public class HMSPropertyStoreRemoteTest extends HMSPropertyStoreTest {
-  //private static final String NS = "hms";
-  protected HiveMetaStoreClient client;
-  private boolean isServerStarted = false;
-  protected int port;
+public abstract class HMSTestBase {
+  /**
+   * Abstract the property client access on a given namespace.
+   */
+  interface PropertyClient {
+    boolean setProperties(Map<String, String> properties);
+    Map<String, Map<String, String>> getProperties(String mapPrefix, String mapPredicate, String... selection) throws IOException;
+  }
 
-  boolean createStore(Configuration conf, Warehouse wh) {
+  protected ObjectStore objectStore = null;
+  protected Warehouse warehouse = null;
+  protected Configuration conf = null;
+
+  protected static final Logger LOG = LoggerFactory.getLogger(TestObjectStore.class.getName());
+  static Random RND = new Random(20230424);
+  protected final String NS = "hms" + RND.nextInt(100);
+  protected final String DB = "dbtest" + RND.nextInt(100);
+  protected PropertyClient client;
+  protected int port = -1;
+
+  boolean createStore(Configuration conf) {
     try {
       MetaStoreTestUtils.setConfForStandloneMode(conf);
       objectStore = new ObjectStore();
       objectStore.setConf(conf);
       //TestObjectStore.dropAllStoreObjects(objectStore);
-      HMSHandler.createDefaultCatalog(objectStore, wh);
+      warehouse = new Warehouse(conf);
+      HMSHandler.createDefaultCatalog(objectStore, warehouse);
       // configure object store
       objectStore.createDatabase(new DatabaseBuilder()
           .setCatalogName("hive")
-          .setName(DB1)
+          .setName(DB)
           .setDescription("description")
           .setLocation("locationurl")
           .build(conf));
-    } catch(InvalidObjectException | MetaException | InvalidOperationException xmeta) {
-      throw new PropertyException("unable to initialize server", xmeta);
+    } catch (InvalidObjectException | MetaException | InvalidOperationException xmeta) {
+      throw new PropertyException("unable to initialize store", xmeta);
     }
     return true;
   }
@@ -89,66 +105,114 @@ public class HMSPropertyStoreRemoteTest extends HMSPropertyStoreTest {
     // Events that get cleaned happen in batches of 1 to exercise batching code
     MetastoreConf.setLongVar(conf, MetastoreConf.ConfVars.EVENT_CLEAN_MAX_EVENTS, 1L);
     MetaStoreTestUtils.setConfForStandloneMode(conf);
-    if (isServerStarted) {
+    if (port > 0) {
       Assert.assertNotNull("Unable to connect to the MetaStore server", client);
       return;
     }
-    port = MetaStoreTestUtils.startMetaStoreWithRetry(HadoopThriftAuthBridge.getBridge(), conf);
+    // The server
+    port = createServer(conf);
     System.out.println("Starting MetaStore Server on port " + port);
-    isServerStarted = true;
 
-    Warehouse wh = new Warehouse(conf);
-    boolean inited = createStore(conf, wh);
-    LOG.info("MetaStore Thrift Server test initialization " + (inited? "successful":"failed"));
-    // This is default case with setugi off for both client and server
-    client = createClient();
-  }
-
-
-  protected HiveMetaStoreClient createClient() throws Exception {
-    MetastoreConf.setVar(conf, MetastoreConf.ConfVars.THRIFT_URIS, "thrift://localhost:" + port);
-    MetastoreConf.setBoolVar(conf, MetastoreConf.ConfVars.EXECUTE_SET_UGI, false);
-    HiveMetaStoreClient client = new HiveMetaStoreClient(conf);
-    return client;
+    // The store
+    boolean inited = createStore(conf);
+    LOG.info("MetaStore store initialization " + (inited ? "successful" : "failed"));
+    // The client
+    client = createClient(conf, port);
+    Assert.assertNotNull("Unable to connect to the MetaStore server", client);
   }
 
   @After
   public void tearDown() throws Exception {
-    MetaStoreTestUtils.close(port);
-    super.tearDown();
+    if (objectStore != null) {
+      objectStore.flushCache();
+      objectStore.dropDatabase("hive", DB);
+      objectStore = null;
+    }
+    if (port >= 0) {
+      MetaStoreTestUtils.close(port);
+    }
+    // Clear the SSL system properties before each test.
+    System.clearProperty(ObjectStore.TRUSTSTORE_PATH_KEY);
+    System.clearProperty(ObjectStore.TRUSTSTORE_PASSWORD_KEY);
+    System.clearProperty(ObjectStore.TRUSTSTORE_TYPE_KEY);
   }
 
-  @Test
-  public void testHMSProperties() throws Exception {
-    // configure hms
-    HMSPropertyManager.declareClusterProperty("clstrp0", STRING, "Spark");
-    HMSPropertyManager.declareDatabaseProperty("store", STRING, "ORC");
-    HMSPropertyManager.declareTableProperty("id", INTEGER, null);
-    HMSPropertyManager.declareTableProperty("name", STRING, null);
-    HMSPropertyManager.declareTableProperty("uuid", STRING, null);
-    HMSPropertyManager.declareTableProperty("fillfactor", DOUBLE, 0.75d);
-    HMSPropertyManager.declareTableProperty("creation date", DATETIME, "2023-01-06T12:16:00");
-    HMSPropertyManager.declareTableProperty("project", STRING, "Hive");
-    // link store and manager
-    PropertyStore store = new CachingPropertyStore(new JdoPropertyStore(objectStore));
-    HMSPropertyManager hms = new HMSPropertyManager(store);
-    try {
-      hms.setProperty("ser.der.id", 42);
-      hms.setProperty("ser.der.name", "serder");
-      hms.setProperty("ser.der.project", "Metastore");
-      hms.commit();
-      ((CachingPropertyStore) store).clearCache();
-      Assert.assertEquals(42, hms.getPropertyValue("ser.der.id"));
-      Assert.assertEquals("serder", hms.getPropertyValue("ser.der.name"));
-      Assert.assertEquals("Metastore", hms.getPropertyValue("ser.der.project"));
-    } finally {
+  /**
+   * A Thrift based property client.
+   */
+  static class ThriftPropertyClient implements PropertyClient {
+    private final String namespace;
+    private final HiveMetaStoreClient client;
+    ThriftPropertyClient(String ns, HiveMetaStoreClient c) {
+      this.namespace = ns;
+      this.client = c;
+    }
+
+    @Override
+    public boolean setProperties(Map<String, String> properties) {
+      try {
+        return client.setProperties(namespace, properties);
+      } catch(TException tex) {
+        return false;
+      }
+    }
+
+    @Override
+    public Map<String, Map<String, String>> getProperties(String mapPrefix, String mapPredicate, String... selection) throws IOException {
+      try {
+        return client.getProperties(namespace, mapPrefix, mapPredicate, selection);
+      } catch(TException tex) {
+        return null;
+      }
     }
   }
 
-  @Test
-  public void testOtherProperties() throws Exception {
-    final String NS = "hms1";
-    PropertyManager.declare(NS, HMSPropertyManager.class);
+  /**
+   * Creates and starts the server.
+   * @param conf
+   * @return the server port
+   * @throws Exception
+   */
+  protected int createServer(Configuration conf) throws Exception {
+    MetastoreConf.setVar(conf, MetastoreConf.ConfVars.THRIFT_TRANSPORT_MODE, "http");
+    MetastoreConf.setVar(conf, MetastoreConf.ConfVars.THRIFT_PROTOCOL, "json");
+    return MetaStoreTestUtils.startMetaStoreWithRetry(HadoopThriftAuthBridge.getBridge(), conf);
+  }
+
+  /**
+   * Creates a client.
+   * @return the client instance
+   * @throws Exception
+   */
+  protected PropertyClient createClient(Configuration conf, int port) throws Exception {
+    MetastoreConf.setVar(conf, MetastoreConf.ConfVars.THRIFT_URIS, "http://localhost:" + port);
+    MetastoreConf.setVar(conf, MetastoreConf.ConfVars.METASTORE_CLIENT_THRIFT_TRANSPORT_MODE, "http");
+    MetastoreConf.setVar(conf, MetastoreConf.ConfVars.METASTORE_CLIENT_THRIFT_PROTOCOL, "json");
+    MetastoreConf.setBoolVar(conf, MetastoreConf.ConfVars.EXECUTE_SET_UGI, false);
+    HiveMetaStoreClient client = new HiveMetaStoreClient(conf);
+    return new ThriftPropertyClient(NS, client);
+  }
+
+
+  public void runOtherProperties0(PropertyClient client) throws Exception {
+    Map<String, String> ptyMap = createProperties0(NS);
+    boolean commit = client.setProperties(ptyMap);
+    Assert.assertTrue(commit);
+    // select tables whose policy table name starts with table0
+    Map<String, Map<String, String>> maps = client.getProperties("db0.table", "policy.'Table-name' =^ 'table0'");
+    Assert.assertNotNull(maps);
+    Assert.assertEquals(8, maps.size());
+
+    // select
+    Map<String, Map<String, String>> project = client.getProperties("db0.tabl", "fillFactor > 92",
+        "fillFactor",
+        "{ 'policy' : { 'Compaction' : { 'target-size' : policy.Compaction.'target-size' } } }");
+    Assert.assertNotNull(project);
+    Assert.assertEquals(2, project.size());
+  }
+
+  static Map<String, String> createProperties0(String ns) {
+    PropertyManager.declare(ns, HMSPropertyManager.class);
     // configure hms
     HMSPropertyManager.declareTableProperty("fillFactor", DOUBLE, 0.75d);
     HMSPropertyManager.declareTableProperty("policy", JSON, null);
@@ -157,7 +221,7 @@ public class HMSPropertyStoreRemoteTest extends HMSPropertyStoreTest {
     // link store and manager
     try {
       String json = IOUtils.toString(
-          this.getClass().getResourceAsStream("pol0.json"),
+          HMSDirectTest.class.getResourceAsStream("pol0.json"),
           "UTF-8"
       );
       JxltEngine JXLT = JEXL.createJxltEngine();
@@ -181,28 +245,23 @@ public class HMSPropertyStoreRemoteTest extends HMSPropertyStoreTest {
         Assert.assertNotNull(type);
         ptyMap.put(tb + "maint_operation", type.toString());
       }
-      boolean commit = client.setProperties(NS, ptyMap);
-      Assert.assertTrue(commit);
-      // select tables whose policy table name starts with table0
-      Map<String, Map<String, String>> maps = client.getProperties(NS, "db0.table", "policy.'Table-name' =^ 'table0'");
-      Assert.assertNotNull(maps);
-      Assert.assertEquals(8, maps.size());
-
-      // select
-      Map<String, Map<String, String>> project = client.getProperties( NS, "db0.tabl", "fillFactor > 92",
-          "fillFactor",
-                   "{ 'policy' : { 'Compaction' : { 'target-size' : policy.Compaction.'target-size' } } }");
-      Assert.assertNotNull(project);
-      Assert.assertEquals(2, project.size());
-    } finally {
+      return ptyMap;
+    } catch (IOException xio) {
+      return null;
     }
   }
 
+  public void runOtherProperties1(PropertyClient client) throws Exception {
+    Map<String, String> ptyMap = createProperties1(NS);
+    boolean commit = client.setProperties(ptyMap);
+    Assert.assertTrue(commit);
+    // go get some
+    Map<String, Map<String, String>> maps = client.getProperties("db0.table", "someb && fillFactor < 95");
+    Assert.assertNotNull(maps);
+  }
 
-  @Test
-  public void testPropertiesScript0() throws Exception {
-    final String NS = "hms2";
-    PropertyManager.declare(NS, HMSPropertyManager.class);
+  static Map<String, String> createProperties1(String ns) {
+    PropertyManager.declare(ns, HMSPropertyManager.class);
     // configure hms
     HMSPropertyManager.declareTableProperty("id", INTEGER, null);
     HMSPropertyManager.declareTableProperty("name", STRING, null);
@@ -221,13 +280,9 @@ public class HMSPropertyStoreRemoteTest extends HMSPropertyStoreTest {
       ptys.put(tb + "id", Integer.toString(1000 + i));
       ptys.put(tb + "name", "TABLE_" + i);
       ptys.put(tb + "fillFactor", Integer.toString(100 - i));
-      ptys.put(tb + "someb", (i % 2) == 0? "true" :"false");
+      ptys.put(tb + "someb", (i % 2) == 0 ? "true" : "false");
     }
-    boolean commit = client.setProperties(NS, ptys);
-    Assert.assertTrue(commit);
-    // go get some
-    Map<String, Map<String, String>> maps = client.getProperties(NS, "db0.table", "someb && fillFactor < 95");
-    Assert.assertNotNull(maps);
+    return ptys;
   }
 
 }
