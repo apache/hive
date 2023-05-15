c28fc68f2b5be4c2babd9bdc077a4a8988cee46a
tarak271 <tarakaramarao.lethavadla@gmail.com>
2023-05-15 22:27:15 +0530

    HIVE-27118 - implement array_intersect UDF in Hive (#4094) (Taraka Rama Rao Lethavadla, reviewed by Sourabh Badhya, Sai Hemanth G)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/FunctionRegistry.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/FunctionRegistry.java
@@ -605,6 +605,7 @@ public final class FunctionRegistry {
     system.registerGenericUDF("array_distinct", GenericUDFArrayDistinct.class);
     system.registerGenericUDF("array_join", GenericUDFArrayJoin.class);
     system.registerGenericUDF("array_slice", GenericUDFArraySlice.class);
+    system.registerGenericUDF("array_intersect", GenericUDFArrayIntersect.class);
     system.registerGenericUDF("deserialize", GenericUDFDeserialize.class);
     system.registerGenericUDF("sentences", GenericUDFSentences.class);
     system.registerGenericUDF("map_keys", GenericUDFMapKeys.class);
