e56befe6f526f5f06b149e7a962e5b6500cd4fc3
zhangbutao <zhangbutao@cmss.chinamobile.com>
2023-06-05 17:55:12 +0800

    Added Deprecated on isMetadataTableSupported()

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/HiveStorageHandler.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/HiveStorageHandler.java
@@ -550,6 +550,20 @@ public interface HiveStorageHandler extends Configurable {
     return false;
   }
 
+  /**
+   * Introduced by HIVE-25457 for iceberg to query metadata table.
+   * @return true if the storage handler can support it
+   * @deprecated Use {@link #isTableMetaRefSupported()}
+   */
+  @Deprecated
+  default boolean isMetadataTableSupported() {
+    return false;
+  }
+
+  /**
+   * Check whether the table supports metadata references which mainly include branch, tag and metadata tables.
+   * @return true if the storage handler can support it
+   */
   default boolean isTableMetaRefSupported() {
     return false;
   }
