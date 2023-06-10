d734992c3ba5a412273ea9c54d90e38a3d461e55
Steve Carlin <scarlin@cloudera.com>
2023-06-10 09:07:28 -0700

    HIVE-27399: Add support for lateral views with CBO (Stephen Carlin reviewed by Krisztian Kasa, Stamatis Zampetakis)
    Previously, the CBO step was skipped when query contained lateral views.
    
    Among the changes that needed to be done:
    
    When the HiveTableFunctionScan is created for a lateral view, all of the inputs from the RelNode are mapped into the new "lateral() function, along with the function containing the lateral view. So for example, if the syntax for the SQL were ...LATERAL VIEW explode(field1) on a table with field1 and field2 where field1 is an array, the resulting HiveTableFunctionScan function would be lateral(explode(field1), field1, field2).
    
    The ASTConverter needed to be changed to handle conversion of the lateral view back into an AST that contains the LATERAL_VIEW node. The non-"lateral view" HiveTableFunctionScan still generates an AST without the LATERAL_VIEW node which is more optimal. The ASTConverter checks to see if the HiveTableFunctionScan contains the a "LATERAL" function in order to keep the LATERAL_VIEW node.
    
    The OpProcFactory code needed to be changed in order to continue to allow a filter optimization. There is pushdown (ppd) logic that was being hit when there was no CBO for lateral views. CBO runs a different path for the ppd logic which is in the OpProcFactory. The lateral view contains 2 node paths that are joined together. The filter can be pushed down the path that contains the base table, but not the path that contains the UDTF. By adding this logic, it prevents a performance regression.
    
    A new rule was also needed for CBO. There is a HiveFilter-HiveTableFunctionScan transpose rule. These RelNodes can be transposed when there is at least one AND condition that can be pushed through the HiveTableFunctionScan. A condition can be pushed through if there are no inputRefs that rely on the output of the UDTF function. We cannot use the Calcite FilterTableScanTransposeRule since that rule uses LogicalFilter and LogicalTableFunctionScan which do not exist in the HiveFilter and HiveTableFunctionScan
    
    The hive.cbo.fallback.strategy property was set to NEVER in some tests involving lateral views to ensure that CBO runs fine without errors.
    
    The q.out changes due to this commit are highlighted below.
    
    In some places, an additional "Select Operator" was added. This should not have any performance impact.
    
    Small changes (UDFToInteger, IF CASE transformation, etc.,) in plans since lateral view queries are now pass through CBO.
    
    In some cases lineage information is lost due to constant folding perfomed in CBO. This is not a regression but something that was always there before, since we don't output lineage for constants (HIVE-27726).
    
    Minor column name changes in plans.
    
    Minor stat changes most likely cause we pass through CBO.
    
    Close apache/hive#4442

--- ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
@@ -142,7 +142,6 @@ import org.apache.hadoop.hive.ql.ErrorMsg;
 import org.apache.hadoop.hive.ql.QueryProperties;
 import org.apache.hadoop.hive.ql.QueryState;
 import org.apache.hadoop.hive.ql.exec.ColumnInfo;
-import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.FunctionInfo;
 import org.apache.hadoop.hive.ql.exec.FunctionRegistry;
 import org.apache.hadoop.hive.ql.exec.Operator;
@@ -223,6 +222,7 @@ import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveFilterProjectTransp
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveFilterSetOpTransposeRule;
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveFilterSortPredicates;
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveFilterSortTransposeRule;
+import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveFilterTableFunctionTransposeRule;
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveInBetweenExpandRule;
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveInsertExchange4JoinRule;
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.HiveIntersectMergeRule;
@@ -311,9 +311,7 @@ import org.apache.hadoop.hive.ql.plan.mapper.EmptyStatsSource;
 import org.apache.hadoop.hive.ql.plan.mapper.StatsSource;
 import org.apache.hadoop.hive.ql.reexec.ReCompileException;
 import org.apache.hadoop.hive.ql.session.SessionState;
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDFArray;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDTFInline;
 import org.apache.hadoop.hive.ql.util.DirectionUtils;
 import org.apache.hadoop.hive.ql.util.NullOrdering;
 import org.apache.hadoop.hive.serde2.Deserializer;
@@ -1024,30 +1022,8 @@ public class CalcitePlanner extends SemanticAnalyzer {
 
   @Override
   boolean isCBOSupportedLateralView(ASTNode lateralView) {
-    // Lateral view AST has the following shape:
-    // ^(TOK_LATERAL_VIEW
-    //   ^(TOK_SELECT ^(TOK_SELEXPR ^(TOK_FUNCTION Identifier params) identifier* tableAlias)))
-    if (lateralView.getToken().getType() == HiveParser.TOK_LATERAL_VIEW_OUTER) {
-      // LATERAL VIEW OUTER not supported in CBO
-      return false;
-    }
-    // Only INLINE followed by ARRAY supported in CBO
-    ASTNode lvFunc = (ASTNode) lateralView.getChild(0).getChild(0).getChild(0);
-    String lvFuncName = lvFunc.getChild(0).getText();
-    if (lvFuncName.compareToIgnoreCase(
-            GenericUDTFInline.class.getAnnotation(Description.class).name()) != 0) {
-      return false;
-    }
-    if (lvFunc.getChildCount() != 2) {
-      return false;
-    }
-    ASTNode innerFunc = (ASTNode) lvFunc.getChild(1);
-    if (innerFunc.getToken().getType() != HiveParser.TOK_FUNCTION ||
-            innerFunc.getChild(0).getText().compareToIgnoreCase(
-                GenericUDFArray.class.getAnnotation(Description.class).name()) != 0) {
-      return false;
-    }
-    return true;
+    // LATERAL VIEW OUTER not supported in CBO
+    return lateralView.getToken().getType() != HiveParser.TOK_LATERAL_VIEW_OUTER;
   }
 
   @Override
@@ -1838,6 +1814,7 @@ public class CalcitePlanner extends SemanticAnalyzer {
       } else {
         rules.add(HiveFilterProjectTransposeRule.DETERMINISTIC);
       }
+      rules.add(HiveFilterTableFunctionTransposeRule.INSTANCE);
       rules.add(HiveOptimizeInlineArrayTableFunctionRule.INSTANCE);
       rules.add(HiveFilterSetOpTransposeRule.INSTANCE);
       rules.add(HiveFilterSortTransposeRule.INSTANCE);
