7aa2df1063ce77cf9a3eb9b621e5fdcfbf8fe60c
Denys Kuzmenko <deniskuzZ@gmail.com>
2023-06-10 12:31:25 +0300

    HIVE-27416: Vectorization: ClassCastException for string to decimal64 conversion (Denys Kuzmenko, reviewed by Attila Turoczy, Krisztian Kasa)
    Closes #4391

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizationContext.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizationContext.java
@@ -3323,8 +3323,12 @@ import com.google.common.annotations.VisibleForTesting;
             returnType, DataTypePhysicalVariation.NONE);
       }
     } else if (isStringFamily(inputType)) {
+      DataTypePhysicalVariation dataTypePhysicalVariation =
+          tryDecimal64Cast && ((DecimalTypeInfo) returnType).precision() <= 18 ?
+              DataTypePhysicalVariation.DECIMAL_64 : 
+              DataTypePhysicalVariation.NONE;
       return createVectorExpression(CastStringToDecimal.class, childExpr, VectorExpressionDescriptor.Mode.PROJECTION,
-          returnType, DataTypePhysicalVariation.NONE);
+          returnType, dataTypePhysicalVariation);
     } else if (inputType.equals("timestamp")) {
       return createVectorExpression(CastTimestampToDecimal.class, childExpr, VectorExpressionDescriptor.Mode.PROJECTION,
           returnType, DataTypePhysicalVariation.NONE);
---
+++ ql/src/test/queries/clientpositive/vector_string_decimal64.q
@@ -0,0 +1,16 @@
+-- Mask the totalSize value as it can have slight variability, causing test flakiness
+--! qt:replace:/(\s+totalSize\s+)\S+(\s+)/$1#Masked#$2/
+set hive.explain.user=false;
+set hive.fetch.task.conversion=none;
+
+create external table parquet_decimal64(salary string) stored as parquet;      
+insert into parquet_decimal64 values ('0'), ('43.5'), ('144.2'), ('45.7'), ('100');
+
+explain select * from parquet_decimal64 
+    where cast(salary as decimal(12, 2)) <= 100 and cast(salary as decimal(12, 2)) >= 0.0;  
+    
+-- SORT_QUERY_RESULTS    
+select * from parquet_decimal64 
+    where cast(salary as decimal(12, 2)) <= 100 and cast(salary as decimal(12, 2)) >= 0.0;
+    
+drop table parquet_decimal64;
