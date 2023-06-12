3d991da1b149b506c67e0ee450208ea54d0dde95
Sourabh Badhya <iamsbadhya@gmail.com>
2023-06-12 21:14:56 +0530

    HIVE-27332: Addendum: Add retry backoff mechanism for abort cleanup

--- ql/src/test/org/apache/hadoop/hive/ql/txn/compactor/TestCompactionMetrics.java
+++ ql/src/test/org/apache/hadoop/hive/ql/txn/compactor/TestCompactionMetrics.java
@@ -57,7 +57,6 @@ import org.apache.hadoop.hive.metastore.txn.TxnStore;
 import org.apache.hadoop.hive.metastore.txn.TxnUtils;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -986,7 +985,6 @@ public class TestCompactionMetrics  extends CompactorTest {
   }
 
   @Test
-  @Ignore("HIVE-27442")
   public void testCleanerFailuresCountedCorrectly() throws Exception {
     final String DEFAULT_DB = "default";
     final String SUCCESS_TABLE_NAME = "success_table";
