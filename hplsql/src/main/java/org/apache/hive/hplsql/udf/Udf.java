55eafa5944fe3386e9b0b0b83fcc3dcebf61b9c4
Soumyakanti Das <soumyakanti.das@cloudera.com>
2023-06-15 20:44:58 -0700

    HIVE-27349: Move HMS call to get stored procedure out of Udf (Soumyakanti Das, reviewed by Dayakar M, Krisztian Kasa)

--- hplsql/src/main/java/org/apache/hive/hplsql/udf/Udf.java
+++ hplsql/src/main/java/org/apache/hive/hplsql/udf/Udf.java
@@ -19,16 +19,12 @@
 package org.apache.hive.hplsql.udf;
 
 import org.apache.hadoop.hive.common.type.*;
-import org.apache.hadoop.hive.metastore.api.StoredProcedure;
-import org.apache.hadoop.hive.metastore.api.StoredProcedureRequest;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.session.SessionState;
 import org.apache.hadoop.hive.ql.udf.UDFType;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
-import org.apache.hadoop.hive.serde2.objectinspector.ConstantObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.*;
@@ -37,22 +33,24 @@ import org.apache.hive.hplsql.Exec;
 import org.apache.hive.hplsql.Scope;
 import org.apache.hive.hplsql.Var;
 import org.apache.hive.hplsql.executor.QueryExecutor;
-import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Description(name = "hplsql", value = "_FUNC_('query' [, :1, :2, ...n]) - Execute HPL/SQL query", extended = "Example:\n" + " > SELECT _FUNC_('CURRENT_DATE') FROM src LIMIT 1;\n")
+@Description(name = "hplsql", value = "_FUNC_('query' [, :1, :2, ...n], 'storedProcedure') - Execute HPL/SQL query",
+        extended = "Example:\n" + " > SELECT _FUNC_('CURRENT_DATE') FROM src LIMIT 1;\n")
 @UDFType(deterministic = false)
 public class Udf extends GenericUDF {
+  private static final Logger LOG = LoggerFactory.getLogger(Udf.class.getName());
   public static String NAME = "hplsql";
-  transient Exec exec;
-  StringObjectInspector queryOI;
-  ObjectInspector[] argumentsOI;
-  private String functionDefinition;
+  private final transient Exec exec = new Exec();
+  private StringObjectInspector queryOI;
+  private ObjectInspector[] argumentsOI;
+  private StringObjectInspector funcDefOI;
+  private String functionDefinition = null;
 
   public Udf() {
-  }
-
-  public Udf(Exec exec) {
-    this.exec = exec;
+    exec.setQueryExecutor(QueryExecutor.DISABLED);
+    exec.init();
   }
 
   /**
@@ -60,40 +58,19 @@ public class Udf extends GenericUDF {
    */
   @Override
   public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
-    if (arguments.length == 0) {
-      throw new UDFArgumentLengthException("At least one argument must be specified");
+    if (arguments.length < 2) {
+      throw new UDFArgumentLengthException("At least two arguments must be specified");
     }
     if (!(arguments[0] instanceof StringObjectInspector)) {
       throw new UDFArgumentException("First argument must be a string");
     }
-    SessionState sessionState = SessionState.get();
-    if (sessionState != null) {
-      // we are still in HiveServer, get the source of the HplSQL function and store it.
-      functionDefinition = loadSource(sessionState, functionName(arguments[0]));
+    if (!(arguments[arguments.length-1] instanceof StringObjectInspector)) {
+      throw new UDFArgumentException("Last argument (stored procedure) must be a string");
     }
     queryOI = (StringObjectInspector)arguments[0];
+    funcDefOI = (StringObjectInspector)arguments[arguments.length-1];
     argumentsOI = arguments;
     return PrimitiveObjectInspectorFactory.javaStringObjectInspector;
-  }
-
-  protected String loadSource(SessionState sessionState, String functionName) throws UDFArgumentException {
-    Exec exec = sessionState.getDynamicVar(Exec.class);
-    try {
-      StoredProcedure storedProcedure = exec.getMsc().getStoredProcedure(
-              new StoredProcedureRequest(
-                      SessionState.get().getCurrentCatalog(),
-                      SessionState.get().getCurrentDatabase(),
-                      functionName));
-      return storedProcedure != null ? storedProcedure.getSource() : null;
-    } catch (TException e) {
-      throw new UDFArgumentException(e);
-    }
-  }
-
-  protected String functionName(ObjectInspector argument) {
-    ConstantObjectInspector inspector = (ConstantObjectInspector) (argument);
-    String functionCall = inspector.getWritableConstantValue().toString();
-    return functionCall.split("\\(")[0].toUpperCase();
   }
 
   /**
@@ -101,13 +78,12 @@ public class Udf extends GenericUDF {
    */
   @Override
   public Object evaluate(DeferredObject[] arguments) throws HiveException {
-    if (exec == null) {
-      exec = new Exec();
-      exec.setQueryExecutor(QueryExecutor.DISABLED);
-      exec.init();
-      if (functionDefinition != null) { // if it's null, it can be a built-in function
-        exec.parseAndEval(Arguments.script(functionDefinition));
-      }
+    if (functionDefinition == null) { // if it's null, it can be a built-in function
+      int idx = arguments.length-1;
+      setParameterForPrimitiveTypeArgument(":" + idx, arguments[idx].get(), funcDefOI);
+      functionDefinition = funcDefOI.getPrimitiveJavaObject(arguments[idx].get());
+      LOG.debug("functionDefinition: {}", functionDefinition);
+      exec.parseAndEval(Arguments.script(functionDefinition));
     }
     exec.enterScope(Scope.Type.ROUTINE);
     setParameters(arguments);
@@ -122,10 +98,19 @@ public class Udf extends GenericUDF {
   }
 
   /**
+   * Getter for Exec object
+   *
+   * @return Exec
+   */
+  public Exec getExec() {
+    return this.exec;
+  }
+
+  /**
    * Set parameters for the current call
    */
   void setParameters(DeferredObject[] arguments) throws HiveException {
-    for (int i = 1; i < arguments.length; i++) {
+    for (int i = 1; i < arguments.length - 1; i++) {
       String name = ":" + i;
       Object inputObject = arguments[i].get();
       ObjectInspector objectInspector = argumentsOI[i];
