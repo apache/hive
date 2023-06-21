92ccda4c1770fda5f53294fbc319374f34a1e323
Zsolt Miskolczi <zsmiskolczi@cloudera.com>
2023-06-21 13:17:40 +0200

    Fix typo in Metastore

--- iceberg/iceberg-catalog/src/main/java/org/apache/iceberg/hive/HiveCatalog.java
+++ iceberg/iceberg-catalog/src/main/java/org/apache/iceberg/hive/HiveCatalog.java
@@ -275,12 +275,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
             namespace);
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to create namespace " + namespace + " in Hive Matastore", e);
+      throw new RuntimeException("Failed to create namespace " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to createDatabase(name) " + namespace + " in Hive Matastore", e);
+          "Interrupted in call to createDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -302,12 +302,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
       return namespaces;
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to list all namespace: " + namespace + " in Hive Matastore",  e);
+      throw new RuntimeException("Failed to list all namespace: " + namespace + " in Hive Metastore",  e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to getAllDatabases() " + namespace + " in Hive Matastore", e);
+          "Interrupted in call to getAllDatabases() " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -336,12 +336,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
       return false;
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to drop namespace " + namespace + " in Hive Matastore", e);
+      throw new RuntimeException("Failed to drop namespace " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to drop dropDatabase(name) " + namespace + " in Hive Matastore", e);
+          "Interrupted in call to drop dropDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -397,11 +397,11 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
 
     } catch (TException e) {
       throw new RuntimeException(
-          "Failed to list namespace under namespace: " + namespace + " in Hive Matastore", e);
+          "Failed to list namespace under namespace: " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
-      throw new RuntimeException("Interrupted in call to getDatabase(name) " + namespace + " in Hive Matastore", e);
+      throw new RuntimeException("Interrupted in call to getDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -421,12 +421,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
       throw new NoSuchNamespaceException(e, "Namespace does not exist: %s", namespace);
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to list namespace under namespace: " + namespace + " in Hive Matastore", e);
+      throw new RuntimeException("Failed to list namespace under namespace: " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to getDatabase(name) " + namespace + " in Hive Matastore", e);
+          "Interrupted in call to getDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
