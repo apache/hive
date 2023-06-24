e77e4129885b1504563ce066098fb6488dce65bd
Simhadri Govindappa <simhadri064@gmail.com>
2023-06-24 22:54:36 +0530

    HIVE-26832: Add "SHOW PARTITIONS" support for Iceberg tables (Simhadri Govindappa, reviewed by Attila Turoczy, Denys Kuzmenko, okumin)
    Closes #4346

--- common/src/java/org/apache/hadoop/hive/conf/Constants.java
+++ common/src/java/org/apache/hadoop/hive/conf/Constants.java
@@ -106,4 +106,8 @@ public class Constants {
 
   public static final String HTTP_HEADER_REQUEST_TRACK = "X-Request-ID";
   public static final String TIME_POSTFIX_REQUEST_TRACK = "_TIME";
+
+  public static final String ICEBERG = "iceberg";
+  public static final String ICEBERG_PARTITION_TABLE_SCHEMA = "partition,record_count,file_count,spec_id";
+  public static final String DELIMITED_JSON_SERDE = "org.apache.hadoop.hive.serde2.DelimitedJSONSerDe";
 }
