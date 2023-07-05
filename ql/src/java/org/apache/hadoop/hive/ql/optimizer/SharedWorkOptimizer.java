6e9d197a24a1dcda769e3019551f28da1e424f45
amanraj2520 <rajaman@microsoft.com>
2023-07-05 14:46:15 +0530

    Revert "HIVE-22227: Tez bucket pruning produces wrong result with shared work optimization (Vineet Garg, reviewed by Jesus Camacho Rodriguez)"
    This reverts commit 02ed079ef0db34f79c02dd32ad5d6a2cfa7ceb4a.

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
@@ -749,17 +749,6 @@ public class SharedWorkOptimizer extends Transform {
     if (!prevTsOpPPList.getPartitions().equals(tsOpPPList.getPartitions())) {
       return false;
     }
-
-    if(!Objects.equals(tsOp1.getConf().getIncludedBuckets(),
-            tsOp2.getConf().getIncludedBuckets())) {
-      return false;
-    }
-
-    return true;
-  }
-
-  private static boolean areMergeableExtendedCheck(ParseContext pctx, SharedWorkOptimizerCache optimizerCache,
-      TableScanOperator tsOp1, TableScanOperator tsOp2) throws SemanticException {
     // If is a DPP, check if actually it refers to same target, column, etc.
     // Further, the DPP value needs to be generated from same subtree
     List<Operator<?>> dppsOp1 = new ArrayList<>(optimizerCache.tableScanToDPPSource.get(tsOp1));
@@ -1166,7 +1155,6 @@ public class SharedWorkOptimizer extends Transform {
           && pctx.getPrunedPartitions(tsOp1).getPartitions().equals(
               pctx.getPrunedPartitions(tsOp2).getPartitions())
           && op1Conf.getRowLimit() == op2Conf.getRowLimit()
-          && Objects.equals(op1Conf.getIncludedBuckets(), op2Conf.getIncludedBuckets())
           && Objects.equals(op1Conf.getOpProps(), op2Conf.getOpProps())) {
         return true;
       } else {
