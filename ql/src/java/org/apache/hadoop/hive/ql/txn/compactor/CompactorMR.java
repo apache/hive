difin/difin_hive

HIVE-21280 : Null pointer exception on running compaction against a MM table. (Aditya Shah via Ashutosh Chauhan)
Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorMR.java
@@ -446,7 +446,7 @@ public class CompactorMR {
       //thinking it already has a txn opened
 
       String user = UserGroupInformation.getCurrentUser().getShortUserName();
-      SessionState sessionState = DriverUtils.setUpSessionState(driverConf, user, false);
+      SessionState sessionState = DriverUtils.setUpSessionState(driverConf, user, true);
 
       // Note: we could skip creating the table and just add table type stuff directly to the
       //       "insert overwrite directory" command if there were no bucketing or list bucketing.

apache/hive

(cherry picked from commit 0dd45a2c59939e0d28b4690952456e67c305cfce)

ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorMR.java
@@ -354,7 +354,7 @@ public class CompactorMR {
       conf.set(ConfVars.HIVE_QUOTEDID_SUPPORT.varname, "column");
 
       String user = UserGroupInformation.getCurrentUser().getShortUserName();
-      SessionState sessionState = DriverUtils.setUpSessionState(conf, user, false);
+      SessionState sessionState = DriverUtils.setUpSessionState(conf, user, true);
 
       // Note: we could skip creating the table and just add table type stuff directly to the
       //       "insert overwrite directory" command if there were no bucketing or list bucketing.