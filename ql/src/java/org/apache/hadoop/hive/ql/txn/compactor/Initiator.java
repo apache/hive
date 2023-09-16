b1d1550f8b9d2b6488fb8222fcaa0bf5fdb70179
Aman Raj <104416558+amanraj2520@users.noreply.github.com>
2023-09-16 14:04:54 +0530

    HIVE-27644: Backport of HIVE-17917, HIVE-21457, HIVE-22582 into branch-3 (Aman Raj, reviewed by Sankar Hariappan)
    * HIVE-17917: VectorizedOrcAcidRowBatchReader.computeOffsetAndBucket optimization (Saurabh Seth via Eugene Koifman)
    (cherry picked from commit 34331f3c7b69200a0177f5446f1f15c8ed69ee86)
    Resolved merge conflict in VectorizedOrcAcidRowBatchReader.java
    
    * HIVE-21457: Perf optimizations in ORC split-generation (Prasanth Jayachandran reviewed by Gopal V)
    (cherry picked from commit 72d72d4df734ccc653a0a6986c319200dea35f0b)
    Resolved conflicts in AcidUtils.java, CompactorMR.java and OrcInputFormat.java
    
    * HIVE-22582: Avoid reading table as ACID when table name is starting with "delta" , but table is not transactional and BI Split Strategy is used (Aditya Shah reviewed by Laszlo Pinter and Peter Vary)
    (cherry picked from commit e6ef2826879fbb9b3ec7987255dda8ec14831a05)
    
    Signed-off-by: Sankar Hariappan <sankarh@apache.org>
    Closes (#4686)

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Initiator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Initiator.java
@@ -269,7 +269,7 @@ public class Initiator extends CompactorThread {
     boolean noBase = false;
     Path location = new Path(sd.getLocation());
     FileSystem fs = location.getFileSystem(conf);
-    AcidUtils.Directory dir = AcidUtils.getAcidState(location, conf, writeIds, false, false);
+    AcidUtils.Directory dir = AcidUtils.getAcidState(fs, location, conf, writeIds, false, false);
     Path base = dir.getBaseDirectory();
     long baseSize = 0;
     FileStatus stat = null;
