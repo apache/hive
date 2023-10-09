5eebd5147889b2a523ffab779155439daf3ad3e7
Soumyakanti Das <soumyakanti.das@cloudera.com>
2023-10-09 11:26:14 -0700

    HIVE-27761: Addendum: Compilation of nested CTEs throws SemanticException
    Cycle was getting detected incorrectly because the cte names were not scoped.
    In this patch, we are adding the query block id to the cte name to differentiate
    ctes with same names but different scopes.

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -1345,9 +1345,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       ASTNode cteQry = (ASTNode) cte.getChild(0);
       String alias = unescapeIdentifier(cte.getChild(1).getText());
       ASTNode withColList = cte.getChildCount() == 3 ? (ASTNode) cte.getChild(2) : null;
-
-      String qName = qb.getId() == null ? "" : qb.getId() + ":";
-      qName += alias.toLowerCase();
+      String qName = getAliasId(alias, qb);
 
       if ( aliasToCTEs.containsKey(qName)) {
         throw new SemanticException(ASTErrorUtils.getMsg(
@@ -2241,11 +2239,6 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
 
       CTEClause cte = findCTEFromName(qb, cteName, materializationAliasToCTEs);
       if (cte != null) {
-        if (ctesExpanded.contains(cteName)) {
-          throw new SemanticException("Recursive cte " + cteName +
-              " detected (cycle: " + StringUtils.join(ctesExpanded, " -> ") +
-              " -> " + cteName + ").");
-        }
         cte.reference++;
         current.parents.add(cte);
         if (cte.qbExpr != null) {
@@ -2253,10 +2246,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
         }
         cte.qbExpr = new QBExpr(cteName);
         doPhase1QBExpr(cte.cteNode, cte.qbExpr, qb.getId(), cteName, cte.withColList, materializationAliasToCTEs);
-
-        ctesExpanded.add(cteName);
         gatherCTEReferences(cte.qbExpr, cte, materializationAliasToCTEs);
-        ctesExpanded.remove(ctesExpanded.size() - 1);
       }
     }
     for (String alias : qb.getSubqAliases()) {
@@ -2264,6 +2254,20 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     }
     for (String alias : qb.getSubqExprAliases()) {
       gatherCTEReferences(qb.getSubqExprForAlias(alias), current, materializationAliasToCTEs);
+    }
+  }
+
+  private void checkRecursiveCTE(CTEClause current, Set<String> path) throws SemanticException {
+
+    for (CTEClause child : current.parents) {
+      if (path.contains(child.alias)) {
+        throw new SemanticException("Recursive cte " + child.alias +
+            " detected (cycle: " + StringUtils.join(path, " -> ") +
+            " -> " + child.alias + ").");
+      }
+      path.add(child.alias);
+      checkRecursiveCTE(child, path);
+      path.remove(child.alias);
     }
   }
 
@@ -2277,6 +2281,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       if (enableMaterialization) {
         materializationAliasToCTEs = getMaterializationMetadata(qb);
       }
+      checkRecursiveCTE(rootClause, new HashSet<>());
       getMetaData(qb, null);
       if (materializationAliasToCTEs != null && !materializationAliasToCTEs.isEmpty()) {
         this.aliasToCTEs.putAll(materializationAliasToCTEs);
