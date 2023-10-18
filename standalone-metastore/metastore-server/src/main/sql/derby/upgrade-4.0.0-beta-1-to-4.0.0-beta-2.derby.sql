b749aa354551851b57f324181d1555b190c0ee1d
saihemanth <saihemanth@cloudera.com>
2023-10-18 15:05:22 -0700

    remove index as it is not utilized by DBMS

--- standalone-metastore/metastore-server/src/main/sql/derby/upgrade-4.0.0-beta-1-to-4.0.0-beta-2.derby.sql
+++ standalone-metastore/metastore-server/src/main/sql/derby/upgrade-4.0.0-beta-1-to-4.0.0-beta-2.derby.sql
@@ -1,5 +1,2 @@
--- HIVE-27499
-CREATE UNIQUE INDEX "APP"."NOTIFICATION_LOG_UNIQUE_DB" ON "APP"."NOTIFICATION_LOG" ("DB_NAME", "EVENT_ID");
-
 -- This needs to be the last thing done.  Insert any changes above this line.
 UPDATE "APP".VERSION SET SCHEMA_VERSION='4.0.0-beta-2', VERSION_COMMENT='Hive release version 4.0.0-beta-2' where VER_ID=1;
