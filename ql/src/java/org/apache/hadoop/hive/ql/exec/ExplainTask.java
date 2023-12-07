62f594a54d02d65095f883ca8627899f739075c4
Shubham Sharma <shubh.luck@yahoo.in>
2023-12-08 02:49:27 +0530

    HIVE-27240: NPE on Hive Hook Proto Log Writer. (#4925). (Shubham Sharma, reviewed by Ayush Saxena)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/ExplainTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/ExplainTask.java
@@ -950,7 +950,7 @@ public class ExplainTask extends Task<ExplainWork> implements Serializable {
   JSONObject outputPlan(Object work, PrintStream out,
       boolean extended, boolean jsonOutput, int indent, String appendToHeader) throws Exception {
     return outputPlan(work, out, extended, jsonOutput, indent, appendToHeader,
-            queryState.getConf().getBoolVar(ConfVars.HIVE_IN_TEST));
+              queryState != null && queryState.getConf().getBoolVar(ConfVars.HIVE_IN_TEST));
   }
 
   public JSONObject outputPlan(Object work, PrintStream out,
