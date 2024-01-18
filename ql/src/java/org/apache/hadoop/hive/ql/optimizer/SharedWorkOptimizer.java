74faf7118f6d2879087fd7210f7727957160c88f
kasakrisz <kasakrisz2@gmail.com>
2024-01-18 10:19:15 +0100

    HIVE-28009: Shared work optimizer ignores schema merge setting in case of virtual column difference

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
@@ -650,7 +650,8 @@ public class SharedWorkOptimizer extends Transform {
 
   private static boolean compatibleSchema(TableScanOperator tsOp1, TableScanOperator tsOp2) {
     return tsOp1.getNeededColumns().equals(tsOp2.getNeededColumns())
-        && tsOp1.getNeededColumnIDs().equals(tsOp2.getNeededColumnIDs());
+        && tsOp1.getNeededColumnIDs().equals(tsOp2.getNeededColumnIDs())
+        && tsOp1.getConf().getVirtualCols().equals(tsOp2.getConf().getVirtualCols());
   }
 
 
