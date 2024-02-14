7792841f3fe71c53a4d7c8fea992070d3283b0f4
kuczoram <kuczoram@kuczoram-loaner.local>
2024-02-14 15:20:29 +0100

    HIVE-27956: Query based compactor implementation separation

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/MmMajorQueryCompactor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/MmMajorQueryCompactor.java
@@ -82,13 +82,12 @@ final class MmMajorQueryCompactor extends QueryCompactor {
   private List<String> getCreateQueries(String tmpTableName, Table table,
       StorageDescriptor storageDescriptor, String baseLocation) {
     return Lists.newArrayList(
-        new CompactionQueryBuilder(
-            CompactionType.MAJOR,
-            CompactionQueryBuilder.Operation.CREATE,
-            true,
-            tmpTableName)
-            .setSourceTab(table)
+        compactionQueryBuilderFactory.getCompactionQueryBuilder(
+            CompactionType.MAJOR, true)
+            .setOperation(CompactionQueryBuilder.Operation.CREATE)
+            .setResultTableName(tmpTableName)
             .setStorageDescriptor(storageDescriptor)
+            .setSourceTab(table)
             .setLocation(baseLocation)
             .build()
     );
@@ -96,11 +95,10 @@ final class MmMajorQueryCompactor extends QueryCompactor {
 
   private List<String> getCompactionQueries(Table t, Partition p, String tmpName) {
     return Lists.newArrayList(
-        new CompactionQueryBuilder(
-            CompactionType.MAJOR,
-            CompactionQueryBuilder.Operation.INSERT,
-            true,
-            tmpName)
+        compactionQueryBuilderFactory.getCompactionQueryBuilder(
+            CompactionType.MAJOR, true)
+            .setOperation(CompactionQueryBuilder.Operation.INSERT)
+            .setResultTableName(tmpName)
             .setSourceTab(t)
             .setSourcePartition(p)
             .build()
@@ -109,10 +107,10 @@ final class MmMajorQueryCompactor extends QueryCompactor {
 
   private List<String> getDropQueries(String tmpTableName) {
     return Lists.newArrayList(
-        new CompactionQueryBuilder(
-            CompactionType.MAJOR,
-            CompactionQueryBuilder.Operation.DROP,
-            true,
-            tmpTableName).build());
+        compactionQueryBuilderFactory.getCompactionQueryBuilder(
+            CompactionType.MAJOR, true)
+            .setOperation(CompactionQueryBuilder.Operation.DROP)
+            .setResultTableName(tmpTableName)
+            .build());
   }
 }
