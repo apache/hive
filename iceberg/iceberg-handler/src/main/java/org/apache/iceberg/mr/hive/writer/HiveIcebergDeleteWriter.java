d24764a674ac6efae26414c10e4c8996967b7f7f
Ayush Saxena <ayushsaxena@apache.org>
2024-03-04 23:26:40 +0530

    HIVE-28102: Iceberg: Invoke validateDataFilesExist for RowDelta operations. (#5111). (Ayush Saxena, reviewed by Denys Kuzmenko)
    (cherry picked from commit 41bf5d55f6ca9bc2ab6af2f3fc34cc64c7b26f01)

--- iceberg/iceberg-handler/src/main/java/org/apache/iceberg/mr/hive/writer/HiveIcebergDeleteWriter.java
+++ iceberg/iceberg-handler/src/main/java/org/apache/iceberg/mr/hive/writer/HiveIcebergDeleteWriter.java
@@ -22,6 +22,7 @@ package org.apache.iceberg.mr.hive.writer;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.apache.hadoop.io.Writable;
 import org.apache.iceberg.DeleteFile;
 import org.apache.iceberg.PartitionSpec;
@@ -68,6 +69,7 @@ class HiveIcebergDeleteWriter extends HiveIcebergWriterBase {
   @Override
   public FilesForCommit files() {
     List<DeleteFile> deleteFiles = ((DeleteWriteResult) writer.result()).deleteFiles();
-    return FilesForCommit.onlyDelete(deleteFiles);
+    Set<CharSequence> referencedDataFiles = ((DeleteWriteResult) writer.result()).referencedDataFiles();
+    return FilesForCommit.onlyDelete(deleteFiles, referencedDataFiles);
   }
 }
