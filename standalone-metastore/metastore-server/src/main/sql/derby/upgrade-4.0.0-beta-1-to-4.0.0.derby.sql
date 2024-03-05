6c074526ab8abcfd36b5c85941db61ed8a6966dc
Denys Kuzmenko <dkuzmenko@cloudera.com>
2024-03-05 11:06:43 +0100

    fixed upgrade scripts

--- standalone-metastore/metastore-server/src/main/sql/derby/upgrade-4.0.0-beta-1-to-4.0.0.derby.sql
+++ standalone-metastore/metastore-server/src/main/sql/derby/upgrade-4.0.0-beta-1-to-4.0.0.derby.sql
@@ -7,4 +7,4 @@ DROP INDEX "APP"."UNIQUEPARTITION";
 CREATE UNIQUE INDEX "APP"."UNIQUEPARTITION" ON "APP"."PARTITIONS" ("TBL_ID", "PART_NAME");
 
 -- This needs to be the last thing done.  Insert any changes above this line.
-UPDATE "APP".VERSION SET SCHEMA_VERSION='4.1.0', VERSION_COMMENT='Hive release version 4.1.0' where VER_ID=1;
+UPDATE "APP".VERSION SET SCHEMA_VERSION='4.0.0', VERSION_COMMENT='Hive release version 4.0.0' where VER_ID=1;
--- standalone-metastore/metastore-server/src/main/sql/oracle/upgrade-4.0.0-beta-1-to-4.0.0.oracle.sql
+++ standalone-metastore/metastore-server/src/main/sql/oracle/upgrade-4.0.0-beta-1-to-4.0.0.oracle.sql
@@ -1,4 +1,4 @@
-SELECT 'Upgrading MetaStore schema from 4.0.0-beta-1 to 4.1.0' AS Status from dual;
+SELECT 'Upgrading MetaStore schema from 4.0.0-beta-1 to 4.0.0' AS Status from dual;
 
 
 -- HIVE-24815: Remove "IDXS" Table from Metastore Schema
@@ -11,5 +11,5 @@ CREATE UNIQUE INDEX UNIQUEPARTITION ON PARTITIONS (TBL_ID, PART_NAME);
 DROP INDEX PARTITIONS_N50;
 
 -- These lines need to be last.  Insert any changes above.
-UPDATE VERSION SET SCHEMA_VERSION='4.1.0', VERSION_COMMENT='Hive release version 4.1.0' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 4.0.0-beta-1 to 4.1.0' AS Status from dual;
+UPDATE VERSION SET SCHEMA_VERSION='4.0.0', VERSION_COMMENT='Hive release version 4.0.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 4.0.0-beta-1 to 4.0.0' AS Status from dual;
