ef5723b295c6f6d8707408164425049a7b442b6f
Rui Li <lirui@apache.org>
2024-03-15 22:45:47 +0800

    HIVE-28121: Use direct SQL for transactional altering table parameter

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/RawStore.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/RawStore.java
@@ -2317,4 +2317,14 @@ public interface RawStore extends Configurable {
     return null;
   }
 
+  /**
+   * Updates a given table parameter with expected value.
+   *
+   * @return the number of rows updated
+   */
+  default int updateParameterWithExpectedValue(Table table, String key, String expectedValue, String newValue)
+      throws MetaException {
+    throw new UnsupportedOperationException("This Store doesn't support updating table parameter with expected value");
+  }
+
 }
