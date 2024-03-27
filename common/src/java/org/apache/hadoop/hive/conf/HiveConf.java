17ac697b7242e4510e88b2a40b143e16a0857a61
Soumyakanti Das <soumyakanti.das.08@gmail.com>
2024-03-26 18:35:20 -0700

    Introduce config for query plan cache

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -492,6 +492,8 @@ public class HiveConf extends Configuration {
                     "Only in effect when the cache is enabled"),
     MSC_CACHE_RECORD_STATS("hive.metastore.client.cache.v2.recordStats", false,
             "This property enables recording metastore client cache stats in DEBUG logs"),
+    QUERY_PLAN_CACHE_ENABLED("hive.query.plan.cache.enabled", true,
+        "This property enables Query Plan Cache"),
     // QL execution stuff
     DFS_XATTR_ONLY_SUPPORTED_ON_RESERVED_NAMESPACE("dfs.xattr.supported.only.on.reserved.namespace", false,
       "DFS supports xattr only on Reserved Name space (/.reserved/raw)"),
