7c8151f3710103487d35f307f844105a719541a7
seonggon <lngsg@postech.ac.kr>
2024-04-01 21:26:32 +0900

    Fix debug message

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
@@ -1892,7 +1892,7 @@ public class SharedWorkOptimizer extends Transform {
         Set<Cluster> cc2 = cluster2.childClusters(edgePredicate);
 
         if (!Collections.disjoint(cc1, cc2)) {
-          LOG.debug("merge would create an unsupported parallel edge(CHILDS)", op1, op2);
+          LOG.debug("merging {} and {} would create an unsupported parallel edge(CHILDS)", op1, op2);
           return false;
         }
       }
@@ -1929,7 +1929,7 @@ public class SharedWorkOptimizer extends Transform {
         }
 
         if (pc.size() > 0) {
-          LOG.debug("merge would create an unsupported parallel edge(PARENTS)", op1, op2);
+          LOG.debug("merging {} and {} would create an unsupported parallel edge(PARENTS)", op1, op2);
           return false;
         }
       }
