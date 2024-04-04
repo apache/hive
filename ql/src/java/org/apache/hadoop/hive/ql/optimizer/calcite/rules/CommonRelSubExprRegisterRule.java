89675019bf3319e6be3fab0555c950aab55bcb83
Stamatis Zampetakis <zabetak@gmail.com>
2024-04-04 19:19:02 +0200

    AssertionError: Type mismatch when using MaterializedViewProjectFilterRule
    java.lang.AssertionError:
    Type mismatch:
    rel rowtype:
    RecordType(NULL int_col) NOT NULL
    equivRel rowtype:
    RecordType(BOOLEAN NOT NULL boolean_col, BOOLEAN NOT NULL literalTrue) NOT NULL
    	at org.apache.calcite.util.Litmus$1.fail(Litmus.java:31)
    	at org.apache.calcite.plan.RelOptUtil.equal(RelOptUtil.java:2193)
    	at org.apache.calcite.plan.volcano.VolcanoPlanner.register(VolcanoPlanner.java:580)
    	at org.apache.calcite.plan.volcano.VolcanoPlanner.ensureRegistered(VolcanoPlanner.java:604)
    	at org.apache.calcite.plan.volcano.VolcanoRuleCall.transformTo(VolcanoRuleCall.java:148)
    	at org.apache.calcite.plan.RelOptRuleCall.transformTo(RelOptRuleCall.java:268)
    	at org.apache.calcite.plan.RelOptRuleCall.transformTo(RelOptRuleCall.java:283)
    	at org.apache.calcite.rel.rules.materialize.MaterializedViewRule.perform(MaterializedViewRule.java:454)
    	at org.apache.calcite.rel.rules.materialize.MaterializedViewProjectFilterRule.onMatch(MaterializedViewProjectFilterRule.java:50)
    	at org.apache.calcite.plan.volcano.VolcanoRuleCall.onMatch(VolcanoRuleCall.java:229)
    	at org.apache.calcite.plan.volcano.IterativeRuleDriver.drive(IterativeRuleDriver.java:58)
    	at org.apache.calcite.plan.volcano.VolcanoPlanner.findBestExp(VolcanoPlanner.java:510)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner$CalcitePlannerAction.rewriteUsingViews(CalcitePlanner.java:2113)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner$CalcitePlannerAction.applyCteRewriting(CalcitePlanner.java:2147)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner$CalcitePlannerAction.apply(CalcitePlanner.java:1708)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner$CalcitePlannerAction.apply(CalcitePlanner.java:1579)
    	at org.apache.calcite.tools.Frameworks.lambda$withPlanner$0(Frameworks.java:131)
    	at org.apache.calcite.prepare.CalcitePrepareImpl.perform(CalcitePrepareImpl.java:914)
    	at org.apache.calcite.tools.Frameworks.withPrepare(Frameworks.java:180)
    	at org.apache.calcite.tools.Frameworks.withPlanner(Frameworks.java:126)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner.logicalPlan(CalcitePlanner.java:1331)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner.genOPTree(CalcitePlanner.java:580)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.analyzeInternal(SemanticAnalyzer.java:13177)
    	at org.apache.hadoop.hive.ql.parse.CalcitePlanner.analyzeInternal(CalcitePlanner.java:473)
    	at org.apache.hadoop.hive.ql.parse.BaseSemanticAnalyzer.analyze(BaseSemanticAnalyzer.java:327)
    	at org.apache.hadoop.hive.ql.parse.ExplainSemanticAnalyzer.analyzeInternal(ExplainSemanticAnalyzer.java:180)
    	at org.apache.hadoop.hive.ql.parse.BaseSemanticAnalyzer.analyze(BaseSemanticAnalyzer.java:327)
    	at org.apache.hadoop.hive.ql.Compiler.analyze(Compiler.java:224)
    	at org.apache.hadoop.hive.ql.Compiler.compile(Compiler.java:107)
    
    	The AssertionError can be reproduced by running subquery_null_agg.q
    
    	The problem happens due to two things:
    	* the rule matches a plan with a filter condition that is simplified to false during the rewritting
    	* there is a view (cte suggestion) that is basically a trivial project on top of the table
    
    	CTE suggestions with just project+scan do not make much sense so we can drop them by tuning the CommonRelSubExprRegisterRule and workaround the problem for now.
    
    	Depending on the bandwidth we may want to attack the bug in the MaterializedViewProjectFilterRule and make the latter more robust; that would be the actual fix.

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/CommonRelSubExprRegisterRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/CommonRelSubExprRegisterRule.java
@@ -24,20 +24,21 @@ import org.apache.calcite.rel.core.Aggregate;
 import org.apache.calcite.rel.core.Filter;
 import org.apache.calcite.rel.core.Join;
 import org.apache.calcite.rel.core.Project;
-import org.apache.calcite.rel.core.TableScan;
+import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.hadoop.hive.ql.optimizer.calcite.CommonTableExpressionRegistry;
+
+import com.google.common.collect.Multimap;
 
 import java.util.function.Predicate;
 
 public final class CommonRelSubExprRegisterRule extends CommonRelSubExprRule {
-  private static final Predicate<RelNode> NO_SCAN_PREDICATE = rel -> !(rel instanceof TableScan);
   public static final CommonRelSubExprRegisterRule JOIN = new CommonRelSubExprRegisterRule(operand(Join.class, any()));
   public static final CommonRelSubExprRegisterRule AGGREGATE =
       new CommonRelSubExprRegisterRule(operand(Aggregate.class, any()));
   public static final CommonRelSubExprRegisterRule FILTER =
       new CommonRelSubExprRegisterRule(operand(Filter.class, any()));
   public static final CommonRelSubExprRegisterRule PROJECT =
-      new CommonRelSubExprRegisterRule(operand(Project.class, operandJ(RelNode.class, null, NO_SCAN_PREDICATE, any())));
+      new CommonRelSubExprRegisterRule(operandJ(Project.class, null, new InterestingRelNodePredicate(), any()));
 
   private CommonRelSubExprRegisterRule(RelOptRuleOperand operand) {
     super(operand);
@@ -50,4 +51,30 @@ public final class CommonRelSubExprRegisterRule extends CommonRelSubExprRule {
       r.add(call.rel(0));
     }
   }
+
+  /**
+   * A predicate determining if a relational expression is interesting.
+   *
+   * The notion of interesting is loosely defined on purpose since it may change as the API evolves. At the moment an
+   * expression is considered interesting if it contains at least one of the following RelNode types:
+   * <ul>
+   *   <li>{@link Join}</li>
+   *   <li>{@link Aggregate}</li>
+   *   <li>{@link Filter}</li>
+   * </ul>
+   */
+  private static final class InterestingRelNodePredicate implements Predicate<RelNode> {
+    @Override
+    public boolean test(final RelNode rel) {
+      RelMetadataQuery mq = rel.getCluster().getMetadataQuery();
+      Multimap<Class<? extends RelNode>, RelNode> types = mq.getNodeTypes(rel);
+      if (types == null) {
+        return false;
+      }
+      return types.keySet().stream().anyMatch(
+          t -> Join.class.isAssignableFrom(t) || Aggregate.class.isAssignableFrom(t) || Filter.class.isAssignableFrom(
+              t));
+    }
+  }
+
 }
