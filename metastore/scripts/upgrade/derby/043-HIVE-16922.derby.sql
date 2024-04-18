7263b4ee8166adcbb8050889c37febffc33f3371
zdeng <dengzh@apache.org>
2024-04-18 11:33:48 +0800

    HIVE-28204: Remove the HMS 1.x init script

--- metastore/scripts/upgrade/derby/043-HIVE-16922.derby.sql
+++
@@ -1,3 +0,0 @@
-UPDATE SERDE_PARAMS
-SET PARAM_KEY='collection.delim'
-WHERE PARAM_KEY='colelction.delim';
--- metastore/scripts/upgrade/mysql/upgrade-0.12.0-to-0.13.0.mysql.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'Upgrading MetaStore schema from 0.12.0 to 0.13.0' AS ' ';
-
-SOURCE 016-HIVE-6386.mysql.sql;
-SOURCE 017-HIVE-6458.mysql.sql;
-SOURCE 018-HIVE-6757.mysql.sql;
-SOURCE hive-txn-schema-0.13.0.mysql.sql;
-
-UPDATE VERSION SET SCHEMA_VERSION='0.13.0', VERSION_COMMENT='Hive release version 0.13.0' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 0.12.0 to 0.13.0' AS ' ';
