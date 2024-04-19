ef4e16c18719261fea395fbb7ad682d82220ad41
Soumyakanti Das <soumyakanti.das.08@gmail.com>
2024-04-19 11:18:59 -0700

    Fix error caused by use of deprecated API which was removed
    Change is similar to https://github.com/apache/calcite/blob/calcite-1.25.0/core/src/main/java/org/apache/calcite/tools/RelBuilder.java#L789

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveAggregateJoinTransposeRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveAggregateJoinTransposeRule.java
@@ -224,7 +224,7 @@ public class HiveAggregateJoinTransposeRule extends AggregateJoinTransposeRule {
             }
           }
           side.newInput = relBuilder.push(joinInput)
-              .aggregate(relBuilder.groupKey(belowAggregateKey, null),
+              .aggregate(relBuilder.groupKey(belowAggregateKey, ImmutableList.of(belowAggregateKey)),
                   belowAggCalls)
               .build();
         }
