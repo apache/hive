636b0d3abf00afbe2cf71dc89f762acca48867ca
scarlin-cloudera <55709772+scarlin-cloudera@users.noreply.github.com>
2024-05-02 03:55:55 -0700

    HIVE-28173: Fixed staging dir issues on materialized views on HDFS encrypted tables (Stephen Carlin reviewed by Krisztian Kasa, Denys Kuzmenko)

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/HiveMaterializedViewsRegistry.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/HiveMaterializedViewsRegistry.java
@@ -53,6 +53,7 @@ import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.metastore.DefaultMetaStoreFilterHookImpl;
 import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
+import org.apache.hadoop.hive.ql.Context;
 import org.apache.hadoop.hive.ql.exec.ColumnInfo;
 import org.apache.hadoop.hive.ql.log.PerfLogger;
 import org.apache.hadoop.hive.ql.optimizer.calcite.CalciteSemanticException;
@@ -227,7 +228,7 @@ public final class HiveMaterializedViewsRegistry {
     }
     final CBOPlan plan;
     try {
-      plan = ParseUtils.parseQuery(conf, viewQuery);
+      plan = ParseUtils.parseQuery(createContext(conf), viewQuery);
     } catch (Exception e) {
       LOG.warn("Materialized view " + materializedViewTable.getCompleteName() +
           " ignored; error parsing original query; " + e);
@@ -360,6 +361,13 @@ public final class HiveMaterializedViewsRegistry {
 
   public List<HiveRelOptMaterialization> getRewritingMaterializedViews(ASTNode ast) {
     return materializedViewsCache.get(ast);
+  }
+
+  private Context createContext(HiveConf conf) {
+    Context ctx = new Context(conf);
+    ctx.setIsLoadingMaterializedView(true);
+    ctx.setHDFSCleanup(true);
+    return ctx;
   }
 
   public boolean isEmpty() {
