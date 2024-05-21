8b83759f31d8ebdfb9002804969b01fd97450a30
tanishq-chugh <tanishqchugh@cloudera.com>
2024-05-21 16:40:30 +0530

    HIVE-28391: Change final fields modification to be invoked using MethodHandles

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestJdbcWithMiniHS2.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestJdbcWithMiniHS2.java
@@ -25,6 +25,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -1234,10 +1236,10 @@ public class TestJdbcWithMiniHS2 {
       constructorCacheField = ReflectionUtil.class.getDeclaredField("CONSTRUCTOR_CACHE");
       if (constructorCacheField != null) {
         constructorCacheField.setAccessible(true);
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(constructorCacheField, constructorCacheField.getModifiers()
-            & ~Modifier.FINAL);
+        VarHandle modifiersHandle = MethodHandles.privateLookupIn(Field.class, MethodHandles.lookup())
+                .findVarHandle(Field.class, "modifiers", int.class);
+        int modifiers = constructorCacheField.getModifiers();
+        modifiersHandle.set(constructorCacheField, modifiers & ~Modifier.FINAL);
         tmp =
             CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.SECONDS).concurrencyLevel(64)
                 .weakKeys().weakValues().build();
