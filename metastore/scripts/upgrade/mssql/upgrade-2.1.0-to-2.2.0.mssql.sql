1032b7a8db9351dac02bef4966510d98714bd4c0
dengzh <dengzhhu653@gmail.com>
2024-06-11 13:47:01 +0800

    HIVE-28204: Remove some HMS obsolete scripts (#5201) (Zhihua Deng, reviewed by Zsolt Miskolczi, Denys Kuzmenko, Henri Biestro, Sai Hemanth Gantasala)

--- metastore/scripts/upgrade/mssql/upgrade-2.1.0-to-2.2.0.mssql.sql
+++
@@ -1,8 +0,0 @@
-SELECT 'Upgrading MetaStore schema from 2.1.0 to 2.2.0' AS MESSAGE;
-
-:r 022-HIVE-14496.mssql.sql
-:r 023-HIVE-10562.mssql.sql
-:r 024-HIVE-12274.mssql.sql
-
-UPDATE VERSION SET SCHEMA_VERSION='2.2.0', VERSION_COMMENT='Hive release version 2.2.0' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 2.1.0 to 2.2.0' AS MESSAGE;
--- metastore/scripts/upgrade/oracle/upgrade-0.9.0-to-0.10.0.oracle.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'Upgrading MetaStore schema from 0.9.0 to 0.10.0' AS Status from dual;
-@010-HIVE-3072.oracle.sql
-@011-HIVE-3649.oracle.sql
-@012-HIVE-1362.oracle.sql
-SELECT 'Finished upgrading MetaStore schema from 0.9.0 to 0.10.0' AS Status from dual;
