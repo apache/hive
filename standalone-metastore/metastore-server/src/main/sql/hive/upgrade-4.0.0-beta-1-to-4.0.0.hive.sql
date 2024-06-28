7f6367e0c6e21b11ef62da1ea6681a54d547de07
okumin <git@okumin.com>
2024-06-28 19:59:46 +0900

    HIVE-28352: Schematool fails to upgradeSchema on dbType=hive (Shohei Okumiya, reviewed by Zhihua Deng, Denys Kuzmenko)
    Closes #5324

--- standalone-metastore/metastore-server/src/main/sql/hive/upgrade-4.0.0-beta-1-to-4.0.0.hive.sql
+++ standalone-metastore/metastore-server/src/main/sql/hive/upgrade-4.0.0-beta-1-to-4.0.0.hive.sql
@@ -1,3 +1,8 @@
 SELECT 'Upgrading MetaStore schema from 4.0.0-beta-1 to 4.0.0';
 
+USE SYS;
+
+CREATE OR REPLACE VIEW `VERSION` AS SELECT 1 AS `VER_ID`, '4.0.0' AS `SCHEMA_VERSION`,
+  'Hive release version 4.0.0' AS `VERSION_COMMENT`;
+
 SELECT 'Finished upgrading MetaStore schema from 4.0.0-beta-1 to 4.0.0';
