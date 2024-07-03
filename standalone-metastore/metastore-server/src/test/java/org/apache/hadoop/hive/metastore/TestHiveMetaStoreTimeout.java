bb326e74de98972f2b592bc883b25ebb1cf2ae05
tanishq-chugh <157357971+tanishq-chugh@users.noreply.github.com>
2024-07-03 23:20:02 +0530

    HIVE-28392: Enable netty to use reflection setAccessible & fix SocketTimeoutException error message case issue

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestHiveMetaStoreTimeout.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestHiveMetaStoreTimeout.java
@@ -182,7 +182,7 @@ public class TestHiveMetaStoreTimeout {
       try(HiveMetaStoreClient c = new HiveMetaStoreClient(newConf)) {
         Assert.fail("should throw connection timeout exception.");
       } catch (MetaException e) {
-        Assert.assertTrue("unexpected Exception", e.getMessage().contains("connect timed out"));
+        Assert.assertTrue("unexpected Exception", e.getMessage().toLowerCase().contains("connect timed out"));
       }
       return null;
     });
