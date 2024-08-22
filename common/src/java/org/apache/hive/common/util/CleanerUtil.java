7553a00233c94f723915dac6ed73a1d64d2291f4
akshat0395 <akshatatstps@gmail.com>
2024-08-22 17:04:37 +0530

    HIVE-28463: Java 17 formats and convention upgrades

--- common/src/java/org/apache/hive/common/util/CleanerUtil.java
+++ common/src/java/org/apache/hive/common/util/CleanerUtil.java
@@ -75,8 +75,8 @@ public final class CleanerUtil {
   static {
     final Object hack = AccessController.doPrivileged(
         (PrivilegedAction<Object>) CleanerUtil::unmapHackImpl);
-    if (hack instanceof BufferCleaner) {
-      CLEANER = (BufferCleaner) hack;
+    if (hack instanceof BufferCleaner cleaner) {
+      CLEANER = cleaner;
       UNMAP_SUPPORTED = true;
       UNMAP_NOT_SUPPORTED_REASON = null;
     } else {
