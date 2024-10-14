429982f30a5eeac441de155bfca2897f11bfcbce
kasakrisz <kasakrisz2@gmail.com>
2024-10-14 14:03:45 +0200

    add parent select to file sink to convert schema to expected schema

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/opconventer/HiveOpConverterUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/opconventer/HiveOpConverterUtils.java
@@ -183,14 +183,8 @@ final class HiveOpConverterUtils {
       }
     }
 
-    ReduceSinkDesc rsDesc;
-    if (order.isEmpty()) {
-      rsDesc = PlanUtils.getReduceSinkDesc(reduceKeys, reduceValues, outputColumnNames, false, tag,
-          reduceKeys.size(), numReducers, acidOperation, NullOrdering.defaultNullOrder(hiveConf));
-    } else {
-      rsDesc = PlanUtils.getReduceSinkDesc(reduceKeys, reduceValues, outputColumnNames, false, tag,
+    ReduceSinkDesc rsDesc = PlanUtils.getReduceSinkDesc(reduceKeys, reduceValues, outputColumnNames, false, tag,
           partitionCols, order, nullOrder, NullOrdering.defaultNullOrder(hiveConf), numReducers, acidOperation, false);
-    }
 
     ReduceSinkOperator rsOp = (ReduceSinkOperator) OperatorFactory.getAndMakeChild(
         rsDesc, new RowSchema(outputColumns), input);
--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/opconventer/HiveSortExchangeVisitor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/opconventer/HiveSortExchangeVisitor.java
@@ -19,17 +19,29 @@
 package org.apache.hadoop.hive.ql.optimizer.calcite.translator.opconventer;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.calcite.rel.RelDistribution;
 import org.apache.calcite.rel.RelDistribution.Type;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rel.type.RelDataTypeField;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.metastore.model.MPartition;
+import org.apache.hadoop.hive.ql.exec.ColumnInfo;
 import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.ReduceSinkOperator;
 import org.apache.hadoop.hive.ql.io.AcidUtils.Operation;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveSortExchange;
+import org.apache.hadoop.hive.ql.optimizer.calcite.translator.ASTConverter;
 import org.apache.hadoop.hive.ql.optimizer.calcite.translator.opconventer.HiveOpConverter.OpAttr;
 import org.apache.hadoop.hive.ql.parse.SemanticException;
+import org.apache.hadoop.hive.ql.plan.ExprNodeColumnDesc;
 import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
+import org.apache.hadoop.hive.ql.plan.SelectDesc;
+
+import static java.util.Arrays.asList;
 
 class HiveSortExchangeVisitor extends HiveRelNodeVisitor<HiveSortExchange> {
   HiveSortExchangeVisitor(HiveOpConverter hiveOpConverter) {
@@ -50,8 +62,24 @@ class HiveSortExchangeVisitor extends HiveRelNodeVisitor<HiveSortExchange> {
     }
 
     RelDistribution distribution = exchangeRel.getDistribution();
-    if (distribution.getType() != Type.HASH_DISTRIBUTED) {
-      throw new SemanticException("Only hash distribution supported for LogicalExchange");
+    ArrayList<ExprNodeDesc> partitionKeyList;
+    if (distribution.getType() == Type.HASH_DISTRIBUTED) {
+      partitionKeyList = new ArrayList<>(exchangeRel.getDistribution().getKeys().size());
+      for (int index = 0; index < exchangeRel.getDistribution().getKeys().size(); index++) {
+        partitionKeyList.add(HiveOpConverterUtils.convertToExprNode(
+                exchangeRel.getCluster().getRexBuilder().makeInputRef(exchangeRel.getInput(), index),
+                exchangeRel.getInput(), inputOpAf.tabAlias, inputOpAf.vcolsInCalcite));
+      }
+//      for (int i = 0; i < distribution.getKeys().size(); i++) {
+//        int key = distribution.getKeys().get(i);
+//        ColumnInfo colInfo = inputOpAf.inputs.get(0).getSchema().getSignature().get(key);
+//        ExprNodeDesc column = new ExprNodeColumnDesc(colInfo);
+//        distributeKeys[i] = column;
+//      }
+    } else if (distribution.getType() != Type.ANY) {
+      throw new SemanticException("Only hash distribution supported for HiveSortExchange");
+    } else {
+      partitionKeyList = new ArrayList<>(0);
     }
     ExprNodeDesc[] expressions = new ExprNodeDesc[exchangeRel.getKeys().size()];
     for (int index = 0; index < exchangeRel.getKeys().size(); index++) {
@@ -60,15 +88,22 @@ class HiveSortExchangeVisitor extends HiveRelNodeVisitor<HiveSortExchange> {
     }
     exchangeRel.setKeyExpressions(expressions);
 
-    ReduceSinkOperator rsOp = genReduceSink(inputOpAf.inputs.get(0), tabAlias, expressions,
-        -1, -1, Operation.NOT_ACID, hiveOpConverter.getHiveConf());
+    Operator<?> inputOp = inputOpAf.inputs.get(0);
+    List<String> keepColumns = new ArrayList<>();
+//    final ImmutableBitSet sortColsPos = sortColsPosBuilder.build();
+//    final ImmutableBitSet sortOutputColsPos = sortOutputColsPosBuilder.build();
+    final List<ColumnInfo> inputSchema = inputOp.getSchema().getSignature();
+    for (int pos=0; pos<inputSchema.size(); pos++) {
+//      if ((sortColsPos.get(pos) && sortOutputColsPos.get(pos)) ||
+//              (!sortColsPos.get(pos) && !sortOutputColsPos.get(pos))) {
+        keepColumns.add(inputSchema.get(pos).getInternalName());
+//      }
+    }
 
-    return new OpAttr(tabAlias, inputOpAf.vcolsInCalcite, rsOp);
-  }
+    Operator<?> resultOp = HiveOpConverterUtils.genReduceSinkAndBacktrackSelect(
+            inputOpAf.inputs.get(0), expressions, -1, partitionKeyList, "", "",
+            -1, Operation.NOT_ACID, hiveOpConverter.getHiveConf(), keepColumns);
 
-  private static ReduceSinkOperator genReduceSink(Operator<?> input, String tableAlias, ExprNodeDesc[] keys, int tag,
-      int numReducers, Operation acidOperation, HiveConf hiveConf) throws SemanticException {
-    return HiveOpConverterUtils.genReduceSink(input, tableAlias, keys, tag, new ArrayList<ExprNodeDesc>(), "", "",
-        numReducers, acidOperation, hiveConf);
+    return new OpAttr(tabAlias, inputOpAf.vcolsInCalcite, resultOp);
   }
 }
