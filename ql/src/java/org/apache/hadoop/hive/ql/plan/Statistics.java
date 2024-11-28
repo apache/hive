4654aea51b3369d7845ab129ece496672979fee9
yiqijiu <35646730+yiqijiu@users.noreply.github.com>
2024-11-28 20:23:42 +0800

    update Statistics.java
    Add spaces in the function parameters.

--- ql/src/java/org/apache/hadoop/hive/ql/plan/Statistics.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/Statistics.java
@@ -220,14 +220,14 @@ public class Statistics implements Serializable {
   }
 
   public void addBasicStats(Statistics stats) {
-    dataSize = StatsUtils.safeAdd(dataSize,stats.dataSize);
+    dataSize = StatsUtils.safeAdd(dataSize, stats.dataSize);
     numRows += stats.numRows;
     basicStatsState = inferColumnStatsState(basicStatsState, stats.basicStatsState);
   }
 
   @Deprecated
   public void addToDataSize(long rds) {
-    dataSize = StatsUtils.safeAdd(dataSize,rds);
+    dataSize = StatsUtils.safeAdd(dataSize, rds);
   }
 
   public void setColumnStats(Map<String, ColStatistics> colStats) {
