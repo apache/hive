0abf87eaf9eff4b5d6c57115d0e765934bf207e4
Ayush Saxena <ayushsaxena@apache.org>
2025-01-08 12:14:25 +0530

    HIVE-28698: OTEL: Add Retry Policy in the Exporter.

---
+++ common/src/java/org/apache/hadoop/hive/common/OTELUtils.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hive.common;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.common.export.RetryPolicy;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.conf.HiveConf;
+
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_COLLECTOR_ENDPOINT;
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_EXPORTER_TIMEOUT;
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_RETRY_BACKOFF_MULTIPLIER;
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_RETRY_INITIAL_BACKOFF;
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_RETRY_MAX_BACKOFF;
+
+public class OTELUtils {
+
+  public static OpenTelemetry getOpenTelemetry(Configuration conf) {
+    OpenTelemetry otel = GlobalOpenTelemetry.get();
+    if (otel == null || otel == OpenTelemetry.noop()) {
+
+      String endPoint = HiveConf.getVar(conf, HIVE_OTEL_COLLECTOR_ENDPOINT);
+      long timeOut = HiveConf.getTimeVar(conf, HIVE_OTEL_EXPORTER_TIMEOUT, TimeUnit.SECONDS);
+      long initialBackOff = HiveConf.getTimeVar(conf, HIVE_OTEL_RETRY_INITIAL_BACKOFF, TimeUnit.SECONDS);
+      long maxBackOff = HiveConf.getTimeVar(conf, HIVE_OTEL_RETRY_MAX_BACKOFF, TimeUnit.SECONDS);
+      double backoff = HiveConf.getFloatVar(conf, HIVE_OTEL_RETRY_BACKOFF_MULTIPLIER);
+      RetryPolicy retryPolicy = RetryPolicy.builder()
+          .setInitialBackoff(Duration.ofSeconds(initialBackOff))
+          .setMaxAttempts(5)
+          .setMaxBackoff(Duration.ofSeconds(maxBackOff))
+          .setBackoffMultiplier(backoff)
+          .build();
+
+      SpanExporter otlpExporter =
+          OtlpGrpcSpanExporter.builder()
+              .setEndpoint(endPoint)
+              .setTimeout(timeOut, TimeUnit.SECONDS)
+              .setRetryPolicy(retryPolicy).build();
+
+      // Set up BatchSpanProcessor (with retry logic) to handle telemetry data
+      BatchSpanProcessor batchSpanProcessor =
+          BatchSpanProcessor.builder(otlpExporter).setExporterTimeout(timeOut, TimeUnit.SECONDS).build();
+
+      io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder builder =
+          AutoConfiguredOpenTelemetrySdk.builder();
+      otel = builder.addTracerProviderCustomizer((tracerProviderBuilder, configProperties) -> {
+        // Add the custom BatchSpanProcessor to the TracerProvider
+        return tracerProviderBuilder.addSpanProcessor(batchSpanProcessor);
+      }).setResultAsGlobal().build().getOpenTelemetrySdk();
+    }
+    return otel;
+  }
+}
