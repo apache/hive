d35d2dff32fdabfa852a2ecfed69e97e00fe46ec
Ayush Saxena <ayushsaxena@apache.org>
2025-01-09 00:08:06 +0530

    Add meter level changes.

--- common/src/java/org/apache/hadoop/hive/common/OTELUtils.java
+++ common/src/java/org/apache/hadoop/hive/common/OTELUtils.java
@@ -23,9 +23,13 @@ import java.util.concurrent.TimeUnit;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.common.export.RetryPolicy;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.MetricExporter;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
@@ -39,6 +43,7 @@ import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_EXPORTER_T
 import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_RETRY_BACKOFF_MULTIPLIER;
 import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_RETRY_INITIAL_BACKOFF;
 import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_RETRY_MAX_BACKOFF;
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.HIVE_OTEL_METRICS_FREQUENCY_SECONDS;
 
 public class OTELUtils {
 
@@ -47,6 +52,7 @@ public class OTELUtils {
     if (otel == null || otel == OpenTelemetry.noop()) {
 
       String endPoint = HiveConf.getVar(conf, HIVE_OTEL_COLLECTOR_ENDPOINT);
+      long otelExporterFrequency = HiveConf.getTimeVar(conf, HIVE_OTEL_METRICS_FREQUENCY_SECONDS, TimeUnit.SECONDS);
       long timeOut = HiveConf.getTimeVar(conf, HIVE_OTEL_EXPORTER_TIMEOUT, TimeUnit.SECONDS);
       long initialBackOff = HiveConf.getTimeVar(conf, HIVE_OTEL_RETRY_INITIAL_BACKOFF, TimeUnit.SECONDS);
       long maxBackOff = HiveConf.getTimeVar(conf, HIVE_OTEL_RETRY_MAX_BACKOFF, TimeUnit.SECONDS);
@@ -68,9 +74,22 @@ public class OTELUtils {
       BatchSpanProcessor batchSpanProcessor =
           BatchSpanProcessor.builder(otlpExporter).setExporterTimeout(timeOut, TimeUnit.SECONDS).build();
 
+      MetricExporter otlpMetricExporter =
+          OtlpGrpcMetricExporter.builder()
+              .setEndpoint(endPoint)
+              .setTimeout(timeOut, TimeUnit.SECONDS)
+              .build();
+
+      PeriodicMetricReader metricReader =
+          PeriodicMetricReader.builder(otlpMetricExporter)
+              .setInterval(Duration.ofSeconds(otelExporterFrequency))
+              .build();
+
       otel = OpenTelemetrySdk.builder().setTracerProvider(
               SdkTracerProvider.builder().addSpanProcessor(batchSpanProcessor)
                   .addResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "hive_otel"))).build())
+          .setMeterProvider(SdkMeterProvider.builder().registerMetricReader(metricReader)
+              .addResource(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "hive_otel"))).build())
           .buildAndRegisterGlobal();
     }
     return otel;
