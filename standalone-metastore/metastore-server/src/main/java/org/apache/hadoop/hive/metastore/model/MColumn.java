8a29201bb20ab10a06125a61aee6154f529186ce
saihemanth <saihemanth@cloudera.com>
2025-04-29 08:32:29 -0700

    Dev patch for JDO issue-V1

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/model/MColumn.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/model/MColumn.java
@@ -16,64 +16,86 @@
  * limitations under the License.
  */
 
-/**
- * 
- */
 package org.apache.hadoop.hive.metastore.model;
 
-/**
- * Represent a column or a type of a table or object
- */
-public class MFieldSchema {
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.datanucleus.identity.LongId;
+
+public class MColumn {
+  private MColumnDescriptor cd;
   private String name;
   private String type;
   private String comment;
-  public MFieldSchema() {}
 
-  /**
-   * @param comment
-   * @param name
-   * @param type
-   */
-  public MFieldSchema(String name, String type, String comment) {
-    this.comment = comment;
+  @SuppressWarnings("serial")
+  public static class PK implements Serializable {
+    public LongId cd;
+    public String name;
+
+    public PK() {}
+
+    public PK(LongId id, String name) {
+      this.cd = id;
+      this.name = name;
+    }
+
+    public String toString() {
+      return String.format("%s-%s", cd.toString(), name);
+    }
+
+    public int hashCode() {
+      return Objects.hash(cd.getKey(), name);
+    }
+
+    public boolean equals(Object other) {
+      if (other instanceof PK otherPK) {
+        return Objects.equals(otherPK.name, name)
+                && Objects.equals(otherPK.cd.getKey(), cd.getKey());
+      }
+      return false;
+    }
+  }
+
+  public MColumn() {
+  }
+
+  public MColumn(String name, String type, String comment) {
     this.name = name;
     this.type = type;
+    this.comment = comment;
   }
-  /**
-   * @return the name
-   */
+
+  public MColumnDescriptor getCd() {
+    return cd;
+  }
+
+  public void setCd(MColumnDescriptor cd) {
+    this.cd = cd;
+  }
+
   public String getName() {
     return name;
   }
-  /**
-   * @param name the name to set
-   */
+
   public void setName(String name) {
     this.name = name;
   }
-  /**
-   * @return the comment
-   */
-  public String getComment() {
-    return comment;
-  }
-  /**
-   * @param comment the comment to set
-   */
-  public void setComment(String comment) {
-    this.comment = comment;
-  }
-  /**
-   * @return the type
-   */
+
   public String getType() {
     return type;
   }
-  /**
-   * @param field the type to set
-   */
-  public void setType(String field) {
-    this.type = field;
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
   }
 }
