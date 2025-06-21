f7e2bc316f9320efba89f8eecf03122846a380d6
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2025-06-21 12:53:11 +0530

    HIVE-28983: Log HS2 and HMS PID and update hive-env.sh template

--- service/src/java/org/apache/hive/service/server/HiveServer2.java
+++ service/src/java/org/apache/hive/service/server/HiveServer2.java
@@ -1206,7 +1206,8 @@ public class HiveServer2 extends CompositeService {
   private static void startHiveServer2() throws Throwable {
     long attempts = 0, maxAttempts = 1;
     while (true) {
-      LOG.info("Starting HiveServer2");
+      long pid = ProcessHandle.current().pid();
+      LOG.info("Starting HiveServer2. PID is: {}", pid);
       HiveConf hiveConf = new HiveConf();
       maxAttempts = hiveConf.getLongVar(HiveConf.ConfVars.HIVE_SERVER2_MAX_START_ATTEMPTS);
       long retrySleepIntervalMs = hiveConf
