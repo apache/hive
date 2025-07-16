bc8e9f1fe7f2f8ddb4e7144d1ff3b9e791f69d2c
zdeng <dengzh@apache.org>
2025-07-16 12:23:30 +0800

    HIVE-29101: Add test coverage for the standalone jdbc

---
+++ itests/test-jdbc/src/test/java/org/apache/hive/jdbc/ItHiveServer2.java
@@ -0,0 +1,270 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hive.jdbc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.minikdc.MiniKdc;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.AbstractWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ItHiveServer2 extends ItAbstractContainer {
+  protected File workDir;
+  private final MiniJdbcKdc miniKdc;
+  protected GenericContainer<?> container;
+  protected final String imageName = "apache/hive:4.0.1";
+  private int[] realmPorts;
+
+  public ItHiveServer2() {
+    this.miniKdc = null;
+  }
+
+  public ItHiveServer2(File workDir) throws Exception {
+    this.workDir = new File(workDir, "test-standalone-jdbc-file");
+    this.miniKdc = new MiniJdbcKdc(this.workDir);
+
+    String principal = MiniJdbcKdc.HIVE_TEST_USER_1;
+    Configuration configuration = new Configuration();
+    configuration.set("hadoop.security.authentication", "kerberos");
+    UserGroupInformation.setConfiguration(configuration);
+    UserGroupInformation.loginUserFromKeytab(principal, miniKdc.getKeyTabFile(principal));
+    assertEquals(principal, UserGroupInformation.getLoginUser().getShortUserName());
+  }
+
+  protected Map<String, String> prepareEnvArgs() {
+    Map<String, String> envArgs = new HashMap<>();
+    envArgs.put("SERVICE_NAME", "hiveserver2");
+    envArgs.put("HIVE_SERVER2_TRANSPORT_MODE", "all");
+    List<String> properties = new ArrayList<>();
+    properties.add("hive.server2.authentication=KERBEROS");
+    properties.add("hive.server2.authentication.kerberos.principal=hive/_HOST@" + miniKdc.getRealm());
+    properties.add("hive.server2.authentication.kerberos.keytab=/opt/hive/conf/hive.keytab");
+    StringBuilder builder = new StringBuilder();
+    for (String prop : properties) {
+      builder.append("-D").append(prop).append(" ");
+    }
+    envArgs.put("SERVICE_OPTS", builder.toString());
+    return envArgs;
+  }
+
+  private Map<String, String> prepareFiles() {
+    Map<String, String> boundFiles = new HashMap<>();
+
+    boundFiles.put(createNewKrbConf().getPath(), "/etc/krb5.conf");
+    boundFiles.put(miniKdc.getKeyTabFile(miniKdc.getServicePrincipalForUser("hive")), "/opt/hive/conf/hive.keytab");
+    StringBuilder core_site = new StringBuilder("<configuration>").append(System.lineSeparator());
+    core_site.append("<property>\n" + "    <name>hadoop.security.authentication</name>\n" + "    <value>kerberos</value>\n" + "  </property>");
+    core_site.append("<property>\n" + "    <name>hadoop.proxyuser.hive.groups</name>\n" + "    <value>*</value>\n" + "  </property>");
+    core_site.append("<property>\n" + "    <name>hadoop.proxyuser.hive.hosts</name>\n" + "    <value>*</value>\n" + "  </property>");
+    core_site.append("</configuration>");
+    boundFiles.put(genConfFile("core-site.xml", core_site.toString()), "/opt/hive/conf/core-site.xml");
+
+    // java.io.IOException: Can't get Master Kerberos principal for use as renewer if missing the yarn-site
+    StringBuilder yarn_site = new StringBuilder("<configuration>").append(System.lineSeparator());
+    yarn_site.append("<property>\n" + "    <name>yarn.resourcemanager.principal</name>\n" +
+        "    <value>yarn/_HOST@" + miniKdc.getRealm() + "</value>\n" + "  </property>");
+    yarn_site.append("</configuration>");
+    boundFiles.put(genConfFile("yarn-site.xml", yarn_site.toString()), "/opt/hive/conf/yarn-site.xml");
+
+    return boundFiles;
+  }
+
+  @Override
+  public void start() {
+    container = new GenericContainer<>(DockerImageName.parse(imageName))
+        .withEnv(prepareEnvArgs())
+        .waitingFor(new AbstractWaitStrategy() {
+          @Override
+          protected void waitUntilReady() {
+            long timeout = TimeUnit.MINUTES.toMillis(15);
+            long start;
+            do {
+              start = System.currentTimeMillis();
+              try (Connection conn = DriverManager.getConnection(getBaseJdbcUrl())) {
+                break;
+              } catch (Exception e) {
+                try {
+                  Thread.sleep(10 * 1000);
+                } catch (InterruptedException ex) {
+                  break;
+                }
+              }
+            } while ((timeout += start - System.currentTimeMillis()) > 0);
+          }
+        });
+    beforeStart(container);
+    container.start();
+  }
+
+  protected void beforeStart(GenericContainer<?> container) {
+    Map<String, String> boundFiles = prepareFiles();
+    for (Map.Entry<String, String> entry : boundFiles.entrySet()) {
+      container.withFileSystemBind(entry.getKey(), entry.getValue(), BindMode.READ_ONLY);
+    }
+    container
+        .withCreateContainerCmdModifier(it -> it.withHostName(MiniJdbcKdc.HOST))
+        .withExposedPorts(10000, 10001);
+    if (realmPorts != null) {
+      org.testcontainers.Testcontainers.exposeHostPorts(realmPorts);
+    }
+  }
+
+  private String genConfFile(String fileName, String content) {
+    File coreFile = new File(workDir, fileName);
+    try (FileWriter writer = new FileWriter(coreFile, false)) {
+      writer.write(content);
+      writer.flush();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return coreFile.getPath();
+  }
+
+  private File createNewKrbConf() {
+    File krb5 = miniKdc.getKrb5conf();
+    File newKrb5 = new File(workDir, krb5.getName() + "_new");
+    try (BufferedReader reader = new BufferedReader(new FileReader(krb5));
+         FileWriter writer = new FileWriter(newKrb5, false)) {
+      String line;
+      List<Integer> hostPorts = new ArrayList<>();
+      String localhost = "localhost:";
+      while ((line = reader.readLine()) != null) {
+        if (line.contains(localhost)) {
+          hostPorts.add(Integer.valueOf(line.split(localhost)[1]));
+          line = line.replace("localhost", "host.testcontainers.internal");
+        }
+        writer.write(line);
+        writer.write(System.lineSeparator());
+      }
+      this.realmPorts = new int[hostPorts.size()];
+      for (int i = 0; i < hostPorts.size(); i++) {
+        realmPorts[i] = hostPorts.get(i);
+      }
+      writer.flush();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return newKrb5;
+  }
+
+  @Override
+  public void stop() throws Exception {
+    try {
+      miniKdc.stop();
+    } finally {
+      if (container != null) {
+        container.stop();
+      }
+    }
+  }
+
+  @Override
+  protected String getHttpJdbcUrl() {
+    return "jdbc:hive2://" + MiniJdbcKdc.HOST + ":" + container.getMappedPort(10001) +
+        "/;principal=" + miniKdc.getServicePrincipalForUser("hive") + "@" + miniKdc.getRealm() +
+        ";transportMode=http;httpPath=cliservice";
+  }
+
+  @Override
+  protected String getBaseJdbcUrl() {
+    return "jdbc:hive2://" + MiniJdbcKdc.HOST + ":" + container.getMappedPort(10000) + "/;principal=" +
+        miniKdc.getServicePrincipalForUser("hive") + "@" + miniKdc.getRealm();
+  }
+
+  public static class MiniJdbcKdc {
+    protected static String HOST = "test-standalone-jdbc-kerberos";
+    public static String HIVE_SERVICE_PRINCIPAL = "hive";
+    public static String YARN_SERVICE_PRINCIPAL = "yarn";
+    public static String HIVE_TEST_USER_1 = "user1";
+    public static String HIVE_TEST_USER_2 = "user2";
+    public static String HIVE_TEST_SUPER_USER = "superuser";
+    private static int keyTabCounter = 1;
+    private final MiniKdc miniKdc;
+    private final Properties kdcConf = MiniKdc.createConf();
+    private final Map<String, String> userPrincipals =
+        new HashMap<String, String>();
+    private final File workDir;
+
+    public MiniJdbcKdc(File workDir) throws Exception {
+      this.workDir = workDir;
+      /**
+       *  Hadoop security classes read the default realm via static initialization,
+       *  before miniKdc is initialized. Hence we set the realm via a test configuration
+       *  and propagate that to miniKdc.
+       */
+      assertNotNull("java.security.krb5.conf is needed for hadoop security",
+          System.getProperty("java.security.krb5.conf"));
+      System.clearProperty("java.security.krb5.conf");
+
+      miniKdc = new MiniKdc(kdcConf, new File(workDir, "miniKdc"));
+      miniKdc.start();
+
+      // create default users
+      addUserPrincipal(getServicePrincipalForUser(HIVE_SERVICE_PRINCIPAL));
+      addUserPrincipal(getServicePrincipalForUser(YARN_SERVICE_PRINCIPAL));
+      addUserPrincipal(HIVE_TEST_USER_1);
+      addUserPrincipal(HIVE_TEST_USER_2);
+      addUserPrincipal(HIVE_TEST_SUPER_USER);
+    }
+
+    public void addUserPrincipal(String principal) throws Exception {
+      File keytab = new File(workDir, "miniKdc" + keyTabCounter++ + ".keytab");
+      miniKdc.createPrincipal(keytab, principal);
+      userPrincipals.put(principal, keytab.getPath());
+    }
+
+    public String getServicePrincipalForUser(String shortUserName) {
+      return shortUserName + "/" + HOST;
+    }
+
+    public String getKeyTabFile(String principalName) {
+      return userPrincipals.get(principalName);
+    }
+
+    public File getKrb5conf() {
+      return miniKdc.getKrb5conf();
+    }
+
+    public String getRealm() {
+      return miniKdc.getRealm();
+    }
+
+    public void stop() {
+      miniKdc.stop();
+    }
+  }
+}
---
+++ itests/test-jdbc/src/test/java/org/apache/hive/jdbc/TestStandaloneJdbc.java
@@ -0,0 +1,226 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hive.jdbc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.hadoop.hive.metastore.utils.SecurityUtils;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class TestStandaloneJdbc {
+  private static File workDir = new File(createTempDir(), "test-it-standalone-jdbc");
+  private static ItAbstractContainer HS2;
+  private static ItAbstractContainer ZK_HS2;
+
+  public static File createTempDir() {
+    File baseDir = new File(System.getProperty("java.io.tmpdir"));
+    String baseName = System.currentTimeMillis() + "-";
+
+    for (int counter = 0; counter < 100; counter++) {
+      File tempDir = new File(baseDir, baseName + counter);
+      if (tempDir.mkdir()) {
+        return tempDir;
+      }
+    }
+    throw new IllegalStateException(
+        "Failed to create directory within 100 attempts (tried "
+            + baseName
+            + "0 to "
+            + baseName
+            + 99
+            + ')');
+  }
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    workDir.mkdirs();
+    HS2 = new ItHiveServer2(workDir);
+    ZK_HS2 = new ItZKHiveServer2(workDir);
+
+    HS2.start();
+    ZK_HS2.start();
+  }
+
+  @Test
+  public void testBinaryJdbc() throws Exception {
+    testMetaOp(HS2.getBaseJdbcUrl(), true);
+    //testMetaOp(ZK_HS2.getJdbcUrl(), false);
+    testDataSourceOp(HS2.getBaseJdbcUrl());
+    testNegativeJdbc(HS2.getBaseJdbcUrl());
+  }
+
+  private void testMetaOp(String url, boolean kerberos) throws Exception {
+    try (Connection con = DriverManager.getConnection(url)) {
+      try (Statement stmt = con.createStatement()) {
+        String tableName = "testHiveDriverTable1";
+        stmt.execute("drop table if exists " + tableName);
+        stmt.execute("create table " + tableName + " (key string, value string)");
+        // show tables
+        String sql = "show tables '" + tableName + "'";
+        System.out.println("Running: " + sql);
+        ResultSet res = stmt.executeQuery(sql);
+        if (res.next()) {
+          System.out.println(res.getString(1));
+        }
+        // describe table
+        sql = "describe " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+          System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+
+        // "Client cannot authenticate via:[TOKEN, KERBEROS]" in running the Kerberized tez local mode
+        if (!kerberos) {
+          String values = "('a','b'),('c','d'),('e','f'),('g','h')";
+          stmt.execute("insert into table " + tableName + " values" + values);
+          // select * query
+          sql = "select * from " + tableName;
+          System.out.println("Running: " + sql);
+          res = stmt.executeQuery(sql);
+          List<String> list = new ArrayList<>();
+          while (res.next()) {
+            list.add("('" + res.getString(1) + "','" + res.getString(2) + "')");
+          }
+          assertEquals(values, list.stream().collect(Collectors.joining(",")));
+
+          // regular hive query
+          sql = "select count(1) from " + tableName;
+          System.out.println("Running: " + sql);
+          res = stmt.executeQuery(sql);
+          assertTrue(res.next());
+          assertEquals(4, res.getInt(1));
+        }
+      }
+    }
+  }
+
+  private void testDataSourceOp(String url) throws Exception {
+    try (Connection con = DriverManager.getConnection(url)) {
+      DatabaseMetaData metaData = con.getMetaData();
+      ResultSet rs = metaData.getSchemas();
+      assertTrue(rs.next());
+      while (rs.next()) {
+        System.out.println("DatabaseMetaData.getSchemas: " + rs.getString(1));
+      }
+      ResultSet resultSet = metaData.getColumns("hive", ".*", ".*", ".*");
+      assertTrue(resultSet.next());
+      while (resultSet.next()) {
+        System.out.println("DatabaseMetaData.getColumns: " + rs.getString(1));
+      }
+      resultSet = metaData.getTypeInfo();
+      assertTrue(resultSet.next());
+      while (resultSet.next()) {
+        System.out.println("DatabaseMetaData.getTypeInfo: " + rs.getString(1));
+      }
+    }
+  }
+
+  @Test
+  public void testHttpJdbc() throws Exception {
+    testMetaOp(HS2.getHttpJdbcUrl(), true);
+    testMetaOp(ZK_HS2.getHttpJdbcUrl(), false);
+    testDataSourceOp(HS2.getHttpJdbcUrl());
+    testNegativeJdbc(HS2.getHttpJdbcUrl());
+  }
+
+  private void testNegativeJdbc(String url) throws Exception {
+    Connection con = DriverManager.getConnection(url);
+    try {
+      Statement stmt = con.createStatement();
+      stmt.execute("insert into table this_is_not_exist_table values (1),(3),(5)");
+      fail("A SQLException is expected");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      con.close();
+    }
+  }
+
+  @Test
+  public void testZookeeperJdbc() throws Exception {
+    testMetaOp(ZK_HS2.getZkConnectionUrl(), false);
+    testDataSourceOp(ZK_HS2.getZkConnectionUrl());
+    testNegativeJdbc(ZK_HS2.getZkConnectionUrl());
+  }
+
+  @Test
+  public void testTokenAuthentication() throws Exception {
+    String url = HS2.getBaseJdbcUrl();
+    String dt;
+    try (HiveConnection con =  (HiveConnection) DriverManager.getConnection(url)) {
+      dt = con.getDelegationToken(ItHiveServer2.MiniJdbcKdc.HIVE_TEST_USER_1, "hive");
+    }
+    File dtFile = new File(workDir, "delegation-token-file");
+    try (FileOutputStream os = new FileOutputStream(dtFile)) {
+      os.write(dt.getBytes());
+    }
+    SecurityUtils.setTokenStr(UserGroupInformation.getCurrentUser(), dt, "hiveserver2ClientToken");
+    //  HADOOP_TOKEN_FILE_LOCATION
+    updateEnv("HADOOP_TOKEN_FILE_LOCATION", dtFile.getPath());
+    url += ";auth=delegationToken";
+    testMetaOp(url, true);
+  }
+
+  public void updateEnv(String name, String val) throws ReflectiveOperationException {
+    Map<String, String> env = System.getenv();
+    Field field = env.getClass().getDeclaredField("m");
+    field.setAccessible(true);
+    ((Map<String, String>) field.get(env)).put(name, val);
+  }
+
+  @AfterClass
+  public static void destroy() throws Exception {
+    try {
+      workDir.delete();
+    } finally {
+      killContainer(ZK_HS2, HS2);
+    }
+  }
+
+  private static void killContainer(ItAbstractContainer... containers) {
+    for (ItAbstractContainer container : containers) {
+      try {
+        if (container != null) {
+          container.stop();
+        }
+      } catch (Exception e) {
+        // ignore this exception
+      }
+    }
+  }
+}
