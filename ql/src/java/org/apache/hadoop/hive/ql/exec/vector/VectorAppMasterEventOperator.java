bec97f54df3051265fd4ff6914f3e14cefad6f55
seonggon <lngsg@postech.ac.kr>
2025-08-22 18:26:43 +0900

    HIVE-29160: Replace forward() in vectorized operators with vectorForward()

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorAppMasterEventOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorAppMasterEventOperator.java
@@ -136,7 +136,7 @@ public class VectorAppMasterEventOperator extends AppMasterEventOperator
       throw new HiveException(e);
     }
 
-    forward(data, rowInspector);
+    vectorForward(batch);
   }
 
   @Override
