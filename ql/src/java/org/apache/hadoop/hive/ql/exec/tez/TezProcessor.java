0e57acb7594ba35b7cd419a41e7450d6f1460291
zhengchenyu001 <zhengchenyu001@ke.com>
2025-09-05 19:03:52 +0800

    update

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/TezProcessor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/TezProcessor.java
@@ -295,11 +295,11 @@ public class TezProcessor extends AbstractLogicalIOProcessor {
       // If there are other speculative attempt execute canCommit first, then wait until the attempt is killed
       // or the committed task fails.
       while (!getContext().canCommit()) {
-        // If canCommit returns false and fall into this loop, it means another task attempt has committed.
-        // And this task attempt is only needs to sleep for a relatively long time to wait for being killed.
-        // However, we need to avoid low-probability events: the rare case where a task attempt fails after
-        // committed, so we can't set an excessively long delay so that this task attempt could react on time.
-        // 500ms is a trade-off value.
+        // If canCommit returns false, and we enter this loop, it means another task attempt has already committed.
+        // This task attempt only needs to sleep for a relatively long time while waiting to be killed.
+        // However, we must avoid a low-probability scenario: a rare case where a task attempt fails after committing.
+        // In that situation, the delay must not be excessively long so this attempt can still react in time.
+        // 500ms is chosen as a trade-off value.
         Thread.sleep(500);
       }
     } catch (Throwable t) {
