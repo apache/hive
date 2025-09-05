4c3e20d2055ac2dcd3c946a556b03001d2b6d86c
Soumyakanti Das <soumyakanti.das.08@gmail.com>
2025-09-05 11:47:25 -0700

    Revert changes to QB.java as we are only relying on HMS to get Tables

--- ql/src/java/org/apache/hadoop/hive/ql/parse/QB.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/QB.java
@@ -55,7 +55,6 @@ public class QB {
   private List<String> aliases;
   private QBParseInfo qbp;
   private QBMetaData qbm;
-  private List<QBMetaData> subqueryMetaDataList;
   private QBJoinTree qbjoin;
   private String id;
   private boolean isQuery;
@@ -130,7 +129,6 @@ public class QB {
     }
     qbp = new QBParseInfo(alias, isSubQ);
     qbm = new QBMetaData();
-    subqueryMetaDataList = new ArrayList<>();
     // Must be deterministic order maps - see HIVE-8707
     ptfNodeToSpec = new LinkedHashMap<ASTNode, PTFInvocationSpec>();
     destToWindowingSpec = new LinkedHashMap<String, WindowingSpec>();
@@ -162,10 +160,6 @@ public class QB {
 
   public QBMetaData getMetaData() {
     return qbm;
-  }
-
-  public List<QBMetaData> getSubqueryMetaDataList() {
-    return subqueryMetaDataList;
   }
 
   public void setQBParseInfo(QBParseInfo qbp) {
@@ -257,23 +251,6 @@ public class QB {
 
   public String getTabNameForAlias(String alias) {
     return aliasToTabs.get(alias.toLowerCase());
-  }
-
-  public Table getTableForAlias(String alias) {
-    if (alias == null) {
-      return null;
-    }
-    Table result = qbm.getTableForAlias(alias);
-    if (result == null) {
-      for (QBMetaData qbMetaData : subqueryMetaDataList) {
-        result = qbMetaData.getTableForAlias(alias);
-        if (result != null) {
-          return result;
-        }
-      }
-    }
-
-    return result;
   }
 
   public Map<String, String> getTabPropsForAlias(String alias) {
