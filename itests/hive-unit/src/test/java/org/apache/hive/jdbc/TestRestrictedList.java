950a048d32ad62fd58436a907486c79a8ac06ca9
Vikram Ahuja <viahuja@visa.com>
2025-09-23 16:40:16 +0530

    review comments handling

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestRestrictedList.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestRestrictedList.java
@@ -70,7 +70,6 @@ public class TestRestrictedList {
 
     // Add the parameter here if it cannot change at runtime
     addToExpectedRestrictedMap("hive.conf.restricted.list");
-    addToExpectedRestrictedMap("hive.ignore.removed.configs.list");
     addToExpectedRestrictedMap("hive.security.authenticator.manager");
     addToExpectedRestrictedMap("hive.security.authorization.manager");
     addToExpectedRestrictedMap("hive.security.metastore.authorization.manager");
