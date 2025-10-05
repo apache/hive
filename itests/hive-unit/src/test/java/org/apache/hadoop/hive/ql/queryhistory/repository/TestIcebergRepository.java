4fe9bd60bdd3fc99a1284409f3f400b4aa6cabac
okumin <okumin@apache.org>
2025-10-05 15:16:13 +0900

    Remove trailing asterisk

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/queryhistory/repository/TestIcebergRepository.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/queryhistory/repository/TestIcebergRepository.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one§
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
