9ca722b4d11310290899b11bc2a24a13b244cb4d
Butao Zhang <zhangbutao@apache.org>
2025-10-30 10:54:39 +0800

    Remove primary key ut from beeline

--- beeline/src/test/org/apache/hive/beeline/TestTableOutputFormat.java
+++ beeline/src/test/org/apache/hive/beeline/TestTableOutputFormat.java
@@ -21,8 +21,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyInt;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -31,9 +29,6 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.when;
 
 import org.mockito.invocation.InvocationOnMock;
@@ -42,7 +37,6 @@ import static org.mockito.Mockito.mock;
 
 public class TestTableOutputFormat {
 
-  private static final String CYAN = "\033[1;36m";
   private static final String GREEN = "\033[1;32m";
 
   private final String[][] mockRowData = {
@@ -53,12 +47,8 @@ public class TestTableOutputFormat {
   };
 
   private BeelineMock mockBeeline;
-  private DatabaseConnection dbConnection;
-  private Connection mockSqlConnection;
-  private DatabaseMetaData mockDatabaseMetaData;
   private ResultSet mockResultSet;
   private ResultSetMetaData mockResultSetMetaData;
-  private ResultSet mockMetadataResultSet;
   private TestBufferedRows.MockRow mockRow;
 
   /**
@@ -85,7 +75,6 @@ public class TestTableOutputFormat {
   @Test
   public void testColoringWithNoTableMetadata() throws SQLException {
     setupMockData();
-    when(mockResultSetMetaData.getTableName(anyInt())).thenReturn(null);
 
     BufferedRows bfRows = new BufferedRows(mockBeeline, mockResultSet);
     TableOutputFormat instance = new TableOutputFormat(mockBeeline);
@@ -94,27 +83,6 @@ public class TestTableOutputFormat {
 
     List<String> allPrintedLines = mockBeeline.getAllPrintedLines();
     assertColumnHasColor(allPrintedLines, 1, GREEN);
-    assertColumnHasColor(allPrintedLines, 2, GREEN);
-  }
-
-  /**
-   * The primary key has one column. It should be CYAN. The second column should be green
-   */
-  @Test
-  public void testSingleColumnPrimaryKey() throws SQLException {
-    setupMockData();
-    mockBeeline.getOpts().setColor(true);
-
-    BufferedRows bfRows = new BufferedRows(mockBeeline, mockResultSet);
-    TableOutputFormat instance = new TableOutputFormat(mockBeeline);
-
-    instance.print(bfRows);
-
-    List<String> allPrintedLines = mockBeeline.getAllPrintedLines();
-
-    // PK column is CYAN
-    assertColumnHasColor(allPrintedLines, 1, CYAN);
-    // non-PK column is green
     assertColumnHasColor(allPrintedLines, 2, GREEN);
   }
 
@@ -136,77 +104,6 @@ public class TestTableOutputFormat {
     }
   }
 
-  /**
-   * No primary key column - all columns should be green
-   */
-  @Test
-  public void testColoredWithoutPrimaryKey() throws SQLException {
-    setupMockData();
-    mockBeeline.getOpts().setColor(true);
-    when(mockMetadataResultSet.next()).thenReturn(false);
-
-    BufferedRows bfRows = new BufferedRows(mockBeeline, mockResultSet);
-    TableOutputFormat instance = new TableOutputFormat(mockBeeline);
-
-    instance.print(bfRows);
-
-    List<String> allPrintedLines = mockBeeline.getAllPrintedLines();
-
-    assertColumnHasColor(allPrintedLines, 1, GREEN);
-    assertColumnHasColor(allPrintedLines, 2, GREEN);
-  }
-
-  /**
-   * The output contains one single table. And both columns are part of the PK
-   */
-  @Test
-  public void testColoredWithPrimaryKeyHasMultipleColumns() throws SQLException {
-    setupMockData();
-    mockBeeline.getOpts().setColor(true);
-    when(mockMetadataResultSet.next()).thenReturn(true).thenReturn(true).thenReturn(false);
-
-    BufferedRows bfRows = new BufferedRows(mockBeeline, mockResultSet);
-    TableOutputFormat instance = new TableOutputFormat(mockBeeline);
-
-    instance.print(bfRows);
-
-    List<String> allPrintedLines = mockBeeline.getAllPrintedLines();
-
-    // PK column are CYAN
-    assertColumnHasColor(allPrintedLines, 1, CYAN);
-    assertColumnHasColor(allPrintedLines, 2, CYAN);
-  }
-
-  /**
-   * The output contains two tables. Both columns are PKs
-   */
-  @Test
-  public void testColoredWithMultipleTablesWithPrimaryKeys() throws SQLException {
-    setupMockData();
-    mockBeeline.getOpts().setColor(true);
-
-    when(mockResultSetMetaData.getTableName(eq(1))).thenReturn("Table");
-    when(mockResultSetMetaData.getTableName(eq(2))).thenReturn("Table2");
-
-    DatabaseMetaData databaseMetaData2 = mock(DatabaseMetaData.class);
-    ResultSet resultSet2 = mock(ResultSet.class);
-    when(resultSet2.next()).thenReturn(true).thenReturn(false);
-    when(resultSet2.getString(eq("COLUMN_NAME"))).thenReturn("Value");
-    when(databaseMetaData2.getPrimaryKeys(anyString(), isNull(), eq("Table2"))).thenReturn(resultSet2);
-    when(databaseMetaData2.getConnection()).thenReturn(mockSqlConnection);
-    when(dbConnection.getDatabaseMetaData()).thenReturn(mockDatabaseMetaData, databaseMetaData2);
-
-    BufferedRows bfRows = new BufferedRows(mockBeeline, mockResultSet);
-    TableOutputFormat instance = new TableOutputFormat(mockBeeline);
-
-    instance.print(bfRows);
-
-    List<String> allPrintedLines = mockBeeline.getAllPrintedLines();
-
-    // PK column are CYAN
-    assertColumnHasColor(allPrintedLines, 1, CYAN);
-    assertColumnHasColor(allPrintedLines, 2, CYAN);
-  }
 
   private boolean cellHasColor(List<String> table, int row, int col, String color) {
     String rowText = table.get(row);
@@ -227,30 +124,11 @@ public class TestTableOutputFormat {
     mockBeeline.getOpts().setColor(true);
     mockResultSet = mock(ResultSet.class);
 
-    mockSqlConnection = mock(Connection.class);
-    when(mockSqlConnection.getCatalog()).thenReturn("Catalog");
-
     mockResultSetMetaData = mock(ResultSetMetaData.class);
     when(mockResultSetMetaData.getColumnCount()).thenReturn(2);
     when(mockResultSetMetaData.getColumnLabel(1)).thenReturn("Key");
     when(mockResultSetMetaData.getColumnLabel(2)).thenReturn("Value");
-    when(mockResultSetMetaData.getTableName(anyInt())).thenReturn("Table");
-    when(mockResultSetMetaData.getColumnName(eq(1))).thenReturn("Key");
-    when(mockResultSetMetaData.getColumnName(eq(2))).thenReturn("Value");
     when(mockResultSet.getMetaData()).thenReturn(mockResultSetMetaData);
-
-    mockMetadataResultSet = mock(ResultSet.class);
-    when(mockMetadataResultSet.next()).thenReturn(true).thenReturn(false);
-    when(mockMetadataResultSet.getString(eq("COLUMN_NAME"))).thenReturn("Key", "Value");
-
-    mockDatabaseMetaData = mock(DatabaseMetaData.class);
-    when(mockDatabaseMetaData.getPrimaryKeys(anyString(), isNull(), anyString())).thenReturn(mockMetadataResultSet);
-
-    dbConnection = mock(DatabaseConnection.class);
-    when(dbConnection.getDatabaseMetaData()).thenReturn(mockDatabaseMetaData);
-    when(mockDatabaseMetaData.getConnection()).thenReturn(mockSqlConnection);
-
-    mockBeeline.getDatabaseConnections().setConnection(dbConnection);
 
     mockRow = new TestBufferedRows.MockRow();
     // returns true as long as there is more data in mockResultData array
