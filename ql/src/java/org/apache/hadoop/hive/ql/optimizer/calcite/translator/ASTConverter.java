3e7c15d1e996363af461f45d1918ec2088c805b1
Stamatis Zampetakis <zabetak@gmail.com>
2025-12-16 15:03:30 +0100

    Add sample window function calls for each function kind

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/ASTConverter.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/ASTConverter.java
@@ -1001,10 +1001,13 @@ public class ASTConverter {
       }
 
       // 1. Translate the UDAF
+      // Case for the common window function calls: e.g., MIN(salary)
       ASTNode wUDAFAst = ASTBuilder.createAST(HiveParser.TOK_FUNCTION, "TOK_FUNCTION");
+      // Case for window function calls with star syntax: e.g., COUNT(*), SUM(*), AVG(*)
       if (over.getOperands().isEmpty() && over.op.getSyntax() == SqlSyntax.FUNCTION_STAR) {
         wUDAFAst = ASTBuilder.createAST(HiveParser.TOK_FUNCTIONSTAR, "TOK_FUNCTIONSTAR");
       }
+      // Case for window functions with DISTINCT: e.g., COUNT(DISTINCT deptno)
       if (over.isDistinct()) {
         wUDAFAst = ASTBuilder.createAST(HiveParser.TOK_FUNCTIONDI, "TOK_FUNCTIONDI");
       }
