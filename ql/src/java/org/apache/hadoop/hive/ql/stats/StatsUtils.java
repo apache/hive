75dbdf8d57c5cf4abaf6e307c2f88e1fdddf52c0
Konstantin Bereznyakov <konstantin.bereznyakov@treasure-data.com>
2025-12-19 13:07:37 -0800

    HIVE-29368: further tuning NDV handling, including reading stats for timestamp/date columns

--- ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
@@ -832,6 +832,7 @@ public class StatsUtils {
       cs.setNumNulls(csd.getBinaryStats().getNumNulls());
     } else if (colTypeLowerCase.equals(serdeConstants.TIMESTAMP_TYPE_NAME)) {
       cs.setAvgColLen(JavaDataModel.get().lengthOfTimestamp());
+      cs.setCountDistint(csd.getTimestampStats().getNumDVs());
       cs.setNumNulls(csd.getTimestampStats().getNumNulls());
       Long lowVal = (csd.getTimestampStats().getLowValue() != null) ? csd.getTimestampStats().getLowValue()
           .getSecondsSinceEpoch() : null;
@@ -862,6 +863,7 @@ public class StatsUtils {
       cs.setHistogram(csd.getDecimalStats().getHistogram());
     } else if (colTypeLowerCase.equals(serdeConstants.DATE_TYPE_NAME)) {
       cs.setAvgColLen(JavaDataModel.get().lengthOfDate());
+      cs.setCountDistint(csd.getDateStats().getNumDVs());
       cs.setNumNulls(csd.getDateStats().getNumNulls());
       Long lowVal = (csd.getDateStats().getLowValue() != null) ? csd.getDateStats().getLowValue()
           .getDaysSinceEpoch() : null;
@@ -2087,9 +2089,16 @@ public class StatsUtils {
     for (ColStatistics cs : colStats) {
       if (cs != null) {
         long ndv = cs.getCountDistint();
-        // Only increment ndv value if it is "known"
-        if (ndv > 0 && cs.getNumNulls() > 0) {
-          ndv = StatsUtils.safeAdd(ndv, 1);
+
+        if (ndv == 0L) {
+          // Typically, ndv == 0 means "NDV unknown", and no safe GROUPBY adjustments are possible
+          // However, there is a special exception for "constant NULL" columns. They are intentionally generated
+          // with NDV values of 0 and numNulls == numRows, while their actual NDV is 1
+          if (cs.getNumNulls() >= parentStats.getNumRows()) {
+            ndv = 1L;
+          }
+        } else if (cs.getNumNulls() > 0L) {
+          ndv = StatsUtils.safeAdd(ndv, 1L);
         }
         ndvValues.add(ndv);
       } else {
