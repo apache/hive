924e6b948b7f96bb30dd94f2f2c7c4b4fc6e36a4
tanishq-chugh <tanishqchugh@cloudera.com>
2025-12-30 19:31:35 +0530

    HIVE-29389: HiveProtoLoggingHook populates duplicate entries in TablesRead metrics for tables with multiple partitions

--- ql/src/java/org/apache/hadoop/hive/ql/hooks/HiveProtoLoggingHook.java
+++ ql/src/java/org/apache/hadoop/hive/ql/hooks/HiveProtoLoggingHook.java
@@ -92,6 +92,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -489,13 +490,13 @@ public class HiveProtoLoggingHook implements ExecuteWithHookContext {
     }
 
     private List<String> getTablesFromEntitySet(Set<? extends Entity> entities) {
-      List<String> tableNames = new ArrayList<>();
+      Set<String> tableNames = new HashSet<>();
       for (Entity entity : entities) {
         if (entity.getType() == TABLE || entity.getType() == PARTITION) {
           tableNames.add(entity.getTable().getDbName() + "." + entity.getTable().getTableName());
         }
       }
-      return tableNames;
+      return new ArrayList<>(tableNames);
     }
 
     private ExecutionMode getExecutionMode(QueryPlan plan, List<ExecDriver> mrTasks,
