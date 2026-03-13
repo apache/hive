ac637314ae4f4a49e88c2178f94c8faddcd93193
Konstantin Bereznyakov <konstantin.bereznyakov@treasure-data.com>
2026-03-13 11:30:14 +0900

    HIVE-29503: don't trigger "NDV==0 fallback" on empty or super small tables

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/stats/annotation/StatsRulesProcFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/stats/annotation/StatsRulesProcFactory.java
@@ -3189,6 +3189,9 @@ public class StatsRulesProcFactory {
   }
 
   static boolean satisfyPrecondition(Statistics stats, List<String> joinKeys) {
+    if (stats.getNumRows() <= 1) {
+      return true;
+    }
     for (String col : joinKeys) {
       ColStatistics cs = stats.getColumnStatisticsFromColName(col);
       if (cs != null && cs.getCountDistint() == 0L) {
