0e30e7f879aa85d71055b30a9985595a8839988b
akpatra <asishkumar_patra@yahoo.in>
2026-03-25 15:00:27 +0530

    HIVE-27193: Fix Sonar and reviewer comments for parseDbName

--- standalone-metastore/metastore-common/src/main/java/org/apache/hadoop/hive/metastore/utils/MetaStoreUtils.java
+++ standalone-metastore/metastore-common/src/main/java/org/apache/hadoop/hive/metastore/utils/MetaStoreUtils.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -1093,10 +1093,8 @@ public class MetaStoreUtils {
    *            in the database name.
    * @return an array of two elements, the first being the catalog name, the second the database
    * name.
-   * @throws MetaException if the name is not either just a database name or a catalog plus
-   * database name with the proper delimiters.
    */
-  public static String[] parseDbName(String dbName, Configuration conf) throws MetaException {
+  public static String[] parseDbName(String dbName, Configuration conf) {
     if (dbName == null) {
       return Arrays.copyOf(nullCatalogAndDatabase, nullCatalogAndDatabase.length);
     }
--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/utils/TestMetastoreUtilsParseDbName.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/utils/TestMetastoreUtilsParseDbName.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,9 +19,7 @@ package org.apache.hadoop.hive.metastore.utils;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.annotation.MetastoreUnitTest;
-import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -32,7 +30,7 @@ import static org.junit.Assert.assertArrayEquals;
 public class TestMetastoreUtilsParseDbName {
 
   @Test
-  public void testParseDbNameEdgeCases() throws MetaException {
+  public void testParseDbNameEdgeCases() {
     Configuration conf = MetastoreConf.newMetastoreConf();
     MetastoreConf.setVar(conf, MetastoreConf.ConfVars.CATALOG_DEFAULT, "hive");
 
