b24899674bdd8fb9d57d7252506942663125ce6a
Ramit Gupta <ramitg254@gmail.com>
2026-04-10 11:34:14 +0530

    corrected update statements

--- ql/src/java/org/apache/hadoop/hive/ql/parse/rewrite/MergeRewriter.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/rewrite/MergeRewriter.java
@@ -250,8 +250,10 @@ public class MergeRewriter implements Rewriter<MergeStatement>, MergeStatement.D
         }
       }
       
-      targetTable.getPartCols().forEach(fieldSchema -> values.add(
-          formatter.apply(fieldSchema.getName())));
+      if (!targetTable.hasNonNativePartitionSupport()) {
+        targetTable.getPartCols().forEach(fieldSchema -> values.add(
+            formatter.apply(fieldSchema.getName())));
+      }
     }
     
     protected String getRhsExpValue(String newValue, String alias) {
