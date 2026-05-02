ef988f75560edd1b3e2af7ebf5a3f03347e3f84c
okumin <okumin@apache.org>
2026-05-02 14:00:12 +0900

    Refactor IcebergVendedCredentialProvider assignment

--- standalone-metastore/metastore-rest-catalog/src/main/java/org/apache/iceberg/rest/HMSCatalogFactory.java
+++ standalone-metastore/metastore-rest-catalog/src/main/java/org/apache/iceberg/rest/HMSCatalogFactory.java
@@ -112,11 +112,9 @@ public class HMSCatalogFactory {
     // Iceberg REST client uses "catalog" by default
     List<String> scopes = Collections.singletonList("catalog");
     ServletSecurity security = new ServletSecurity(AuthType.fromString(authType), configuration, req -> scopes);
-    final IcebergVendedCredentialProvider vendedCredentialProvider;
+    IcebergVendedCredentialProvider vendedCredentialProvider = null;
     if (MetastoreConf.getBoolVar(configuration, ConfVars.ICEBERG_CATALOG_VENDED_CREDENTIALS_ENABLED)) {
       vendedCredentialProvider = new IcebergVendedCredentialProvider(configuration);
-    } else {
-      vendedCredentialProvider = null;
     }
     return security.proxy(new HMSCatalogServlet(new HMSCatalogAdapter(catalog, vendedCredentialProvider)));
   }
