53b0b1d1d7624699aa2f1c148530807d33f582c9
Ramit Gupta <ramitg254@gmail.com>
2026-06-01 12:02:34 +0530

    non part cols retrieval made lazy

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/Table.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/Table.java
@@ -117,6 +117,7 @@ public class Table implements Serializable {
    */
   private List<FieldSchema> tablePartCols;
   private transient Map<String, Pair<Integer, FieldSchema>> inputColnameToIndFsMap;
+  private transient List<FieldSchema> tableNonPartCols;
   private transient Deserializer deserializer;
   private Class<? extends OutputFormat> outputFormatClass;
   private Class<? extends InputFormat> inputFormatClass;
@@ -787,8 +788,11 @@ public class Table implements Serializable {
   }
 
   public List<FieldSchema> getCols() {
+    if (tableNonPartCols != null) {
+      return tableNonPartCols;
+    }
     if (!isNonNative()) {
-      return getColsInternal(false);
+      tableNonPartCols = getColsInternal(false);
     } else {
       List<FieldSchema> nonPartFields = new ArrayList<>();
       Set<String> partFieldsName = getPartCols().stream().map(FieldSchema::getName).collect(Collectors.toSet());
@@ -797,8 +801,9 @@ public class Table implements Serializable {
           nonPartFields.add(field);
         }
       }
-      return nonPartFields;
+      tableNonPartCols = nonPartFields;
     }
+    return tableNonPartCols;
   }
 
   public List<FieldSchema> getColsForMetastore() {
@@ -890,6 +895,9 @@ public class Table implements Serializable {
 
   public void setFields(List<FieldSchema> fields) {
     tTable.getSd().setCols(fields);
+    tableNonPartCols = null;
+    tablePartCols = null;
+    inputColnameToIndFsMap = null;
   }
 
   public void setNumBuckets(int nb) {
