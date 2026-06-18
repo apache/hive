48bc4eb5c46794fd3cc4e598391a756a0956defe
Ruben Quesada Lopez <rubenql@gmail.com>
2026-06-18 12:24:29 +0100

    Override HiveTypeSystemImpl#deriveDecimalDivideType because CALCITE-6464 (introduce in 1.38) changed the default behavior to match MS-SQL-Server-style algorithm, which can cause a drop in the scale computation, hence a precision loss in certain cases. We override this method to keep the "old behavior" (pre-CALCITE-6464); an alternative could be not overridding it (and keep the new Calcite default MS-SQL-style decimal-divide semantics), but that would lead to "regressions" (precision loss) and would require test adjustments.

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveTypeSystemImpl.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/HiveTypeSystemImpl.java
@@ -19,8 +19,10 @@ package org.apache.hadoop.hive.ql.optimizer.calcite;
 
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.rel.type.RelDataTypeFactoryImpl;
 import org.apache.calcite.rel.type.RelDataTypeSystemImpl;
 import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.calcite.sql.type.SqlTypeUtil;
 
 import static org.apache.hadoop.hive.ql.udf.generic.GenericUDAFSum.SUM_RESULT_PRECISION_INCREASE;
 
@@ -177,6 +179,42 @@ public class HiveTypeSystemImpl extends RelDataTypeSystemImpl {
                 argumentType.getScale()), argumentType.isNullable());
     }
     return argumentType;
+  }
+
+  /**
+   * Overridden because CALCITE-6464 changed the default behavior to match MS-SQL-Server-style algorithm,
+   * which can cause a drop in the scale computation, hence a precision loss in certain cases.
+   * We override this method to keep the "old behavior" (pre-CALCITE-6464); an alternative could be not
+   * overridding it (and keep the new Calcite default MS-SQL-style decimal-divide semantics), but that
+   * would lead to "regressions" (precision loss) and would require test adjustments.
+   */
+  @Override
+  public RelDataType deriveDecimalDivideType(RelDataTypeFactory typeFactory,
+      RelDataType type1, RelDataType type2) {
+    if (SqlTypeUtil.isExactNumeric(type1) && SqlTypeUtil.isExactNumeric(type2)) {
+      if (SqlTypeUtil.isDecimal(type1) || SqlTypeUtil.isDecimal(type2)) {
+        // Java numeric will always have invalid precision/scale,
+        // use its default decimal precision/scale instead.
+        type1 = RelDataTypeFactoryImpl.isJavaType(type1) ? typeFactory.decimalOf(type1) : type1;
+        type2 = RelDataTypeFactoryImpl.isJavaType(type2) ? typeFactory.decimalOf(type2) : type2;
+        int p1 = type1.getPrecision();
+        int p2 = type2.getPrecision();
+        int s1 = type1.getScale();
+        int s2 = type2.getScale();
+
+        final int maxNumericPrecision = getMaxNumericPrecision();
+        int dout = Math.min(p1 - s1 + s2, maxNumericPrecision);
+        int scale = Math.max(6, s1 + p2 + 1);
+        scale = Math.min(scale, maxNumericPrecision - dout);
+        scale = Math.min(scale, getMaxNumericScale());
+
+        int precision = dout + scale;
+        assert precision <= maxNumericPrecision;
+        assert precision > 0;
+        return typeFactory.createSqlType(SqlTypeName.DECIMAL, precision, scale);
+      }
+    }
+    return null;
   }
 
 }
