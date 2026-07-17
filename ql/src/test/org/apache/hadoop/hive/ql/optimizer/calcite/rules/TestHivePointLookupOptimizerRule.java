ed6c6350c01778f62fe6615c585527407b361908
Ruben Quesada Lopez <rubenql@gmail.com>
2026-07-17 21:17:36 +0100

    Fix TestHivePointLookupOptimizerRule

--- ql/src/test/org/apache/hadoop/hive/ql/optimizer/calcite/rules/TestHivePointLookupOptimizerRule.java
+++ ql/src/test/org/apache/hadoop/hive/ql/optimizer/calcite/rules/TestHivePointLookupOptimizerRule.java
@@ -389,8 +389,8 @@ public class TestHivePointLookupOptimizerRule {
     RexNode condition = filter.getCondition();
     System.out.println(condition);
     assertEquals("IN($1, " +
-                    "_UTF-16LE'AAA111':VARCHAR(30) CHARACTER SET \"UTF-16LE\", " +
-                    "_UTF-16LE'BBB222':VARCHAR(30) CHARACTER SET \"UTF-16LE\")",
+                    "'AAA111':VARCHAR(30) CHARACTER SET \"UTF-16LE\", " +
+                    "'BBB222':VARCHAR(30) CHARACTER SET \"UTF-16LE\")",
             condition.toString());
   }
 
@@ -490,8 +490,8 @@ public class TestHivePointLookupOptimizerRule {
     RexNode condition = filter.getCondition();
     System.out.println(condition);
     assertEquals("IN($1, " +
-                    "_UTF-16LE'AAA111':VARCHAR(30) CHARACTER SET \"UTF-16LE\", " +
-                    "_UTF-16LE'BBB222':VARCHAR(30) CHARACTER SET \"UTF-16LE\")",
+                    "'AAA111':VARCHAR(30) CHARACTER SET \"UTF-16LE\", " +
+                    "'BBB222':VARCHAR(30) CHARACTER SET \"UTF-16LE\")",
             condition.toString());
   }
 
