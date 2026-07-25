e5d5f3adcbb56f33450941fef17927cc1f43ff47
Hemanth Umashankar <hemanthumashankar5555@gmail.com>
2026-07-25 14:13:10 +0530

    permissions fix

--- itests/tez-yarn-it/src/test/java/org/apache/hive/tez/yarn/TestTezYarnLocalization.java
+++ itests/tez-yarn-it/src/test/java/org/apache/hive/tez/yarn/TestTezYarnLocalization.java
@@ -86,7 +86,10 @@ public class TestTezYarnLocalization {
     cluster.start();
 
     GenericContainer<?> nn = cluster.namenodeContainer();
-    nn.execInContainer("hdfs", "dfs", "-chmod", "777", "/tmp");
+    // 1. Explicitly create and open up the global /tmp directory first
+    nn.execInContainer("hdfs", "dfs", "-mkdir", "-p", "/tmp");
+    nn.execInContainer("hdfs", "dfs", "-chmod", "-R", "777", "/tmp");
+
     nn.execInContainer("hdfs", "dfs", "-mkdir", "-p", HDFS_ROOT + "/warehouse");
     nn.execInContainer("hdfs", "dfs", "-mkdir", "-p", HDFS_ROOT + "/scratch");
     nn.execInContainer("hdfs", "dfs", "-mkdir", "-p", HDFS_ROOT + "/user-install");
