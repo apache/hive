98ed66620f84c5413fc7aa8cf2af0459b8c20e0b
InvisibleProgrammer <zsolt.miskolczi@gmail.com>
2026-07-27 16:41:03 +0200

    In progress - upgrade httpclient

--- jdbc/src/java/org/apache/hive/jdbc/HttpRequestInterceptorBase.java
+++ jdbc/src/java/org/apache/hive/jdbc/HttpRequestInterceptorBase.java
@@ -26,12 +26,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 import org.apache.hadoop.hive.conf.Constants;
 import org.apache.hadoop.util.Time;
-import org.apache.http.Header;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.client.CookieStore;
-import org.apache.http.protocol.HttpContext;
+import org.apache.hc.client5.http.cookie.CookieStore;
+import org.apache.hc.core5.http.EntityDetails;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.HttpRequest;
+import org.apache.hc.core5.http.HttpRequestInterceptor;
+import org.apache.hc.core5.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,7 +64,7 @@ public abstract class HttpRequestInterceptorBase implements HttpRequestIntercept
   }
 
   @Override
-  public void process(HttpRequest httpRequest, HttpContext httpContext)
+  public void process(HttpRequest httpRequest, EntityDetails entity, HttpContext httpContext)
     throws HttpException, IOException {
     try {
       // If cookie based authentication is allowed, generate ticket only when necessary.
