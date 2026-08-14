bfe74f9b3f92188095d50cf103b86431f8aaa821
Hemanth Umashankar <157152031+hemanthumashankar0511@users.noreply.github.com>
2026-08-14 07:48:34 +0530

    HIVE-29755: Standardize ASF license headers across Hive source Java files (#6668)
    * Standardize asf licence header across all java files
    
    * Standardize ASF license headers in XML and resource files
    
    * HIVE-29755: Standardize ASF license headers in patched-iceberg poms
    
    * deleted checkstyle files
    
    * Remove incremental RegexpHeader checks from checkstyle configs after deleting asf.header.incremental files
    
    * mvn checkstyle validated across all modules for java files
    
    * Using the right second paragraph from the asf header across all java files
    
    * Update checkstyle/suppressions.xml
    
    Co-authored-by: Shohei Okumiya <okumin@apache.org>
    
    * Update checkstyle/suppressions.xml
    
    Co-authored-by: Shohei Okumiya <okumin@apache.org>
    
    ---------
    
    Co-authored-by: Hemanth Umashankar <humashankar@cloudera.com>
    Co-authored-by: Shohei Okumiya <okumin@apache.org>

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/conf/ConfTemplatePrinter.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/conf/ConfTemplatePrinter.java
@@ -6,14 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.hadoop.hive.metastore.conf;
 
@@ -51,11 +52,12 @@ public class ConfTemplatePrinter {
         "\n" +
         "       http://www.apache.org/licenses/LICENSE-2.0\n" +
         "\n" +
-        "   Unless required by applicable law or agreed to in writing, software\n" +
-        "   distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-        "   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-        "   See the License for the specific language governing permissions and\n" +
-        "   limitations under the License.\n"));
+        "   Unless required by applicable law or agreed to in writing,\n" +
+        "   software distributed under the License is distributed on an\n" +
+        "   \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n" +
+        "   KIND, either express or implied.  See the License for the\n" +
+        "   specific language governing permissions and limitations\n" +
+        "   under the License.\n"));
 
     Element root = doc.createElement("configuration");
     doc.appendChild(root);
