8ef424463e7c90090d23338f1fb2144711162fad
Ruben Quesada Lopez <rubenql@gmail.com>
2026-08-25 19:02:15 +0100

    Put back HiveFilterTableFunctionTransposeRule code, since it does not seem identical to Calcite's version, the latter causes issues with certain tests: - Testing / split-14 / PostProcess / testCliDriver[lvj_mapjoin] – org.apache.hadoop.hive.cli.split13.TestMiniLlapLocalCliDriver - Testing / split-05 / PostProcess / testCliDriver[lateral_view_cbo] – org.apache.hadoop.hive.cli.split12.TestMiniLlapLocalCliDriver - Testing / split-05 / PostProcess / testCliDriver[lateral_view_ppd] – org.apache.hadoop.hive.cli.split12.TestMiniLlapLocalCliDriver - Testing / split-10 / PostProcess / testCliDriver[tez_union_udtf] – org.apache.hadoop.hive.cli.TestMiniTezCliDriver - Testing / split-19 / PostProcess / testCliDriver[union26] – org.apache.hadoop.hive.cli.split27.TestMiniLlapLocalCliDriver - Testing / split-04 / PostProcess / testCliDriver[nonmr_fetch] – org.apache.hadoop.hive.cli.split7.TestCliDriver

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveFilterTableFunctionTransposeRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveFilterTableFunctionTransposeRule.java
@@ -19,26 +19,132 @@
 package org.apache.hadoop.hive.ql.optimizer.calcite.rules;
 
 import org.apache.calcite.plan.RelOptRule;
-import org.apache.calcite.rel.rules.FilterTableFunctionTransposeRule;
+import org.apache.calcite.plan.RelOptRuleCall;
+import org.apache.calcite.plan.RelOptUtil;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.core.Filter;
+import org.apache.calcite.rel.metadata.RelColumnMapping;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.rex.RexUtil;
+import org.apache.calcite.tools.RelBuilder;
+import org.apache.calcite.tools.RelBuilderFactory;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveCalciteUtil;
 import org.apache.hadoop.hive.ql.optimizer.calcite.HiveRelFactories;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveFilter;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveTableFunctionScan;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Rule to transpose Filter and TableFunctionScan RelNodes.
+ * This rule's code has diverged from Calcite's FilterTableFunctionTransposeRule.
  */
-public final class HiveFilterTableFunctionTransposeRule {
+// TODO could this be reconciled with Calcite's rule?
+public class HiveFilterTableFunctionTransposeRule extends RelOptRule {
 
-  private HiveFilterTableFunctionTransposeRule() {
-    throw new IllegalStateException("Instantiation not allowed");
+  public static final HiveFilterTableFunctionTransposeRule INSTANCE =
+      new HiveFilterTableFunctionTransposeRule(HiveRelFactories.HIVE_BUILDER);
+
+  public HiveFilterTableFunctionTransposeRule(RelBuilderFactory relBuilderFactory) {
+    super(operand(HiveFilter.class, operand(HiveTableFunctionScan.class, any())),
+        relBuilderFactory, null);
   }
 
-  public static final RelOptRule INSTANCE =
-      FilterTableFunctionTransposeRule.Config.DEFAULT
-          .withOperandSupplier(b0 ->
-              b0.operand(HiveFilter.class).oneInput(
-                  b1 -> b1.operand(HiveTableFunctionScan.class).anyInputs()))
-          .withRelBuilderFactory(HiveRelFactories.HIVE_BUILDER)
-          .toRule();
+  @Override
+  public boolean matches(RelOptRuleCall call) {
+    final Filter filterRel = call.rel(0);
+    final HiveTableFunctionScan tfs = call.rel(1);
 
+    RexNode condition = filterRel.getCondition();
+    if (!HiveCalciteUtil.isDeterministic(condition)) {
+      return false;
+    }
+
+    // The TableFunctionScan is always created such that all the input RelNode
+    // fields are present in its RelNode.  If a Filter has an InputRef that is
+    // greater then the number of the RelNode below the TableFunctionScan, that
+    // means it was a field created by the TableFunctionScan and thus the Filter
+    // cannot be pushed through.
+    //
+    // We check for each individual conjunction (breaking it up by top level 'and'
+    // conditions).
+    for (RexNode ce : RelOptUtil.conjunctions(filterRel.getCondition())) {
+      if (canBePushed(HiveCalciteUtil.getInputRefs(ce), tfs)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void onMatch(RelOptRuleCall call) {
+    final Filter filter = call.rel(0);
+    final HiveTableFunctionScan tfs = call.rel(1);
+    final RelBuilder builder = call.builder();
+
+    final List<RexNode> newPartKeyFilterConditions = new ArrayList<>();
+    final List<RexNode> unpushedFilterConditions = new ArrayList<>();
+
+    // Check for each individual 'and' condition so that we can push partial
+    // expressions through.
+    for (RexNode ce : RelOptUtil.conjunctions(filter.getCondition())) {
+      // We can only push if all the InputRef pointers are referencing the
+      // input RelNode to the TableFunctionScan
+      if (canBePushed(HiveCalciteUtil.getInputRefs(ce), tfs)) {
+        newPartKeyFilterConditions.add(ce);
+      } else {
+        unpushedFilterConditions.add(ce);
+      }
+    }
+
+    // The "matches" check should guarantee there's something to push.
+    final RexNode filterCondToPushBelowProj = RexUtil.composeConjunction(
+        filter.getCluster().getRexBuilder(), newPartKeyFilterConditions, true);
+
+    builder.push(tfs.getInput(0)).filter(filterCondToPushBelowProj);
+
+    // If there are conditions that cannot be pushed through, generate the RexNode
+    final RexNode unpushedFilCondAboveProj = unpushedFilterConditions.isEmpty()
+        ? null
+        : RexUtil.composeConjunction(filter.getCluster().getRexBuilder(),
+            unpushedFilterConditions, true);
+
+    // Generate the new TableFunctionScanNode with the Filter InputRel
+    final RelNode tableFunctionScanNode = tfs.copy(tfs.getTraitSet(),
+        ImmutableList.of(builder.build()), tfs.getCall(), tfs.getElementType(),
+        tfs.getRowType(), tfs.getColumnMappings());
+
+    builder.clear();
+    builder.push(tableFunctionScanNode);
+
+    if (unpushedFilCondAboveProj != null) {
+      builder.filter(unpushedFilCondAboveProj);
+    }
+
+    call.transformTo(builder.build());
+  }
+
+  // If any of the inputRefs are references to a field that is not mapped into the inputRelNode,
+  // the condition cannot be pushed.
+  private boolean canBePushed(Set<Integer> inputRefs, HiveTableFunctionScan tfs) {
+    Set<RelColumnMapping> columnMappings = tfs.getColumnMappings();
+    if (inputRefs.isEmpty()) {
+      return true;
+    }
+
+    if (CollectionUtils.isEmpty(columnMappings)) {
+      return false;
+    }
+
+    for (Integer inputRef : inputRefs) {
+      if (!tfs.containsInputRefMapping(inputRef)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
