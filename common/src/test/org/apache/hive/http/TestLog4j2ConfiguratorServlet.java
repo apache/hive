c1c7a30a1ff91ff88f6c943b87ea92c0dad143ba
akpatra <asishkumar_patra@yahoo.in>
2026-08-28 09:05:40 +0530

    HIVE-28184: Fix checkstyle header and array-init whitespace in test

--- common/src/test/org/apache/hive/http/TestLog4j2ConfiguratorServlet.java
+++ common/src/test/org/apache/hive/http/TestLog4j2ConfiguratorServlet.java
@@ -9,11 +9,12 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.hive.http;
 
@@ -38,8 +39,9 @@ public class TestLog4j2ConfiguratorServlet {
   private static final String CHILD_LOGGER = "org.apache.hive.test.conflog.child";
 
   // The levels offered by the WebUI "Configure logging" page dropdown.
-  private static final Level[] SUPPORTED_LEVELS =
-      { Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR, Level.FATAL };
+  private static final Level[] SUPPORTED_LEVELS = new Level[] {
+      Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR, Level.FATAL
+  };
 
   private Log4j2ConfiguratorServlet servlet;
   private Configuration configuration;
