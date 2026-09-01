8e92eae891f332a44bbfaef2f0c5c8ae8b68f44e
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2026-09-02 02:33:31 +0530

    HIVE-29878: HMS REST Catalog ignores pagination for listTables and listNamespaces

--- standalone-metastore/metastore-rest-catalog/src/main/java/org/apache/iceberg/rest/HMSCatalogAdapter.java
+++ standalone-metastore/metastore-rest-catalog/src/main/java/org/apache/iceberg/rest/HMSCatalogAdapter.java
@@ -285,7 +285,17 @@ public class HMSCatalogAdapter implements Closeable {
     } else {
       namespace = Namespace.empty();
     }
-    return castResponse(ListNamespacesResponse.class, CatalogHandlers.listNamespaces(asNamespaceCatalog, namespace));
+    String pageToken = PropertyUtil.propertyAsString(vars, "pageToken", null);
+    String pageSize = PropertyUtil.propertyAsString(vars, "pageSize", null);
+    if (pageSize != null) {
+      return castResponse(
+          ListNamespacesResponse.class,
+          CatalogHandlers.listNamespaces(asNamespaceCatalog, namespace, pageToken, pageSize));
+    } else {
+      return castResponse(
+          ListNamespacesResponse.class,
+          CatalogHandlers.listNamespaces(asNamespaceCatalog, namespace));
+    }
   }
 
   private CreateNamespaceResponse createNamespace(Object body) {
@@ -322,7 +332,15 @@ public class HMSCatalogAdapter implements Closeable {
 
   private ListTablesResponse listTables(Map<String, String> vars) {
     Namespace namespace = namespaceFromPathVars(vars);
-    return castResponse(ListTablesResponse.class, CatalogHandlers.listTables(catalog, namespace));
+    String pageToken = PropertyUtil.propertyAsString(vars, "pageToken", null);
+    String pageSize = PropertyUtil.propertyAsString(vars, "pageSize", null);
+    if (pageSize != null) {
+      return castResponse(
+          ListTablesResponse.class,
+          CatalogHandlers.listTables(catalog, namespace, pageToken, pageSize));
+    } else {
+      return castResponse(ListTablesResponse.class, CatalogHandlers.listTables(catalog, namespace));
+    }
   }
 
   private LoadTableResponse createTable(Map<String, String> vars, Object body) {
