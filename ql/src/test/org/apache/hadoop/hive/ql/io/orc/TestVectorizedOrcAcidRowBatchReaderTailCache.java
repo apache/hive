6e13676ab1e8b55950f6d5c17ff734dd061161f0
Denys Kuzmenko <deniskuzz@gmail.com>
2026-09-16 12:30:20 +0300

    HIVE-30044: Close the reader the test is handed
    - ReaderData owns an open ORC reader, and closing it is what its caller must do

--- ql/src/test/org/apache/hadoop/hive/ql/io/orc/TestVectorizedOrcAcidRowBatchReaderTailCache.java
+++ ql/src/test/org/apache/hadoop/hive/ql/io/orc/TestVectorizedOrcAcidRowBatchReaderTailCache.java
@@ -31,9 +31,12 @@ import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.llap.io.api.LlapIo;
 import org.apache.hadoop.hive.llap.io.api.LlapProxy;
+import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.hadoop.hive.ql.io.orc.VectorizedOrcAcidRowBatchReader.ReaderData;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.orc.OrcFile;
+import org.apache.orc.Reader;
 import org.apache.orc.TypeDescription;
 import org.apache.orc.Writer;
 import org.apache.orc.impl.OrcTail;
@@ -63,8 +66,8 @@ public class TestVectorizedOrcAcidRowBatchReaderTailCache {
     conf = new JobConf();
     TypeDescription schema = TypeDescription.fromString("struct<id:int>");
     try (Writer writer = OrcFile.createWriter(path, OrcFile.writerOptions(conf).setSchema(schema))) {
-      org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch batch = schema.createRowBatch();
-      ((org.apache.hadoop.hive.ql.exec.vector.LongColumnVector) batch.cols[0]).vector[0] = 1;
+      VectorizedRowBatch batch = schema.createRowBatch();
+      ((LongColumnVector) batch.cols[0]).vector[0] = 1;
       batch.size = 1;
       writer.addRowBatch(batch);
     }
@@ -75,7 +78,7 @@ public class TestVectorizedOrcAcidRowBatchReaderTailCache {
   @Test
   public void testCachedTailIsTheOneReturned() throws Exception {
     OrcTail cachedTail;
-    try (org.apache.orc.Reader reader = OrcFile.createReader(path, OrcFile.readerOptions(conf))) {
+    try (Reader reader = OrcFile.createReader(path, OrcFile.readerOptions(conf))) {
       cachedTail = new OrcTail(reader.getFileTail(), reader.getSerializedFileFooter());
     }
     LlapIo<?> llapIo = mock(LlapIo.class);
@@ -85,10 +88,11 @@ public class TestVectorizedOrcAcidRowBatchReaderTailCache {
       proxy.when(LlapProxy::isDaemon).thenReturn(true);
       proxy.when(LlapProxy::getIo).thenReturn(llapIo);
 
-      ReaderData readerData = VectorizedOrcAcidRowBatchReader.getOrcReaderData(path, conf, null, null);
-
-      assertSame("the cached tail must not be replaced by a filesystem read", cachedTail, readerData.orcTail);
-      assertNotNull(readerData.reader);
+      try (ReaderData readerData =
+            VectorizedOrcAcidRowBatchReader.getOrcReaderData(path, conf, null, null)) {
+        assertSame("the cached tail must not be replaced by a filesystem read", cachedTail, readerData.orcTail);
+        assertNotNull(readerData.reader);
+      }
     }
   }
 }
